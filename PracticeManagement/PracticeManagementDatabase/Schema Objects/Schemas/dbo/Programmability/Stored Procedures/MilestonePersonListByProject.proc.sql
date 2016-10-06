a291784a1d553054fd36843e3eff2f0596d0a04d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MilestonePersonListByProject.proc.sql
@@ -0,0 +1,50 @@
+﻿
+CREATE PROCEDURE dbo.MilestonePersonListByProject
+(
+	@ProjectId   INT,
+	@IncludeStrawman BIT = 1
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+	
+	SELECT mp.MilestonePersonId,
+	       mp.MilestoneId,
+	       mp.PersonId,
+	       mp.SeniorityId,
+	       mp.StartDate,
+	       mp.EndDate,
+	       mp.HoursPerDay,
+	       mp.FirstName,
+	       mp.LastName,
+	       mp.ProjectId,
+	       mp.ProjectName,
+	       mp.ProjectStartDate,
+	       mp.ProjectEndDate,
+	       mp.Discount,
+	       mp.ClientId,
+	       mp.ClientName,
+	       mp.MilestoneName,
+	       mp.MilestoneStartDate,
+	       mp.MilestoneProjectedDeliveryDate,
+	       mp.ExpectedHoursWithVacationDays,
+	       mp.PersonRoleId,
+	       mp.RoleName,
+	       mp.Amount,
+	       mp.IsHourlyAmount,
+	       mp.MilestoneExpectedHours,
+	       mp.MilestoneHourlyRevenue,
+	       mp.ConsultantsCanAdjust,
+	       mp.ClientIsChargeable,
+	       mp.ProjectIsChargeable,
+	       mp.MilestoneIsChargeable
+	  FROM dbo.v_MilestonePerson AS mp
+	  INNER JOIN dbo.Person AS P ON (P.PersonId = MP.PersonId AND (@IncludeStrawman = 1  OR P.IsStrawman = 0))
+	 WHERE mp.ProjectId = @ProjectId 
+
+	 
+	 SELECT  personid,Date,ActualHours 
+	 FROM dbo.[GetPersonTimeoffValuesByMilestoneId](@ProjectId,NULL,NULL,NULL)
+
+END
+
