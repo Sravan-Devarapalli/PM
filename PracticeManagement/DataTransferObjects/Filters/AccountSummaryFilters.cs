9b9fd2591913005940e01c62316fc2a2c7fac6cc
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Filters/AccountSummaryFilters.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTransferObjects.Filters
+{
+    [Serializable]
+    public class AccountSummaryFilters
+    {
+        public string ReportPeriod
+        {
+            get;
+            set;
+        }
+
+        public string AccountId
+        {
+            get;
+            set;
+        }
+        public string BusinessUnitIds
+        {
+            get;
+            set;
+        }
+        public string ProjectStatusIds
+        {
+            get;
+            set;
+        }
+
+        public DateTime? ReportStartDate
+        {
+            get;
+            set;
+        }
+
+        public DateTime? ReportEndDate
+        {
+            get;
+            set;
+        }
+    }
+}
+
