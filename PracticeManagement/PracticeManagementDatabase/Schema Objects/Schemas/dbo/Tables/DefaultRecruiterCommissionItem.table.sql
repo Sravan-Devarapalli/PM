7ca0c4b53a51d7633e0c3f339c184135c7a41c81
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/DefaultRecruiterCommissionItem.table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[DefaultRecruiterCommissionItem] (
+    [DefaultRecruiterCommissionHeaderId] INT             NOT NULL,
+    [HoursToCollect]                     INT             NOT NULL,
+    [Amount]                             DECIMAL (18, 2) NOT NULL
+);
+
+
