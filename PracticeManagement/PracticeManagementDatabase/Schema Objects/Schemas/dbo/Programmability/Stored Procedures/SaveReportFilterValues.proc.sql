32407df8b3c12a025ca5ecc1584314021e9a3ddc
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SaveReportFilterValues.proc.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [dbo].[SaveReportFilterValues]
+	@CurrentUserId INT,
+	@ReportId	   INT,
+	@ReportFilters NVARCHAR (MAX),
+	@PreviousUserId INT,
+	@SessionId     NVARCHAR (MAX)
+AS
+BEGIN
+
+	DECLARE @ExpiresOn DATETIME
+			
+	SET @ExpiresOn = DATEADD(day,2,GETDATE())
+
+	IF(NOT EXISTS(SELECT * FROM ReportFilterValues WHERE CurrentUserId=@CurrentUserId AND ReportId=@ReportId AND PreviousUserId=@PreviousUserId AND SessionId=@SessionId))
+		BEGIN 
+			INSERT INTO ReportFilterValues (Id,CurrentUserId,ReportId,ReportFilters,PreviousUserId,SessionId,ExpiresOn) 
+			VALUES (NEWID(),@CurrentUserId,@ReportId,@ReportFilters,@PreviousUserId,@SessionId,@ExpiresOn)
+		END
+	ELSE
+		BEGIN
+			UPDATE ReportFilterValues SET ReportFilters=@ReportFilters,ExpiresOn= @ExpiresOn WHERE CurrentUserId=@CurrentUserId AND ReportId=@ReportId AND PreviousUserId=@PreviousUserId AND SessionId=@SessionId
+		END
+END
+
