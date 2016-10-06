1fc58c54e84e930abe1b3a32bd8fa1c21990bfff
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementServiceTest/PracticeServiceTest.cs
@@ -0,0 +1,49 @@
+﻿using DataTransferObjects;
+using NUnit.Framework;
+using PracticeManagementServiceTest.PracticeClient;
+
+namespace PracticeManagementServiceTest
+{
+    [TestFixture]
+    public class PracticeServiceTest
+    {
+        [Test]
+        public void GetPracticeListTest()
+        {
+            PracticeServiceClient serviceClient = new PracticeServiceClient();
+
+            Practice[] list = serviceClient.GetPracticeList();
+            int found = 0;
+            for (int i = 0; i < list.Length; i++)
+            {
+                Practice practice = list[i];
+                if (practice.Id == 1 && practice.Name == "Offshore")
+                {
+                    ++found;
+                }
+                if (practice.Id == 2 && practice.Name == "Management Consulting")
+                {
+                    ++found;
+                }
+                if (practice.Id == 3 && practice.Name == "Technology Consulting")
+                {
+                    ++found;
+                }
+                if (practice.Id == 4 && practice.Name == "Administration")
+                {
+                    ++found;
+                }
+            }
+            Assert.AreEqual(4, found, "All practices should be found");
+        }
+
+        [Test]
+        public void GetPracticeListCountTest()
+        {
+            PracticeServiceClient serviceClient = new PracticeServiceClient();
+            Practice[] list = serviceClient.GetPracticeList();
+            Assert.AreEqual(4, list.Length, "There should be four practices");
+        }
+    }
+}
+
