a2bc95836262e46ff20a53bb031a6482028eb559
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetApprovedByManagerList.proc.sql
@@ -0,0 +1,20 @@
+﻿-- ==================================================
+-- Author:		Sainathc
+-- Create date: 05-14-2012
+-- Description : Returns all the Approved by managers list
+-- ==================================================
+CREATE PROCEDURE [dbo].[GetApprovedByManagerList]
+AS
+BEGIN
+
+	SELECT DISTINCT p.PersonId ,
+			p.LastName ,
+			p.FirstName 
+	FROM dbo.Person AS p
+	INNER JOIN dbo.aspnet_Users AS u ON p.Alias = u.UserName
+	INNER JOIN dbo.aspnet_UsersInRoles AS ur ON u.UserId = ur.UserId
+	INNER JOIN dbo.aspnet_Roles AS r ON ur.RoleId = r.RoleId
+	WHERE r.RoleName IN ('System Administrator','Practice Area Manager','Business Unit Manager','Client Director','HR')
+		AND p.PersonStatusId IN (1,5)
+	ORDER BY p.LastName,p.FirstName 
+END
