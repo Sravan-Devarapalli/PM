2aaded2d5c787058bf2f4d63f29712a89f1e8a46
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CSATUpdate.proc.sql
@@ -0,0 +1,44 @@
+﻿CREATE PROCEDURE [dbo].[CSATUpdate]
+(
+	@ProjectCSATId		INT, 
+	@ReviewStartDate DATETIME,
+	@ReviewEndDate DATETIME,
+	@CompletionDate DATETIME,
+	@ReviewerId		INT,
+	@ReferralScore	INT,
+	@Comments		NVARCHAR(MAX),
+	@UserLogin      NVARCHAR(255)
+)
+AS
+BEGIN
+	-- Start logging session
+	EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
+
+	DECLARE @InsertingTime DATETIME,
+	        @ModifiedBy    INT
+	SELECT @InsertingTime=dbo.InsertingTime(),@ModifiedBy=PersonId FROM Person WHERE Alias=@UserLogin;
+
+	UPDATE dbo.ProjectCSAT
+	SET ReviewStartDate = @ReviewStartDate,
+		ReviewEndDate = @ReviewEndDate,
+		CompletionDate = @CompletionDate,
+		Comments = @Comments,
+		ReferralScore = @ReferralScore,
+		ReviewerId = @ReviewerId,
+		ModifiedDate = @InsertingTime,
+		ModifiedBy =@ModifiedBy
+	WHERE [CSATId] = @ProjectCSATId AND 
+		  (
+			ReviewStartDate != @ReviewStartDate OR 
+			ReviewEndDate != @ReviewEndDate OR
+			CompletionDate != @CompletionDate OR
+			Comments != @Comments OR
+			ReferralScore != @ReferralScore OR
+			ReviewerId != @ReviewerId
+		  )
+
+	-- End logging session
+	EXEC dbo.SessionLogUnprepare
+
+END
+
