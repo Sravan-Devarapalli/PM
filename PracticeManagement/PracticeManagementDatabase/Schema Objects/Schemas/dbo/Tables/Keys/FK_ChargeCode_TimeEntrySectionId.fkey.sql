5ff3d5ba0909a4b67ef4aa8cf475388934581ae4
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_ChargeCode_TimeEntrySectionId.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[ChargeCode]
+	ADD CONSTRAINT [FK_ChargeCode_TimeEntrySectionId] 
+	FOREIGN KEY (TimeEntrySectionId)
+	REFERENCES TimeEntrySection (Id) ON DELETE NO ACTION ON UPDATE NO ACTION;
