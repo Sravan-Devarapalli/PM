25d24ed5717725efd9994a4e95f7bcbd23a1a0df
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/CompanyLogoSetting.table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[CompanyLogoSetting](
+	[Id]        [int] IDENTITY(1,1) PRIMARY KEY  NOT NULL,
+	[Title]     [nvarchar](256)      NOT NULL,
+	[FileName]  [nvarchar](256)      NULL,
+	[FilePath]  [nvarchar](1024)     NULL,
+	[Data]      [varbinary](max)     NULL
+)
+
