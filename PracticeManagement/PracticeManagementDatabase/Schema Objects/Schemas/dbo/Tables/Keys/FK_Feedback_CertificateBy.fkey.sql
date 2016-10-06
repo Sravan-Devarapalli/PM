c2338a25c7e19b0611e19e6c76542f6d76942b2f
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_Feedback_CertificateBy.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[ProjectFeedback]
+	ADD CONSTRAINT [FK_Feedback_CertificateBy] FOREIGN KEY ([CompletionCertificateBy]) REFERENCES [dbo].[Person] ([PersonId])	ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
