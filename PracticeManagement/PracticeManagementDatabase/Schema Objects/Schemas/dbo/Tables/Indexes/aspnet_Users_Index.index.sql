801e30463a4555e16b0ad02199b841a3fdf1b640
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Indexes/aspnet_Users_Index.index.sql
@@ -0,0 +1,4 @@
+﻿CREATE UNIQUE CLUSTERED INDEX [aspnet_Users_Index]
+    ON [dbo].[aspnet_Users]([ApplicationId] ASC, [LoweredUserName] ASC) WITH ( DROP_EXISTING = OFF, IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF, ONLINE = OFF, MAXDOP = 0);
+
+
