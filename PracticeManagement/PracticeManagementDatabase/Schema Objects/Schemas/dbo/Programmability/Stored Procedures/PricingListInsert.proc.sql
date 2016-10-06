26cf76849699a7d6d1010f824c0411812a94f44e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PricingListInsert.proc.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[PricingListInsert]
+(
+    @PricingListId	INT OUT,
+	@ClientId    	INT,
+	@Name		    NVARCHAR(200),
+	@IsActive	    	    BIT,
+	@UserLogin          NVARCHAR(255)
+)
+AS
+BEGIN
+	-- Start logging session
+	EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
+
+	INSERT PricingList(ClientId, Name,IsActive) 
+	VALUES (@ClientId, @Name,@IsActive)
+
+	SET @PricingListId = SCOPE_IDENTITY()
+
+	-- End logging session
+	EXEC dbo.SessionLogUnprepare
+END
