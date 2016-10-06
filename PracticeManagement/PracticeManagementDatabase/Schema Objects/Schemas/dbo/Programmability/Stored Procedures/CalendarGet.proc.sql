9f8908f572f13499055fc839c61822b12bfe167b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CalendarGet.proc.sql
@@ -0,0 +1,29 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 6-5-2008
+-- Updated by:	Sainath C
+-- Update Date:	06-01-2012
+-- Description:	Lists the days within the specified period
+-- =============================================
+CREATE PROCEDURE [dbo].[CalendarGet]
+    (
+      @StartDate DATETIME ,
+      @EndDate DATETIME 
+    )
+AS 
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT  cal.Date ,
+			cal.DayOff ,
+            cal.DayOff AS CompanyDayOff ,
+            cal.IsRecurring ,
+            cal.RecurringHolidayId ,
+            cal.HolidayDescription ,
+            cal.RecurringHolidayDate 
+    FROM    dbo.Calendar AS cal
+    WHERE   cal.Date BETWEEN @StartDate AND @EndDate
+    ORDER BY cal.Date
+
+END
+
