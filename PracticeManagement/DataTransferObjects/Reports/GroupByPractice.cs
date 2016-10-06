0b4ddf14d5c6785d12729954b74f8e45a9c52842
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Reports/GroupByPractice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Reports
+{
+    [DataContract]
+    [Serializable]
+    public class GroupByPractice
+    {
+        [DataMember]
+        public Practice Practice
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public List<BadgedResourcesByTime> ResourcesCount
+        {
+            get;
+            set;
+        }
+    }
+}
+
