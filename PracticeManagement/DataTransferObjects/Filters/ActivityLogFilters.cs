6d02ae474e8475e5a873baf07cbbc399fa4dd128
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Filters/ActivityLogFilters.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTransferObjects.Filters
+{
+    [Serializable]
+    public class ActivityLogFilters
+    {
+        public string EventSource
+        {
+            get;
+            set;
+        }
+
+        public string ReportPeriod
+        {
+            get;
+            set;
+        }
+
+        public DateTime? ReportStartDate
+        {
+            get;
+            set;
+        }
+
+        public DateTime? ReportEndDate
+        {
+            get;
+            set;
+        }
+
+        public string SelectedPerson
+        {
+            get;
+            set;
+        }
+
+        public string SelectedProject
+        {
+            get;
+            set;
+        }
+    }
+}
+
