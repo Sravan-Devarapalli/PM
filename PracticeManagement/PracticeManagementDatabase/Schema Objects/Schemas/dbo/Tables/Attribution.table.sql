526fe96fe13f0f5cee739405547472a5e65b39ba
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Attribution.table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Attribution]
+(
+	[AttributionId]				INT	IDENTITY (1, 1)	NOT NULL,
+	[ProjectId]					INT					NOT NULL,
+	[AttributionTypeId]			INT					NOT NULL,
+	[AttributionRecordTypeId]	INT					NOT NULL,
+	[TargetId]					INT					NOT NULL,
+	[StartDate]					DATETIME			NULL,
+	[EndDate]					DATETIME			NULL,
+	[Percentage]				DECIMAL(5,2)		NULL
+)
+
