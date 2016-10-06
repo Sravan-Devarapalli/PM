02332322778ff5341959bb102cd7f2876739dda2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/Configuration/RecruitingMetricsHeader.ascx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PraticeManagement.Controls.Configuration
+{
+    public partial class RecruitingMetricsHeader : System.Web.UI.UserControl
+    {
+        
+        private void ApplyCssHeader()
+        {
+            if (Page is PraticeManagement.SourceRecruitingMetrics)
+            {
+                thSourceRecruitingMetrics.Attributes["class"] = "bgcolorE2EBFF";
+                thTargetCompanyRecruitingMetrics.Attributes["class"] = "bgcolorwhite";
+            }
+            else if (Page is PraticeManagement.TargetedCompanyRecruitingMetrics)
+            {
+                thSourceRecruitingMetrics.Attributes["class"] = "bgcolorwhite";
+                thTargetCompanyRecruitingMetrics.Attributes["class"] = "bgcolorE2EBFF";
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ApplyCssHeader();
+            }
+        }
+    }
+}
