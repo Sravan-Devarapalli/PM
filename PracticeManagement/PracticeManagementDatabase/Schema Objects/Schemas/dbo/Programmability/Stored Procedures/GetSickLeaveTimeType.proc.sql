ada9c1acc08f67751f6f0c493e4f9440c5a2bdc6
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetSickLeaveTimeType.proc.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[GetSickLeaveTimeType]
+AS
+BEGIN
+  SELECT  TT.TimeTypeId,
+		  TT.Name
+	FROM dbo.TimeType AS TT
+    WHERE   TT.TimeTypeId = dbo.[GetSickLeaveTimeTypeId]()
+END
+	
