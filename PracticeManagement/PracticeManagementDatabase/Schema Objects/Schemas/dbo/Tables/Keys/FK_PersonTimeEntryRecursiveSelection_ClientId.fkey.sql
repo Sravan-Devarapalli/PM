79e1e56f211ac7868b182450b38e1e48a06dce39
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_PersonTimeEntryRecursiveSelection_ClientId.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[PersonTimeEntryRecursiveSelection]
+	ADD CONSTRAINT [FK_PersonTimeEntryRecursiveSelection_ClientId] 
+	FOREIGN KEY (ClientId)
+	REFERENCES dbo.Client (ClientId) ON DELETE NO ACTION ON UPDATE NO ACTION;
