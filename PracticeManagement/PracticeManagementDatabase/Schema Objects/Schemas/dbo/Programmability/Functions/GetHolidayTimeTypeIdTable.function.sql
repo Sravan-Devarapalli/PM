32aa355407a887f335ece5712007a98f9210e523
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetHolidayTimeTypeIdTable.function.sql
@@ -0,0 +1,9 @@
+﻿CREATE FUNCTION [dbo].[GetHolidayTimeTypeIdTable]
+(
+)
+RETURNS TABLE 
+AS
+	RETURN SELECT tt.TimeTypeId AS HolidayTimeTypeId
+			FROM TimeType tt 
+			WHERE tt.Code = 'W9320' --Here 'W9320' is code of Holiday Work Type.
+GO
