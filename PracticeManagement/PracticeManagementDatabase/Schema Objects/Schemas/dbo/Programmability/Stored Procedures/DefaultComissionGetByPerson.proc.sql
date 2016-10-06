a9f5d3e848eae5ba03fd985d47b34798ddb42c06
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DefaultComissionGetByPerson.proc.sql
@@ -0,0 +1,27 @@
+﻿--The source is 
+-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 6-2-2008
+-- Description:	Gets a person's default actual commisions
+-- =============================================
+CREATE PROCEDURE [dbo].[DefaultComissionGetByPerson]
+(
+	@PersonId   INT
+)
+AS
+	SET NOCOUNT ON
+	
+	DECLARE @Today DATETIME
+	SET @Today = dbo.[Today]()
+	
+	SELECT c.[PersonId],
+	       c.[FractionOfMargin],
+	       c.[StartDate],
+	       c.[EndDate],
+	       c.[type],
+	       c.[MarginTypeId]
+	  FROM dbo.DefaultCommission AS c
+	 WHERE c.[PersonId] = @PersonId
+	   AND c.[StartDate] <= @Today
+	   AND c.[EndDate] > @Today
+
