90c31a73be0b146d646db06223e9626d6f9bc332
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/CompanyRecurringHoliday.table.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[CompanyRecurringHoliday](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Description] [nvarchar](255) NOT NULL,
+	[IsSet] [bit] NOT NULL,
+	[Month] [int] NOT NULL,
+	[Day] [int] NULL,
+	[NumberInMonth] [int] NULL,
+	[DayOfTheWeek] [int] NULL,
+	[DateDescription] [nvarchar](255) NULL,
+	 CONSTRAINT [PK_CompanyRecurringHoliday] PRIMARY KEY CLUSTERED 
+	(
+		[Id] ASC
+	)
+)
