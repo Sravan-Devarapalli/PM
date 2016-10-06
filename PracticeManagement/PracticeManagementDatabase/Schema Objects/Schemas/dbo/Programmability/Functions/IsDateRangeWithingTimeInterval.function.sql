f6dac489f95a2bf768709cfbf8406d0b6576a005
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/IsDateRangeWithingTimeInterval.function.sql
@@ -0,0 +1,41 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2010-05-07
+-- Description:	If given date range is withing given time interval
+-- =============================================
+CREATE FUNCTION dbo.IsDateRangeWithingTimeInterval 
+(
+	@IntervalStart datetime,
+	@IntervalEnd datetime,
+	@RangeStart datetime,
+	@RangeEnd datetime
+)
+RETURNS bit
+AS
+BEGIN
+
+	-- When interval start date is between range start and end dates
+	if datediff(day, @RangeStart, @IntervalStart) >= 0 AND
+		datediff(day, @IntervalStart, @RangeEnd) >= 0
+		return 1
+
+	-- When interval end date is between range start and end dates
+	if datediff(day, @RangeStart, @IntervalEnd) >= 0 AND
+		datediff(day, @IntervalEnd, @RangeEnd) >= 0
+		return 1
+
+	-- When interval is within range
+	if datediff(day, @RangeStart, @IntervalStart) >= 0 AND
+		datediff(day, @IntervalEnd, @RangeEnd) >= 0
+		return 1
+
+	-- When range is within interval
+	if datediff(day, @IntervalStart, @RangeStart) >= 0 AND
+		datediff(day, @RangeEnd, @IntervalEnd) >= 0
+		return 1
+
+
+	return 0
+
+END
+
