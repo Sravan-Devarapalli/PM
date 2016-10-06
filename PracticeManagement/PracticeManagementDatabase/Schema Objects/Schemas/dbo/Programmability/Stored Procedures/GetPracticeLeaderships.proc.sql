9098fd648103cd21e3e8b59a56f999d77891a0f1
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetPracticeLeaderships.proc.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[GetPracticeLeaderships]
+(
+	@DivisionId	INT=NULL	
+)
+AS
+BEGIN
+
+	SELECT P.PersonId,
+		   P.LastName,
+		   P.FirstName,
+		   PD.DivisionId,
+		   PD.DivisionName
+	FROM dbo.PracticeLeadership PL
+	JOIN v_Person P ON P.PersonId = PL.PersonId
+	JOIN dbo.PersonDivision PD ON PD.DivisionId = PL.DivisionId
+	WHERE (@DivisionId IS NULL OR PL.DivisionId = @DivisionId)
+
+END
