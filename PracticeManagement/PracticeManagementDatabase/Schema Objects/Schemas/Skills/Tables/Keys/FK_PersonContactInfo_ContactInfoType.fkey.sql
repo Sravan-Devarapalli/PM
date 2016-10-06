7f88e7f4f110090274c1a9fa3a114df28bdd4efc
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/Keys/FK_PersonContactInfo_ContactInfoType.fkey.sql
@@ -0,0 +1,5 @@
+﻿ALTER TABLE [Skills].[PersonContactInfo]  WITH CHECK ADD  CONSTRAINT [FK_PersonContactInfo_ContactInfoType] FOREIGN KEY([ContactInfoTypeId])
+REFERENCES [Skills].[ContactInfoType] ([ContactInfoTypeId])
+ON UPDATE CASCADE
+GO
+ALTER TABLE [Skills].[PersonContactInfo] CHECK CONSTRAINT [FK_PersonContactInfo_ContactInfoType]
