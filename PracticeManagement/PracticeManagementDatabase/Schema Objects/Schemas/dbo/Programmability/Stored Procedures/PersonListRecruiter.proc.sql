216533982e2efaacef0a0e7973c619611b9e3b6c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonListRecruiter.proc.sql
@@ -0,0 +1,30 @@
+﻿CREATE PROCEDURE dbo.PersonListRecruiter
+AS 
+    SELECT  p.PersonId ,
+            p.FirstName ,
+			p.PreferredFirstName ,
+            p.LastName ,
+            p.HireDate ,
+            p.TerminationDate ,
+            p.Alias ,
+            p.DefaultPractice ,
+            p.PracticeName ,
+            p.PersonStatusId ,
+            p.PersonStatusName ,
+            p.EmployeeNumber ,
+            p.SeniorityId ,
+            p.SeniorityName ,
+            p.ManagerId ,
+            p.ManagerAlias ,
+            p.ManagerFirstName ,
+            p.ManagerLastName ,
+            p.PracticeOwnedId ,
+            p.PracticeOwnedName,
+            p.TelephoneNumber
+    FROM    dbo.v_Person AS p
+    INNER JOIN  v_UsersInRoles AS ur ON ur.UserName = p.Alias
+                                    AND ur.RoleName = 'Recruiter' 
+	WHERE   p.PersonStatusId IN (1,5)	-- Active person only
+    ORDER BY p.LastName ,
+            p.FirstName
+
