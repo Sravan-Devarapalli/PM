cbefc92843f9d61021e5bc18f61ea3a90890bbd2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ProjectStatusHistory.table.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[ProjectStatusHistory]
+(
+	ProjectId		INT NOT NULL,
+	ProjectStatusId	INT NOT NULL,
+	StartDate		DATETIME NOT NULL,
+	EndDate			DATETIME NULL,
+	CONSTRAINT PK_ProjectStatusHistory PRIMARY KEY CLUSTERED
+	(
+		ProjectId ASC,	
+		ProjectStatusId ASC,	
+		StartDate ASC
+	)
+)
+
