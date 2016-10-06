809ea4fb4decc47d1842932138808b68fa37ec66
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/PricingList.table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[PricingList]
+(
+	[PricingListId]  INT IDENTITY(0,1)             NOT NULL, 
+    [ClientId]       INT                           NOT NULL,
+    [Name]           NVARCHAR(200)                 NOT NULL,
+	[IsDefault]      BIT                           NOT NULL CONSTRAINT DF_PricingList_IsDefault DEFAULT(0),
+	[IsActive]      BIT                           NOT NULL CONSTRAINT DF_PricingList_IsActive DEFAULT(1),
+	PRIMARY KEY CLUSTERED ([PricingListId] ASC) WITH (IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF)
+)
+
