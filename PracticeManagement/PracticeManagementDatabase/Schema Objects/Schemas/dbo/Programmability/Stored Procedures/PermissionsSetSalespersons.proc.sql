9e74dc843263eebc58438b57817e32760643dce6
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PermissionsSetSalespersons.proc.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[PermissionsSetSalespersons]
+	@PersonId INT,
+	@TargetIdsList VARCHAR(500)
+AS
+	EXECUTE PermissionsSet
+		@PersonId = @PersonId,
+		@TargetType = 3,
+		@TargetIdsList = @TargetIdsList
+
