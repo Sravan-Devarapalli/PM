38bfdfb58b18b7e4f01cab95be763b65b53079ae
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/PK_UserActivityLogRecordPerChange_ActivityId.pkey.sql
@@ -0,0 +1,2 @@
+﻿ALTER TABLE [dbo].[UserActivityLogRecordPerChange]
+	ADD CONSTRAINT [PK_UserActivityLogRecordPerChange_ActivityId] PRIMARY KEY CLUSTERED ([ActivityID] ASC) WITH (IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF);
