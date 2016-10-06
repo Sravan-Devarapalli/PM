0f81027c04f82cb92ccc5dbe07e7bce5e3241c38
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MonthlyExpenseDeleteItem.proc.sql
@@ -0,0 +1,18 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 7-24-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	Deletes a month expense item by the specified name
+-- =============================================
+CREATE PROCEDURE [dbo].[MonthlyExpenseDeleteItem]
+(
+	@Name   NVARCHAR(50)
+)
+AS
+	SET NOCOUNT ON
+
+	DELETE
+	  FROM dbo.MonthlyExpense
+	 WHERE Name = @Name
+
