cd318f1fbc0920cd5770e8a3d637d44fe9a96aef
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/Projects/ProjectPersons.ascx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using PraticeManagement.Utils;
+
+namespace PraticeManagement.Controls.Projects
+{
+    public partial class ProjectPersons : System.Web.UI.UserControl
+    {
+        public const string PERSON_TARGET = "person";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected string GetDoubleFormat(decimal value)
+        {
+            return value.ToString(Constants.Formatting.DoubleValue);
+        }
+
+        #region Redirects
+
+        protected string GetMilestonePersonRedirectUrl(object milestoneId, object milestonePersonId)
+        {
+            return Urls.GerMilestonePersonDetailsUrlWithReturn(milestoneId, milestonePersonId, Request.Url.AbsoluteUri);
+        }
+
+        #endregion
+    }
+}
