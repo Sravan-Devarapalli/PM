4823c086cf1d44b465f765fb6c0a4ed50dae0a66
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CanMoveFutureMilestones.proc.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[CanMoveFutureMilestones]
+(
+	@MilestoneId            INT,
+	@ShiftDays              INT 
+)
+AS
+	SET NOCOUNT ON
+
+	IF EXISTS (SELECT 1 FROM Milestone fm
+				JOIN Milestone cm ON fm.ProjectId = cm.ProjectId 
+				AND fm.StartDate>=cm.StartDate 
+				AND fm.MilestoneId<>cm.MilestoneId and cm.MilestoneId = @MilestoneId
+				)
+	 AND @ShiftDays <0
+	 AND EXISTS(SELECT 1 FROM ProjectExpense PE 
+				JOIN Project P ON P.ProjectId = PE.ProjectId
+				WHERE PE.EndDate> DATEADD(DD,@ShiftDays,P.EndDate)
+				)
+	BEGIN
+		SELECT 'False'
+	END 
+	ELSE
+	BEGIN
+		SELECT 'True'
+	END
