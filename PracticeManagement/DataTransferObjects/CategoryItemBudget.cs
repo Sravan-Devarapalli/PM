e7fae8123a75e58777862c435e786867a48a6c61
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/CategoryItemBudget.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class CategoryItemBudget
+    {
+        [DataMember]
+        public int ItemId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public BudgetCategoryType CategoryTypeId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int Month
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public decimal Amount
+        {
+            get;
+            set;
+        }
+    }
+}
