f4ef655058ab164a23aa67af834701a05cdeff25
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_Commission_CommissionType.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[Commission]
+    ADD CONSTRAINT [FK_Commission_CommissionType] FOREIGN KEY ([CommissionType]) REFERENCES [dbo].[CommissionType] ([CommissionTypeId]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
