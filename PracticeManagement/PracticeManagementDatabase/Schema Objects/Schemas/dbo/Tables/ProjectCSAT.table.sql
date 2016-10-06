ed2d52c5d7209bc625e4af5655857f1c34bdcc6d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ProjectCSAT.table.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[ProjectCSAT]
+(
+	[CSATId]			INT		IDENTITY (1, 1) NOT NULL,
+	[ProjectId]			INT						NOT NULL,
+	ReviewStartDate		DATETIME				NOT NULL,
+	ReviewEndDate		DATETIME				NOT NULL,
+	CompletionDate		DATETIME				NOT NULL,
+	ReviewerId			INT						NOT NULL,
+	ReferralScore		INT						NOT NULL,
+	Comments			NVARCHAR (MAX)				NULL,
+	CreatedDate			DATETIME				NOT NULL,
+	ModifiedDate		DATETIME				NOT NULL,
+	ModifiedBy			INT             		NOT NULL
+	CONSTRAINT PK_ProjectCSAT_CSATId PRIMARY KEY CLUSTERED([CSATId])
+)
+
