756dc0e64c781a892ee704017740885782c882a9
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MembershipDelete.proc.sql
@@ -0,0 +1,20 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 8-18-2008
+-- Updated by:	
+-- Update date: 
+-- Description:	Removes the users membership
+-- =============================================
+CREATE PROCEDURE [dbo].[MembershipDelete]
+(
+	@UserId   UNIQUEIDENTIFIER
+)
+AS
+	SET NOCOUNT ON
+
+	DELETE FROM dbo.aspnet_UsersInRoles
+	 WHERE UserId = @UserId
+
+	DELETE FROM dbo.aspnet_Membership
+	 WHERE UserId = @UserId
+
