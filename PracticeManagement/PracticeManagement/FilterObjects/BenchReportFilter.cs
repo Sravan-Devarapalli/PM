d372f70cc936c65df1475e6c5054176b619b071c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/FilterObjects/BenchReportFilter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataTransferObjects.ContextObjects;
+using DataTransferObjects;
+using System.Web.UI.WebControls;
+using System.Runtime.Serialization;
+
+namespace PraticeManagement.FilterObjects
+{
+    [Serializable]
+    public class BenchReportFilter : BenchReportContext
+    {
+        public BenchReportSortExpression SortExpression { get; set; }
+        public SortDirection SortOrder { get; set; }
+        public bool FiltersChanged { get; set; }
+        public int PeriodSelected { get; set; }
+    }
+}
