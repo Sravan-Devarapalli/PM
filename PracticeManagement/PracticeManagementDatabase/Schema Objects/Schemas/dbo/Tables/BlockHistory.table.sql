107fcc47ad8a80d97fd255f8f22bde47027edef9
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/BlockHistory.table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[BlockHistory]
+(
+	[Id]				INT IDENTITY(1,1) NOT NULL,
+	[PersonId]			INT         NOT NULL,
+	BlockStartDate		DATETIME	NULL,
+	BlockEndDate		DATETIME	NULL,
+	ModifiedDate		DATETIME	NOT NULL,
+	ModifiedBy			INT			NULL,
+	CONSTRAINT PK_BlockHistory PRIMARY KEY (Id)
+)
+
