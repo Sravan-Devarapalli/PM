9b86a26be32808bae1b87ed78e5ba8376daacf95
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CapabilityDelete.proc.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [dbo].[CapabilityDelete]
+(
+	@CapabilityId INT,
+	@UserLogin NVARCHAR(MAX)
+)
+AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRAN CapabilityDelete_Tran;
+			EXEC SessionLogPrepare @UserLogin = @UserLogin
+
+			DELETE [dbo].PracticeCapabilities
+			WHERE CapabilityId = @CapabilityId
+
+		COMMIT TRAN CapabilityDelete_Tran;
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN CapabilityDelete_Tran;
+		DECLARE	 @ERROR_STATE	tinyint
+				,@ERROR_SEVERITY		tinyint
+				,@ERROR_MESSAGE		    nvarchar(2000)
+				,@InitialTranCount		tinyint
+
+		SET	 @ERROR_MESSAGE		= ERROR_MESSAGE()
+		SET  @ERROR_SEVERITY	= ERROR_SEVERITY()
+		SET  @ERROR_STATE		= ERROR_STATE()
+		RAISERROR ('%s', @ERROR_SEVERITY, @ERROR_STATE, @ERROR_MESSAGE)
+	END CATCH
+	EXEC dbo.SessionLogUnprepare
+
+END
