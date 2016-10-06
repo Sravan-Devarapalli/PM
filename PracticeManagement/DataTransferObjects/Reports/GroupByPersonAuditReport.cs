28397591f90f2bb12153bc87a8eeb6edccdedd6b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Reports/GroupByPersonAuditReport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Reports
+{
+    [DataContract]
+    [Serializable]
+    public class GroupByPersonAuditReport
+    {
+     
+
+
+        [DataMember]
+        public List<TimeEntryAudit> TimeEntries
+        {
+            get;
+            set;
+        }
+
+
+    }
+}
+
