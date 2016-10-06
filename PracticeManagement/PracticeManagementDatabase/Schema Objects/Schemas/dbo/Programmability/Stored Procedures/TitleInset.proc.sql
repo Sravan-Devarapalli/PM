bc1ff04aa8d19ff3918fc8890b723c97e0a75ee7
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TitleInset.proc.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[TitleInset]
+(
+	@Title			NVARCHAR(100),
+	@TitleTypeId	INT,
+	@SortOrder		INT,
+	@PTOAccrual		INT,
+	@MinimumSalary	INT,
+	@MaximumSalary	INT,
+	@UserLogin  NVARCHAR(255) 
+)
+AS
+BEGIN
+	EXEC SessionLogPrepare @UserLogin = @UserLogin
+
+	INSERT INTO dbo.Title(Title,TitleTypeId,SortOrder,PTOAccrual,MinimumSalary,MaximumSalary)
+	SELECT @Title,@TitleTypeId,@SortOrder,@PTOAccrual,@MinimumSalary,@MaximumSalary
+	
+	EXEC SessionLogUnprepare 
+END
+
