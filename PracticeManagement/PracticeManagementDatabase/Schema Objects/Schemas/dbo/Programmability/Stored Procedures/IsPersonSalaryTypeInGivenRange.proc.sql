df145e4a49d3d8d1a216c7fd8ca3111b04941b7b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/IsPersonSalaryTypeInGivenRange.proc.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[IsPersonSalaryTypeInGivenRange]
+(
+	@PersonId    INT,
+	@StartDate   DATETIME,
+	@EndDate     DATETIME
+)
+AS
+BEGIN
+	DECLARE @IsSalaryType NVARCHAR(10)='False'	
+	IF EXISTS(	SELECT 1
+				FROM  Pay
+				WHERE Person = @PersonId
+					  AND StartDate <= @EndDate AND @StartDate <= EndDate-1 
+					  AND Timescale = 2 -- W2 SALARY
+			 )
+	BEGIN
+		   SET @IsSalaryType = 'True'
+	END
+	SELECT @IsSalaryType AS IsSalary
+END
+
