65cd40b832b2219f36221a236344b13a5a3b664e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetPersonMilestonesOnPreviousHireDate.proc.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[GetPersonMilestonesOnPreviousHireDate]
+	(
+		@PersonId	INT,
+		@PreviousHireDate	DATETIME 
+	)
+AS
+BEGIN
+
+	SELECT DISTINCT P.ProjectId,
+			M.MilestoneId,
+		   P.ProjectNumber,
+		   P.Name AS ProjectName,
+		   M.Description 
+	FROM dbo.Project P
+ 	JOIN dbo.Milestone M ON M.ProjectId = P.ProjectId
+	JOIN dbo.MilestonePerson MP ON MP.MilestoneId = M.MilestoneId
+	JOIN dbo.MilestonePersonEntry MPE ON MPE.MilestonePersonId = MP.MilestonePersonId
+	WHERE MP.PersonId = @PersonId AND MPE.StartDate = @PreviousHireDate
+	ORDER BY ProjectNumber
+END
