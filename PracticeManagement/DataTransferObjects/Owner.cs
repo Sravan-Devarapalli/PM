4a5ee0c50b0270e902421e30bfd14d21f6eeda08
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Owner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class Owner
+    {
+        [DataMember]
+        public string Target
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public bool IsDivisionOwner
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public bool isAssignedToProjects
+        {
+            get;
+            set;
+        }
+    }
+}
+
