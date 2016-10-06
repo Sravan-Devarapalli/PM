cf8b7550bdcae2718ad47e6bf0a9dc637910c57a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ContextObjects/OpportunitySortingContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.ContextObjects
+{
+    [DataContract]
+    [Serializable]
+    public class OpportunitySortingContext
+    {
+        [DataMember]
+        public string OrderBy { get; set; }
+
+        [DataMember]
+        public string SortDirection { get; set; }
+    }
+}
