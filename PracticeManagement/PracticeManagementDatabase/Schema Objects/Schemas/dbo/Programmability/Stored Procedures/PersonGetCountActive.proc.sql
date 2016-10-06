d00494a8d6a03ae8267c05013941ca67786d6312
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonGetCountActive.proc.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE dbo.PersonGetCountActive
+(
+	@StartDate   DATETIME,
+	@EndDate     DATETIME
+)
+AS
+	SET NOCOUNT ON
+	SELECT EmployeesNumber = dbo.GetEmployeeNumber(@StartDate, @EndDate),		
+		   ConsultantsNumber = dbo.GetCounsultantsNumber(@StartDate, @EndDate)
+
+
