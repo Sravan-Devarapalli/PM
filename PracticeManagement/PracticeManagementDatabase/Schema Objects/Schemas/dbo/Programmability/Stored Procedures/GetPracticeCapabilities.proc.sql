03aca61e462f6da86f50661ab71e997c32b06252
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetPracticeCapabilities.proc.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[GetPracticeCapabilities]
+(
+	@PracticeId INT = NULL,
+	@CapabilityId INT = NULL
+)
+AS
+BEGIN
+	SELECT	CapabilityId,
+			PC.[CapabilityName] AS Name,
+			PC.PracticeId,
+			ISNULL(P.Abbreviation,P.Name)  AS PracticeAbbreviation,
+			PC.IsActive
+	FROM dbo.[PracticeCapabilities] PC
+	INNER JOIN dbo.Practice  P ON P.PracticeId = PC.PracticeId 
+	WHERE	( @PracticeId IS NULL OR P.PracticeId = @PracticeId ) 
+			AND ( @CapabilityId IS NULL OR PC.CapabilityId = @CapabilityId ) 
+	ORDER BY ISNULL(P.Abbreviation,P.Name),PC.[CapabilityName]
+END
+
