6d556a5c81426496ab84d3c4de4f389753d4bfc6
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetWorkTypeById.proc.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[GetWorkTypeById]
+(
+	@TimeTypeId INT
+)
+AS
+BEGIN
+	DECLARE	@ORTTimeTypeId	INT = dbo.GetORTTimeTypeId(),
+			@UnpaidTimeTypeId	INT = dbo.GetUnpaidTimeTypeId()
+
+	SELECT	TT.TimeTypeId,
+			TT.Name,
+			CASE WHEN TT.TimeTypeId = @ORTTimeTypeId THEN CONVERT(BIT, 1) ELSE CONVERT(BIT, 0) END [IsORTTimeType],
+			CASE WHEN TT.TimeTypeId = @UnpaidTimeTypeId THEN CONVERT(BIT, 1) ELSE CONVERT(BIT, 0) END [IsUnpaidTimeType],
+			TT.IsW2HourlyAllowed,
+			TT.IsW2SalaryAllowed
+	 FROM dbo.TimeType TT
+	 WHERE TimeTypeId = @TimeTypeId
+END
+
