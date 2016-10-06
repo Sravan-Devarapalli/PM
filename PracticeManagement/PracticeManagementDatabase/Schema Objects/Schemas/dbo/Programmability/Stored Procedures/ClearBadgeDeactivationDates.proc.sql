97077cde1f769269a66808333b9497908b29c514
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ClearBadgeDeactivationDates.proc.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE [dbo].[ClearBadgeDeactivationDates]
+(
+	@PersonId	INT
+)
+AS
+BEGIN
+
+	IF EXISTS (
+				SELECT 1 
+				FROM dbo.MilestonePersonEntry MPE 
+				JOIN dbo.MilestonePerson MP ON MP.MilestonePersonId = MPE.MilestonePersonId
+				JOIN dbo.Milestone MS ON MS.MilestoneId = MP.MilestoneId
+				JOIN dbo.MSBadge M ON M.PersonId = MP.PersonId
+				JOIN dbo.Project P ON P.ProjectId = MS.ProjectId
+				WHERE P.ProjectStatusId IN (1,2,3,4) AND MP.PersonId = @PersonId AND MPE.IsApproved = 1 AND M.DeactivatedDate IS NOT NULL AND (MPE.BadgeStartDate <= M.OrganicBreakEndDate AND M.DeactivatedDate <= MPE.BadgeEndDate)
+			  )
+	BEGIN
+
+		UPDATE dbo.MSBadge
+		SET DeactivatedDate = NULL,
+			OrganicBreakStartDate = NULL,
+			OrganicBreakEndDate = NULL
+		WHERE PersonId = @PersonId
+
+	END
+	
+END
+
