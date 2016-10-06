5e667432e96f9b000c1499fd4c713111d31703b7
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Programmability/Stored Procedures/GetPersonIndustriesByPersonId.proc.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [Skills].[GetPersonIndustriesByPersonId]
+	@PersonId INT
+AS
+BEGIN
+
+	SELECT PInd.IndustryId
+		,PInd.YearsExperience
+	FROM [Skills].[PersonIndustry] PInd
+	JOIN Skills.Industry I ON I.IndustryId = PInd.IndustryId
+	WHERE PInd.PersonId = @PersonId
+		AND I.IsActive = 1
+		AND I.IsDeleted = 0
+	ORDER BY I.Description
+END
+
