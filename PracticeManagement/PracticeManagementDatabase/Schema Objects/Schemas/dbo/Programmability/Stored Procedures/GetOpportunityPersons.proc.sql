c141ce2043f9d09fe8c5e3d0c71eb25aed8b55d5
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetOpportunityPersons.proc.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[GetOpportunityPersons]
+	@OpportunityId int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT   op.PersonId
+			,p.FirstName
+			,p.LastName,
+			op.OpportunityPersonTypeId,
+			op.RelationTypeId,
+			op.Quantity,
+			op.NeedBy,
+			p.PersonStatusId 
+	FROM dbo.OpportunityPersons op
+	INNER JOIN dbo.Person p ON p.PersonId = op.PersonId
+	WHERE op.OpportunityId = @OpportunityId
+			AND (
+					p.PersonStatusId IN(1,3,5)
+					OR
+					p.IsStrawman = 1
+				)
+END
