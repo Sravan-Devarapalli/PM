2b6894186b0d2a6a94729c15b5568b0d5891dfa8
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_TimeEntryHistory_ActivityTypeId.fkey.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE [dbo].[TimeEntryHistory]  
+WITH NOCHECK ADD  CONSTRAINT [FK_TimeEntryHistory_ActivityTypeId] FOREIGN KEY([ActivityTypeId])
+REFERENCES [dbo].[UserActivityType] ([ActivityTypeID])
