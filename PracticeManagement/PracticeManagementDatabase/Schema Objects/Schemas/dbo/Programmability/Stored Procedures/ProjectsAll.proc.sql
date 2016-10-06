d0e3f71bb72349356a84cbc0b8e03967f93881f1
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectsAll.proc.sql
@@ -0,0 +1,38 @@
+﻿CREATE PROCEDURE [dbo].[ProjectsAll]
+AS
+	SELECT DISTINCT p.ClientId,
+		   p.ProjectId,
+		   p.Discount,
+		   p.Terms,
+		   p.Name,
+		   p.PracticeManagerId,
+		   p.PracticeId,
+		   p.StartDate,
+		   p.EndDate,
+		   p.ClientName,
+		   p.PracticeName,
+		   p.ProjectStatusId,
+		   p.ProjectStatusName,
+		   p.ProjectNumber,
+	       p.BuyerName,
+           p.OpportunityId,
+           p.GroupId,
+	       p.ClientIsChargeable,
+	       p.ProjectIsChargeable,
+		   p.ProjectManagerId,
+		   p.ProjectManagerFirstName,
+		   p.ProjectManagerLastName,
+		   p.DirectorId,
+		   p.DirectorLastName,
+		   p.DirectorFirstName,
+		   pg.Name AS GroupName,
+		   1 AS InUse,
+		   c.PersonId AS 'SalespersonId',
+		   cp.LastName+' , ' +cp.FirstName AS 'SalespersonName'
+	FROM v_Project p
+	LEFT JOIN dbo.ProjectGroup AS pg ON p.GroupId = pg.GroupId
+	LEFT JOIN dbo.Commission c ON c.ProjectId = p.ProjectId AND c.CommissionType = 1
+	LEFT JOIN dbo.Person cp ON cp.PersonId = c.PersonId 
+
+GO
+
