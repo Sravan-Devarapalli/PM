983c8e3d5a2dc7b392c9bbac6e0486a892cc819d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ProjectCapabilities.table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ProjectCapabilities]
+(
+	[Id]				INT	IDENTITY (1, 1) NOT NULL,
+    [ProjectId]			INT					NOT NULL,
+    [CapabilityId]		INT					NOT NULL,
+	CONSTRAINT PK_ProjectCapabilities_Id PRIMARY KEY CLUSTERED([Id]),
+	CONSTRAINT [UQ_ProjectCapabilities_ProjectId_CapabilityId] UNIQUE NONCLUSTERED([ProjectId] ASC, [CapabilityId] ASC) ON [PRIMARY]
+)
+
