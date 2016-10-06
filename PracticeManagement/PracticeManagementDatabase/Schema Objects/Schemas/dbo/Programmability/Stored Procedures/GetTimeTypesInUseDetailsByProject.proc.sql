52620e62fe6d750d0bbc71fccf7ddb721311367c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetTimeTypesInUseDetailsByProject.proc.sql
@@ -0,0 +1,29 @@
+﻿-- =========================================================================
+-- Author:		Sainath.CH
+-- Create date: 03-05-2012
+-- Description:  Gets the WorkType List InUse Details.
+-- =========================================================================
+CREATE PROCEDURE [dbo].[GetTimeTypesInUseDetailsByProject]
+    (
+      @ProjectId INT ,
+      @TimeTypeIds NVARCHAR(MAX)
+    )
+AS 
+    BEGIN
+
+        SELECT  TT.TimeTypeId ,
+                TT.Name ,
+                MAX(CASE WHEN TE.ChargeCodeId IS NULL THEN 0
+                         ELSE 1
+                    END) AS [InUse]
+        FROM    dbo.TimeType TT
+                LEFT JOIN dbo.ChargeCode CC ON TT.TimeTypeId = CC.TimeTypeId
+                                               AND CC.ProjectId = @ProjectId
+                LEFT JOIN dbo.TimeEntry TE ON CC.ID = TE.ChargeCodeId
+        WHERE   TT.TimeTypeId IN (
+                SELECT  ResultId
+                FROM    dbo.ConvertStringListIntoTable(@TimeTypeIds) )
+        GROUP BY TT.TimeTypeId ,
+                TT.Name
+
+    END
