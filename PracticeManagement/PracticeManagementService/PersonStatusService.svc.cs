3f3de94f58240bf1b95d6b7bb12623d8fa89d994
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementService/PersonStatusService.svc.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel.Activation;
+using DataAccess;
+using DataTransferObjects;
+
+namespace PracticeManagementService
+{
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class PersonStatusService : IPersonStatusService
+    {
+        #region IPersonStatusService Members
+
+        /// <summary>
+        /// Retrives the list of the person statuses.
+        /// </summary>
+        /// <returns>The list of the <see cref="PersonStatus"/> objects.</returns>
+        public List<PersonStatus> GetPersonStatuses()
+        {
+            return PersonStatusDAL.PersonStatusListAll();
+        }
+
+        #endregion IPersonStatusService Members
+    }
+}
