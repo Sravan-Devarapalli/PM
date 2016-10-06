0eed551b9dc1b9dc8f41034377cbd0d0fd1c160a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SaveFeedbackCancelationDetails.proc.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE [dbo].[SaveFeedbackCancelationDetails]
+(
+	@FeedbackId	INT,
+	@StatusId	INT=2,--status 2 is not completed
+	@IsCanceled BIT,
+	@CancelationReason NVARCHAR(MAX)=NULL,
+	@UserLogin       NVARCHAR(255)
+)
+AS
+BEGIN
+ 
+	DECLARE @ModifiedBy INT,
+			@CurrentPMTime DATETIME
+	SELECT @ModifiedBy = PersonId FROM Person WHERE Alias = @UserLogin
+	SELECT @CurrentPMTime = dbo.InsertingTime()
+
+	EXEC SessionLogUnprepare
+	EXEC SessionLogPrepare @UserLogin = @UserLogin
+
+	UPDATE dbo.ProjectFeedback 
+	SET	IsCanceled = @IsCanceled,
+		CancelationReason = @CancelationReason,
+		FeedbackStatusId = CASE WHEN @IsCanceled = 1 THEN 3 ELSE @StatusId END,
+		CompletionCertificateBy = @ModifiedBy,
+		CompletionCertificateDate = @CurrentPMTime
+	WHERE FeedbackId =  @FeedbackId
+		  
+END
