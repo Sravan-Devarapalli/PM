85d9371437894bf627f04cca85d6d706b11978a3
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DefaultRecruiterCommissionGetByPersonDate.proc.sql
@@ -0,0 +1,24 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 9-02-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	Selects the default recruiter commission for the specified person and date
+-- =============================================
+CREATE PROCEDURE [dbo].[DefaultRecruiterCommissionGetByPersonDate]
+(
+	@PersonId   INT,
+	@Date       DATETIME
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+	DECLARE @FutureDate DATETIME
+	SET @FutureDate = dbo.GetFutureDate()
+
+	SELECT h.DefaultRecruiterCommissionHeaderId, h.PersonId, h.StartDate,
+	       CASE h.EndDate WHEN @FutureDate THEN CAST(NULL AS DATETIME) ELSE h.EndDate END AS EndDate,
+	       dbo.MakeDefaultRecruiterCommissionText(h.defaultrecruitercommissionheaderid) AS TextLine
+	  FROM dbo.DefaultRecruiterCommissionHeader as h
+	 WHERE h.PersonId = @PersonId AND @Date >= h.StartDate AND @Date < h.EndDate
+END
