f0444bdd7a7447067bad9d0ca68648d4acc01acf
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Reports/UtilizationTimeline.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using PraticeManagement.Utils;
+using DataTransferObjects;
+using DataTransferObjects.Filters;
+
+namespace PraticeManagement.Reporting
+{
+    public partial class UtilizationTimeline : System.Web.UI.Page
+    {
+        public Controls.Reports.ConsultantsWeeklyReport ConsultantsControl
+        {
+            get
+            {
+                return repWeekly;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void RegisterClientScripts()
+        {
+            System.Web.UI.ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "", "EnableDisableRadioButtons();", true);
+        }
+    }
+}
+
