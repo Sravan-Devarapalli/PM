4846b70ef76ff72a95d697e5db34edb2c64b907b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/EmailData.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class EmailData
+    {
+        #region Properties
+
+        /// <summary>
+        /// Data to be converted into the template
+        /// </summary>
+        [DataMember]
+        public DataSet Data
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Email template
+        /// </summary>
+        [DataMember]
+        public EmailTemplate EmailTemplate
+        {
+            get;
+            set;
+        }
+
+        #endregion Properties
+
+        #region Methods
+
+        public bool IsEmpty
+        {
+            get { return Data == null || Data.Tables[0].Rows.Count == 0; }
+        }
+
+        public IEnumerable<string> DataHeaders
+        {
+            get
+            {
+                return from DataColumn column in Data.Tables[0].Columns select column.ColumnName;
+            }
+        }
+
+        #endregion Methods
+    }
+}
