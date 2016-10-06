47cc40f4a477b193f3c26671d2f70dd3bbb2a464
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CSATInsert.proc.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [dbo].[CSATInsert]
+(
+	@ProjectCSATId			INT OUTPUT,
+	@ProjectId		INT, 
+	@ReviewStartDate DATETIME,
+	@ReviewEndDate DATETIME,
+	@CompletionDate DATETIME,
+	@ReviewerId		INT,
+	@ReferralScore	INT,
+	@Comments		NVARCHAR(MAX),
+	@UserLogin      NVARCHAR(255)
+)
+AS
+BEGIN
+	-- Start logging session
+	EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
+
+	DECLARE @InsertingTime DATETIME,
+	        @ModifiedBy    INT
+	SELECT @InsertingTime=dbo.InsertingTime(),@ModifiedBy=PersonId FROM Person WHERE Alias=@UserLogin;
+
+	INSERT INTO ProjectCSAT(ProjectId,ReviewStartDate,ReviewEndDate,CompletionDate,ReferralScore,ReviewerId,Comments,CreatedDate,ModifiedDate,ModifiedBy)
+	VALUES (@ProjectId,@ReviewStartDate,@ReviewEndDate,@CompletionDate,@ReferralScore,@ReviewerId,@Comments,@InsertingTime,@InsertingTime,@ModifiedBy)
+
+	SET @ProjectCSATId = SCOPE_IDENTITY()
+
+	-- End logging session
+	EXEC dbo.SessionLogUnprepare
+
+END
+
