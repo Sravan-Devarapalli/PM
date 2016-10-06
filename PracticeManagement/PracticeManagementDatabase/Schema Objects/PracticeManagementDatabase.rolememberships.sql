ca960715c69eff8dc1cbab21d6f75daa9556a227
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/PracticeManagementDatabase.rolememberships.sql
@@ -0,0 +1,135 @@
+﻿EXECUTE sp_addrolemember @rolename = N'aspnet_Membership_BasicAccess', @membername = N'setuptest';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'aspnet_Membership_ReportingAccess', @membername = N'aspnet_Membership_FullAccess';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'aspnet_Membership_ReportingAccess', @membername = N'setuptest';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'aspnet_Profile_FullAccess', @membername = N'setuptest';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'aspnet_Profile_BasicAccess', @membername = N'aspnet_Profile_FullAccess';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'aspnet_Profile_BasicAccess', @membername = N'setuptest';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'aspnet_Profile_ReportingAccess', @membername = N'aspnet_Profile_FullAccess';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'aspnet_Profile_ReportingAccess', @membername = N'setuptest';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'aspnet_Roles_FullAccess', @membername = N'setuptest';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'aspnet_Roles_BasicAccess', @membername = N'aspnet_Roles_FullAccess';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'aspnet_Roles_BasicAccess', @membername = N'setuptest';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'aspnet_Roles_ReportingAccess', @membername = N'aspnet_Roles_FullAccess';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'aspnet_Roles_ReportingAccess', @membername = N'setuptest';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'aspnet_Personalization_FullAccess', @membername = N'setuptest';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'aspnet_Personalization_BasicAccess', @membername = N'aspnet_Personalization_FullAccess';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'aspnet_Personalization_BasicAccess', @membername = N'setuptest';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'aspnet_Personalization_ReportingAccess', @membername = N'aspnet_Personalization_FullAccess';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'aspnet_Personalization_ReportingAccess', @membername = N'setuptest';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'aspnet_WebEvent_FullAccess', @membername = N'setuptest';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'db_owner', @membername = N'setuptest';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'db_accessadmin', @membername = N'setuptest';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'db_securityadmin', @membername = N'setuptest';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'db_ddladmin', @membername = N'setuptest';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'db_backupoperator', @membername = N'setuptest';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'db_datareader', @membername = N'setuptest';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'db_datawriter', @membername = N'setuptest';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'db_denydatareader', @membername = N'setuptest';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'db_denydatawriter', @membername = N'setuptest';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'PracticeManager', @membername = N'PracticeManagementUser';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'PracticeManager', @membername = N'setuptest';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'PracticeUnitTest', @membername = N'PracticeManagementUser';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'PracticeUnitTest', @membername = N'setuptest';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'aspnet_Membership_FullAccess', @membername = N'setuptest';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'aspnet_Membership_BasicAccess', @membername = N'aspnet_Membership_FullAccess';
+
+
