1dda609004594b3c46a576c9f6619938a4c05324
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CheckIfDatesInDeactivationHistory.proc.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[CheckIfDatesInDeactivationHistory]
+(
+	@PersonId	INT,
+	@StartDate	DATETIME,
+	@EndDate	DATETIME
+)
+AS
+BEGIN
+
+	DECLARE @OrganicBreaksExists BIT=0,
+			@18MonthBreakExists	 BIT=0
+			
+	IF EXISTS(SELECT 1 FROM BadgeDeactivationHisroty WHERE PersonId = @PersonId AND ((OrganicBreakStartDate <= @EndDate AND @StartDate <= OrganicBreakEndDate) OR DeactivatedDate BETWEEN @StartDate AND @EndDate))
+	BEGIN
+		SET @OrganicBreaksExists = 1
+	END
+	IF EXISTS(SELECT 1 FROM v_CurrentMSBadge WHERE PersonId = @PersonId AND BreakStartDate <= @EndDate AND @StartDate <= BreakEndDate)
+	BEGIN
+		SET @18MonthBreakExists = 1
+	END
+
+	SELECT @OrganicBreaksExists AS OrganicBreaksExists, @18MonthBreakExists AS BadgeBreakExists
+END
