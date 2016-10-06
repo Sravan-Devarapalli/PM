c31b2d91c2500c8b1305f25c2241320933c1d15b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Filters/PersonsFilters.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTransferObjects.Filters
+{
+    [Serializable]
+    public class PersonsFilters
+    {
+        public string SearchText
+        {
+            get;
+            set;
+        }
+        public string ReportView
+        {
+            get;
+            set;
+        }
+
+        public string RecruiterIds
+        {
+            get;
+            set;
+        }
+
+        public string PracticeAreaIds
+        {
+            get;
+            set;
+        }
+
+        public string DivisionIds
+        {
+            get;
+            set;
+        }
+
+        public string PayTypeIds
+        {
+            get;
+            set;
+        }
+
+        public bool IsActive
+        {
+            get;
+            set;
+        }
+
+        public bool IsTerminated
+        {
+            get;
+            set;
+        }
+
+        public bool IsContingent
+        {
+            get;
+            set;
+        }
+
+        public bool IsTeminationPending
+        {
+            get;
+            set;
+        }
+    }
+}
+
