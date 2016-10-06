bd58683027cd90614cda6d5831c5da2b353f169b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Reports/AttainmentBillableutlizationReport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Reports
+{
+    [DataContract]
+    [Serializable]
+    public class AttainmentBillableutlizationReport
+    {
+        [DataMember]
+        public Person Person { get; set; }
+
+        [DataMember]
+        public List<BillableUtlizationByRange> BillableUtilizationList { get; set; }
+    }
+}
