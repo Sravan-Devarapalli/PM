b0277daa4dafc2606e29d13d686d8c49a75c4ca1
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonRoleListAll.proc.sql
@@ -0,0 +1,16 @@
+﻿--The source is 
+-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 7-9-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	List person roles
+-- =============================================
+CREATE PROCEDURE [dbo].[PersonRoleListAll]
+AS
+	SET NOCOUNT ON
+
+	SELECT r.PersonRoleId, r.Name
+	  FROM dbo.PersonRole AS r
+	ORDER BY r.PersonRoleId
+
