782621f08c731156bf83a51c84a756f4f6a027fc
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Reports/Bench.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using PraticeManagement.Controls;
+
+namespace PraticeManagement.Reporting
+{
+    public partial class Bench : PracticeManagementPageBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override void Display()
+        {
+            
+        }
+    }
+}
+
