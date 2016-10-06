85fce901483a1228214f4e31aaca7811391a35a7
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementTasks/HireDateExpiredEmailNotifier/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HireDateExpiredEmailNotifier.ConfigurationService;
+
+namespace HireDateExpiredEmailNotifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ConfigurationServiceClient service = new ConfigurationServiceClient();
+                service.CheckProjectedProjectsByHireDate(Properties.Settings.Default.EmailTemplateID, Properties.Settings.Default.CompanyName);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
+
