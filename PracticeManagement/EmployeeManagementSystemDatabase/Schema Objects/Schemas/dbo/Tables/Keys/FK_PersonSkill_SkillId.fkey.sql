c033b16adbbe30597722baa8722b0ada2eff26e3
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/EmployeeManagementSystemDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_PersonSkill_SkillId.fkey.sql
@@ -0,0 +1,5 @@
+﻿ALTER TABLE [dbo].[PersonSkill]  WITH CHECK ADD  CONSTRAINT [FK_PersonSkill_SkillId] FOREIGN KEY([SkillId])
+REFERENCES [dbo].[Skill] ([SkillId])
+ON UPDATE CASCADE
+GO
+ALTER TABLE [dbo].[PersonSkill] CHECK CONSTRAINT [FK_PersonSkill_SkillId]
