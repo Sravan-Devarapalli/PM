4e438ac48cf20e3d6d78c1b1e86beeb3720eb5ee
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_MilestonePersonVacations.view.sql
@@ -0,0 +1,21 @@
+﻿CREATE VIEW dbo.v_MilestonePersonVacations
+AS  
+	SELECT  m.MilestoneId,
+			mp.MilestonePersonId,
+			mp.PersonId,
+			mpe.StartDate,
+			mpe.EndDate,
+			ISNULL(SUM(mpe.HoursPerDay), 0) AS 'VacationHours'			
+    FROM    dbo.Milestone AS m
+            INNER JOIN dbo.MilestonePerson AS mp ON m.MilestoneId = mp.MilestoneId
+            INNER JOIN dbo.MilestonePersonEntry AS mpe ON mp.MilestonePersonId = mpe.MilestonePersonId
+            INNER JOIN dbo.v_PersonCalendar AS cal ON cal.Date BETWEEN mpe.StartDate
+                                                                 AND   mpe.EndDate
+                                                        AND cal.PersonId = mp.PersonId
+            WHERE cal.DayOff = 1 AND 
+				(DATEPART(weekday, cal.Date) != 1 AND DATEPART(weekday, cal.Date) != 7)
+	GROUP BY m.MilestoneId,
+			mp.MilestonePersonId,
+			mp.PersonId,
+			mpe.StartDate,
+			mpe.EndDate
