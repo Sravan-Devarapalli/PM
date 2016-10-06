4d6a6e1f8fec5c8c9340fa04991abea7333ae3ef
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/PersonStatus.table.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[PersonStatus] (
+    [PersonStatusId] INT           NOT NULL,
+    [Name]           NVARCHAR (25) NULL,
+	[IsPersonStatus]	BIT NOT NULL CONSTRAINT DF_PersonStatus_IsPersonStatus DEFAULT(1)
+);
