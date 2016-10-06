ff49533a4c8bb513b2dff02c01a8cd897a2f28b5
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/VendorAttachement.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class VendorAttachment
+    {
+        [DataMember]
+        public int AttachmentId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public String AttachmentFileName
+        {
+            get;
+            set;
+        }
+
+
+        [DataMember]
+        public Byte[] AttachmentData
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int AttachmentSize
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime? UploadedDate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string Uploader
+        {
+            get;
+            set;
+        }
+    }
+}
+
