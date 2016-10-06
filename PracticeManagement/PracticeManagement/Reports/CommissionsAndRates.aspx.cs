b330088236036c66ddd7dc40f5f0f054e74ff17e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Reports/CommissionsAndRates.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PraticeManagement
+{
+    public partial class CommissionsAndRates : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
