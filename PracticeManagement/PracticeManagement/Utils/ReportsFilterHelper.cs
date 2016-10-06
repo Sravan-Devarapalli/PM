8c9c9bc871e6616f4fac545f618bda8241bf8600
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Utils/ReportsFilterHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataTransferObjects;
+using PraticeManagement.Controls;
+using System.Web.UI;
+
+namespace PraticeManagement.Utils
+{
+    public class ReportsFilterHelper
+    {
+
+        private static string SessionId
+        {
+            get
+            {
+                var session = HttpContext.Current.Request.Cookies["ASP.NET_SessionId"];
+                if (session != null)
+                {
+                    return session.Value;
+                }
+                return string.Empty;
+            }
+        }
+
+        public static void SaveFilterValues(ReportName report, object filter)
+        {
+            if (!string.IsNullOrEmpty(SessionId))
+            {
+                string filterData = SerializationHelper.SerializeBase64(filter);
+                ServiceCallers.Custom.Person(p => p.SaveReportFilterValues(PracticeManagementMain.CurrentUserID, (int)report, filterData, PracticeManagementMain.PreviousUserId, SessionId));
+            }
+        }
+
+        public static object GetFilterValues(ReportName report)
+        {
+            if (!string.IsNullOrEmpty(SessionId))
+            {
+                string filterData = ServiceCallers.Custom.Person(p => p.GetReportFilterValues(PracticeManagementMain.CurrentUserID, (int)report, PracticeManagementMain.PreviousUserId, SessionId));
+                if (!string.IsNullOrEmpty(filterData))
+                {
+                    return SerializationHelper.DeserializeBase64(filterData);
+                }
+            }
+            return null;
+        }
+    }
+}
+
