d0525e728e041b8dae31818156a58d012a1de8eb
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DeleteStrawMan.proc.sql
@@ -0,0 +1,30 @@
+﻿CREATE PROCEDURE [dbo].[DeleteStrawman]
+(
+	@PersonId	INT,
+	@UserLogin	NVARCHAR(255)	 
+)
+AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRAN DeleteStrawman
+		EXEC dbo.SessionLogPrepare @userLogin = @UserLogin
+		
+		DELETE MilestonePerson
+		WHERE personid = @PersonId
+
+		--Deleting the Strawman
+		DELETE P
+		FROM dbo.Person P
+		WHERE P.PersonId = @PersonId AND P.IsStrawman = 1
+
+		EXEC dbo.SessionLogUnprepare
+		COMMIT TRAN DeleteStrawman
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN DeleteStrawman
+		DECLARE @Error NVARCHAR(2000)
+		SET @Error = ERROR_MESSAGE()
+		RAISERROR(@Error, 16, 1)
+	END CATCH
+END
+
