75a8db69822eaf1408acbe26d76e518b27c47e4c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetProjectCapabilitiesNames.function.sql
@@ -0,0 +1,19 @@
+﻿CREATE FUNCTION [dbo].[GetProjectCapabilitiesNames]
+(
+	@ProjectId	INT
+)
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN
+	DECLARE @rep NVARCHAR(MAX)
+	SET @rep = ''
+
+	SELECT @rep= @rep + CONVERT(NVARCHAR(255),ISNULL(P.Abbreviation,P.Name)) +' - '+ CONVERT(NVARCHAR(255),C.CapabilityName) + ';'
+	FROM dbo.ProjectCapabilities PC
+	JOIN dbo.PracticeCapabilities C ON C.CapabilityId = PC.CapabilityId
+	JOIN dbo.Practice P ON P.PracticeId = C.PracticeId
+    WHERE PC.ProjectId = @ProjectId
+	ORDER BY ISNULL(P.Abbreviation,P.Name),C.CapabilityName
+
+	RETURN @rep
+END
