1088936f3247b294acfecbab5d2d09576fa8eeec
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MilestoneGetById.proc.sql
@@ -0,0 +1,36 @@
+﻿CREATE PROCEDURE dbo.MilestoneGetById
+(
+	@MilestoneId   INT
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	SELECT m.ClientId,
+	       m.ProjectId,
+	       m.MilestoneId,
+	       m.Description,
+	       m.Amount,
+	       m.StartDate,
+		   m.ProjectStatusId,
+		   m.ProjectStatusName,
+	       m.ProjectedDeliveryDate,
+	       m.IsHourlyAmount,
+	       m.ProjectName,
+	       m.ProjectStartDate,
+	       m.ProjectEndDate,
+		   m.ProjectNumber,
+	       m.Discount,
+	       m.ClientName,
+	       m.ExpectedHours,
+	       m.PersonCount,
+	       m.ProjectedDuration,
+	       m.ConsultantsCanAdjust,
+	       m.ClientIsChargeable,
+	       m.ProjectIsChargeable,
+	       m.MilestoneIsChargeable,		  
+		   m.IsMarginColorInfoEnabled
+	  FROM dbo.v_Milestone AS m
+	 WHERE m.MilestoneId = @MilestoneId
+END
+
