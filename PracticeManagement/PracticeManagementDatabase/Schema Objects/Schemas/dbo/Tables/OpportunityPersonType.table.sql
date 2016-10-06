1eee3abed9b994b9092f1dca00f9f238992e575c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/OpportunityPersonType.table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE dbo.OpportunityPersonType
+(
+	[Id]   INT IDENTITY(1,1) NOT NULL,
+	[PersonType] NVARCHAR(255) NOT NULL,
+	CONSTRAINT PK_OpportunityPersonType_Id PRIMARY KEY CLUSTERED([Id])
+); 
