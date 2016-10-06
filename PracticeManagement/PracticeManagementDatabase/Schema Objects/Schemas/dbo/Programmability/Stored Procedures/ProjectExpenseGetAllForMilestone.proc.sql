9abd04a86598a737c2613cecc7694742ec29ebd5
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectExpenseGetAllForMilestone.proc.sql
@@ -0,0 +1,29 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2010-04-20
+-- =============================================
+CREATE PROCEDURE dbo.ProjectExpenseGetAllForMilestone 
+	@MilestoneId int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT  PE.Id ExpenseId,
+			PE.Name ExpenseName,
+			PE.ProjectId,
+			PE.StartDate,
+			PE.EndDate,
+			PE.Amount ExpenseAmount,
+			PE.ExpectedAmount ExpectedExpenseAmount,
+			PE.Reimbursement ExpenseReimbursement,
+			M.Description AS MilestoneName,
+			PE.ExpenseTypeId,
+			ET.Name ExpenseTypeName
+	FROM dbo.ProjectExpense PE
+	LEFT JOIN dbo.Milestone M ON M.MilestoneId = PE.MilestoneId
+	LEFT JOIN dbo.ExpenseType ET ON PE.ExpenseTypeId=ET.Id
+	WHERE PE.MilestoneId = @MilestoneId
+	ORDER BY PE.StartDate
+
+END
+
