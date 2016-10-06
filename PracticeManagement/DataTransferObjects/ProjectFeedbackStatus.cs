6154bf8c14bac245c42cd820da87f6e03228963a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ProjectFeedbackStatus.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace DataTransferObjects
+{
+    [Serializable]
+    [DataContract]
+    public class ProjectFeedbackStatus
+    {
+        [DataMember]
+        public int? Id
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string HtmlEncodedName
+        {
+            get
+            {
+                return HttpUtility.HtmlEncode(Name);
+            }
+        }
+    }
+}
+
