b03407c7486146be887fd9047f126362a3840678
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_MonthlyExpense_WeekPaidOptionId.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[MonthlyExpense]
+    ADD CONSTRAINT [FK_MonthlyExpense_WeekPaidOptionId] FOREIGN KEY ([WeekPaidOptionId]) REFERENCES [dbo].[WeekPaidOption] ([WeekPaidOptionId]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
