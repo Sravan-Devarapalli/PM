bfde80986b1273a7590357ba69f1c98047964a07
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ComputedFinancials/ComputedFinancialsBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Financials
+{
+    /// <summary>
+    /// This class will be a replacement to the legacy ComputedFinancials class
+    /// because the later have become hard to maintain
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class ComputedFinancialsBase
+    {
+    }
+}
+
