3cd57a16f157131b26dcf1379b27629222814b0a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetTitleById.proc.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[GetTitleById]
+(
+	@TitleId		INT
+)
+AS
+BEGIN
+	SELECT T.TitleId,T.Title,TT.TitleTypeId,TT.TitleType,T.SortOrder,T.PTOAccrual,T.MinimumSalary,T.MaximumSalary
+	FROM dbo.Title T
+	INNER JOIN dbo.TitleType TT ON T.TitleTypeId = TT.TitleTypeId
+	WHERE T.TitleId = @TitleId
+END
