8b5b948c8c3246e70382879daf0d705fd19174f0
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DefaultRecruiterCommissionCleanup.proc.sql
@@ -0,0 +1,28 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2009-11-05
+-- Description:	Cleans up unused headers
+-- =============================================
+CREATE PROCEDURE DefaultRecruiterCommissionCleanup
+    (
+      @DefaultRecruiterCommissionHeaderId INT
+    )
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	-- Remove headers from headers table if there's no 
+	--   corresponding values in the items table
+    DECLARE @headersCount INT
+
+    SELECT  @headersCount = COUNT(*)
+    FROM    dbo.DefaultRecruiterCommissionItem
+    WHERE   DefaultRecruiterCommissionHeaderId = @DefaultRecruiterCommissionHeaderId
+
+    IF @headersCount = 0 
+        BEGIN
+            DELETE  FROM dbo.DefaultRecruiterCommissionHeader
+            WHERE   DefaultRecruiterCommissionHeaderId = @DefaultRecruiterCommissionHeaderId
+        END
+END
+
