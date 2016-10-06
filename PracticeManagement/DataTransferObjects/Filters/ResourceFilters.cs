944c5f4d019cba6b3d1d387079151a8b6d968fb3
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Filters/ResourceFilters.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTransferObjects.Filters
+{
+    [Serializable]
+    public class ResourceFilters
+    {
+        public string PersonStatusIds
+        {
+            get;
+            set;
+        }
+        public string PayTypeIds
+        {
+            get;
+            set;
+        }
+
+        public int SelectedView
+        {
+            get;
+            set;
+        }
+
+        public DateTime? ReportStartDate
+        {
+            get;
+            set;
+        }
+
+        public DateTime? ReportEndDate
+        {
+            get;
+            set;
+        }
+    }
+}
+
