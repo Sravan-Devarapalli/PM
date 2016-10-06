5a659dd51b0c4c68026ef9e54bce30a1a7bb414d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetProjectCapabilities.function.sql
@@ -0,0 +1,16 @@
+﻿CREATE FUNCTION [dbo].[GetProjectCapabilities]
+(
+	@ProjectId INT
+)
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN
+	DECLARE @rep NVARCHAR(MAX)
+	SET @rep = ''
+
+	SELECT @rep= @rep + CONVERT(NVARCHAR(255),PC.CapabilityId) + ','
+	FROM dbo.ProjectCapabilities PC
+    WHERE PC.ProjectId = @ProjectId
+
+	RETURN @rep
+END
