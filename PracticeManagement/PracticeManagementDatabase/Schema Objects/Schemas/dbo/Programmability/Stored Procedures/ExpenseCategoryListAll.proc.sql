97f91c640a7b8b36415258dc0af8f8a4eef3ef1b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ExpenseCategoryListAll.proc.sql
@@ -0,0 +1,15 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 7-22-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	Retrieves the list of the categories for the fixed expenses
+-- =============================================
+CREATE PROCEDURE [dbo].[ExpenseCategoryListAll]
+AS
+	SET NOCOUNT ON
+
+	SELECT ec.ExpenseCategoryId, ec.Name
+	  FROM dbo.ExpenseCategory AS ec
+	ORDER BY ec.Name
+
