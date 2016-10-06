b13b869fe3eccb1eb64e1102ef79da32d89d9319
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectStatusListAll.proc.sql
@@ -0,0 +1,14 @@
+﻿--The source is 
+-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 6-5-2008
+-- Description:	List project statuses
+-- =============================================
+CREATE PROCEDURE [dbo].[ProjectStatusListAll]
+AS
+	SET NOCOUNT ON
+	
+	SELECT s.ProjectStatusId, s.Name
+	  FROM dbo.ProjectStatus AS s
+	ORDER BY s.ProjectStatusId
+
