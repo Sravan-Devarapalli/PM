e4964713363918337921a91ef140e111d15313ca
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetBadgeRecordsAfterDeactivatedDate.proc.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[GetBadgeRecordsAfterDeactivatedDate]
+(
+	@PersonId			INT,
+	@DeactivatedDate	DATETIME
+)
+AS
+BEGIN 
+
+	SELECT MPE.BadgeStartDate,MPE.BadgeEndDate
+	FROM dbo.MilestonePersonEntry MPE
+	INNER JOIN dbo.MilestonePerson MP ON MP.MilestonePersonId = MPE.MilestonePersonId
+	INNER JOIN dbo.Milestone M ON M.MilestoneId = MP.MilestoneId
+	INNER JOIN dbo.Project P ON P.ProjectId = M.ProjectId
+	WHERE MP.PersonId = @PersonId AND MPE.IsBadgeRequired = 1 AND (@DeactivatedDate < MPE.BadgeEndDate) AND MPE.IsApproved = 1 AND P.ProjectStatusId IN (2,3)
+
+END
