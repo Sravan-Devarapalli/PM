de1721032802dd6df180a692ba3b203ef1106776
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CheckIfPersonConsultantTypeInAPeriod.proc.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE [dbo].[CheckIfPersonConsultantTypeInAPeriod]
+	(
+		@PersonId	INT,
+		@StartDate	DATETIME,
+		@EndDate	DATETIME
+	)
+AS
+BEGIN
+
+	DECLARE @NotValidPerson		BIT,
+			@W2SalaryId			INT,
+			@W2HourlyId			INT
+	
+	SELECT	@W2SalaryId = TimescaleId FROM dbo.Timescale WHERE Name = 'W2-Salary'
+	SELECT	@W2HourlyId = TimescaleId FROM dbo.Timescale WHERE Name = 'W2-Hourly'
+	
+	IF EXISTS(
+				SELECT	*
+				FROM	v_PayTimescaleHistory P 
+				WHERE	P.PersonId = @PersonId AND P.StartDate <= @StartDate  AND (@EndDate <=  P.EndDate) AND P.Timescale IN (@W2SalaryId,@W2HourlyId)
+			)
+		SET @NotValidPerson  = 0
+	ELSE
+		SET @NotValidPerson = 1
+
+	SELECT @NotValidPerson
+
+END
