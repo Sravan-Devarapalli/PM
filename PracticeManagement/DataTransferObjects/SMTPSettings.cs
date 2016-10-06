4165c7d6490ea59ec1cdd6c00ee1991c1129f06d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/SMTPSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class SMTPSettings
+    {
+        [DataMember]
+        public string MailServer { get; set; }
+
+        [DataMember]
+        public int PortNumber { get; set; }
+
+        [DataMember]
+        public bool SSLEnabled { get; set; }
+
+        [DataMember]
+        public string UserName { get; set; }
+
+        [DataMember]
+        public string Password { get; set; }
+
+        [DataMember]
+        public string PMSupportEmail { get; set; }
+    }
+}
