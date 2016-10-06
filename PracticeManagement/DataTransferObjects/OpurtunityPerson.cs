74a23bc57a098a7ab9c30abe7365ba84be320c20
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/OpurtunityPerson.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class OpportunityPerson
+    {
+        [DataMember]
+        public Opportunity opportunity
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Person Person
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int RelationType
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int PersonType
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int Quantity
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime? NeedBy
+        {
+            get;
+            set;
+        }
+    }
+}
