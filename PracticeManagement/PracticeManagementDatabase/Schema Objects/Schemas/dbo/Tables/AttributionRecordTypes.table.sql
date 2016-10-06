cc4f97ed62edacad7d4d8b628c2bd5d52ca12e9a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/AttributionRecordTypes.table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[AttributionRecordTypes]
+(
+	[AttributionRecordId] INT  NOT NULL,
+	[Name]				  NVARCHAR(100) NOT NULL,
+	[IsPercentageType]	  BIT NOT NULL,
+	[IsRangeType]		  BIT NOT NULL
+)
+
