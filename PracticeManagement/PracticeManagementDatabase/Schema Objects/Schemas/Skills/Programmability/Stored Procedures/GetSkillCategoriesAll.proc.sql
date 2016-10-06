ff7f918506de1af5cfd2e450277ce29885ee7dff
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Programmability/Stored Procedures/GetSkillCategoriesAll.proc.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE Skills.[GetSkillCategoriesAll]
+AS
+	SELECT S.[Description] SkillCategoryName,
+		   S.[SkillTypeId],
+		   S.SkillCategoryId,
+		   S.DisplayOrder
+	FROM Skills.SkillCategory S
+	WHERE S.IsActive = 1
+		AND S.IsDeleted = 0
+	ORDER BY S.[Description] 
+
