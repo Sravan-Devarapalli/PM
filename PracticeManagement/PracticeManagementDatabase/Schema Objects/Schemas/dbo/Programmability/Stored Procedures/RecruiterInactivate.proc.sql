2f3cb88e800eeec545de2c68851874964910e903
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/RecruiterInactivate.proc.sql
@@ -0,0 +1,17 @@
+﻿-- =============================================
+-- Author:		Skip Sailors
+-- Create date: 5-10-2008
+-- Description:	Inactivare recruiter role
+-- =============================================
+CREATE PROCEDURE [dbo].[RecruiterInactivate] 
+	-- Add the parameters for the stored procedure here
+	@RecruiterId int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	Update Recruiter
+		set Inactive = 1
+		where RecruiterId = @RecruiterId
+END
+
