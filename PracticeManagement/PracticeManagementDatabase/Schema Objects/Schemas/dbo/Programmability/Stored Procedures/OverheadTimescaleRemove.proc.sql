0275060fea682c2ad870a3b594deade7601e94d2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OverheadTimescaleRemove.proc.sql
@@ -0,0 +1,19 @@
+﻿--The source is 
+-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 5-27-2008
+-- Description:	Set the timescale for the overhead as non-applicable.
+-- =============================================
+CREATE PROCEDURE [dbo].[OverheadTimescaleRemove]
+(
+	@OverheadFixedRateId   INT,
+	@TimescaleId           INT
+)
+AS
+	SET NOCOUNT ON
+
+	DELETE
+	  FROM dbo.OverheadFixedRateTimescale
+	 WHERE OverheadFixedRateId = @OverheadFixedRateId
+	   AND TimescaleId = @TimescaleId
+
