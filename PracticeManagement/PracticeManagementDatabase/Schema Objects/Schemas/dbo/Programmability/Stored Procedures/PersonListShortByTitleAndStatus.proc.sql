d8d591662003a0d9b763b401943463e5f5aee075
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonListShortByTitleAndStatus.proc.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [dbo].[PersonListShortByTitleAndStatus]
+	@PersonStatusIdsList NVARCHAR(50) = NULL,
+	@TitleNames		NVARCHAR(MAX) = NULL
+AS
+BEGIN
+	DECLARE @PersonStatusIds TABLE(ID INT)
+	INSERT INTO @PersonStatusIds (ID)
+	SELECT ResultId 
+	FROM dbo.ConvertStringListIntoTable(@PersonStatusIdsList)
+
+	DECLARE @PersonTitleNames TABLE (TitleName NVARCHAR(MAX)) 
+	INSERT INTO @PersonTitleNames (TitleName)
+	SELECT ResultName
+	FROM dbo.ConvertStringListIntoStringTable(@TitleNames)
+
+	SELECT DISTINCT p.PersonId,
+					p.FirstName,
+					p.LastName,
+					p.IsDefaultManager
+	FROM dbo.Person p
+	INNER JOIN dbo.Title T ON t.TitleId = p.TitleId
+	WHERE (p.PersonStatusId IN (SELECT ID FROM @PersonStatusIds) OR @PersonStatusIdsList IS NULL)
+			AND (t.Title IN (SELECT TitleName FROM @PersonTitleNames)OR @TitleNames IS NULL)
+	ORDER BY LastName, FirstName
+END
+
