a915dee85e9f67456080bde108b782522b0626f0
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ChargeCodeTurnOffHistory.table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ChargeCodeTurnOffHistory]
+(
+    ChargeCodeId        INT        NOT NULL,
+    StartDate           DATETIME   NOT NULL,
+    EndDate             DATETIME   NULL,
+	CONSTRAINT PK_ChargeCodeTurnOffHistory_ChargeCodeIdStartDate PRIMARY KEY CLUSTERED (ChargeCodeId ASC, StartDate ASC)
+);
+
