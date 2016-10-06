69d29b916c0c6034b16895338b7ea44c8b5cff92
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/OpportunityTransitionStatus.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// Represents an Opportunity Transition Status.
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class OpportunityTransitionStatus
+    {
+        /// <summary>
+        /// Gets or sets an ID of the OpportunityTransitionStatus.
+        /// </summary>
+        [DataMember]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a name of the OpportunityTransitionStatus.
+        /// </summary>
+        [DataMember]
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public static OpportunityTransitionStatus FromType(OpportunityTransitionStatusType transitionStatusType)
+        {
+            return new OpportunityTransitionStatus
+                       {
+                           Id = (int)transitionStatusType
+                       };
+        }
+
+        public OpportunityTransitionStatusType StatusType
+        {
+            get { return (OpportunityTransitionStatusType)Id; }
+        }
+    }
+}
