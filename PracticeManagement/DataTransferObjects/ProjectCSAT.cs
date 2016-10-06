b5c7ddce4ff3cffb982fb56ca6cff74081a784f9
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ProjectCSAT.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class ProjectCSAT
+    {
+        [DataMember]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int ProjectId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime ReviewStartDate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime ReviewEndDate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime CompletionDate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int ReviewerId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string ReviewerName
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int ReferralScore
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string Comments
+        {
+            get;
+            set;
+        }
+    }
+}
