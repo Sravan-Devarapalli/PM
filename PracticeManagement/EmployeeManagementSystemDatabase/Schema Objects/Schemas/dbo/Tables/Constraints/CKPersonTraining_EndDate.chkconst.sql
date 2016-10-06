65eb2da36f28c57ad0aeb09f496beb077b74d328
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/EmployeeManagementSystemDatabase/Schema Objects/Schemas/dbo/Tables/Constraints/CKPersonTraining_EndDate.chkconst.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[PersonTraining]  WITH CHECK ADD  CONSTRAINT [CKPersonTraining_EndDate] CHECK  (([EndDate]>='1970-1-1' AND [EndDate]<=getdate()))
+GO
+ALTER TABLE [dbo].[PersonTraining] CHECK CONSTRAINT [CKPersonTraining_EndDate]
+GO
