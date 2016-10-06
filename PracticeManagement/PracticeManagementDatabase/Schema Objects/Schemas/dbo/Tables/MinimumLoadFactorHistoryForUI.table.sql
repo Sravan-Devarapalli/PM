5c52274d1bd5a01b8fe5b092cdda482b26302938
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/MinimumLoadFactorHistoryForUI.table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[MinimumLoadFactorHistoryForUI](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[StartDate] [datetime] NOT NULL,
+	[EndDate] [datetime] NULL,
+	[W2-Hourly] [decimal](18, 5) NOT NULL,
+	[W2-Salary] [decimal](18, 5) NOT NULL,
+	[1099] [decimal](18, 5) NOT NULL,
+CONSTRAINT PK_MinimumLoadFactorHistoryForUI_Id PRIMARY KEY CLUSTERED(Id)
+)
+
