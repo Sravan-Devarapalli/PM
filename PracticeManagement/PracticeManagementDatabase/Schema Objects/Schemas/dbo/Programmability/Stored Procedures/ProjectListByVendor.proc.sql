809b38e3950b69d2afc0c63d425c40be976171af
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectListByVendor.proc.sql
@@ -0,0 +1,35 @@
+﻿CREATE PROCEDURE [dbo].[ProjectListByVendor]
+(
+	@VendorId INT
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	DECLARE @VendorName NVARCHAR(100)
+
+	SELECT @VendorName= Name FROM dbo.Vendor WHERE Id=@VendorId
+
+	SELECT	   p.ProjectId,
+			   p.Name,
+			   p.ProjectNumber,
+			   p.ClientId,
+			   p.ClientName,
+			   p.DivisionId,
+			   p.DivisionName,
+			   p.PracticeId,
+			   p.PracticeName,
+			   p.StartDate,
+			   p.EndDate,
+			   p.ProjectStatusId,
+			   p.ProjectStatusName,
+			   p.GroupId,
+			   pg.Name AS GroupName,
+			   p.ProjectIsChargeable
+			     
+		  FROM dbo.v_Project AS p
+		  INNER JOIN dbo.ProjectGroup AS pg ON p.GroupId = pg.GroupId
+		  LEFT JOIN dbo.Channel as C ON C.ChannelId=p.ChannelId 
+		  WHERE  p.IsAllowedToShow = 1 AND C.IsSubChannelVendor=1 AND p.SubChannel=@VendorName
+
+END
