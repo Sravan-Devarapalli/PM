aad81d2451987209829c8cd1c2119240485efd42
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/IsPersonAlreadyHavingStatus.proc.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE  [dbo].[IsPersonAlreadyHavingStatus]
+@PersonStatusId       INT,
+@PersonId             INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	SELECT COUNT(*) as [Count]
+	FROM PersonStatusHistory AS psh
+	WHERE psh.PersonId = @PersonId AND psh.PersonStatusId =@PersonStatusId
+	
+END
+GO
