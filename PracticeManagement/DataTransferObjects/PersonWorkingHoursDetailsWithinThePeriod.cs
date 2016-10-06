a1dda5645f1fca37eee900f54183a605dad04381
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/PersonWorkingHoursDetailsWithinThePeriod.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class PersonWorkingHoursDetailsWithinThePeriod
+    {
+        [DataMember]
+        public int PersonId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime StartDate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime EndDate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int VacationDays
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int TotalWorkDaysIncludingVacationDays
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public decimal TotalWorkHoursExcludingVacationHours
+        {
+            get;
+            set;
+        }
+    }
+}
