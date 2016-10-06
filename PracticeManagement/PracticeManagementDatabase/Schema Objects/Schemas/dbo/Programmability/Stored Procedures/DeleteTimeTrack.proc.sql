949cec1a5bb540fa6862b59a82940bcf9eab417c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DeleteTimeTrack.proc.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE dbo.DeleteTimeTrack
+	@ClientId INT,
+	@ProjectId INT,
+	@TimeTypeId INT,
+	@StartDate DATETIME,
+	@EndDate DATETIME,
+	@personId INT
+AS
+BEGIN
+
+	DELETE TT 
+	FROM dbo.TimeTrack TT INNER JOIN dbo.ChargeCode cc
+		ON TT.ChargeCodeId = cc.Id AND cc.ClientId = @ClientId AND cc.ProjectId =  @ProjectId AND cc.TimeTypeId = @TimeTypeId 
+	WHERE TT.ChargeCodeDate BETWEEN @StartDate AND @EndDate AND TT.PersonId = @personId
+END
