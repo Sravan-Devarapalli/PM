440d0c37737815e38b6e669e0a7d7ad87c24c47e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementScheduler/Settings/TaskSection.cs
@@ -0,0 +1,29 @@
+﻿using System.Configuration;
+
+namespace PracticeManagementScheduler.Settings
+{
+    public class TaskSection : ConfigurationSection
+    {
+        public const string SectionName = "schedule";
+
+        public TaskSection()
+        {
+            SectionInformation.AllowExeDefinition = ConfigurationAllowExeDefinition.MachineToApplication;
+        }
+
+        [ConfigurationProperty("tasks")]
+        public TasksCollection Tasks
+        {
+            get { return this["tasks"] as TasksCollection; }
+            set { this["tasks"] = value; }
+        }
+
+        [ConfigurationProperty("tasks_period", DefaultValue=3600)]
+        public int TasksPeriod
+        {
+            get { return (int)this["tasks_period"]; }
+            set { this["tasks_period"] = value; }
+        }
+    }
+}
+
