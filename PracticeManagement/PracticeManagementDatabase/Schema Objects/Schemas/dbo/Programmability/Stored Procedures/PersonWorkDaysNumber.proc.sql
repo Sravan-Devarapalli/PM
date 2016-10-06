64c1a80f9909f179f386c399e18ad130a3b727a8
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonWorkDaysNumber.proc.sql
@@ -0,0 +1,22 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 7-21-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	Retrieves a number of work days for the specified person and period
+-- =============================================
+CREATE PROCEDURE [dbo].[PersonWorkDaysNumber]
+(
+	@PersonId    INT,
+	@StartDate   DATETIME,
+	@EndDate     DATETIME
+)
+AS
+	SET NOCOUNT ON
+
+	SELECT COUNT(*) AS WorkDays
+	  FROM dbo.v_PersonCalendar AS c
+	 WHERE c.PersonId = @PersonId
+	   AND c.Date BETWEEN @StartDate AND @EndDate
+	   AND c.DayOff = 0
+
