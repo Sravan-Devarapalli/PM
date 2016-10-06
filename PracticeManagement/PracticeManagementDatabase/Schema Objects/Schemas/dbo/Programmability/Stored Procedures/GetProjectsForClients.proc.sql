a1221347b0a24105adb3567daf7b03640514c85d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetProjectsForClients.proc.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[GetProjectsForClients]
+(
+	@ClientIds NVARCHAR(MAX) = NULL	
+)
+AS 
+BEGIN
+
+		DECLARE @ClientIdsTable TABLE ( Id INT )
+		INSERT  INTO @ClientIdsTable
+				SELECT  ResultId
+				FROM    [dbo].[ConvertStringListIntoTable](@ClientIds)
+
+		SELECT	vp.ProjectId,
+				vp.ProjectNumber+'-'+vp.Name as 'Name'
+		FROM v_Project vp
+		WHERE (@ClientIds IS NULL OR vp.ClientId IN (select Id FROM @ClientIdsTable))
+
+END
