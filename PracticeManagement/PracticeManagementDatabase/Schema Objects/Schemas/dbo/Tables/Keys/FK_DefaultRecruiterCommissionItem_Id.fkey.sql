8538545ba99d38b9ae8b9188bbff91763f1b03ae
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_DefaultRecruiterCommissionItem_Id.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[DefaultRecruiterCommissionItem]
+    ADD CONSTRAINT [FK_DefaultRecruiterCommissionItem_Id] FOREIGN KEY ([DefaultRecruiterCommissionHeaderId]) REFERENCES [dbo].[DefaultRecruiterCommissionHeader] ([DefaultRecruiterCommissionHeaderId]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
