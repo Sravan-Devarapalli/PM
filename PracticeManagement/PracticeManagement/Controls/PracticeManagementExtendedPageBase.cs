c396395c2e446ddf7dceaabf9b66ecf935827d1d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/PracticeManagementExtendedPageBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PraticeManagement.Controls
+{
+    public abstract class PracticeManagementSearchPageBase : PracticeManagementPageBase
+    {
+        protected override void Display()
+        {
+            
+        }
+
+        public abstract string SearchText
+        {
+            get;
+        }
+    }
+}
