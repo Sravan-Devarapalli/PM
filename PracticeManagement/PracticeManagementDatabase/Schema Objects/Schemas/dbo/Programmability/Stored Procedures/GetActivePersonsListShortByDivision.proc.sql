76aa63ff3590bba9e2aa7964314f0b3fd3af81d6
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetActivePersonsListShortByDivision.proc.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[GetActivePersonsListShortByDivision]
+(
+	@DivisionId INT
+)
+AS
+BEGIN
+  
+  SELECT	P.PersonId,
+			P.FirstName,
+			P.LastName
+  FROM		dbo.Person P
+  WHERE		P.DivisionId = @DivisionId AND P.PersonStatusId IN (1,5) --Active and Terminated Status
+
+END
