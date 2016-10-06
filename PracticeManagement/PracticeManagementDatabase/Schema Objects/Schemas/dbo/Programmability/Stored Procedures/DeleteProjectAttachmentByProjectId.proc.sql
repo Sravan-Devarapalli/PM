abe3024451b4ba4795af5e1e580124c6d046fb45
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DeleteProjectAttachmentByProjectId.proc.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[DeleteProjectAttachmentByProjectId]
+(
+	@AttachmentId	INT,
+	@ProjectId		INT,
+	@UserLogin      NVARCHAR(255)
+)
+AS
+BEGIN
+
+    IF EXISTS(SELECT 1 FROM ProjectAttachment WHERE ProjectId = @ProjectId)
+    BEGIN
+    EXEC SessionLogPrepare @UserLogin = @UserLogin
+    
+		DELETE
+		FROM [dbo].ProjectAttachment
+		WHERE ProjectId = @ProjectId
+			AND (Id = @AttachmentId OR @AttachmentId IS NULL)
+		
+	EXEC dbo.SessionLogUnprepare
+	END
+
+END
