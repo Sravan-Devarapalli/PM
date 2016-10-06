b6fb7f22bd246ca839dfc9fc6d97ab04036511b8
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SaveProjectAttachment.proc.sql
@@ -0,0 +1,38 @@
+﻿CREATE PROCEDURE [dbo].[SaveProjectAttachment]
+(
+	@ProjectId			  INT,
+	@CategoryId			  INT, 
+    @FileName			  NVARCHAR(256),	
+	@AttachmentData	      VARBINARY(MAX),
+	@UploadedDate         DATETIME ,
+	@UserLogin            NVARCHAR(255)
+)
+AS
+	SET NOCOUNT ON
+	BEGIN
+	
+	-- Start logging session
+	EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
+
+	DECLARE @PersonId INT
+	SELECT @PersonId = PersonId FROM dbo.Person P WHERE P.alias = @UserLogin
+
+	  INSERT INTO ProjectAttachment
+			   ([ProjectId]
+			   ,[CategoryId]
+			   ,[FileName]
+			   ,[AttachmentData]
+			   ,UploadedDate
+			   ,ModifiedBy
+			   )     
+		 VALUES
+			   (@ProjectId
+			   ,@CategoryId
+			   ,@FileName
+			   ,@AttachmentData
+			   ,@UploadedDate
+			   ,@PersonId
+			   )
+      
+      EXEC dbo.SessionLogUnprepare
+	END
