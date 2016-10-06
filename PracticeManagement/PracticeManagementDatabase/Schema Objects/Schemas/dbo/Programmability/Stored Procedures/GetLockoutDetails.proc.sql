16b33315d59ee11c7e507f6c913405bb54a47286
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetLockoutDetails.proc.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[GetLockoutDetails]
+(
+	@LockoutPageId		INT=NULL
+)
+AS
+BEGIN
+
+	SELECT	L.LockoutId,
+			L.LockoutPageId,
+			L.FunctionalityName,
+			L.Lockout,
+			L.LockoutDate
+	FROM dbo.Lockout L
+	JOIN dbo.LockoutPages LP ON LP.LockoutPageId = L.LockoutPageId
+	WHERE @LockoutPageId IS NULL OR L.LockoutPageId = @LockoutPageId
+	ORDER BY L.LockoutPageId
+
+END
+
