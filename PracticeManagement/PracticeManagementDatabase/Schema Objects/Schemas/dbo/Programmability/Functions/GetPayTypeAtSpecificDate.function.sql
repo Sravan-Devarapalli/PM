0305c8aee07cee27a779bff8b5ef5a475ba30676
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetPayTypeAtSpecificDate.function.sql
@@ -0,0 +1,36 @@
+﻿CREATE FUNCTION [dbo].[GetPayTypeAtSpecificDate]
+(
+	@PersonId		INT, 
+	@Date			DATETIME
+)
+RETURNS DATETIME 
+AS
+BEGIN
+	DECLARE @result DATETIME -- VARCHAR(50)
+	SET @result = NULL  --'n/a'
+	
+	;WITH CurrentPay AS (
+	SELECT p.StartDate
+	  FROM dbo.v_Pay AS p
+	 WHERE p.PersonId = @PersonId
+	   AND @Date >= p.StartDate
+	   AND @Date < p.EndDateOrig
+	UNION ALL
+	SELECT TOP 1
+	       p.StartDate
+	  FROM dbo.v_Pay AS p
+	 WHERE p.PersonId = @PersonId
+	   AND NOT EXISTS(SELECT 1
+	                    FROM dbo.v_Pay AS p
+	                   WHERE p.PersonId = @PersonId
+	                     AND @Date >= p.StartDate
+	                     AND @Date < p.EndDateOrig)
+	   AND @Date < p.StartDate
+	)
+	SELECT @result = c.StartDate
+	FROM CurrentPay AS c
+--	INNER JOIN dbo.Timescale AS t ON c.Timescale = t.TimescaleId
+	
+	RETURN @result
+END
+
