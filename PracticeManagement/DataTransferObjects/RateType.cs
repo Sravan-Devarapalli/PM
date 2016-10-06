b13d3776539d425ce319ad015de4cc266ecbb102
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/RateType.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// determines how to calculate an imputed hourly rate
+    /// </summary>
+    [DataContract]
+    public enum RateType
+    {
+		[EnumMember]
+		Hourly = 1,
+		
+		[EnumMember]
+		Salary = 2
+    }
+}
