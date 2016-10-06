37278fe6b8bad07be263c56ebfa1b53bb7bc52e1
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementService/DefaultCommissionService.svc.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel.Activation;
+using DataAccess;
+using DataTransferObjects;
+
+namespace PracticeManagementService
+{
+	[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+	public class DefaultCommissionService : IDefaultCommissionService
+	{
+		#region IDefaultCommissionService Members
+
+		/// <summary>
+		/// Retrieves the actual default sales commission for the specified person.
+		/// </summary>
+		/// <param name="personId">An ID of the person to retrieve the data for.</param>
+		/// <returns>The list of the <see cref="DefaultCommission"/> objects.</returns>
+		public DefaultCommission DefaultSalesCommissionByPerson(int personId)
+		{
+			return GetDefaultCommission(personId, CommissionType.Sales);
+		}
+
+		/// <summary>
+		/// Retrieves the actual default management commission for the specified person.
+		/// </summary>
+		/// <param name="personId">An ID of the person to retrieve the data for.</param>
+		/// <returns>The list of the <see cref="DefaultCommission"/> objects.</returns>
+		public DefaultCommission DefaultManagementCommissionByPerson(int personId)
+		{
+			return GetDefaultCommission(personId, CommissionType.PracticeManagement);
+		}
+
+		private static DefaultCommission GetDefaultCommission(int personId, CommissionType commissionType)
+		{
+			List<DefaultCommission> commissions = DefaultCommissionDAL.DefaultCommissionListByPerson(personId);
+			DefaultCommission result = null;
+
+			foreach (DefaultCommission commission in commissions)
+			{
+				if (commission.TypeOfCommission == commissionType)
+				{
+					result = commission;
+					break;
+				}
+			}
+
+			return result;
+		}
+
+		#endregion
+	}
+}
+
