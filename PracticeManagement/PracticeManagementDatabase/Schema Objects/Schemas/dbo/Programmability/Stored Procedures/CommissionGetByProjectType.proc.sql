c0029e5c70427e59612ce20022430becdf2b409d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CommissionGetByProjectType.proc.sql
@@ -0,0 +1,26 @@
+﻿--The source is 
+-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 6-2-2008
+-- Description:	Gets a person's commision for the specific project
+-- =============================================
+CREATE PROCEDURE [dbo].[CommissionGetByProjectType]
+(
+	@ProjectId        INT,
+	@CommissionType   INT
+)
+AS
+	SET NOCOUNT ON
+
+	SELECT c.ProjectId,
+	       c.PersonId,
+	       c.CommissionId,
+	       c.FractionOfMargin,
+	       c.CommissionType,
+	       c.ExpectedDatePaid,
+	       c.ActualDatePaid,
+	       c.MarginTypeId
+	  FROM dbo.Commission AS c
+	 WHERE c.ProjectId = @ProjectId
+	   AND c.CommissionType = @CommissionType
+
