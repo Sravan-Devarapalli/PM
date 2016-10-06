d8105e838d7b64bda8d9de54ed19243ffc443bbd
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Location.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class Location
+    {
+        [DataMember]
+        public int LocationId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string LocationCode
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string LocationName
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Location Parent
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string Country
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string TimeZone
+        {
+            get;
+            set;
+        }
+
+        public string FormattedLocation
+        {
+            get
+            {
+                return LocationCode + ": " + LocationName;
+            }
+        }
+    }
+}
+
