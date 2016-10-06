e3a00b0833cfeb2544b1716f4bb8fc04527f298f
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetCompanyHolidays.proc.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].GetCompanyHolidays
+(
+@Year INT
+)
+AS
+	SET NOCOUNT ON
+SELECT COUNT(*) AS CompanyHolidays
+FROM dbo.Calendar 
+WHERE DayOff = 1 
+		AND DATEPART(DW,[Date]) NOT IN (1,7) 
+		AND YEAR([Date]) = @Year 
