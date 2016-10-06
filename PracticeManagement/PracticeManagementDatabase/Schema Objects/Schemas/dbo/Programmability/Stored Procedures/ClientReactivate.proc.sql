c55dc5b515020b86d00909d33ea223e266a2a3f7
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ClientReactivate.proc.sql
@@ -0,0 +1,16 @@
+﻿-- =============================================
+-- Author:		Skip Sailors
+-- Create date: 4-22-2008
+-- Description:	Mark a client active
+-- =============================================
+CREATE PROCEDURE [dbo].[ClientReactivate] 
+	@ClientID int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	Update Client
+		Set Inactive = 0
+		Where ClientId = @ClientID
+END
+
