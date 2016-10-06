a150796991a2ed406d22933c7158813378885954
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Skills/PersonDocument.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Skills
+{
+    [DataContract]
+    [Serializable]
+    public class PersonDocument
+    {
+        [DataMember]
+        public DocumentType Type
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Person Person
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string Url
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime? ModificationDate
+        {
+            get;
+            set;
+        }
+    }
+}
