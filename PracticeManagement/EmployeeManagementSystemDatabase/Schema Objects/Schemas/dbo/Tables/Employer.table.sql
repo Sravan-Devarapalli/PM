5e244f9318719e2861615307abcbf8ffc38adfde
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/EmployeeManagementSystemDatabase/Schema Objects/Schemas/dbo/Tables/Employer.table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Employer](
+	[EmployerId] [int] NOT NULL IDENTITY(1,1),
+	[Description] [nvarchar](max) NULL,
+	[DisplayOrder] [tinyint] NULL,
+ CONSTRAINT [PK_Employer] PRIMARY KEY CLUSTERED 
+(
+	[EmployerId] ASC
+)
+)
