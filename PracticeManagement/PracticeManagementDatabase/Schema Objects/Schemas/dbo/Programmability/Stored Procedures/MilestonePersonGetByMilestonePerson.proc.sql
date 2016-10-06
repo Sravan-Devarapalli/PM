c1472f6fc487bc3b43fefd1534538f907ceba04c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MilestonePersonGetByMilestonePerson.proc.sql
@@ -0,0 +1,46 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 6-3-2008
+-- Updated by:	Anatoliy Lokshin
+-- Update date: 9-09-2008
+-- Description:	Selects persons-milestones details for the specified milestone and person.
+-- =============================================
+CREATE PROCEDURE [dbo].[MilestonePersonGetByMilestonePerson]
+(
+	@MilestoneId   INT,
+	@PersonId      INT
+)
+AS
+	SET NOCOUNT ON
+	
+	SELECT mp.MilestonePersonId,
+	       mp.MilestoneId,
+	       mp.PersonId,
+	       mp.StartDate,
+	       mp.EndDate,
+	       mp.HoursPerDay,
+	       mp.FirstName,
+	       mp.LastName,
+	       mp.ProjectId,
+	       mp.ProjectName,
+	       mp.ProjectStartDate,
+	       mp.ProjectEndDate,
+	       mp.Discount,
+	       mp.ClientId,
+	       mp.ClientName,
+	       mp.MilestoneName,
+	       mp.MilestoneStartDate,
+	       mp.MilestoneProjectedDeliveryDate,
+	       mp.ExpectedHours,
+	       mp.PersonRoleId,
+	       mp.RoleName,
+	       mp.Amount,
+	       mp.IsHourlyAmount,
+	       mp.SalesCommission,
+	       mp.MilestoneExpectedHours,
+	       mp.MilestoneActualDeliveryDate,
+	       mp.MilestoneHourlyRevenue
+	  FROM dbo.v_MilestonePerson AS mp
+	 WHERE mp.MilestoneId = @MilestoneId
+	   AND mp.PersonId = @PersonId
+
