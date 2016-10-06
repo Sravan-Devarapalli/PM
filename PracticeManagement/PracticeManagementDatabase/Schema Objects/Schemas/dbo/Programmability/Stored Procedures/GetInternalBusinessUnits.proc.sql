0dc88037221aacaa6c6fcd5278b58d9f4a209ca7
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetInternalBusinessUnits.proc.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[GetInternalBusinessUnits]
+AS
+BEGIN
+
+	SELECT [GroupId] ,[Code],[Name]
+	FROM dbo.ProjectGroup 
+	WHERE IsInternal = 1 AND Active = 1
+	ORDER BY Name ASC
+END
