49398883529212f19a7fb90ed338b9c0b223de0a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/EmployeeManagementSystemDatabase/Schema Objects/Schemas/dbo/Tables/PersonDocument.table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[PersonDocument](
+	[DocumentTypeId] [int] NOT NULL,
+	[PersonId] [bigint] NOT NULL,
+	[Url] [nvarchar](max) NOT NULL,
+	[ModificationDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_PersonDocument] PRIMARY KEY CLUSTERED 
+(
+	[DocumentTypeId] ASC,
+	[PersonId] ASC
+)
+)
