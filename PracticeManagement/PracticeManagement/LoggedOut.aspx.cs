9ad346b48335b01b8dd99a03c4ad5e74085b3e3b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/LoggedOut.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PraticeManagement
+{
+    public partial class LoggedOut : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ((PracticeManagementMain)Master)._PageTitle = "Logged Out";
+            var session = Request.Cookies["ASP.NET_SessionId"];
+            if (session != null)
+            {
+                ServiceCallers.Custom.Person(p => p.DeleteReportFilterValues(PracticeManagementMain.CurrentUserID, PracticeManagementMain.PreviousUserId, session.Value));
+            }
+        }
+    }
+}
+
