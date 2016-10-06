7ea1114e7ae21b2b80ca2fb6d8b4fee4b9e207c8
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/Reports/BillableAndNonBillableGraph.ascx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PraticeManagement.Controls.Reports
+{
+    public partial class BillableAndNonBillableGraph : System.Web.UI.UserControl
+    {
+        public string BillablValue
+        {
+            set 
+            {
+                hdnBillable.Value = value;
+            }
+        }
+
+        public string NonBillablValue
+        {
+            set
+            {
+                hdnNonBillable.Value = value;
+            }
+        }
+    }
+}
