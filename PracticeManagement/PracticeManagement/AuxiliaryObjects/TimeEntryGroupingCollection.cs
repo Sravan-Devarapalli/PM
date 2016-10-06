667440f19f165a46c693ad9557548f61da996180
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/AuxiliaryObjects/TimeEntryGroupingCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PraticeManagement.AuxiliaryObjects
+{
+    public class TimeEntryGroupingCollection : List<TimeEntryGroupingItem>
+    {
+        // protected MilestoneP
+    }
+}
+
