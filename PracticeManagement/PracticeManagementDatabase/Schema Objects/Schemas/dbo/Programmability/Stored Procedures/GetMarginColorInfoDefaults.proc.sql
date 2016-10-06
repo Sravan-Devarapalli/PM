28292101a7a9f0efa27d4456e658df5d38707d99
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetMarginColorInfoDefaults.proc.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[GetMarginColorInfoDefaults]
+(
+	@GoalTypeId INT
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	SELECT cmci.[ColorId]
+		  ,cmci.[StartRange]
+		  ,cmci.[EndRange]
+		  ,color.Value
+		  ,color.Description
+	  FROM [dbo].DefaultMarginColorInfo AS cmci 
+	  INNER JOIN dbo.Color color ON cmci.[ColorId] = color.Id
+	  WHERE cmci.GoalTypeId =@GoalTypeId
+	  ORDER BY cmci.StartRange
+END
+GO
