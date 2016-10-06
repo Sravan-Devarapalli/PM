d63ad6da64ef4a44a44ac5d67c2e1d0f223b70fa
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OverheadTimescaleSet.proc.sql
@@ -0,0 +1,24 @@
+﻿--The source is 
+-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 5-27-2008
+-- Description:	Set the timescale for the overhead as applicable.
+-- =============================================
+CREATE PROCEDURE [dbo].[OverheadTimescaleSet]
+(
+	@OverheadFixedRateId   INT,
+	@TimescaleId           INT
+)
+AS
+	SET NOCOUNT ON
+
+	IF NOT EXISTS (SELECT 1
+	                 FROM dbo.OverheadFixedRateTimescale AS rt
+	                WHERE rt.OverheadFixedRateId = @OverheadFixedRateId
+	                  AND rt.TimescaleId = @TimescaleId)
+	BEGIN
+		INSERT INTO dbo.OverheadFixedRateTimescale
+		            (OverheadFixedRateId, TimescaleId)
+		     VALUES (@OverheadFixedRateId, @TimescaleId)
+	END
+
