ef8c0fa841ea2eedabef310ee96a70576b194b05
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Filters/BenchCostReportFilters.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTransferObjects.Filters
+{
+    [Serializable]
+    public class BenchCostReportFilters
+    {
+        public DateTime? ReportStartDate
+        {
+            get;
+            set;
+        }
+
+        public DateTime? ReportEndDate
+        {
+            get;
+            set;
+        }
+
+        public string ReportPeriod
+        {
+            get;
+            set;
+        }
+
+        public string PracticeIds
+        {
+            get;
+            set;
+        }
+
+        public bool IsActive
+        {
+            get;
+            set;
+        }
+
+        public bool IsProjected
+        {
+            get;
+            set;
+        }
+
+        public bool IsCompleted
+        {
+            get;
+            set;
+        }
+
+        public bool IsExperimental
+        {
+            get;
+            set;
+        }
+
+        public bool IsProposed
+        {
+            get;
+            set;
+        }
+
+        public bool IncludeOverheads
+        {
+            get;
+            set;
+        }
+
+        public bool IncludeZeroCost
+        {
+            get;
+            set;
+        }
+
+        public bool SeperateInternalExternalTables
+        {
+            get;
+            set;
+        }
+    }
+}
+
