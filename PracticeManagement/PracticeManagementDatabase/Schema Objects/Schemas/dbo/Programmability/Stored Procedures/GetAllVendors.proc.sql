446b008490be578a3527576dee7e2ac2ffc3d547
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetAllVendors.proc.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[GetAllActiveVendors]
+AS
+BEGIN
+
+	SELECT v.Id,
+		   v.Name,
+		   v.ContactName,
+		   v.Email,
+		   v.TelephoneNumber,
+		   v.Status,
+		   vt.Id as VendorTypeId,
+		   vt.Name as VendorTypeName
+	FROM dbo.Vendor v
+	JOIN dbo.VendorType vt on v.VendorTypeId=vt.Id
+	WHERE v.Status=1
+END
