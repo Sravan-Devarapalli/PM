9de2e15a0bcd3c3d8f5c93d23c4025b978e1609d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/PersonTimeEntryRecursiveSelection.table.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[PersonTimeEntryRecursiveSelection]
+(
+	Id					INT IDENTITY (1, 1) NOT NULL,
+	PersonId			INT NOT NULL,
+	ClientId			INT NOT NULL,
+	ProjectId			INT NOT NULL,
+	ProjectGroupId		INT NOT NULL,
+	TimeEntrySectionId	INT NOT NULL,
+	StartDate			DATETIME NOT NULL,
+	EndDate				DATETIME NULL,
+	IsRecursive			BIT NOT NULL CONSTRAINT DF_PersonTimeEntryRecursiveSelection_IsRecursive DEFAULT 0,
+	CONSTRAINT PK_PersonTimeEntryRecursiveSelection_Id PRIMARY KEY CLUSTERED (Id ASC)
+);
+
