fec57bea91e30cd9b330b50c4bd2e47c223f3e28
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/UN_MilestonePerson.ukey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[MilestonePerson]
+    ADD CONSTRAINT [UN_MilestonePerson] UNIQUE NONCLUSTERED ([MilestoneId] ASC, [PersonId] ASC) WITH (IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF) ON [PRIMARY];
+
+
