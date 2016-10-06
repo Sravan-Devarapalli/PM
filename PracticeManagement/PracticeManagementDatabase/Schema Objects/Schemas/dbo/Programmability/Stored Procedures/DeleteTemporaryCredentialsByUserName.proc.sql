0e52b328562b7694ab3d2d27c1523d2421b66ddb
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DeleteTemporaryCredentialsByUserName.proc.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[DeleteTemporaryCredentialsByUserName]
+(
+	@UserName		NVARCHAR (256) 
+)
+AS
+BEGIN
+
+	DELETE
+	FROM [dbo].[UserTemporaryCredentials]
+	WHERE UserName = @UserName
+
+END
