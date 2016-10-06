5cdc5d0b5c11d00479c5ad59bf9fe95c2f0c3893
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Note.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// Represents a note for an entity.
+    /// </summary>
+    [Serializable]
+    [DataContract]
+    public class Note
+    {
+        /// <summary>
+        /// Gets or sets an ID of the note.
+        /// </summary>
+        [DataMember]
+        public int? Id
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets an ID of the entity the note was written for.
+        /// </summary>
+        [DataMember]
+        public int TargetId
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets of an target of the note.
+        /// </summary>
+        [DataMember]
+        public NoteTarget Target
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets of an author of the note.
+        /// </summary>
+        [DataMember]
+        public Person Author
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets of a date when the note was created.
+        /// </summary>
+        [DataMember]
+        public DateTime CreateDate
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a text of the note.
+        /// </summary>
+        [DataMember]
+        public string NoteText
+        {
+            get;
+            set;
+        }
+    }
+}
