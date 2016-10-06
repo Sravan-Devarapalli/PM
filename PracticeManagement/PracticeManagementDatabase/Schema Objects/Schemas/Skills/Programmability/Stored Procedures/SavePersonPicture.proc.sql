7116ef992fc5b26e7d736ceb2a141e9b96104226
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Programmability/Stored Procedures/SavePersonPicture.proc.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [Skills].[SavePersonPicture]
+(
+	@PersonId         INT,		
+	@PictureFileName  NVARCHAR(MAX),
+	@PictureData	  VARBINARY(MAX) = NULL,
+	@UserLogin		  NVARCHAR (100)      
+)
+AS
+BEGIN
+	-- Start logging session
+	EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
+	DECLARE @Now DATETIME
+	SET @Now = dbo.InsertingTime()	
+
+	UPDATE dbo.Person
+	SET PictureData = @PictureData,
+	    PictureFileName = @PictureFileName,
+		PictureModifiedDate = @Now
+	WHERE PersonId = @PersonId 
+		AND ISNULL(PictureData,  CONVERT(VARBINARY(2), '')) <> ISNULL(@PictureData,  CONVERT(VARBINARY(2), ''))
+
+	EXEC dbo.SessionLogUnprepare
+END
+
