e726dabb3064984ee49c3a28a623ffadfd6eb25b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/PK_ProjectBillingInfo.pkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[ProjectBillingInfo]
+    ADD CONSTRAINT [PK_ProjectBillingInfo] PRIMARY KEY CLUSTERED ([ProjectId] ASC) WITH (IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF);
+
+
