bd5a8663916081c6918f0d4fbd0d5b55b426fa2d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/RecruitingMetrics.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class RecruitingMetrics
+    {
+        [DataMember]
+        public int? RecruitingMetricsId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public RecruitingMetricsType RecruitingMetricsType
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int SortOrder
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public bool InUse
+        {
+            get;
+            set;
+        }
+
+        public string HtmlEncodedName
+        {
+            get
+            {
+                return HttpUtility.HtmlEncode(Name);
+            }
+        }
+    }
+}
+
