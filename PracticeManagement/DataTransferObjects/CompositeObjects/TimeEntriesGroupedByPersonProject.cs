515ea9e7c0628d6ff67730001e7565ad3b64bd4c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/CompositeObjects/TimeEntriesGroupedByPersonProject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using DataTransferObjects.TimeEntry;
+
+namespace DataTransferObjects.CompositeObjects
+{
+    [DataContract(Name = "TimeEntriesGroupedBy{0}{1}")]
+    [Serializable]
+    public class TimeEntriesGroupedByPersonProject
+    {
+        [DataMember]
+        public string PersonName { get; set; }
+
+        [DataMember]
+        public int PersonId { get; set; }
+
+        [DataMember]
+        public Dictionary<Project, List<TimeEntryRecord>> GroupedTimeEtnries
+        { get; set; }
+    }
+}
