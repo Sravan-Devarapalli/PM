7990e1c55b844adde05df963d5ecc8a3b60fe9dc
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetProjectLastChangeDateFortheGivenStatus.proc.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].GetProjectLastChangeDateFortheGivenStatus
+(
+ @ProjectId      INT,
+ @ProjectStatusId INT
+)
+AS
+BEGIN
+   SELECT MAX(StartDate) AS StartDate
+   FROM dbo.ProjectStatusHistory 
+   WHERE ProjectId=@ProjectId AND ProjectStatusId=@ProjectStatusId 
+END
