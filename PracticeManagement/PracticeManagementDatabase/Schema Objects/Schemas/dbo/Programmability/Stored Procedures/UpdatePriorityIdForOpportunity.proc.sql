550658ac0f89251b0a7bb920e8756cf2d51b7664
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/UpdatePriorityIdForOpportunity.proc.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[UpdatePriorityIdForOpportunity]
+(
+	@PriorityId			INT,
+	@OpportunityId      INT,
+	@UserLogin          NVARCHAR(255)
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	-- Start logging session
+		EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
+
+	UPDATE Opportunity
+	SET PriorityId = @PriorityId
+	WHERE OpportunityId = @OpportunityId
+
+		-- End logging session
+		EXEC dbo.SessionLogUnprepare
+END
