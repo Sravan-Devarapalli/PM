d5a68733349fd120af2ce1e1d24491fa81619ebc
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/dbo.CompanyLogoDataSave.proc.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE dbo.CompanyLogoDataSave
+(
+	@Title                NVARCHAR(256),
+	@FileName            NVARCHAR(256),
+	@FilePath            NVARCHAR(1024),
+	@Data                VARBINARY(MAX)
+)
+AS
+	SET NOCOUNT ON
+	BEGIN
+		DECLARE @COUNT INT 
+		SELECT @COUNT=COUNT(*) 
+		FROM dbo.CompanyLogoSetting
+		IF(@COUNT = 0)
+		BEGIN
+			INSERT INTO CompanyLogoSetting(Title,FileName,FilePath,Data)
+       		VALUES (@Title,@FileName,@FilePath,@Data)
+		END
+		ELSE
+		BEGIN
+			UPDATE  CompanyLogoSetting
+			SET  Title = ISNULL(@Title,Title),
+				 FileName = ISNULL(@FileName,FileName),
+				 FilePath = ISNULL(@FilePath,FilePath),
+				 Data = ISNULL(@Data,Data)
+		END
+	END
+
