a67890c3f0bf88ac08a70333ad4c51d2e30d6587
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/PK_RecruiterCommission_1.pkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[RecruiterCommission]
+    ADD CONSTRAINT [PK_RecruiterCommission_1] PRIMARY KEY CLUSTERED ([RecruitId] ASC,[RecruiterId] ASC, [HoursToCollect] ASC) WITH (IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF);
+
+
