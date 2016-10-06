9c6cfc0509c248da65ff1a7e8c3db87345622aa2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/EmailTemplate.table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[EmailTemplate] (
+    [EmailTemplateId]      INT            IDENTITY (1, 1) NOT NULL,
+    [EmailTemplateName]    NVARCHAR (50)  NOT NULL,
+    [EmailTemplateTo]    NVARCHAR (250),
+    [EmailTemplateCc]    NVARCHAR (250),
+    [EmailTemplateSubject] NVARCHAR (255) NOT NULL,
+    [EmailTemplateBody]    NVARCHAR (MAX) NOT NULL,
+	Name				   NVARCHAR (50)  NULL
+);
+
+
