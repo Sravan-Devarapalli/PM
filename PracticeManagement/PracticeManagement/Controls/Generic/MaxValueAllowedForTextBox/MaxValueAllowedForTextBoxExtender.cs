8a6cfc5bfd4c18918b63a1beda206d02578f7b39
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/Generic/MaxValueAllowedForTextBox/MaxValueAllowedForTextBoxExtender.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.UI;
+using AjaxControlToolkit;
+[assembly: WebResource("PraticeManagement.Controls.Generic.MaxValueAllowedForTextBox.MaxValueAllowedForTextBoxExtender.js", "text/javascript")]
+namespace PraticeManagement.Controls.Generic.MaxValueAllowedForTextBox
+{
+    using System.ComponentModel;
+    using System;
+    [ClientScriptResource("PraticeManagement.Controls.Generic.MaxValueAllowedForTextBox.MaxValueAllowedForTextBoxExtender", "PraticeManagement.Controls.Generic.MaxValueAllowedForTextBox.MaxValueAllowedForTextBoxExtender.js")]
+    [TargetControlType(typeof(Control))]
+    public class MaxValueAllowedForTextBoxExtender : ExtenderControlBase
+    {
+        [ExtenderControlProperty]
+        [DefaultValue("")]
+        [ClientPropertyName("controlsToCheck")]
+        public string ControlsToCheck
+        {
+            get { return GetPropertyValue("controlsToCheck", string.Empty); }
+            set { SetPropertyValue("controlsToCheck", value); }
+        }
+
+        [ExtenderControlProperty]
+        [DefaultValue(Int32.MaxValue)]
+        [ClientPropertyName("maximumValue")]
+        public int MaximumValue
+        {
+            get { return GetPropertyValue("maximumValue", Int32.MaxValue); }
+            set { SetPropertyValue("maximumValue", value); }
+        }
+    }
+}
+
