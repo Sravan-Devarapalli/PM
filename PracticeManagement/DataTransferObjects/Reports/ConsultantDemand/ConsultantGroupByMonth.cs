517c884b41f630c72189bc355afc74bc5c472628
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Reports/ConsultantDemand/ConsultantGroupByMonth.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Reports.ConsultingDemand
+{
+    public class ConsultantGroupByMonth
+    {
+        [DataMember]
+        public DateTime MonthStartDate { get; set; }
+
+        [DataMember]
+        public List<ConsultantDemandDetailsByMonth> ConsultantDetailsByMonth { get; set; }
+
+        public int TotalCount
+        {
+            get
+            {
+                return ConsultantDetailsByMonth != null ? ConsultantDetailsByMonth.Sum(p => p.Count) : 0;
+            }
+        }
+    }
+}
