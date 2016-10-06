4008477a67eb9b343be0e3659717806eca35b012
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/CompanyLogo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    public class CompanyLogo
+    {
+        [DataMember]
+        public string Title { get; set; }
+
+        [DataMember]
+        public string FileName { get; set; }
+
+        [DataMember]
+        public string FilePath { get; set; }
+
+        [DataMember]
+        public Byte[] Data { get; set; }
+    }
+}
