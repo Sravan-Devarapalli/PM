036f458c261e9ee88fa7bafb4b425e7d14e24f31
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/UserPasswordsHistory.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    public class UserPasswordsHistory
+    {
+        [DataMember]
+        public string HashedPassword { get; set; }
+
+        [DataMember]
+        public string PasswordSalt { get; set; }
+    }
+}
