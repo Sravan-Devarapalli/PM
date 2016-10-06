2a51d646b6e08be685d52458dc350b9b6bebd288
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/UpdateIsChargableForClient.proc.sql
@@ -0,0 +1,23 @@
+﻿-- =============================================
+-- Author:		ThulasiRam.P
+-- Updated date: 06-08-2012
+-- Description:	Update Status for a client.
+-- =============================================
+CREATE PROCEDURE dbo.UpdateIsChargableForClient
+(
+	@ClientID           INT,
+	@IsChargeable       BIT,
+	@UserLogin          NVARCHAR(255)
+)
+
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	-- Start logging session
+	EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
+
+	Update dbo.Client
+	SET IsChargeable = @IsChargeable
+	WHERE ClientId = @ClientID
+END
