ac8a8ca5f1a1efbc09755fe4eef8ed6b9abac790
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetPTOTimeTypeId.function.sql
@@ -0,0 +1,12 @@
+﻿CREATE FUNCTION [dbo].[GetPTOTimeTypeId]()
+RETURNS INT
+AS
+BEGIN
+	DECLARE @Id	INT
+
+	SELECT @Id = TimeTypeId
+	FROM TimeType
+	WHERE Code = 'W9310'--Here 'W9310' is the code of PTO work type.
+
+	RETURN @Id
+END
