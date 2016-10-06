08ecf1077cc3dda103ee324fcc0062e3c796fa2d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Vendor.table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Vendor]
+(
+	 [Id]			    INT     IDENTITY (1, 1) NOT NULL,
+	 [Name]				NVARCHAR (50)			NOT NULL,
+	 [ContactName]		NVARCHAR (50)			NOT NULL,
+	 [Status]			INT						NOT NULL,
+	 [Email]			NVARCHAR (50)			NOT NULL,
+	 [TelephoneNumber]	NVARCHAR (20)				NOT NULL,
+	 [VendorTypeId]		INT						NOT NULL
+	 CONSTRAINT [PK_Vendor_Id]	PRIMARY KEY ([Id])
+)
+
