021264824ed6657623936bb6400c9ffe786d248c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementService/IDefaultCommissionService.cs
@@ -0,0 +1,28 @@
+﻿using System.ServiceModel;
+
+using DataTransferObjects;
+
+namespace PracticeManagementService
+{
+	// NOTE: If you change the interface name "IDefaultCommissionService" here, you must also update the reference to "IDefaultCommissionService" in Web.config.
+	[ServiceContract]
+	public interface IDefaultCommissionService
+	{
+		/// <summary>
+		/// Retrieves the actual default sales commission for the specified person.
+		/// </summary>
+		/// <param name="personId">An ID of the person to retrieve the data for.</param>
+		/// <returns>The list of the <see cref="DefaultCommission"/> objects.</returns>
+		[OperationContract]
+		DefaultCommission DefaultSalesCommissionByPerson(int personId);
+
+		/// <summary>
+		/// Retrieves the actual default management commission for the specified person.
+		/// </summary>
+		/// <param name="personId">An ID of the person to retrieve the data for.</param>
+		/// <returns>The list of the <see cref="DefaultCommission"/> objects.</returns>
+		[OperationContract]
+		DefaultCommission DefaultManagementCommissionByPerson(int personId);
+	}
+}
+
