967e3f5b00e8c87e9856af0fe300387c92bc5fd6
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/Persons_old/PersonChangedEventArguments.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataTransferObjects;
+
+namespace PraticeManagement.Controls.Persons
+{
+    public class PersonChangedEventArguments : EventArgs
+    {
+        public Person SelectedPerson
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Init constructor of PersonChangedEventArguments.
+        /// </summary>
+        public PersonChangedEventArguments(Person selectedPerson)
+        {
+            this.SelectedPerson = selectedPerson;
+        }
+    }
+}
+
