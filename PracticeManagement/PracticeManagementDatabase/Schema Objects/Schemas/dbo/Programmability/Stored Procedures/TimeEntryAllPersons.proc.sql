01aec9d5d0a1e26bdf0ed9391caf22604372f491
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimeEntryAllPersons.proc.sql
@@ -0,0 +1,22 @@
+﻿-- =============================================
+-- Author:		Nikita G.
+-- Create date: 2009-12-25
+-- Description:	Retrieves all persons that have entered TE at least once
+-- =============================================
+CREATE  PROCEDURE dbo.TimeEntryAllPersons
+	@EntryStartDate DATETIME = NULL,
+	@EntryEndDate DATETIME = NULL
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+    SELECT  DISTINCT objp.PersonId,
+            objp.FirstName AS 'ObjectFirstName',
+            objp.LastName AS 'ObjectLastName'
+    FROM    TimeEntries AS te
+            INNER JOIN dbo.MilestonePerson AS mp ON mp.MilestonePersonId = te.MilestonePersonId
+            INNER JOIN dbo.MilestonePersonEntry AS mpe ON mp.MilestonePersonId = mpe.MilestonePersonId
+            INNER JOIN dbo.Person AS objp ON objp.PersonId = mp.PersonId
+	WHERE (te.MilestoneDate BETWEEN @EntryStartDate AND @EntryEndDate) OR @EntryStartDate IS NULL OR @EntryEndDate IS NULL
+END
+
