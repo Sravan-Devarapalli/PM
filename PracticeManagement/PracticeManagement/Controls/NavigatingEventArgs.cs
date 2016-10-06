7effec286d42c2cdaad197d4209615b6fb70b5dc
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/NavigatingEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PraticeManagement.Controls
+{
+	public class NavigatingEventArgs : EventArgs
+	{
+		public bool Cancel
+		{
+			get;
+			set;
+		}
+	}
+
+	public delegate void NavigatingEventHandler(object sender, NavigatingEventArgs e);
+}
+
