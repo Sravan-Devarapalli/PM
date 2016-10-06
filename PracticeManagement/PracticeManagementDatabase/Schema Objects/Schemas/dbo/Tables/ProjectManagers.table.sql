48c7233861b9f29db28d4b3705be7f2464ee6840
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ProjectManagers.table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ProjectAccess]
+(
+	Id                      INT IDENTITY(1,1) NOT NULL,
+	ProjectId	            INT NOT NULL, 
+	ProjectAccessId			INT NOT NULL
+	CONSTRAINT PK_ProjectManagers_Id PRIMARY KEY CLUSTERED(Id),
+    CONSTRAINT FK_ProjectManagers_ProjectId FOREIGN KEY(ProjectId) REFERENCES  dbo.Project(ProjectId),
+	CONSTRAINT FK_ProjectManagers_ProjectManagerId FOREIGN KEY(ProjectAccessId) REFERENCES  dbo.Person(PersonId)
+);
