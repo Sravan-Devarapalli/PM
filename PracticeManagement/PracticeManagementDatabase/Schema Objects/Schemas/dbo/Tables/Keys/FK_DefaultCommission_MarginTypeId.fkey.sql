7da9986916b1b1112e5894633cf7c14433032156
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_DefaultCommission_MarginTypeId.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[DefaultCommission]
+    ADD CONSTRAINT [FK_DefaultCommission_MarginTypeId] FOREIGN KEY ([MarginTypeId]) REFERENCES [dbo].[MarginType] ([MarginTypeId]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
