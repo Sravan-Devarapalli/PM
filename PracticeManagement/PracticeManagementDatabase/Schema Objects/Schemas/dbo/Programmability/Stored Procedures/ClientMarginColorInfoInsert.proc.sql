92f37d3b37f8c05bff7c5474ed2b3977bbc282a2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ClientMarginColorInfoInsert.proc.sql
@@ -0,0 +1,30 @@
+﻿CREATE PROCEDURE dbo.ClientMarginColorInfoInsert
+(
+	@ClientId   INT,
+	@ColorId    INT,
+	@StartRange INT,
+	@EndRange   INT,
+	@isDeletePreviousMarginInfo BIT
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	IF(@isDeletePreviousMarginInfo=1)
+	BEGIN
+	DELETE dbo.ClientMarginColorInfo 
+	WHERE ClientId = @ClientId
+	END
+	
+	INSERT INTO [dbo].[ClientMarginColorInfo]
+           ([ClientId]
+           ,[ColorId]
+           ,[StartRange]
+           ,[EndRange])
+     VALUES
+           (@ClientId
+           ,@ColorId
+           ,@StartRange
+           ,@EndRange)
+	
+END
