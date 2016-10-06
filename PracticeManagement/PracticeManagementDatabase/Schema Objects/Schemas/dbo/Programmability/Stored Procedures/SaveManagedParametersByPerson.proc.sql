970ad9a32551109c3e42430ac74a195262e1e718
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SaveManagedParametersByPerson.proc.sql
@@ -0,0 +1,29 @@
+﻿CREATE PROCEDURE [dbo].[SaveManagedParametersByPerson]
+(
+	@UserLogin			  NVARCHAR(255),
+	@ActualRevenuePerHour DECIMAL(32,2),
+	@TargetRevenuePerHour DECIMAL(32,2),
+	@HoursUtilization DECIMAL(32,2),
+	@TargetRevenuePerAnnum DECIMAL(32,2)
+)
+AS 
+BEGIN
+	DECLARE @PersonId INT
+	SELECT @PersonId = PersonId FROM dbo.Person WHERE Alias = @UserLogin
+
+	IF EXISTS(SELECT * FROM dbo.ManagedParametersByPerson WHERE PersonId=@PersonId)
+	BEGIN 
+		UPDATE dbo.ManagedParametersByPerson 
+		SET ActualRevenuePerHour=@ActualRevenuePerHour,
+			TargetRevenuePerHour=@TargetRevenuePerHour,
+			HoursUtilization=@HoursUtilization,
+			TargetRevenuePerAnnum=@TargetRevenuePerAnnum
+		WHERE PersonId=@PersonId
+	END
+	ELSE
+	BEGIN
+		INSERT INTO dbo.ManagedParametersByPerson (PersonId,ActualRevenuePerHour,TargetRevenuePerHour,HoursUtilization,TargetRevenuePerAnnum)
+		VALUES(@PersonId,@ActualRevenuePerHour,@TargetRevenuePerHour,@HoursUtilization,@TargetRevenuePerAnnum)
+	END
+END
+
