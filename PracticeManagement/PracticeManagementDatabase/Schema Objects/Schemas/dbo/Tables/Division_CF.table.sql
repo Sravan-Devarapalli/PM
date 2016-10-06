c1059be8f1c36479b9ff279a75881bb3b1d6d1da
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Division_CF.table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Division_CF]
+(
+	DivisionId			INT			IDENTITY (1, 1) NOT NULL,
+	DivisionCode		NVARCHAR(20)	NOT NULL,
+	DivisionName		NVARCHAR(50)	NOT NULL,
+	ParentId			INT				NULL
+)
+
