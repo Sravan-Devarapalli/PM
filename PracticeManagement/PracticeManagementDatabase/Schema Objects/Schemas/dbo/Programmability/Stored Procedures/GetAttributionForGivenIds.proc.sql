f7191ff15d25df0e54bba316da4731c64c4190bf
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetAttributionForGivenIds.proc.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[GetAttributionForGivenIds]
+(
+	@AttributionIds NVARCHAR(MAX) 
+)
+AS
+BEGIN
+
+	SELECT  P.ProjectNumber,
+			P.Name AS ProjectName,
+			A.*
+	FROM dbo.Attribution A
+	INNER JOIN dbo.Project P ON P.ProjectId = A.ProjectId 
+	WHERE A.AttributionId IN (SELECT ResultId	FROM [dbo].[ConvertStringListIntoTable](@AttributionIds))
+
+END
+
