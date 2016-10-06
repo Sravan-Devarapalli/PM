6504df77f9cca8149cb06bdc224aeaebbba019ca
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/OverrideExceptionHistory.table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[OverrideExceptionHistory]
+(
+	[Id]				INT IDENTITY(1,1) NOT NULL,
+	[PersonId]			INT         NOT NULL,
+	OverrideStartDate	DATETIME	NULL,
+	OverrideEndDate		DATETIME	NULL,
+	ModifiedDate		DATETIME	NOT NULL,
+	ModifiedBy			INT			NULL,
+	CONSTRAINT PK_OverrideExceptionHistory PRIMARY KEY (Id)
+)
+
