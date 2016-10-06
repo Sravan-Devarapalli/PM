69cecca13f1a553f3b7e21c3b30dcd1f023fd11c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/QualificationType.table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [Skills].[QualificationType](
+	[QualificationTypeId]	[int] NOT NULL IDENTITY(1,1), 
+	[Description] [nvarchar](max) NULL,
+	[DisplayOrder] [tinyint] NULL,
+	[IsActive]		[bit] NOT NULL CONSTRAINT DF_QualificationType_IsActive DEFAULT(1),
+	[IsDeleted]		[bit] NOT NULL CONSTRAINT DF_QualificationType_IsDeleted DEFAULT(0),
+ CONSTRAINT [PK_QualificationType] PRIMARY KEY CLUSTERED 
+(
+	[QualificationTypeId] ASC
+)
+)
+
