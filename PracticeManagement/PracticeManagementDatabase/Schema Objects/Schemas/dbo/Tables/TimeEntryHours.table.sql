ef3b3c2f02ea6ff9ceb8a8900657bca92a03d2dc
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/TimeEntryHours.table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[TimeEntryHours]
+(
+    [Id]                INT			IDENTITY (1, 1) NOT NULL,
+    [TimeEntryId]       INT 		NOT NULL,
+    [ActualHours]       REAL		NOT NULL,
+    [CreateDate]        DATETIME	NOT NULL,
+    [ModifiedDate]      DATETIME	NOT NULL,
+    [ModifiedBy]        INT			NOT NULL, 
+    [IsChargeable]      BIT			NOT NULL,
+    [ReviewStatusId]    INT			NOT NULL CONSTRAINT DF_TimeEntryHours_ReviewStatusId DEFAULT  1--default constraint to pending status.
+	, CONSTRAINT PK_TimeEntryHours_TimeEntryIdAndId PRIMARY KEY CLUSTERED(TimeEntryId ASC ,Id ASC )
+)
+
