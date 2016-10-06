ffdbfbd18d0addeb6b2024a3dfe6103f9393207e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/RecruiterInsert.proc.sql
@@ -0,0 +1,14 @@
+﻿-- =============================================
+-- Author:		SkipSailors
+-- Create date: 5-10-2008
+-- Description:	Add a recruiter to the system
+-- =============================================
+CREATE PROCEDURE [dbo].[RecruiterInsert] 
+	@RecruiterId int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	Insert Recruiter (RecruiterId) values (@RecruiterId)
+END
+
