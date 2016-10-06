5517e2e54945392ace7db97f03cac3370b1f95c9
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/Generic/BorderlessImage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PraticeManagement.Controls.Generic
+{
+    public class BorderlessImage : System.Web.UI.WebControls.Image
+    {
+        public override System.Web.UI.WebControls.Unit BorderWidth
+        {
+            get
+            {
+                if (base.BorderWidth.IsEmpty)
+                {
+                    return System.Web.UI.WebControls.Unit.Pixel(0);
+                }
+                else
+                {
+                    return base.BorderWidth;
+                }
+            }
+            set
+            {
+                base.BorderWidth = value;
+            }
+        }
+    }
+}
