c7d986d3e28e09f97f149723706a46742b54a484
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/IsProjectSummaryCachedToday.proc.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[IsProjectSummaryCachedToday]
+AS
+BEGIN
+
+	DECLARE @InsertingTime DATETIME
+	SELECT	@InsertingTime = CONVERT(DATE, dbo.GettingPMTime(GETUTCDATE()))
+	
+	SELECT CASE WHEN COUNT(*) > 0 THEN CONVERT(BIT,1)
+			ELSE CONVERT(BIT,0)
+		   END AS IsCacheExistsForToday
+	FROM [ProjectSummaryCache] p
+	WHERE CacheDate = @InsertingTime
+	
+END
