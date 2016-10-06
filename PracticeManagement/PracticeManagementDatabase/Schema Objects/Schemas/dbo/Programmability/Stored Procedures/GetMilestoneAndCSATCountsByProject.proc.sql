d85151addbe138fa98e04e1e78e144e63ab24145
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetMilestoneAndCSATCountsByProject.proc.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[GetMilestoneAndCSATCountsByProject]
+(
+	@ProjectId   INT
+)
+AS
+BEGIN
+
+	SELECT	(SELECT COUNT(*) FROM dbo.Milestone M WHERE ProjectId= @ProjectId) AS MilestoneCount,
+			(SELECT COUNT(*) FROM dbo.ProjectCSAT PC WHERE ProjectId= @ProjectId) AS CSATCount,
+			(SELECT COUNT(*) FROM dbo.Attribution a WHERE ProjectId= @ProjectId) AS AttributionCount
+
+END
