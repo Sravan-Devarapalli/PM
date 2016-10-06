1bda242ddeb521c6c11fa59a03c31bd042a0cb88
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ProjectAttachment.table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE dbo.ProjectAttachment (
+	Id							INT IDENTITY(1,1) NOT NULL,
+	ProjectId					INT				  NOT NULL,
+	CategoryId					INT				  NOT NULL,
+	[FileName]					[nvarchar](256)       NULL,	
+	[AttachmentData]			[varbinary](max)      NULL,
+	UploadedDate				DATETIME			  NULL,
+	ModifiedBy					INT					  NULL,
+   CONSTRAINT PK_ProjectSOW_Id PRIMARY KEY CLUSTERED(Id),
+   CONSTRAINT FK_ProjectSOW_ProjectId FOREIGN KEY(ProjectId) REFERENCES  dbo.Project(ProjectId)
+);
