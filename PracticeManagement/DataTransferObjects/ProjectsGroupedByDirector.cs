61cc5b5cf95a8d8ca287f4179ab46fa09f5d6166
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ProjectsGroupedByDirector.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class ProjectsGroupedByPerson
+    {
+        [DataMember]
+        public int PersonId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime HireDate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime? TerminationDate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Dictionary<DateTime, ComputedFinancials> ProjectedFinancialsByMonth
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public List<ProjectsGroupedByClient> GroupedClients
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public ComputedFinancials ComputedFinancials
+        {
+            get;
+            set;
+        }
+    }
+
+    [DataContract]
+    [Serializable]
+    public class ProjectsGroupedByClient : Client
+    {
+        [DataMember]
+        public List<ProjectsGroupedByClientGroup> GroupedClientGroups
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public ComputedFinancials ComputedFinancials
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Dictionary<DateTime, ComputedFinancials> ProjectedFinancialsByMonth
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public List<Project> ProjectsWithoutClientGroup
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public List<MilestonePerson> MilestonePersonsWithoutClientGroup
+        {
+            get;
+            set;
+        }
+    }
+
+    [DataContract]
+    [Serializable]
+    public class ProjectsGroupedByClientGroup : ProjectGroup
+    {
+        [DataMember]
+        public ComputedFinancials ComputedFinancials
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Dictionary<DateTime, ComputedFinancials> ProjectedFinancialsByMonth
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public List<Project> Projects
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public List<MilestonePerson> MilestonePersons
+        {
+            get;
+            set;
+        }
+    }
+
+    [DataContract]
+    [Serializable]
+    public class ProjectsGroupedByPractice
+    {
+        [DataMember]
+        public int PracticeId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string HtmlEncodedName
+        {
+            get
+            {
+                return HttpUtility.HtmlEncode(Name);
+            }
+        }
+
+        [DataMember]
+        public Person PracticeManager
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public List<PracticeManagerHistory> PreviousPracticeManagers
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Dictionary<DateTime, ComputedFinancials> ProjectedFinancialsByMonth
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public List<ProjectsGroupedByClient> GroupedClients
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public ComputedFinancials ComputedFinancials
+        {
+            get;
+            set;
+        }
+    }
+}
