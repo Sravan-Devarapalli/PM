e0189c6a0d96a4b24d13d27f5cf30bb525b0a103
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_MilestoneHours.view.sql
@@ -0,0 +1,13 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 6-11-2008
+-- Updated by:	Anatoliy Lokshin
+-- Update date:	8-26-2008
+-- Description:	List milestone hours
+-- =============================================
+CREATE VIEW [dbo].[v_MilestoneHours]
+AS
+	SELECT s.MilestoneId, SUM(s.HoursPerDay) AS MilestoneHours, COUNT(DISTINCT PersonId) AS PersonCount
+	  FROM dbo.v_MilestonePersonSchedule AS s
+	GROUP BY s.[MilestoneId]
+
