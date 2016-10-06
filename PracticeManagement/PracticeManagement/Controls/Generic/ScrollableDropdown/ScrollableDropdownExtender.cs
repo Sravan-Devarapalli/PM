32e6d94f2700a61031312bc35b80570f8fa5c8b4
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/Generic/ScrollableDropdown/ScrollableDropdownExtender.cs
@@ -0,0 +1,187 @@
+﻿using System.Web.UI;
+using AjaxControlToolkit;
+
+[assembly: WebResource("PraticeManagement.Controls.Generic.ScrollableDropdown.ScrollableDropdownExtender.js", "text/javascript")]
+
+namespace PraticeManagement.Controls.Generic.ScrollableDropdown
+{
+    using System.ComponentModel;
+    using System.Web.UI.WebControls;
+    using System;
+    using System.Web.UI.HtmlControls;
+    using System.Drawing;
+    using System.Web;
+
+    /// <summary>
+    /// Extender that fixes width of select element in IE.
+    /// </summary>
+    [ClientScriptResource("PraticeManagement.Controls.Generic.ScrollableDropdown.ScrollableDropdownBehavior", "PraticeManagement.Controls.Generic.ScrollableDropdown.ScrollableDropdownExtender.js")]
+    [TargetControlType(typeof(ScrollingDropDown))]
+    public class ScrollableDropdownExtender : ExtenderControlBase
+    {
+        #region Constants
+
+        private const string Label_ID = "LabelIdValue";
+        private const string DisplayText_Value = "DisplayTextValue";
+        private const string EditImageUrl_Value = "EditImageUrlValue";
+        private const string ExtenderWidth_Value = "ExtenderWidthValue";
+        private const string ExtenderDisplay_Value = "ExtenderDisplayValue";
+
+
+        #endregion
+
+        #region Fields
+
+        private int _maxNoOfCharacters;
+
+        #endregion
+
+        [ExtenderControlProperty]
+        [ClientPropertyName("labelValue")]
+        [Browsable(false)]
+        public string LabelId
+        {
+            get
+            {
+                return ID + "Label";
+            }
+        }
+
+        [ExtenderControlProperty]
+        [ClientPropertyName("editImageUrl")]
+        [DefaultValue("~/Images/Dropdown_Arrow.png")]
+        public string EditImageUrl
+        {
+            get { return GetPropertyValue(EditImageUrl_Value, string.Empty); }
+            set { SetPropertyValue(EditImageUrl_Value, value); }
+        }
+
+        [ExtenderControlProperty]
+        [ClientPropertyName("displayTextValue")]
+        public string DisplayText
+        {
+            get
+            {
+                if (UseAdvanceFeature)
+                {
+                    return HttpUtility.HtmlEncode(IsMaxLimitExceded ? DecodedSelectedString.Substring(0, MaxNoOfCharacters - 2) + ".." : DecodedSelectedString);
+                }
+                else
+                {
+                    return GetPropertyValue(DisplayText_Value, string.Empty);
+                }
+            }
+            set { SetPropertyValue(DisplayText_Value, value); }
+        }
+
+        public string DecodedSelectedString
+        {
+            get
+            {
+
+                return HttpUtility.HtmlDecode(((ScrollingDropDown)this.TargetControl).SelectedString);
+            }
+        }
+
+        public bool IsMaxLimitExceded
+        {
+            get
+            {
+                return DecodedSelectedString.Length > MaxNoOfCharacters;
+            }
+        }
+
+        public string SelectedStringToolTip
+        {
+            get
+            {
+                if (UseAdvanceFeature)
+                {
+                    if (IsMaxLimitExceded)
+                    {
+                        return ((ScrollingDropDown)this.TargetControl).SelectedString;
+                    }
+                }
+                return string.Empty;
+            }
+        }
+
+        public bool UseAdvanceFeature
+        {
+            get;
+            set;
+        }
+
+        public int MaxNoOfCharacters
+        {
+            get
+            {
+                return _maxNoOfCharacters == 0 ? 33 : _maxNoOfCharacters;
+            }
+            set
+            {
+                _maxNoOfCharacters = value;
+            }
+        }
+
+        [ExtenderControlProperty]
+        [ClientPropertyName("Width")]
+        public string Width
+        {
+            get { return GetPropertyValue(ExtenderWidth_Value, string.Empty); }
+            set { SetPropertyValue(ExtenderWidth_Value, value); }
+        }
+
+        [ExtenderControlProperty]
+        [ClientPropertyName("Display")]
+        public string Display
+        {
+            get { return GetPropertyValue(ExtenderDisplay_Value, string.Empty); }
+            set { SetPropertyValue(ExtenderDisplay_Value, value); }
+        }
+
+        protected override void OnInit(EventArgs e)
+        {
+            Load += Page_Load;
+            base.OnInit(e);
+        }
+
+        protected virtual void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        public override void RenderControl(HtmlTextWriter writer)
+        {
+            writer.WriteLine(GetHTMLToRender());
+            base.RenderControl(writer);
+        }
+
+        private string GetHTMLToRender()
+        {
+            string htmlWithoutImageFormat = "<div ID={0} class=\"scrollextLabel\"{2}><label class=\"vTop\">{1}</label></div>";
+            string htmlWithImageFormat = "<div ID={0} class=\"scrollextLabel\"{3}><label class=\"vTop\" title=\"{4}\">{1}</label><span class=\"padLeft18 fl-right\"><image src={2} /></span></div>";
+            var styleString = "";
+            if (!string.IsNullOrEmpty(Width))
+            {
+                if (!string.IsNullOrEmpty(Display))
+                    styleString = "style=\"width:" + Width + ";display:" + Display + ";white-space:normal\"";
+                else
+                    styleString = "style=\"width:" + Width + ";white-space:normal\"";
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(Display))
+                    styleString = "style=\"display:" + Display;
+            }
+            if (!string.IsNullOrEmpty(EditImageUrl))
+            {
+                return string.Format(htmlWithImageFormat, LabelId, DisplayText, ResolveUrl(EditImageUrl), styleString, SelectedStringToolTip);
+            }
+            else
+            {
+                return string.Format(htmlWithoutImageFormat, LabelId, DisplayText, styleString);
+            }
+        }
+    }
+}
+
