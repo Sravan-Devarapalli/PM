9cb7aa32bbf21bb0194c1302f9064f6e435bad31
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementTasks/PRMAProjectedEmailNotifier/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PRMAProjectedEmailNotifier.ConfigurationService;
+
+namespace PRMAProjectedEmailNotifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ConfigurationServiceClient service = new ConfigurationServiceClient();
+                service.CheckProjectedProjects(PRMAProjectedEmailNotifier.Properties.Settings.Default.EmailTemplateID, PRMAProjectedEmailNotifier.Properties.Settings.Default.CompanyName);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
+
