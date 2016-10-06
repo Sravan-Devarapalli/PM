d1a71418df4993c97bed795f4b41e5cbad19bd1c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementService/ProjectStatusService.svc.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel.Activation;
+using DataAccess;
+using DataTransferObjects;
+
+namespace PracticeManagementService
+{
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class ProjectStatusService : IProjectStatusService
+    {
+        #region IProjectStatusService Members
+
+        /// <summary>
+        /// Retrives the list of the project statuses.
+        /// </summary>
+        /// <returns>The list of the <see cref="ProjectStatus"/> objects.</returns>
+        public List<ProjectStatus> GetProjectStatuses()
+        {
+            return ProjectStatusDAL.ProjectStatusListAll();
+        }
+
+        #endregion IProjectStatusService Members
+    }
+}
