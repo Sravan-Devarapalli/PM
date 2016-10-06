fa35071871460e773e007602c6f7c22ed76d21c6
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectExpenseInsert.proc.sql
@@ -0,0 +1,45 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2010-04-20
+-- =============================================
+CREATE PROCEDURE dbo.ProjectExpenseInsert 
+    @ExpenseId int out,
+    @ExpenseName nvarchar(50),
+    @ExpenseAmount decimal(18, 2),
+	@ExpectedExpenseAmount decimal(18, 2),
+    @ExpenseReimbursement decimal(18, 2),
+	@ProjectId INT,
+	@StartDate	DATETIME,
+	@EndDate	DATETIME,
+	@MilestoneId	INT,
+	@ExpenseTypeId  INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO [dbo].[ProjectExpense]
+           ([Name]
+           ,[Amount]
+		   ,[ExpectedAmount]
+           ,[Reimbursement]
+           ,[ProjectId]
+           ,[StartDate]
+           ,[EndDate]
+		   ,[MilestoneId]
+		   ,[ExpenseTypeId])
+     VALUES
+           (@ExpenseName 
+           ,@ExpenseAmount
+		   ,@ExpectedExpenseAmount
+           ,@ExpenseReimbursement
+           ,@ProjectId
+		   ,@StartDate	
+		   ,@EndDate
+		   ,@MilestoneId
+		   ,@ExpenseTypeId
+		   )
+	SELECT @ExpenseId = SCOPE_IDENTITY()
+
+	SELECT @ExpenseId
+END
+
