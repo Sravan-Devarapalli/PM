4d6ef864c318ac7b89bbf8fb1a93b127741b8ac8
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/TimeManagement/TimeManagement.Master.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TimeManagement
+{
+	public partial class TimeManagement : System.Web.UI.MasterPage
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
+
