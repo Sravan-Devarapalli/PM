1d0459e944755b31263077c5cedc740d5811dff0
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Reports/PersonLevelTimeEntriesHistory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using DataTransferObjects.TimeEntry;
+
+namespace DataTransferObjects.Reports
+{
+    [DataContract]
+    [Serializable]
+    public class PersonLevelTimeEntriesHistory
+    {
+        [DataMember]
+        public Person Person
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public List<TimeEntryRecord> TimeEntryRecords
+        {
+            get;
+            set;
+        }
+
+        public double BillableNetChange
+        {
+            get
+            {
+                return TimeEntryRecords.Sum(t => t.IsChargeable ? t.NetChange : 0);
+            }
+        }
+
+        public double NonBillableNetChange
+        {
+            get
+            {
+                return TimeEntryRecords.Sum(t => !t.IsChargeable ? t.NetChange : 0);
+            }
+        }
+
+        public double NetChange
+        {
+            get
+            {
+                return TimeEntryRecords.Sum(t => t.NetChange);
+            }
+        }
+    }
+}
