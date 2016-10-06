804dcda135516c048451508ec4603ccea6b628ee
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/NoteTarget.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// Determines an opportunity statuses.
+    /// NOTE: these values are linked to NoteTarget database table
+    /// </summary>
+    [DataContract]
+    public enum NoteTarget
+    {
+        [EnumMember]
+        Milestone = 1,
+
+        [EnumMember]
+        Project = 2,
+
+        [EnumMember]
+        Person = 3,
+
+        [EnumMember]
+        Opportunity = 4,
+    }
+}
