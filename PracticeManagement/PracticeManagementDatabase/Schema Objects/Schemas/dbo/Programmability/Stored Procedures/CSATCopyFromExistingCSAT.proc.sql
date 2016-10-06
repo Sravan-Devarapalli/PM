191c592770a0d07cf1dd14cd30c476f0d1a5a03f
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CSATCopyFromExistingCSAT.proc.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [dbo].[CSATCopyFromExistingCSAT]
+(
+	@CopyProjectCSATId		INT, 
+	@ReviewStartDate DATETIME,
+	@ReviewEndDate DATETIME,
+	@CompletionDate DATETIME,
+	@ReviewerId		INT,
+	@ReferralScore	INT,
+	@UserLogin      NVARCHAR(255)
+)
+AS
+BEGIN
+	-- Start logging session
+	EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
+
+	INSERT INTO ProjectCSAT(ProjectId,ReviewStartDate,ReviewEndDate,CompletionDate,ReferralScore,ReviewerId,Comments)
+	SELECT cs.ProjectId,@ReviewStartDate,@ReviewEndDate,@CompletionDate,@ReferralScore,@ReviewerId,cs.Comments
+	FROM dbo.ProjectCSAT cs
+	WHERE cs.CSATId = @CopyProjectCSATId
+
+	-- End logging session
+	EXEC dbo.SessionLogUnprepare
+
+END
