5a43fcdd0716f6b734672afc4ca198fa4679f846
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Reports/UtilizationTable.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PraticeManagement.Reporting
+{
+    public partial class UtilizationTable : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
