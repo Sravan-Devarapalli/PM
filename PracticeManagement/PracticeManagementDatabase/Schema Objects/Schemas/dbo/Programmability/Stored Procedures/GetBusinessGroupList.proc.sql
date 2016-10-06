ccb96c84d088a14861f81302db7be24eaa40b652
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetBusinessGroupList.proc.sql
@@ -0,0 +1,29 @@
+﻿CREATE PROCEDURE [dbo].[GetBusinessGroupList]
+(
+	@ClientIds	NVARCHAR(MAX) = NULL,
+	@BusinessUnitId INT = NULL
+)
+AS
+BEGIN
+
+	DECLARE @ClientIdTable TABLE ( Ids INT )
+
+	INSERT INTO @ClientIdTable( Ids)
+	SELECT ResultId
+	FROM dbo.ConvertStringListIntoTable(@ClientIds)
+	
+	SELECT DISTINCT bg.BusinessGroupId,
+			bg.ClientId,
+			C.Name AS ClientName,
+			bg.Name ,
+			bg.Code,
+			dbo.[IsBusinessGroupInUse](bg.BusinessGroupId,bg.Code) AS InUse,
+			bg.Active
+	FROM dbo.BusinessGroup  bg
+	INNER JOIN dbo.Client C ON C.ClientId = bg.ClientId
+	LEFT JOIN dbo.ProjectGroup pg ON pg.BusinessGroupId = bg.BusinessGroupId
+	WHERE  (@ClientIds IS NULL OR bg.ClientId IN (SELECT Ids FROM @ClientIdTable)) 
+		AND (@BusinessUnitId IS NULL OR pg.GroupId = @BusinessUnitId) 
+	ORDER BY bg.Name
+END
+
