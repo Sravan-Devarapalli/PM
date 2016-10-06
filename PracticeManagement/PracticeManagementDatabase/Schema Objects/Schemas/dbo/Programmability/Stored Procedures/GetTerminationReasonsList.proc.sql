0f610bbaca7face7a95f8e5c2c2da7e7da664eae
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetTerminationReasonsList.proc.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[GetTerminationReasonsList]
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	SELECT TR.TerminationReasonId
+			, TR.TerminationReason
+			, TR.IsW2SalaryRule
+			, TR.IsW2HourlyRule
+			, TR.Is1099Rule
+			, TR.IsContingentRule
+			, TR.IsVisible
+	FROM dbo.TerminationReasons TR
+	ORDER BY TR.TerminationReason
+
+END
