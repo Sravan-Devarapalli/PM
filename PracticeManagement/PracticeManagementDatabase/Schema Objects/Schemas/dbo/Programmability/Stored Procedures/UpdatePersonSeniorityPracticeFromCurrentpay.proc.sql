40ef9ac229a971d537ab31d2f04f939ec19ca632
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/UpdatePersonSeniorityPracticeFromCurrentpay.proc.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE dbo.UpdatePersonSeniorityPracticeFromCurrentPay
+AS
+BEGIN
+	DECLARE @Today DATETIME,@FutureDate DATETIME
+	SELECT @Today = CONVERT(DATETIME,CONVERT(DATE,[dbo].[GettingPMTime](GETDATE()))),@FutureDate = dbo.GetFutureDate()
+
+	UPDATE P
+	SET P.SeniorityId = Pa.SeniorityId,
+		P.DefaultPractice = Pa.PracticeId
+	FROM dbo.Person P
+	JOIN dbo.Pay Pa
+	ON P.PersonId = Pa.Person AND 
+		pa.StartDate <= @Today AND ISNULL(EndDate,@FutureDate) > @Today
+		AND (P.SeniorityId <> Pa.SeniorityId OR P.DefaultPractice <> Pa.PracticeId)
+
+	UPDATE dbo.Pay
+	SET IsActivePay = CASE WHEN StartDate <= @Today AND  EndDate > @Today
+							   THEN 1 ELSE 0 END
+	WHERE IsActivePay <> (CASE WHEN StartDate <= @Today AND  EndDate > @Today
+							   THEN 1 ELSE 0 END)
+END
+
