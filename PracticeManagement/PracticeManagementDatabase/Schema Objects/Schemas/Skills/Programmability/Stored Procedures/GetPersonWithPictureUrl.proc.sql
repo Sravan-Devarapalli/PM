61fb245107ea28f1d0f1782a53b5503b0ecb10ad
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Programmability/Stored Procedures/GetPersonWithPictureUrl.proc.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [Skills].[GetPersonWithPictureUrl]
+(
+	@PersonId         INT
+)
+AS
+BEGIN
+
+	SELECT p.PersonId,p.LastName,p.FirstName,p.PictureUrl
+	FROM dbo.[Person] p
+	WHERE p.PersonId = @PersonId
+END
