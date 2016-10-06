a6f785452c38788794ef9634b38a621d52a03315
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/BusinessGroupUpdate.proc.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[BusinessGroupUpdate]
+(
+	  @BusinessGroupId	INT,
+	  @Name		NVARCHAR(100),
+	  @IsActive   BIT ,
+	  @UserLogin          NVARCHAR(255)
+)
+AS
+BEGIN
+	-- Start logging session
+	EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
+
+	UPDATE BusinessGroup
+	SET Name = @Name,
+		Active = @IsActive
+	WHERE BusinessGroupId = @BusinessGroupId
+
+	-- End logging session
+	EXEC dbo.SessionLogUnprepare
+END
