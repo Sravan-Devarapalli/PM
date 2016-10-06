aa5b27abd9b8bcdc0b0c277e18c872f241500260
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OverheadFixedRateInsert.proc.sql
@@ -0,0 +1,27 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 5-27-2008
+-- Updated by:	Anatoliy Lokshin
+-- Update date:	8-07-2008
+-- Description:	Inserts the overhead.
+-- =============================================
+CREATE PROCEDURE [dbo].[OverheadFixedRateInsert]
+(
+	@Description           NVARCHAR(255),
+	@Rate                  DECIMAL(18,3),
+	@StartDate             DATETIME,
+	@EndDate               DATETIME,
+	@RateType              INT,
+	@Inactive              BIT,
+	@IsCogs                BIT,
+	@OverheadFixedRateId   INT OUT
+)
+AS
+	SET NOCOUNT ON
+
+	INSERT INTO dbo.OverheadFixedRate
+	            (Description, Rate, StartDate, EndDate, RateType, Inactive, IsCogs)
+	     VALUES (@Description, @Rate, @StartDate, @EndDate, @RateType, @Inactive, @IsCogs)
+
+	SET @OverheadFixedRateId = SCOPE_IDENTITY()
+
