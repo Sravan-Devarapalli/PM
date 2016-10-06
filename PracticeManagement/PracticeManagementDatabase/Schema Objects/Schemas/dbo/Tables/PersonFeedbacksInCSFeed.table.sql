11edf1adf378685e82451275137b73870596e0d9
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/PersonFeedbacksInCSFeed.table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[PersonFeedbacksInCSFeed]
+(
+	Id			INT		IDENTITY(1,1) NOT NULL,
+	PersonId	INT		NOT NULL,
+	ReviewStartDate	DATETIME NOT NULL,
+	ReviewEndDate	DATETIME NOT NULL,
+	ProjectId	INT		NULL,
+	Count		INT		NOT NULL,
+	IsDummy		BIT		NULL,
+	ManagerPaychexID INT NULL
+	CONSTRAINT PK_PersonFeedbacksInCSFeed PRIMARY KEY (Id)
+)
+
