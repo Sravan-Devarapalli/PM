cd96e405dfb097955082848c4077244dd4de8bea
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ListManagersSubordinates.proc.sql
@@ -0,0 +1,17 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2010-04-19
+-- Description:	Lists manager's subordinates
+-- =============================================
+CREATE PROCEDURE dbo.ListManagersSubordinates 
+	@PersonId INT AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	SELECT p.FirstName, p.LastName, p.PersonId, p.IsDefaultManager
+	FROM dbo.Person AS p 
+	WHERE p.ManagerId = @PersonId
+END
+
