97a17a113d8af19524ca87a91cd1dcdd6ac37496
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/NoteUpdate.proc.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[NoteUpdate]
+(
+	@NoteId        INT ,
+	@NoteText      NVARCHAR(MAX)
+)
+AS
+	SET NOCOUNT ON
+	UPDATE dbo.Note
+	SET    NoteText = @NoteText
+	WHERE  NoteId = @NoteId
+
