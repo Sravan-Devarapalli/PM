c899a32e2a5eac73aa1e71aa6ff39a30b5df6395
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Reports/TimeEntriesGroupByDate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Reports
+{
+    [DataContract]
+    [Serializable]
+    public class TimeEntriesGroupByDate
+    {
+        [DataMember]
+        public DateTime Date
+        {
+            get;
+            set;
+        }
+
+        public double BillableHours
+        {
+            get
+            {
+                return DayTotalHoursList != null ? DayTotalHoursList.Sum(d => d.BillableHours) : 0;
+            }
+        }
+
+        public double NonBillableHours
+        {
+            get
+            {
+                return DayTotalHoursList != null ? DayTotalHoursList.Sum(d => d.NonBillableHours) : 0;
+            }
+        }
+
+        public double TotalHours
+        {
+            get
+            {
+                return DayTotalHoursList != null ? DayTotalHoursList.Sum(p => p.TotalHours) : 0;
+            }
+        }
+
+        [DataMember]
+        public List<TimeEntryByWorkType> DayTotalHoursList
+        {
+            get;
+            set;
+        }
+    }
+}
