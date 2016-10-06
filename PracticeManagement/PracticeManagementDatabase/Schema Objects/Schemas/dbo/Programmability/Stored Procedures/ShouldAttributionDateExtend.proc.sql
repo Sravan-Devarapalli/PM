dece4c0b6f4aed5131eac07ad92b0f9c02bff71d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ShouldAttributionDateExtend.proc.sql
@@ -0,0 +1,45 @@
+﻿CREATE PROCEDURE [dbo].[ShouldAttributionDateExtend]
+	(
+		@ProjectId		INT,
+		@StartDate		DATETIME,--Milestone start date 
+		@EndDate		DATETIME --Milestone end date
+	)
+AS
+BEGIN
+
+		DECLARE @ProjectStartDate	DATETIME,
+				@ProjectEndDate		DATETIME,
+				@IsAttributionEndDateExtend BIT = 0,
+				@IsAttributionStartDateExtend BIT = 0
+
+		SELECT	@ProjectStartDate = P.StartDate,
+				@ProjectEndDate = P.EndDate
+		FROM	dbo.Project P
+		WHERE	P.ProjectId = @ProjectId
+		--If new milestone range overlapps with old project range  
+		IF @StartDate <= @ProjectEndDate AND @ProjectStartDate <= @EndDate OR (@ProjectStartDate IS NOT NULL)
+		BEGIN
+				IF EXISTS
+								(SELECT A.AttributionId
+								FROM dbo.Attribution A
+								WHERE @StartDate < @ProjectStartDate AND A.ProjectId = @ProjectId AND A.StartDate = @ProjectStartDate)
+				BEGIN
+						SET @IsAttributionStartDateExtend = 1
+				END
+
+				IF EXISTS
+								(SELECT A.AttributionId
+								FROM dbo.Attribution A
+								WHERE @EndDate > @ProjectEndDate AND A.ProjectId = @ProjectId AND A.EndDate = @ProjectEndDate)
+				BEGIN
+						SET @IsAttributionEndDateExtend = 1
+				END
+		END
+		ELSE IF @StartDate > @ProjectEndDate OR @ProjectStartDate > @EndDate
+		BEGIN
+				SET @IsAttributionStartDateExtend = 1
+				SET @IsAttributionEndDateExtend = 1
+		END
+		
+		SELECT @IsAttributionStartDateExtend AS ExtendAttributionStartDate,@IsAttributionEndDateExtend AS ExtendAttributionEndDate
+END
