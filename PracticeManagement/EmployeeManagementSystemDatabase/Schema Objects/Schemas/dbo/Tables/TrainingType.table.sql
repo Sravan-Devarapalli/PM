deaefe8081353a7211b244d6c8d0e7c0ae67ea78
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/EmployeeManagementSystemDatabase/Schema Objects/Schemas/dbo/Tables/TrainingType.table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[TrainingType](
+	[TrainingTypeId] [int] NOT NULL IDENTITY(1,1),
+	[Description] [nvarchar](max) NULL,
+	[DisplayOrder] [tinyint] NULL,
+ CONSTRAINT [PK_TrainingType] PRIMARY KEY CLUSTERED 
+(
+	[TrainingTypeId] ASC
+)
+)
