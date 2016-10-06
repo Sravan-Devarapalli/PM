f4f842e8b99caa7a90946cf490c7e14d3d7af055
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/PK_RecruitingMetricsType.pkey.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE [dbo].[RecruitingMetricsType]
+	ADD CONSTRAINT [PK_RecruitingMetricsType]
+	PRIMARY KEY CLUSTERED (RecruitingMetricsTypeId ASC) WITH (IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF);
