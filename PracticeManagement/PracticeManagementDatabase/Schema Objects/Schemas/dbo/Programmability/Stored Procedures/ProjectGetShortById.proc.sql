d9df68e53ae94bc7fe12a8727aab91ff0810aa4c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectGetShortById.proc.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[ProjectGetShortById]
+(
+	@ProjectId	         INT
+)
+AS
+BEGIN
+
+	SET NOCOUNT ON
+
+		SELECT p.ProjectId,
+			   p.Name,
+			   p.StartDate,
+			   p.EndDate,
+			   p.ProjectStatusId,
+			   p.ProjectStatusName,
+			   p.ProjectNumber,
+			   dbo.GetProjectManagersAliasesList(p.ProjectId) AS ToAddressList
+		  FROM dbo.v_Project AS p
+		  WHERE p.ProjectId = @ProjectId
+
+END
