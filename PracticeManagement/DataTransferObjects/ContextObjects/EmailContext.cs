37b39dbef251f1793674dd983fd80dc0275bc210
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ContextObjects/EmailContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.ContextObjects
+{
+    [DataContract]
+    [Serializable]
+    public class EmailContext
+    {
+        [DataMember]
+        public string StorerProcedureName
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int EmailTemplateId
+        {
+            get;
+            set;
+        }
+    }
+}
+
