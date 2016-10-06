bd7f19adf20cd4ed0a87017c76a361db1c2ef8dc
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetErrorMessage.function.sql
@@ -0,0 +1,17 @@
+﻿CREATE FUNCTION [dbo].[GetErrorMessage]
+(
+	@MessageId	INT
+)
+RETURNS NVARCHAR(2048)
+AS
+BEGIN
+	DECLARE @MessageText NVARCHAR(2048)
+
+	
+	SELECT @MessageText = MessageText 
+	FROM [dbo].[ErrorMessage]
+	WHERE MessageId = @MessageId
+
+	
+	RETURN @MessageText
+END
