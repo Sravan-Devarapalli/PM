46ed15729056cdcf75891f694b1e03c276b9e4b9
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Indexes/aspnet_Paths_index.index.sql
@@ -0,0 +1,4 @@
+﻿CREATE UNIQUE CLUSTERED INDEX [aspnet_Paths_index]
+    ON [dbo].[aspnet_Paths]([ApplicationId] ASC, [LoweredPath] ASC) WITH ( DROP_EXISTING = OFF, IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF, ONLINE = OFF, MAXDOP = 0);
+
+
