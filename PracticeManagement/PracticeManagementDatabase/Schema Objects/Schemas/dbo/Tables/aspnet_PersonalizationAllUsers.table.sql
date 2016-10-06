7b23b576b9f2096b19349ef52fe5f740765f1265
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/aspnet_PersonalizationAllUsers.table.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[aspnet_PersonalizationAllUsers] (
+    [PathId]          UNIQUEIDENTIFIER NOT NULL,
+    [PageSettings]    IMAGE            NOT NULL,
+    [LastUpdatedDate] DATETIME         NOT NULL,
+    PRIMARY KEY CLUSTERED ([PathId] ASC) WITH (IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF),
+    FOREIGN KEY ([PathId]) REFERENCES [dbo].[aspnet_Paths] ([PathId]) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    FOREIGN KEY ([PathId]) REFERENCES [dbo].[aspnet_Paths] ([PathId]) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+
+GO
+EXECUTE sp_tableoption @TableNamePattern = N'[dbo].[aspnet_PersonalizationAllUsers]', @OptionName = N'text in row', @OptionValue = N'6000';
+
+
