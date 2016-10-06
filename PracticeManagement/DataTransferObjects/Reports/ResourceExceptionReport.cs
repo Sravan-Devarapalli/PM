7f779207a8b2bbded44ad164e58ed398511a9622
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Reports/ResourceExceptionReport.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Reports
+{
+    [DataContract]
+    [Serializable]
+    public class ResourceExceptionReport
+    {
+        [DataMember]
+        public Person Person
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public Project Project
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public double ProjectedHours
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public double BillableHours
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public double NonBillableHours
+        {
+            get;
+            set;
+        }
+    }
+}
+
