81e479d440877c629a3e570969da3ef3635764c1
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/PersonCalendarAuto.table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[PersonCalendarAuto] (
+    [Date]     DATETIME NOT NULL,
+    [PersonId] INT      NOT NULL,
+    [DayOff]   BIT      NOT NULL,
+	TimeOffHours REAL		NULL ,
+	CompanyDayOff BIT	NOT	NULL
+);
+
+
