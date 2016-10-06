5dd90a1816a7b553177a8e7fcd7be66d00f3182b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementService/IPersonRoleService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using DataTransferObjects;
+
+namespace PracticeManagementService
+{
+    // NOTE: If you change the interface name "IPersonRoleService" here, you must also update the reference to "IPersonRoleService" in Web.config.
+    [ServiceContract]
+    public interface IPersonRoleService
+    {
+        /// <summary>
+        /// Retrives a list of the avalable role for the person.
+        /// </summary>
+        /// <returns>The list of <see cref="PersonRole"/> objects.</returns>
+        [OperationContract]
+        List<PersonRole> GetPersonRoles();
+    }
+}
