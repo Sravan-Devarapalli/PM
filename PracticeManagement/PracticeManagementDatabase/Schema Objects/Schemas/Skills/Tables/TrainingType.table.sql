53cf43205d0ebe96cc9e01c5a78435864570b9ed
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/TrainingType.table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [Skills].[TrainingType](
+	[TrainingTypeId] [int] NOT NULL IDENTITY(1,1),
+	[Description] [nvarchar](max) NULL,
+	[DisplayOrder] [tinyint] NULL,
+	[IsActive]		[bit] NOT NULL CONSTRAINT DF_TrainingType_IsActive DEFAULT(1),
+	[IsDeleted]		[bit] NOT NULL CONSTRAINT DF_TrainingType_IsDeleted DEFAULT(0),
+ CONSTRAINT [PK_TrainingType] PRIMARY KEY CLUSTERED 
+(
+	[TrainingTypeId] ASC
+)
+)
