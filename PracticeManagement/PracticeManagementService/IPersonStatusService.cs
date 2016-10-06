2ecb5d666a332609d6dcbf29d9a3519fa793b6be
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementService/IPersonStatusService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+using DataTransferObjects;
+
+namespace PracticeManagementService
+{
+    [ServiceContract]
+    public interface IPersonStatusService
+    {
+        /// <summary>
+        /// Retrives the list of the person statuses.
+        /// </summary>
+        /// <returns>The list of the <see cref="PersonStatus"/> objects.</returns>
+        [OperationContract]
+        List<PersonStatus> GetPersonStatuses();
+    }
+}
