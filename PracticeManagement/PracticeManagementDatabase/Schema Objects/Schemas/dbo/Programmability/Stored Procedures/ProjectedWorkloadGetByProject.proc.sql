2e4e5ff472b5848fbd89ab66a778a38ab007c8fa
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectedWorkloadGetByProject.proc.sql
@@ -0,0 +1,25 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 7-10-2008
+-- Updated by:	Anatoliy Lokshin
+-- Update date:	9-09-2008
+-- Description:	Retrives the projected workload for the specified project by the persons
+-- =============================================
+CREATE PROCEDURE [dbo].[ProjectedWorkloadGetByProject]
+(
+	@ProjectId   INT,
+	@StartDate   DATETIME,
+	@EndDate     DATETIME
+)
+AS
+	SET NOCOUNT ON
+	
+	SELECT mh.[MilestoneId],
+	       mh.[PersonId],
+	       mh.[MilestonePersonMonthHours] AS [Hours],
+	       mh.[Month] AS [BilledDate],
+	       mh.EntryStartDate
+	  FROM dbo.v_MilestonePersonMonthHours AS mh
+	 WHERE mh.[ProjectId] = @ProjectId AND mh.[Month] BETWEEN @StartDate AND @EndDate
+	ORDER BY mh.[PersonId], mh.[Month]
+
