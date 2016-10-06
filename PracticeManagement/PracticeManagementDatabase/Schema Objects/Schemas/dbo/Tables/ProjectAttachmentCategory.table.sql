ea4f3a93463df292e394200308e4d17d17d08824
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ProjectAttachmentCategory.table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ProjectAttachmentCategory]
+(
+	Id							INT IDENTITY(1,1) NOT NULL,
+	Name						nvarchar(256)       NULL,
+	CONSTRAINT PK_ProjectAttachmentCategory_Id PRIMARY KEY CLUSTERED(Id)	
+)
+
