7f86f7a231a1fe1131326f16adccc9a307ff47e5
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Skills/PersonEmployer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Skills
+{
+    [DataContract]
+    [Serializable]
+    public class PersonEmployer
+    {
+        [DataMember]
+        public Person Person
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Employer Employer
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime StartDate
+        {
+            set;
+            get;
+        }
+
+        [DataMember]
+        public DateTime EndDate
+        {
+            set;
+            get;
+        }
+
+        [DataMember]
+        public string Title
+        {
+            set;
+            get;
+        }
+
+        [DataMember]
+        public int? DisplayOrder
+        {
+            set;
+            get;
+        }
+
+        [DataMember]
+        public DateTime ModificationDate
+        {
+            set;
+            get;
+        }
+    }
+}
