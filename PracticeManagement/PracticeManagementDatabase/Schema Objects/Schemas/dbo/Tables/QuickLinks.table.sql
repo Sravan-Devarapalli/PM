f20c09fb7aea8f64ec5624dff9e956802074fe2b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/QuickLinks.table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[QuickLinks]
+(
+	Id              INT IDENTITY(1,1) NOT NULL,
+	LinkName	    NVARCHAR (255) NOT NULL, 
+	VirtualPath		NVARCHAR (255) NOT NULL,
+	DashBoardTypeId INT NOT NULL
+	CONSTRAINT PK_QuickLinks_Id PRIMARY KEY CLUSTERED(Id),
+    CONSTRAINT FK_QuickLinks_DashBoardTypeId FOREIGN KEY(DashBoardTypeId) REFERENCES  dbo.DashBoardType(Id)
+); 
