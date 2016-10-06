4a562325af5223301a299d8bafc45608aa901c34
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_MLFOverheadFixedRateTimescale.view.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW [dbo].[v_MLFOverheadFixedRateTimescale]
+AS
+	SELECT	MH.Rate,
+			MH.StartDate,
+			MH.EndDate,
+			MH.TimescaleId
+	FROM dbo.OverheadFixedRate AS o
+	JOIN dbo.MinimumLoadFactorHistory AS MH  ON MH.OverheadFixedRateId = o.OverheadFixedRateId 
+												AND o.IsMinimumLoadFactor = 1 
+												AND o.Inactive = 0 
+												AND MH.Rate > 0 
+												AND o.RateType = 4 --Pay Rate Multiplier
