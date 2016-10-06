56b46fdfc315491524d6ad45b481b419b9b77242
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/WebPublisher/WebPublisher/Bindings/Models/NotifyPropertyChanged.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace WebPublisher.Bindings
+{
+    [Serializable]
+    public abstract class NotifyPropertyChanged : INotifyPropertyChanged
+    {
+        [NonSerialized]
+        private List<PropertyChangedEventHandler> propertyChangedList;
+
+        public event PropertyChangedEventHandler PropertyChanged
+        {
+            add
+            {
+                if (this.propertyChangedList == null)
+                    this.propertyChangedList = new List<PropertyChangedEventHandler>();
+
+                this.propertyChangedList.Add(value);
+            }
+
+            remove
+            {
+                this.propertyChangedList.Remove(value);
+            }
+        }
+
+        protected void Notify(params string[] propertyNames)
+        {
+            if (this.propertyChangedList == null)
+                return;
+
+            foreach (var propertyName in propertyNames)
+            {
+                foreach (var propertyChanged in this.propertyChangedList.ToArray())
+                {
+                    try
+                    {
+                        propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                    }
+
+                    catch (Exception)
+                    {
+                    }
+                }
+            }
+        }
+    }
+}
+
