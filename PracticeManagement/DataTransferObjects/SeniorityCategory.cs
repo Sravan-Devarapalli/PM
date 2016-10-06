bd065faae435293bface056d6d3f99c1b5861dfe
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/SeniorityCategory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [Serializable]
+    [DataContract]
+    public class SeniorityCategory
+    {
+        [DataMember]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public int StartPosition
+        {
+            get;
+            set;
+        }
+
+        public int EndPosition
+        {
+            get;
+            set;
+        }
+    }
+}
