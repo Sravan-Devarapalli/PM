2f8d16d0ee10a5511fed19baf4d337b6fd2e5ad3
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/aspnet_SchemaVersions.table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[aspnet_SchemaVersions] (
+    [Feature]                 NVARCHAR (128) NOT NULL,
+    [CompatibleSchemaVersion] NVARCHAR (128) NOT NULL,
+    [IsCurrentVersion]        BIT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([Feature] ASC, [CompatibleSchemaVersion] ASC) WITH (IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF)
+);
+
+
