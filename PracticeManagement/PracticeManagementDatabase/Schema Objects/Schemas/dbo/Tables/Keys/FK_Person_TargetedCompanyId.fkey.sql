4ef6c8200f9de6fd170d758af30487438301d2f4
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_Person_TargetedCompanyId.fkey.sql
@@ -0,0 +1,6 @@
+﻿ALTER TABLE [dbo].[Person]
+	ADD CONSTRAINT [FK_Person_TargetedCompanyId] 
+	FOREIGN KEY (TargetedCompanyId)
+	REFERENCES [RecruitingMetrics] ([RecruitingMetricsId]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
