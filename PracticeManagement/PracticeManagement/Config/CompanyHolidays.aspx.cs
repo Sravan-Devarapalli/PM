3638a0722f6d0f763c8373f2f995a7c802d948b7
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Config/CompanyHolidays.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PraticeManagement.Controls;
+
+namespace PraticeManagement.Config
+{
+    public partial class CompanyHolidays : PracticeManagementPageBase
+    {
+        public PraticeManagement.Controls.Calendar CalendarControl
+        {
+            get
+            {
+                return calendar;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected override void Display()
+        {
+        }
+    }
+}
