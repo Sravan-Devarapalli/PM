8066d8f752c1b793e6f6779eb4304e719f7cbb1a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/RevenueType.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// Determines the list of revenue types.
+    /// </summary>
+    [DataContract]
+    public enum RevenueType
+    {
+        [EnumMember]
+        Unknown = 0,
+
+        /// <summary>
+        /// New
+        /// </summary>
+        [EnumMember]
+        New = 1,
+
+        /// <summary>
+        /// Old
+        /// </summary>
+        [EnumMember]
+        Old = 2,
+
+        /// <summary>
+        /// Undefined
+        /// </summary>
+        [EnumMember]
+        Undefined = 3
+    }
+}
