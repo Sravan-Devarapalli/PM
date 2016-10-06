47bc8c52be3a566a7ff5b8e199fe8d319ecb8c3d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/PersonBadgeHistories.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class PersonBadgeHistories
+    {
+        [DataMember]
+        public Person Person
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public List<MSBadge> BlockHistory
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public List<MSBadge> OverrideHistory
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public List<MSBadge> BadgeHistory
+        {
+            get;
+            set;
+        }
+    }
+}
+
