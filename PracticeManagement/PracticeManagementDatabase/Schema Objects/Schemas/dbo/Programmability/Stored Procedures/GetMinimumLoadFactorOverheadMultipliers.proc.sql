9c71aadcbe2a0cd9445623700769e1e194235cae
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetMinimumLoadFactorOverheadMultipliers.proc.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[GetMinimumLoadFactorMultipliers]
+	@Description NVARCHAR(225),
+	@Inactive	 BIT  OUTPUT
+AS
+	SELECT MH.TimescaleId, MH.Rate
+	FROM dbo.OverheadFixedRate O
+	JOIN dbo.MinimumLoadFactorHistory AS MH  ON MH.OverheadFixedRateId = o.OverheadFixedRateId AND MH.EndDate IS NULL
+	WHERE O.IsMinimumLoadFactor = 1 AND O.Description = @Description
+
+	SELECT @Inactive = Inactive
+	FROM dbo.OverheadFixedRate WHERE IsMinimumLoadFactor = 1 AND [Description] = @Description
+	IF @Inactive IS NULL
+	SELECT @Inactive = 0
+RETURN 0
