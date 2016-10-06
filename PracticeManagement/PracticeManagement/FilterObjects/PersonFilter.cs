29c31af98dc213b1ad85dc8a5fb78934285c3d37
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/FilterObjects/PersonFilter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace PraticeManagement.FilterObjects
+{
+    [Serializable]
+    public class PersonFilter
+    {
+        public bool ShowActive
+        {
+            set;
+            get;
+        }
+        public bool ShowTerminationPending
+        {
+            set;
+            get;
+        }
+        public bool ShowProjected
+        {
+            set;
+            get;
+        }
+        public bool ShowTerminated
+        {
+            set;
+            get;
+        }
+        public string SelectedPracticeIds
+        {
+            get;
+            set;
+        }
+        public string SelectedPayTypeIds
+        {
+            get;
+            set;
+        }
+        public string SearchText
+        {
+            get;
+            set;
+        }
+        public string SelectedRecruiterIds
+        {
+            get;
+            set;
+        }
+        public int SelectedPageSizeIndex
+        {
+            get;
+            set;
+        }
+        public char? Alphabet
+        {
+            get;
+            set;
+        }
+        public string SortBy
+        {
+            set;
+            get;
+        }
+        public SortDirection SortOrder
+        {
+            set;
+            get;
+        }
+        public int? CurrentIndex
+        {
+            get;
+            set;
+        }
+        public bool DisplaySearchResuts
+        {
+            set;
+            get;
+        }
+    }
+}
