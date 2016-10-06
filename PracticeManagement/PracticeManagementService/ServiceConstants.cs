e8fe32e7270a13211e03f5c2885ad02887ab5b86
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementService/ServiceConstants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Transactions;
+
+namespace PracticeManagementService
+{
+    /// <summary>
+    /// Contains common constants.
+    /// </summary>
+    public static class ServiceConstants
+    {
+        /// <summary>
+        /// Default options for a distributed transaction.
+        /// </summary>
+        public static readonly TransactionOptions DistributedTransactionOptions =
+            new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(600)
+                };
+    }
+}
