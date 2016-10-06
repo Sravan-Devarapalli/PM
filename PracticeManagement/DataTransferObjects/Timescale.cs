45d5776b7c49a5db10401541e81778b250b7d4af
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Timescale.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// Represents a timescale entity
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class Timescale
+    {
+        /// <summary>
+        /// Gets or sets a <see cref="Timescale"/> Id value.
+        /// </summary>
+        [DataMember]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a name of the <see cref="Timescale"/>.
+        /// </summary>
+        [DataMember]
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string TimescaleCode
+        {
+            get;
+            set;
+        }
+    }
+}
+
