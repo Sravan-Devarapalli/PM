58cd6778c53a61bb7edd624ef43ff7f7717f7898
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectReactivate.proc.sql
@@ -0,0 +1,16 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 5-21-1008
+-- Description:	Mark a project active
+-- =============================================
+CREATE PROCEDURE dbo.ProjectReactivate
+(
+	@ProjectId   INT
+)
+AS
+	SET NOCOUNT ON
+
+	UPDATE dbo.Project
+	   SET ProjectStatusId = 1
+	 WHERE ProjectId = @ProjectId
+
