8b7737ffd2b53cc84a702d1d1167f3721cc64e25
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/Constraints/CKPersonEmployer_DisplayOrder.chkconst.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [Skills].[PersonEmployer]  WITH CHECK ADD  CONSTRAINT [CKPersonEmployer_DisplayOrder] CHECK  (([DisplayOrder]>=(1) AND [DisplayOrder]<=(20)))
+GO
+ALTER TABLE [Skills].[PersonEmployer] CHECK CONSTRAINT [CKPersonEmployer_DisplayOrder]
+
