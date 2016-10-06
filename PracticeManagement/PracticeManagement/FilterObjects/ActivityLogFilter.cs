c29a09096c82183527b4c06615dcecc154d77465
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/FilterObjects/ActivityLogFilter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PraticeManagement.FilterObjects
+{
+    [Serializable]
+    public class ActivityLogFilter
+    {
+        public string EventSourceSelected { get; set; }
+        public DateTime? FromDateFilterValue { get; set; }
+        public DateTime? ToDateFilterValue { get; set; }
+        public string PersonSelected { get; set; }
+        public string ProjectSelected { get; set; }
+        public int CurrentIndex { get; set; }
+        public bool FiltersChanged { get; set; }
+        public int PeriodSelected { get; set; }
+    }
+}
