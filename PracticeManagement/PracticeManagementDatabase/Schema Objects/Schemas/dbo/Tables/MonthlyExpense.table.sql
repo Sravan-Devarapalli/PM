0248e225b61755a48479646acd49fa163eada623
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/MonthlyExpense.table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[MonthlyExpense] (
+    [MonthlyExpenseId]  INT             IDENTITY (1, 1) NOT NULL,
+    [ExpenseCategoryId] INT             NOT NULL,
+    [ExpenseBasisId]    INT             NOT NULL,
+    [WeekPaidOptionId]  INT             NOT NULL,
+    [Name]              NVARCHAR (50)   NOT NULL,
+    [Year]              INT             NOT NULL,
+    [Month]             INT             NOT NULL,
+    [Amount]            DECIMAL (18, 3) NOT NULL
+);
+
+
