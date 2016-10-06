34c118e1e0cd894095f3f632764decfb2c76e7d2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OverheadFixedRateUpdate.proc.sql
@@ -0,0 +1,31 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 5-27-2008
+-- Updated by:	Anatoliy Lokshin
+-- Update date:	8-07-2008
+-- Description:	Updates the overhead.
+-- =============================================
+CREATE PROCEDURE [dbo].[OverheadFixedRateUpdate]
+(
+	@OverheadFixedRateId   INT,
+	@Description           NVARCHAR(255),
+	@Rate                  DECIMAL(18,3),
+	@StartDate             DATETIME,
+	@EndDate               DATETIME,
+	@RateType              INT,
+	@Inactive              BIT,
+	@IsCogs                BIT
+)
+AS
+	SET NOCOUNT ON
+
+	UPDATE dbo.OverheadFixedRate
+	   SET Description = @Description,
+	       Rate = @Rate,
+	       StartDate = @StartDate,
+	       EndDate = @EndDate,
+	       RateType = @RateType,
+	       Inactive = @Inactive,
+	       IsCogs = @IsCogs
+	 WHERE OverheadFixedRateId = @OverheadFixedRateId
+
