5c8b505531e24ef50b0122b94439f3a95141b4ca
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ChargeCode.table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[ChargeCode]
+(
+    Id                  INT	IDENTITY (1, 1) NOT NULL,
+    ClientId            INT	NOT NULL,
+    ProjectGroupId      INT	NOT NULL,
+    ProjectId           INT	NOT NULL,
+    PhaseId             INT	NOT NULL,
+    TimeTypeId          INT	NOT NULL,
+	TimeEntrySectionId	INT	NOT NULL,
+	CONSTRAINT PK_ChargeCode_Id PRIMARY KEY CLUSTERED (Id ASC)
+);
+
