aea975615c8db0ac4c8ddac357a27ff2dae425ba
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/RecruiterReactivate.proc.sql
@@ -0,0 +1,16 @@
+﻿-- =============================================
+-- Author:		Skip Sailors
+-- Create date: 5-10-2008
+-- Description:	reactivate a recruiter role
+-- =============================================
+CREATE PROCEDURE [dbo].[RecruiterReactivate]
+	@RecruiterId int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	Update Recruiter
+		set Inactive = 0
+		where RecruiterId = @RecruiterId
+END
+
