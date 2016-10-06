5878ce1403491d3de4d4cb5596899aea5ee5a3b6
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetPracticesForProjectDivision.proc.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[GetPracticesForProjectDivision]
+(
+	@DivisionId	INT
+)
+AS
+BEGIN
+	SELECT 
+		P.PracticeId,
+		P.Name as PracticeAreaName
+	FROM dbo.Practice P
+			INNER JOIN dbo.ProjectDivisionPracticeMapping DP ON DP.PracticeId=p.PracticeId
+	WHERE DP.ProjectDivisionId=@DivisionId and P.IsActive=1
+	ORDER BY P.Name
+END
