6e05a86e71e5a25b5f1959bbb09b072f7f83601c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ListAllSeniorityCategories.proc.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[ListAllSeniorityCategories]
+AS
+BEGIN
+	SELECT  SC.SeniorityCategoryId,
+			SC.Name AS SeniorityCategory
+	FROM dbo.SeniorityCategory SC 
+	ORDER BY SC.SeniorityCategoryId
+
+END
