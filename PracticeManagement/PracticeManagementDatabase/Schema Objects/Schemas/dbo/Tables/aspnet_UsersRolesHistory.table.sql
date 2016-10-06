b7f9a58503667921d8a4ff5796ee9ef92f5bee31
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/aspnet_UsersRolesHistory.table.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[aspnet_UsersRolesHistory](
+	[UserId] [uniqueidentifier] NOT NULL,
+	[RoleId] [uniqueidentifier] NOT NULL,
+	[StartDate]	DATETIME NOT NULL,
+	[EndDate]	DATETIME
+	CONSTRAINT PK_UsersRolesHistory PRIMARY KEY CLUSTERED
+	(
+		[UserId] ASC,
+		[RoleId] ASC,
+		[StartDate]	ASC
+	),
+	CONSTRAINT FK_UsersRolesHistory_UserId FOREIGN KEY ([UserId])
+	REFERENCES [dbo].[aspnet_Users] ([UserId]),
+	CONSTRAINT FK_UsersRolesHistory_RoleId FOREIGN KEY (RoleId)
+	REFERENCES dbo.aspnet_Roles(RoleId)
+)
+
