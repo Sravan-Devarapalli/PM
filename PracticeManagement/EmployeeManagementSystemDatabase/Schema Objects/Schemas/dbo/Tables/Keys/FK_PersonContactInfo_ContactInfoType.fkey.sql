88f374eed7f7732fba1bef73579f25c77cbb8710
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/EmployeeManagementSystemDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_PersonContactInfo_ContactInfoType.fkey.sql
@@ -0,0 +1,5 @@
+﻿ALTER TABLE [dbo].[PersonContactInfo]  WITH CHECK ADD  CONSTRAINT [FK_PersonContactInfo_ContactInfoType] FOREIGN KEY([ContactInfoTypeId])
+REFERENCES [dbo].[ContactInfoType] ([ContactInfoTypeId])
+ON UPDATE CASCADE
+GO
+ALTER TABLE [dbo].[PersonContactInfo] CHECK CONSTRAINT [FK_PersonContactInfo_ContactInfoType]
