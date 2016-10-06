13ac7aa8fac1621244ae0b7ec7e022ca6b57b1af
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ClientGetById.proc.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE dbo.ClientGetById
+(
+	@ClientId	INT
+)
+AS
+	SET NOCOUNT ON;
+
+	SELECT c.ClientId,
+	       c.DefaultDiscount,
+	       c.DefaultTerms,
+	       c.DefaultSalespersonID,
+		   c.DefaultDirectorID,
+	       c.Name,
+	       c.Inactive,
+	       c.IsChargeable,
+		   c.IsMarginColorInfoEnabled,
+		   c.IsInternal,
+		   c.Code AS ClientCode,
+		   c.IsNoteRequired,
+		   c.IsHouseAccount
+	  FROM dbo.Client AS c
+	 WHERE c.ClientId = @ClientId
+
+GO
+
