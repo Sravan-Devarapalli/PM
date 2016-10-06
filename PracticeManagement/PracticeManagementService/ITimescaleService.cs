d2bddf3789b6eae5821085a15552bd8b48d05225
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementService/ITimescaleService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using DataTransferObjects;
+
+namespace PracticeManagementService
+{
+    [ServiceContract]
+    [ServiceKnownType(typeof(Timescale))]
+    public interface ITimescaleService
+    {
+        /// <summary>
+        /// Retrives a <see cref="Timescale"/> by its ID.
+        /// </summary>
+        /// <param name="timescaleId">An ID of the <see cref="Timescale"/> to be retrieved.</param>
+        /// <returns>A <see cref="Timescale"/> object if found and null otherwise.</returns>
+        [OperationContract]
+        Timescale GetById(TimescaleType timescaleId);
+
+        /// <summary>
+        /// Retrives all the timescale types.
+        /// </summary>
+        /// <returns></returns>
+        [OperationContract]
+        List<Timescale> GetAll();
+    }
+}
