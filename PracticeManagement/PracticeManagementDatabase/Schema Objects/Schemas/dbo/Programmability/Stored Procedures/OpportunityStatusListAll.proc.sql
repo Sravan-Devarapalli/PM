8b1cc9634efbe9ea230fec8699bf6ed07543fef9
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OpportunityStatusListAll.proc.sql
@@ -0,0 +1,15 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 11-04-2008
+-- Updated by:	
+-- Update date: 
+-- Description:	Retrives a list of opportunity statuses.
+-- =============================================
+CREATE PROCEDURE [dbo].[OpportunityStatusListAll]
+AS
+	SET NOCOUNT ON
+
+	SELECT s.OpportunityStatusId, s.Name
+	  FROM dbo.OpportunityStatus AS s
+	ORDER BY s.OpportunityStatusId
+
