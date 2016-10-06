d66d49057ad57752fddd5ab3f0fecf6da39c7eb1
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OpportunityTransitionStatusListAll.proc.sql
@@ -0,0 +1,16 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 11-04-2008
+-- Updated by:	Anatoliy Lokshin
+-- Update date: 12-03-2008
+-- Description:	Retrives a list of opportunity transition statuses.
+-- =============================================
+CREATE PROCEDURE [dbo].[OpportunityTransitionStatusListAll]
+AS
+	SET NOCOUNT ON
+
+	SELECT s.OpportunityTransitionStatusId, s.Name
+	  FROM dbo.OpportunityTransitionStatus AS s
+	 WHERE OpportunityTransitionStatusId NOT IN (1, 3, 4, 5, 6)
+	ORDER BY s.OpportunityTransitionStatusId
+
