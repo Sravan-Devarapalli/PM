a6c12fd0851088f8cefe6619d94963de026774eb
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Filters/VendorFilters.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTransferObjects.Filters
+{
+    [Serializable]
+    public class VendorFilters
+    {
+        public bool ShowActive
+        {
+            get;
+            set;
+        }
+
+        public bool ShowInactive
+        {
+            get;
+            set;
+        }
+
+        public string VendorTypeIds
+        {
+            get;
+            set;
+        }
+
+        public string SearchText
+        {
+            get;
+            set;
+        }
+    }
+}
+
