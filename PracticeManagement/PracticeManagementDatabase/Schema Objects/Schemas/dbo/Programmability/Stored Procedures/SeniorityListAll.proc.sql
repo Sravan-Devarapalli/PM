1c1c6e70c776ac226385f6dd6077adbd814cae59
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SeniorityListAll.proc.sql
@@ -0,0 +1,20 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 9-09-2008
+-- Updated by:	
+-- Update date: 
+-- Description:	Selects a list of the seniorities
+-- =============================================
+CREATE PROCEDURE [dbo].[SeniorityListAll]
+AS
+	SET NOCOUNT ON
+
+	SELECT S.SeniorityId,
+			S.Name AS Seniority,
+			S.SeniorityCategoryId,
+			SC.Name AS SeniorityCategory,
+			S.SeniorityValue
+	FROM dbo.Seniority AS S
+	INNER JOIN dbo.SeniorityCategory SC ON S.SeniorityCategoryId = SC.SeniorityCategoryId
+	ORDER BY S.SeniorityValue,S.Name
+
