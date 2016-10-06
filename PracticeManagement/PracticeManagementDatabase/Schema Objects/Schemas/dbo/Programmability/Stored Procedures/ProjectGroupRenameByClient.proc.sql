f56a8d25c8141778b0f9525555c94e7b36814b0c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectGroupRenameByClient.proc.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE ProjectGroupRenameByClient
+	@ClientId		INT,
+	@GroupId    INT ,
+	@GroupName	NVARCHAR(100),
+	@IsActive   BIT 
+AS
+	SET NOCOUNT ON;
+		UPDATE ProjectGroup
+		SET Name = @GroupName , Active = @IsActive
+		WHERE ClientId = @ClientId AND GroupId = @GroupId 
+
+	SELECT 0 Result
+
