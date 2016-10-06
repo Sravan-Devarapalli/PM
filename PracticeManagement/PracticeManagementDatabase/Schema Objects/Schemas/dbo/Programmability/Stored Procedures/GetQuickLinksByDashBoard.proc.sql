d0def067262693fc95682718ffa907e1ff92e79c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetQuickLinksByDashBoard.proc.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[GetQuickLinksByDashBoard]
+(
+ @dashBoardType INT 
+)
+AS
+BEGIN
+
+	SELECT Q.DashBoardTypeId,
+		   Q.Id,
+		   Q.LinkName,
+		   Q.VirtualPath
+	FROM QuickLinks AS Q 
+	WHERE  Q.DashBoardTypeId = @dashBoardType
+
+END
