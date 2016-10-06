461066761010ff343bef53d84825abc509f79e34
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/VendorAttachment.table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[VendorAttachment]
+(
+	Id							INT IDENTITY(1,1) NOT NULL,
+	VendorId					INT				  NOT NULL,
+	[FileName]					[nvarchar](256)       NULL,	
+	[AttachmentData]			[varbinary](max)      NULL,
+	UploadedDate				DATETIME			  NULL,
+	ModifiedBy					INT					  NULL,
+   CONSTRAINT PK_VendorAttachment_Id PRIMARY KEY CLUSTERED(Id),
+);
