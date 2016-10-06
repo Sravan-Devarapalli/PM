979dc6465042abf5f863241a5d2ad6f4557cc606
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetClientsForClientDirector.proc.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[GetClientsForClientDirector]
+(
+	@DirectorId	INT=NULL
+)
+AS
+BEGIN
+	IF @DirectorId IS NOT NULL
+	BEGIN
+		SELECT	DISTINCT C.ClientId,
+				C.Code AS ClientCode,
+				C.Name AS ClientName
+		FROM dbo.Client C
+		INNER JOIN dbo.Project P ON P.ClientId = C.ClientId
+		WHERE P.ProjectStatusId IN (3,4) --Active and Completed status 
+		ORDER BY C.Name
+	END
+	ELSE
+	BEGIN
+		SELECT	ClientId,
+				C.Code AS ClientCode,
+				C.Name AS ClientName
+		FROM dbo.Client C 
+		ORDER BY C.Name
+	END
+END
