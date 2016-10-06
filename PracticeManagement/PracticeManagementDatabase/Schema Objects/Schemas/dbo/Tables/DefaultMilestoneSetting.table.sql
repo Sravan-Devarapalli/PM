b224de93c51e09eecdbcb4e418b2311d3fb37f21
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/DefaultMilestoneSetting.table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[DefaultMilestoneSetting]
+(
+	ClientId		INT,
+	ProjectId		INT,
+	MilestoneId		INT,
+	ModifiedDate	DATETIME,
+	DefaultMilestoneSettingId INT IDENTITY(1,1),
+	LowerBound		INT,
+	UpperBound		INT
+)
+
