35d4c5e48f5e74dbad75f0c88d56001d674d6880
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Indexes/aspnet_Users_Index2.index.sql
@@ -0,0 +1,5 @@
+﻿CREATE NONCLUSTERED INDEX [aspnet_Users_Index2]
+    ON [dbo].[aspnet_Users]([ApplicationId] ASC, [LastActivityDate] ASC) WITH ( DROP_EXISTING = OFF, IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF, ONLINE = OFF, MAXDOP = 0)
+    ON [PRIMARY];
+
+
