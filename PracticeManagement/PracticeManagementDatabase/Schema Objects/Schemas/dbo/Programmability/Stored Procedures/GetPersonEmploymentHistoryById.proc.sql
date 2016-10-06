b4b4f24c184d66ae4928cf8e0fd3826e88e69b77
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetPersonEmploymentHistoryById.proc.sql
@@ -0,0 +1,30 @@
+﻿CREATE PROCEDURE [dbo].[GetPersonEmploymentHistoryById]
+(
+	@PersonId INT
+)
+AS
+BEGIN
+	
+	SELECT PH.PersonId,
+			PH.HireDate,
+			PH.TerminationDate,
+			PH.TerminationReasonId
+	FROM v_PersonHistory PH
+	WHERE PH.PersonId = @PersonId
+
+	--;WITH History AS
+	--(
+	--	SELECT P.*, ROW_NUMBER() OVER(ORDER BY P.Id) AS RowIndex
+	--	FROM dbo.PersonHistory P
+	--	WHERE P.PersonId = @PersonId
+	--)
+
+	--SELECT H.PersonId,
+	--		H.HireDate,
+	--		H.TerminationDate,
+	--		H.TerminationReasonId
+	--FROM History H
+	--LEFT JOIN History H2 ON H.RowIndex + 1 = H2.RowIndex
+	--WHERE H2.PersonId IS NULL OR (H.PersonStatusId = 2 AND H.TerminationDate < H2.HireDate)
+
+END
