71342fdbd44f124173587f271bd57e1310add7e2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonSetDefaultManager.proc.sql
@@ -0,0 +1,20 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2010-01-21
+-- Description:	Sets default manager
+-- =============================================
+CREATE PROCEDURE dbo.PersonSetDefaultManager
+	@DefaultManagerId INT 
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	UPDATE dbo.Person
+	SET IsDefaultManager = 0
+	WHERE IsDefaultManager = 1
+		
+	UPDATE dbo.Person
+	SET IsDefaultManager = 1
+	WHERE PersonId = @DefaultManagerId
+END
+
