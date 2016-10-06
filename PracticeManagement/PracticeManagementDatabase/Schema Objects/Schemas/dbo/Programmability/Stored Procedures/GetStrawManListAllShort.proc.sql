7f0b5281f156d57d97c2e9b2b09ec07cd1c88c15
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetStrawManListAllShort.proc.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[GetStrawManListAllShort]
+(
+	@IncludeInactive BIT = 1
+)
+AS
+BEGIN
+	SELECT	P.PersonId,
+			P.LastName,
+			P.FirstName
+	FROM dbo.Person P
+	WHERE P.IsStrawman = 1 
+	AND (
+		@IncludeInactive = 1 
+		OR P.PersonStatusId = 1  --active status
+		)
+	ORDER BY P.LastName,P.FirstName
+END
