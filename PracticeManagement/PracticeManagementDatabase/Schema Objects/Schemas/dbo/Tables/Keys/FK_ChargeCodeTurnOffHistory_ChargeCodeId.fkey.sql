514bc8bec41abe8dd6ce856a0fbb4a109aa90f84
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_ChargeCodeTurnOffHistory_ChargeCodeId.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[ChargeCodeTurnOffHistory]
+	ADD CONSTRAINT [FK_ChargeCodeTurnOffHistory_ChargeCodeId] 
+	FOREIGN KEY (ChargeCodeId) 
+	REFERENCES dbo.ChargeCode (Id) ON DELETE NO ACTION ON UPDATE NO ACTION;
