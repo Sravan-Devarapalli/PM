9009cf3e449c0a2f4f5e59ab8601b851a724d8b5
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/Practice.table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [Skills].[Practice](
+	[PracticeId] [int] NOT NULL IDENTITY(1,1),
+	[Description] [nvarchar](max) NULL,
+	[DisplayOrder] [tinyint] NULL,
+	[TenantId]		[int] NOT NULL,
+ CONSTRAINT [PK_Practice] PRIMARY KEY CLUSTERED 
+(
+	[TenantId] ASC,
+	[PracticeId] ASC
+)
+)
