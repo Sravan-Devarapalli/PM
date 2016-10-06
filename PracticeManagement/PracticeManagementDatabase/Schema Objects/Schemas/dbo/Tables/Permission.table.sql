7446fa5bd7726f25e602b91cb6b1509b253c5920
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Permission.table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Permission] (
+    [PermissionId] INT      IDENTITY (1, 1) NOT NULL,
+    [PersonId]     INT      NOT NULL,
+    [TargetType]   SMALLINT NOT NULL,
+    [TargetId]     INT      NULL
+);
+
+
