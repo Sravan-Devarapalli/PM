824ba17544602a4f7bd0eeb74d65cf6b6be23b99
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/Title.table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [Skills].[Title](
+	[TitleId] INT NOT NULL IDENTITY(1,1),
+	[Description] [nvarchar](max) NULL,
+	[DisplayOrder] [tinyint] NULL,
+	[TenantId]		[int] NOT NULL,
+ CONSTRAINT [PK_Title] PRIMARY KEY CLUSTERED 
+(
+	[TenantId] ASC,
+	[TitleId] ASC
+)
+)
