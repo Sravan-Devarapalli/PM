95b671d83d848010d6a46788ea13d76177999744
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Utils/ProjectHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using DataTransferObjects;
+
+namespace PraticeManagement.Utils
+{
+    public static class ProjectHelper
+    {
+        private static readonly IDictionary<ProjectStatusType, string> _projectStatuses
+           = new Dictionary<ProjectStatusType, string>
+                 {
+                    {ProjectStatusType.Projected, "ProjectedProject"},
+                    {ProjectStatusType.Experimental, "ExperimentalProject"},
+                    {ProjectStatusType.Completed, "CompletedProject"},
+                    {ProjectStatusType.Active, "pr-indicator"},
+                    {ProjectStatusType.Internal, "pr-internal"},
+                    {ProjectStatusType.Inactive, "InactiveProject"},
+                    {ProjectStatusType.Proposed,"ProposedProject"}
+                };
+
+        public static string GetIndicatorClassByStatusId(int statusId)
+        {
+            return !_projectStatuses.ContainsKey((ProjectStatusType)statusId) ? null : _projectStatuses[(ProjectStatusType)statusId];
+        }
+    }
+}
