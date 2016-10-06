21e0dd7eb6516bbdef607da4012d0b4ea6ef35e6
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/Keys/FK_PersonDocument_DocumentTypeId.fkey.sql
@@ -0,0 +1,5 @@
+﻿ALTER TABLE [Skills].[PersonDocument]  WITH CHECK ADD  CONSTRAINT [FK_PersonDocument_DocumentTypeId] FOREIGN KEY([DocumentTypeId])
+REFERENCES [Skills].[DocumentType] ([DocumentTypeId])
+ON UPDATE CASCADE
+GO
+ALTER TABLE [Skills].[PersonDocument] CHECK CONSTRAINT [FK_PersonDocument_DocumentTypeId]
