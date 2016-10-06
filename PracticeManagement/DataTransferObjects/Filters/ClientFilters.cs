29817a36221d944ff6cff42dd208bdc344eca44d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Filters/ClientFilters.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTransferObjects.Filters
+{
+    [Serializable]
+    public class ClientFilters
+    {
+        public string SearchText
+        {
+            get;
+            set;
+        }
+
+        public string View
+        {
+            get;
+            set;
+        }
+
+        public bool ShowActiveOnly
+        {
+            get;
+            set;
+        }
+    }
+}
+
