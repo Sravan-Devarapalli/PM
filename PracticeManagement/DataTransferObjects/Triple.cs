7536a0964259f19d3d22ebc0b0ff52092783bac0
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Triple.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// Provides a basic utility class that is used to store 3 typed objects
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class Triple<A, B, C>
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the First object.
+        /// </summary>
+        [DataMember]
+        public A First
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the Second object.
+        /// </summary>
+        [DataMember]
+        public B Second
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the Third object.
+        /// </summary>
+        [DataMember]
+        public C Third
+        {
+            get;
+            set;
+        }
+
+        #endregion Properties
+
+        /// <summary>
+        /// Init constructor of Triple.
+        /// </summary>
+        public Triple(A first, B second, C third)
+        {
+            First = first;
+            Second = second;
+            Third = third;
+        }
+    }
+}
