86a316f29cc041704bf46afc2c333c13e61ffeea
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/EmployeeManagementSystemDatabase/Schema Objects/Schemas/dbo/Tables/PersonTraining.table.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[PersonTraining](
+	[PersonId] [bigint] NOT NULL,
+	[TrainingTypeId] [int] NOT NULL,
+	[Info] [nvarchar](max) NOT NULL,
+	[Institution] [nvarchar](max) NOT NULL,
+	[StartDate] [datetime] NULL,
+	[EndDate] [datetime] NOT NULL,
+	[DisplayOrder] [tinyint] NULL,
+	[ModificationDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_PersonTraining] PRIMARY KEY CLUSTERED 
+(
+	[PersonId] ASC,
+	[TrainingTypeId] ASC
+)
+)
