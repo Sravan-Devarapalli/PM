07d1fdfdf7f0c1719a0b78cb9f287c16075eebf3
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Scripts/ExpandOrCollapse.chirp.js
@@ -0,0 +1,55 @@
+﻿function CollapseOrExpandAll(btnExpandOrCollapseAllClientId, hdnCollapsedClientId, hdncpeExtendersIds) {
+    var btn = btnExpandOrCollapseAllClientId;
+    var hdnCollapsed = hdnCollapsedClientId;
+    var isExpand = false;
+    if (btn != null) {
+        if (btn.value == "Expand All") {
+            isExpand = true;
+            btn.value = "Collapse All";
+            btn.title = "Collapse All";
+            hdnCollapsed.value = 'false';
+        }
+        else {
+            btn.value = "Expand All";
+            btn.title = "Expand All";
+            hdnCollapsed.value = 'true';
+        }
+
+        var projectPanelskvPair = jQuery.parseJSON(hdncpeExtendersIds.value);
+        ExpandOrCollapsePanels(projectPanelskvPair, isExpand);
+    }
+    return false;
+}
+
+
+
+
+function ExpandOrcollapseExtender(cpe, isExpand) {
+    if (cpe != null) {
+        if (isExpand) {
+            ExpandPanel(cpe)
+        }
+        else {
+            var isCollapsed = cpe.get_Collapsed();
+            if (!isCollapsed)
+                cpe._doClose();
+        }
+    }
+}
+
+
+function ExpandPanel(cpe) {
+    var isCollapsed = cpe.get_Collapsed();
+    if (isCollapsed) {
+        cpe.expandPanel();
+    }
+}
+
+
+function ExpandOrCollapsePanels(ids, isExpand) {
+    for (var i = 0; i < ids.length; i++) {
+        var cpe = $find(ids[i]);
+        ExpandOrcollapseExtender(cpe, isExpand);
+    }
+}
+
