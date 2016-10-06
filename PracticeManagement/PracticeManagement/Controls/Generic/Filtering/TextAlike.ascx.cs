728cad0df831d0fd980a8dec194574992854dd8b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/Generic/Filtering/TextAlike.ascx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PraticeManagement.Controls.Generic.Filtering
+{
+    public partial class TextAlike : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
