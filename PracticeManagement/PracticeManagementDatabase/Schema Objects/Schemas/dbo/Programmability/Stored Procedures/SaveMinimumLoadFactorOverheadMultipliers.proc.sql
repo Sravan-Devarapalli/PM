d3b88e7cb5c8a193048feb76dc9a56b5609a4e80
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SaveMinimumLoadFactorOverheadMultipliers.proc.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [dbo].[SaveMinimumLoadFactorOverheadMultipliers](
+	@Description NVARCHAR(255),
+	@Inactive BIT,
+	@W2HourlyMultiplier	DECIMAL,
+	@W2SalaryMultiplier	DECIMAL,
+	@Hourly1099Multiplier DECIMAL)
+AS
+	--DECLARE @OverheadFixedRateId INT 
+	--SELECT @OverheadFixedRateId = OverheadFixedRateId
+	--FROM dbo.OverheadFixedRate 
+	--WHERE [Description] = @Description
+
+	--UPDATE dbo.OverheadFixedRate 
+	--SET Inactive = @Inactive
+	--WHERE OverheadFixedRateId = @OverheadFixedRateId 
+
+	--UPDATE dbo.OverheadFixedRateTimescale
+	--SET Rate =  CASE TimescaleId WHEN 1 THEN @W2HourlyMultiplier --W2-Hourly
+	--							 WHEN 2 THEN @W2SalaryMultiplier --W2-Salary
+	--							 ELSE @Hourly1099Multiplier END  --1099 Hourly
+	--WHERE OverheadFixedRateId = @OverheadFixedRateId
+
+RETURN 0
+
