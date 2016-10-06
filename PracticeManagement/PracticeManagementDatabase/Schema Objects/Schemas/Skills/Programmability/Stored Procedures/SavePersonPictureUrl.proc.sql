e6560532810f91960c754faf704ba0336238a469
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Programmability/Stored Procedures/SavePersonPictureUrl.proc.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [Skills].[SavePersonPictureUrl]
+(
+	@PersonId         INT,		
+	@PictureUrl		  NVARCHAR (MAX) = NULL,
+	@UserLogin		  NVARCHAR (100)      
+)
+AS
+BEGIN
+	-- Start logging session
+	EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
+	DECLARE @Now DATETIME
+	SET @Now = dbo.InsertingTime()
+	SET @PictureUrl = RTRIM(LTRIM(@PictureUrl))
+
+	UPDATE dbo.Person
+	SET PictureUrl = @PictureUrl,
+		PictureModifiedDate = @Now
+	WHERE PersonId = @PersonId 
+		AND ISNULL(PictureUrl,0) <> ISNULL(@PictureUrl,0)
+
+	EXEC dbo.SessionLogUnprepare
+END
+
