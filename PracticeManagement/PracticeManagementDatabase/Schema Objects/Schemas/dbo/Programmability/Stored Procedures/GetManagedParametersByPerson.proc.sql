4647488b9657609a2ae8d5756944450436ce9e03
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetManagedParametersByPerson.proc.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[GetManagedParametersByPerson]
+(
+	@UserLogin			  NVARCHAR(255)
+)
+AS
+BEGIN
+
+	DECLARE @PersonId INT
+	SELECT @PersonId = PersonId FROM dbo.Person WHERE Alias = @UserLogin
+
+	SELECT ActualRevenuePerHour,
+		   TargetRevenuePerHour,
+		   HoursUtilization,
+		   TargetRevenuePerAnnum 
+	FROM dbo.ManagedParametersByPerson
+	WHERE  PersonId=@PersonId
+
+END
