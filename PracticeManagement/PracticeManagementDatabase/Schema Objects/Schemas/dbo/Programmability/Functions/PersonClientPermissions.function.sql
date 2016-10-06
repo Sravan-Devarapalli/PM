8f1c9490262b8b2342f0204d92df262f72509d5b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/PersonClientPermissions.function.sql
@@ -0,0 +1,33 @@
+﻿CREATE FUNCTION dbo.PersonClientPermissions
+(
+	@PersonId	INT
+)
+RETURNS 
+@Result table
+(
+	TargetId int
+)
+AS
+BEGIN
+	-- Insert permissions from permissions table
+	insert into @result
+		select p.TargetId 
+		from v_Permissions as p 
+		where p.PermissionTypeId = 1 -- Client
+				and p.PersonId = @PersonId
+
+	-- Select top record from result set
+	delete 
+	from @result
+	where TargetId IS NULL
+
+	-- If it's null, then person has all permissions
+	IF (@@RowCount > 0)
+		-- Insert all targets
+		insert into @result
+			select c.ClientId
+			from dbo.Client as c
+
+	RETURN
+END
+
