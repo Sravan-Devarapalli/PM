21aa0fd9d5eafe6c8320ee559b9125c8da456c3b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetPersonTitleByRange.proc.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[GetPersonTitleByRange]
+	(
+	@PersonId	INT,
+	@StartDate	DATETIME,
+	@EndDate	DATETIME
+	)
+AS
+	BEGIN
+	SELECT	TitleId,
+			Title 
+	FROM	dbo.GetLatestPayInTheGivenRangeTable(@PersonId,@StartDate,@EndDate)
+	END
