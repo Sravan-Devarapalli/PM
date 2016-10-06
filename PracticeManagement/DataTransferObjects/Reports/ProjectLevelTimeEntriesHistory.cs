0de79d1d286e39c48a8bd5684fa7899bcfe4cca3
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Reports/ProjectLevelTimeEntriesHistory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Reports
+{
+    [DataContract]
+    [Serializable]
+    public class ProjectLevelTimeEntriesHistory
+    {
+        [DataMember]
+        public Project Project
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public List<PersonLevelTimeEntriesHistory> PersonLevelTimeEntries
+        {
+            get;
+            set;
+        }
+
+        public double BillableNetChange
+        {
+            get
+            {
+                return PersonLevelTimeEntries.Sum(t => t.BillableNetChange);
+            }
+        }
+
+        public double NonBillableNetChange
+        {
+            get
+            {
+                return PersonLevelTimeEntries.Sum(t => t.NonBillableNetChange);
+            }
+        }
+
+        public double NetChange
+        {
+            get
+            {
+                return PersonLevelTimeEntries.Sum(t => t.NetChange);
+            }
+        }
+    }
+}
