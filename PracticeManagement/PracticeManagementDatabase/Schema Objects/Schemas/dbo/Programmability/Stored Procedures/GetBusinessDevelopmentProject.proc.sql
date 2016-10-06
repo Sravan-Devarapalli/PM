14a68d755677586ccb4df6233168eeb8dae9a3db
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetBusinessDevelopmentProject.proc.sql
@@ -0,0 +1,16 @@
+﻿-- =============================================
+-- Description:	Get BusinessDevelopmentProject.
+-- Updated by:	ThulasiRam.P
+-- Update date:	04-12-2012
+-- =============================================
+CREATE PROCEDURE [dbo].[GetBusinessDevelopmentProject]
+AS
+BEGIN
+	SELECT  P.ProjectId,
+			P.Name,
+			P.ProjectNumber,
+			P.IsNoteRequired
+	FROM [dbo].[Project] AS P
+	WHERE (ProjectNumber IN ('P999918') OR P.IsBusinessDevelopment = 1)
+END
+
