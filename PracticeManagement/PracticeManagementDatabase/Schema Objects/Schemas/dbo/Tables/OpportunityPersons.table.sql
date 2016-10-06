3687fcef3baff30ae16121aff21f63039d824ebe
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/OpportunityPersons.table.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[OpportunityPersons]
+(
+	Id              INT IDENTITY(1,1) NOT NULL,
+	OpportunityId	INT NOT NULL, 
+	PersonId		INT NOT NULL,
+	OpportunityPersonTypeId INT NOT NULL,
+	RelationTypeId	INT NOT NULL,
+	Quantity		INT NULL,
+	NeedBy			DATETIME NULL,
+	CONSTRAINT PK_OpportunityPersons_Id PRIMARY KEY CLUSTERED(Id),
+    CONSTRAINT FK_OpportunityPersons_OpportunityId FOREIGN KEY(OpportunityId) REFERENCES  dbo.Opportunity(OpportunityId),
+	CONSTRAINT FK_OpportunityPersons_OpportunityPersonTypeId FOREIGN KEY(OpportunityPersonTypeId) REFERENCES  dbo.OpportunityPersonType([Id]),
+	CONSTRAINT [FK_OpportunityPersons_RelationType]  FOREIGN KEY (RelationTypeId) REFERENCES dbo.OpportunityPersonRelationType(Id)
+); 
