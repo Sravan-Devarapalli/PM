dcac167e6dd55e3b2c4dadcdd12ca1c89fff3328
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Indexes/UN_DefaultMilestoneSetting.sql
@@ -0,0 +1,9 @@
+﻿CREATE UNIQUE CLUSTERED INDEX [UN_DefaultMilestoneSetting]
+ON [dbo].[DefaultMilestoneSetting](
+									[ClientId] ASC,
+									[ProjectId] ASC, 
+									[MilestoneId] ASC,
+									[ModifiedDate] ASC) WITH 
+	( DROP_EXISTING = OFF,
+	 IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF, ONLINE = OFF, MAXDOP = 0)
+    ON [PRIMARY];
