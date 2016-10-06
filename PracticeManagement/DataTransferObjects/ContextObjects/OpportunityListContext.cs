e1ce7fb4e5fad9e3908d11fa1efae357cceaf841
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ContextObjects/OpportunityListContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.ContextObjects
+{
+    [DataContract]
+    [Serializable]
+    public class OpportunityListContext
+    {
+        public OpportunityListContext()
+        {
+            ActiveClientsOnly = true;
+        }
+
+        [DataMember]
+        public bool ActiveClientsOnly { get; set; }
+
+        [DataMember]
+        public string SearchText { get; set; }
+
+        [DataMember]
+        public int? ClientId { get; set; }
+
+        [DataMember]
+        public int? SalespersonId { get; set; }
+
+        [DataMember]
+        public int? TargetPersonId { get; set; }
+
+        [DataMember]
+        public int? CurrentId { get; set; }
+
+        [DataMember]
+        public bool IsDiscussionReview2 { get; set; }
+    }
+}
