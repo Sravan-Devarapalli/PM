ff72d0dfb028f8f3ab43f455a4ed177af80a76c9
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/PersonDivision.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class PersonDivision
+    {
+        [DataMember]
+        public int DivisionId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string DivisionName
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public bool Inactive
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public bool ShowCareerManagerLink
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Person DivisionOwner
+        {
+            get;
+            set;
+        }
+
+        public int DivisionOwnerId
+        {
+            get { return DivisionOwner != null && DivisionOwner.Id.HasValue ? DivisionOwner.Id.Value : -1; }
+        
+        }
+
+        public string HtmlEncodedName
+        {
+            get
+            {
+                return HttpUtility.HtmlEncode(DivisionName);
+            }
+        }
+    }
+}
+
