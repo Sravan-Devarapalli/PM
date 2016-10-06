4701cd0ca5f6d5dd90171a3b0a9e6e65322f32ef
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Config/SiteMap.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PraticeManagement.Controls;
+
+namespace PraticeManagement.Config
+{
+    public partial class SiteMap : PracticeManagementPageBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected override void Display()
+        {
+        }
+
+        protected void menu_OnMenuItemDataBound(object sender, MenuEventArgs e)
+        {
+            var item = e.Item;
+            if (item.NavigateUrl.Contains("Temp.aspx"))
+            {
+                item.NavigateUrl = string.Empty;
+            }
+
+            item.ToolTip = item.Text;
+        }
+    }
+}
