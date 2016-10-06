093a817a0c3253d3f3b1294a072b8372e13f01f5
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/PK_MilestonePerson.pkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[MilestonePerson]
+    ADD CONSTRAINT [PK_MilestonePerson] PRIMARY KEY CLUSTERED ([MilestonePersonId] ASC) WITH (IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF);
+
+
