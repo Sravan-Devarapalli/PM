6b5451d6af87a0c47624f2d5248323c97dc1fa90
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_OverheadFixedRateTimescale.view.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW [dbo].[v_OverheadFixedRateTimescale]
+AS
+	SELECT	ofr.Rate,
+			ofr.StartDate,
+			ofr.EndDate,
+			ofr.RateType AS OverheadRateTypeId,
+			ortt.TimescaleId
+	FROM dbo.OverheadFixedRate AS ofr
+		INNER JOIN dbo.OverheadFixedRateTimescale AS ortt ON ortt.OverheadFixedRateId = ofr.OverheadFixedRateId AND ofr.IsMinimumLoadFactor = 0 AND ofr.Inactive = 0
+
