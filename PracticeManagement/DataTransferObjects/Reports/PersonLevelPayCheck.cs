3c7e18f0dec40bf85b64a3f9c78d1849430253bb
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Reports/PersonLevelPayCheck.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Reports
+{
+    [DataContract]
+    [Serializable]
+    public class PersonLevelPayCheck
+    {
+        [DataMember]
+        public Person Person
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int BranchID
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int DeptID
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public double TotalHoursExcludingTimeOff
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Dictionary<string, double> WorkTypeLevelTimeOffHours
+        {
+            get;
+            set;
+        }
+
+        public double TotalHoursIncludingTimeOff
+        {
+            get
+            {
+                double timeOffTotal = WorkTypeLevelTimeOffHours.Sum(t => t.Value);
+                return TotalHoursExcludingTimeOff + timeOffTotal;
+            }
+        }
+    }
+}
