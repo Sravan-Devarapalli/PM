900b423ad92cca0ec3d594de1c46f3df5200350d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MilestonePersonActivity.proc.sql
@@ -0,0 +1,25 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2010-01-25
+-- Description:	Report on milestone-person activity
+-- =============================================
+CREATE PROCEDURE dbo.MilestonePersonActivity
+    @MilestonePersonId INT
+AS 
+    BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+        SET NOCOUNT ON ;
+
+        SELECT  tes.TimeEntryId,
+                tes.ObjectLastName,
+                tes.ObjectFirstName,
+                tes.ProjectName,
+                tes.MilestoneName,
+                tes.ActualHours,
+                tes.MilestoneDate
+        FROM    dbo.v_TimeEntries AS tes
+        WHERE   tes.MilestonePersonId = @MilestonePersonId
+        ORDER BY tes.MilestoneDate
+    END
+
