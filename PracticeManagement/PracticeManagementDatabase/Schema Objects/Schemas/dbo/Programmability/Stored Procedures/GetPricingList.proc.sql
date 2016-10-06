a857a1786a41aed18e0e29b390c2c685b866cee0
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetPricingList.proc.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[GetPricingList]
+(
+	@ClientId	INT = NULL
+)
+AS
+BEGIN
+	SELECT PricingListId,
+			ClientId,
+			Name,
+			dbo.IsPricingListInUse(pl.PricingListId,pl.IsDefault) AS InUse,
+			IsDefault,
+			IsActive
+	FROM dbo.PricingList pl
+	WHERE  @ClientId IS NULL OR pl.ClientId = @ClientId
+END
