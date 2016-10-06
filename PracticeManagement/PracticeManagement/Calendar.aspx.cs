3de22e7776a67ede97a5973a311f4a5a3757a974
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Calendar.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ServiceModel;
+using System.Web.Security;
+using System.Web.UI;
+using DataTransferObjects;
+using PraticeManagement.CalendarService;
+using PraticeManagement.Controls;
+using System.Web.UI.WebControls;
+
+namespace PraticeManagement
+{
+    public partial class Calendar : System.Web.UI.Page
+    {
+        public PraticeManagement.Controls.PersonCalendar CalendarControl
+        {
+            get
+            {
+                return calendar;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        { 
+        }
+
+        
+
+    }
+}
+
