64a78255b9bd81e247fa00055cab107ae4857b25
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetSubstituteDate.proc.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[GetSubstituteDate]
+(
+	@HolidayDate DATETIME,
+	@PersonId	INT
+)
+AS
+BEGIN	
+	SELECT PC.SubstituteDate 'SubstituteDate'
+	FROM dbo.PersonCalendar PC
+	WHERE PersonId = @PersonId AND PC.Date = @HolidayDate AND PC.SubstituteDate IS NOT NULL
+END
