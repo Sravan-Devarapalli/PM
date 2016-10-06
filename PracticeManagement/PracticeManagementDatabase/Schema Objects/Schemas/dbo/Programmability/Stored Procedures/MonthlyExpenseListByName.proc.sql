01d67ee0808a331e5205d7aef7042372d7c7d31c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MonthlyExpenseListByName.proc.sql
@@ -0,0 +1,26 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 7-24-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	Retrieves the month expense by the specified Name
+-- =============================================
+CREATE PROCEDURE [dbo].[MonthlyExpenseListByName]
+(
+	@Name   NVARCHAR(50)
+)
+AS
+	SET NOCOUNT ON
+
+	SELECT me.MonthlyExpenseId,
+	       me.ExpenseCategoryId,
+	       me.ExpenseBasisId,
+	       me.WeekPaidOptionId,
+	       me.Name,
+	       me.[Year],
+	       me.[Month],
+	       me.Amount
+	  FROM dbo.MonthlyExpense AS me
+	 WHERE me.Name = @Name
+	ORDER BY me.[Year], me.[Month]
+
