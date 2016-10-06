de6c0d4b1ab94b4ec77f5a109c46e061dcedf9e7
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ProjectFeedback.table.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[ProjectFeedback]
+(
+	[FeedbackId]	INT	IDENTITY (1, 1) NOT NULL,
+	[ProjectId]		INT	NOT NULL,
+	[PersonId]		INT NOT NULL,
+	[ReviewPeriodStartDate]	DATETIME NOT NULL,
+	[ReviewPeriodEndDate]	DATETIME NOT NULL,
+	[DueDate]				DATETIME NOT NULL,
+	[FeedbackStatusId]	INT NOT NULL,
+	[IsCanceled]	BIT NOT NULL,
+	[CompletionCertificateBy] INT,
+	[CompletionCertificateDate] DATETIME,
+	[CancelationReason] NVARCHAR(MAX),
+	[NextIntialMailSendDate] DATETIME,
+	[IsGap]			BIT NULL
+)
+
