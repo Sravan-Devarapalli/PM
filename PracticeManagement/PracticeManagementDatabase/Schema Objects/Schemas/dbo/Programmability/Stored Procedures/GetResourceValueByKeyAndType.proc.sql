67dcb9b037662b2f798ea385f4a6b071c8fb361b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetResourceValueByKeyAndType.proc.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE dbo.GetResourceValueByKeyAndType
+(
+	@SettingType	NVARCHAR(255),
+	@Key			NVARCHAR(255)
+)
+AS
+
+	SELECT  [Value]
+	FROM [dbo].[Settings] S
+	JOIN dbo.SettingsType ST ON S.TypeId = ST.TypeId
+	WHERE ST.[Description]  = @SettingType AND s.SettingsKey = @Key
