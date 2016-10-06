a0098af6f87aac01383ca9c8f470b313fdc700a1
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SessionLogPrepare.proc.sql
@@ -0,0 +1,19 @@
+﻿
+
+
+CREATE PROCEDURE [dbo].[SessionLogPrepare]
+(
+	@UserLogin     NVARCHAR(255)
+)
+AS
+	SET NOCOUNT ON
+
+	IF NOT EXISTS (SELECT 1 FROM dbo.SessionLogData WHERE SessionID = @@SPID)
+	BEGIN
+	INSERT INTO dbo.SessionLogData
+				(SessionID, UserLogin, PersonID, LastName, FirstName)
+	SELECT @@SPID, @UserLogin, MAX(p.PersonId), MAX(p.LastName), MAX(p.FirstName)
+		FROM dbo.Person AS p
+		WHERE p.Alias = @UserLogin
+	END
+
