ccd20ef85ce4e230871c17c8acd2835214cf9036
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetMilestonesForProject.proc.sql
@@ -0,0 +1,25 @@
+﻿-- =============================================
+-- Author:		ThulasiRam.P
+-- Create date: 03-29-2012
+-- Description:	Retrieves the list of Milestones.
+-- Updated by : Sainath.CH
+-- Update Date: 03-30-2012
+-- =============================================
+CREATE PROCEDURE dbo.GetMilestonesForProject
+(
+	@ProjectNumber   NVARCHAR(12)
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	SELECT 
+	       M.Description AS MilestoneName,
+	       M.MilestoneId,
+		   M.StartDate,
+		   M.ProjectedDeliveryDate AS EndDate
+	  FROM dbo.Milestone AS M
+	  INNER JOIN dbo.Project AS P ON M.ProjectId = P.ProjectId
+	  WHERE P.ProjectNumber = @ProjectNumber
+	  ORDER BY m.StartDate, m.ProjectedDeliveryDate
+END
