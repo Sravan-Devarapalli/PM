64a06397b61896d542459a7ef5b3a7927c198942
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Revenue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [Serializable]
+    [DataContract]
+    public class Revenue
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public bool IsDefault { get; set; }
+
+        public static int Compare(Revenue obj1, Revenue obj2)
+        {
+            if (obj1 == null && obj2 != null)
+            {
+                return -1;
+            }
+            else if (obj1 != null && obj2 == null)
+            {
+                return 1;
+            }
+            else if (obj1 == null && obj2 == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return string.Compare(obj1.Name, obj2.Name);
+            }
+        }
+    }
+}
+
