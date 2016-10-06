de926baf24345ec943302a925583907a43e1e051
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Seniority.table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Seniority] (
+    [SeniorityId]	INT           NOT NULL,
+    [Name]			NVARCHAR (30) NOT NULL,
+	[SeniorityValue] INT NOT NULL,
+	[SeniorityCategoryId] INT NOT NULL
+);
+
+
