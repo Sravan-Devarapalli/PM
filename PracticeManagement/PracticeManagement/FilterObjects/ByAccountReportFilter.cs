f211a6082c710d3fb073c744255dc26487bd6f9c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/FilterObjects/ByAccountReportFilter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PraticeManagement.FilterObjects
+{
+    [Serializable]
+    public class ByAccountReportFilter
+    {
+        public String AccountId { get; set; }
+        public String BusinessUnitIds { get; set; }
+        public String ProjectStatusIds { get; set; }
+        public String RangeSelected { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
