3ec2c5b333f622524d7c2058ffe5186b9f9e1ff0
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectExpenseUpdate.proc.sql
@@ -0,0 +1,31 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2010-04-20
+-- =============================================
+CREATE PROCEDURE dbo.ProjectExpenseUpdate 
+    @ExpenseId int,
+    @ExpenseName nvarchar(50),
+    @ExpenseAmount decimal(18, 2),
+	@ExpectedExpenseAmount decimal(18, 2),
+    @ExpenseReimbursement decimal(18, 2),
+	@ProjectId int = null,
+	@StartDate	datetime,
+	@EndDate	datetime,
+	@MilestoneId	INT = NULL,
+	@ExpenseTypeId  INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+		Update [dbo].[ProjectExpense] Set 
+				[Name] = @ExpenseName
+			   ,[Amount] = @ExpenseAmount
+			   ,[ExpectedAmount] =@ExpectedExpenseAmount
+			   ,[Reimbursement] = @ExpenseReimbursement
+			   ,[ProjectId] = ISNULL(@ProjectId,[ProjectId])
+			   ,[StartDate] = @StartDate
+			   ,[EndDate] = @EndDate			   
+			   ,[MilestoneId] = ISNULL(@MilestoneId,[MilestoneId])
+			   ,[ExpenseTypeId]=@ExpenseTypeId
+		 Where Id = @ExpenseId
+END
+
