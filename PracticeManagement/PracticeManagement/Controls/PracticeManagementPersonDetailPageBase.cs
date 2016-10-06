bf70abc7e6c6fbc66d50d42b19bdfae54c84add7
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/PracticeManagementPersonDetailPageBase.cs
@@ -0,0 +1,24 @@
+﻿using DataTransferObjects;
+
+namespace PraticeManagement.Controls
+{
+    public abstract class PracticeManagementPersonDetailPageBase : PracticeManagementPageBase
+    {
+        protected override void Display()
+        {
+
+        }
+
+        /// <summary>
+        /// To transfer the unsaved Person object to other(CompensationDetail) page
+        /// </summary>
+        public abstract Person PersonUnsavedData
+        {
+            get;
+            set;
+        }
+
+        public abstract PersonPermission Permissions { get; set; }
+    }
+}
+
