9e33a62ec9f24f2e3e10b791e4a019152f63b690
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/PersonTimeOff.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using DataTransferObjects.TimeEntry;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class PersonTimeOff
+    {
+        [DataMember]
+        public TimeTypeRecord TimeType
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime TimeOffStartDate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime TimeOffEndDate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Project Project
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public List<Project> Projects
+        {
+            get;
+            set;
+        }
+    }
+}
+
