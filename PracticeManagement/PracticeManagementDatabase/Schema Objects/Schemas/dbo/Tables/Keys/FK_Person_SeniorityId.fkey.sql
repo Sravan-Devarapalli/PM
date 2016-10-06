9bb2972ccd32ae9fd15c9de143d2339c6fac43c4
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_Person_SeniorityId.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[Person]
+    ADD CONSTRAINT [FK_Person_SeniorityId] FOREIGN KEY ([SeniorityId]) REFERENCES [dbo].[Seniority] ([SeniorityId]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
