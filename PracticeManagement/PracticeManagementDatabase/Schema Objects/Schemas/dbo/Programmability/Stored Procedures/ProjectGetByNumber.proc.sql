d1927087a35bff05a8fb19e5ccd9306171311233
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectGetByNumber.proc.sql
@@ -0,0 +1,17 @@
+﻿-- =============================================
+-- Description:	Retrives a Project Id
+-- =============================================
+-- Arguments:
+--     @ProjectNumber - a Number of the project to be retrieved.
+-- =============================================
+CREATE PROCEDURE dbo.ProjectGetByNumber
+(
+	@ProjectNumber nvarchar(24)
+)
+AS
+	SET NOCOUNT ON
+
+	SELECT p.ProjectId	      
+	  FROM dbo.v_Project AS p
+	 WHERE p.ProjectNumber = @ProjectNumber     
+
