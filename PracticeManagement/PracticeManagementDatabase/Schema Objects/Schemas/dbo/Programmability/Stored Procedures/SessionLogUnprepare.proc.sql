3969da5869c501bd6a3fdd5fb4cef4c7b002edc9
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SessionLogUnprepare.proc.sql
@@ -0,0 +1,17 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 10-14-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	Prepares the data for logging the user's activity.
+-- =============================================
+CREATE PROCEDURE [dbo].[SessionLogUnprepare]
+AS
+	SET NOCOUNT ON
+
+
+	DELETE
+		FROM dbo.SessionLogData
+		WHERE SessionID = @@SPID
+
+
