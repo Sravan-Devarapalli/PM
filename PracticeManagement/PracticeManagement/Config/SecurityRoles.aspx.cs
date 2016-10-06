5861c7a32876595d97a2a7cc693bcb41fa47a442
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Config/SecurityRoles.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PraticeManagement.Controls;
+
+namespace PraticeManagement.Config
+{
+    public partial class SecurityRoles : PracticeManagementPageBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected override void Display()
+        {
+        }
+    }
+}
