8b7a3d9cbcbce31fc47a52d61af1e3da8f05b5a9
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/MakeDefaultRecruiterCommissionText.function.sql
@@ -0,0 +1,26 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 9-02-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	Converts the Default Recruiter commission items to the text line.
+-- =============================================
+CREATE FUNCTION [dbo].[MakeDefaultRecruiterCommissionText]
+(
+	@DefaultRecruiterCommissionHeaderId   INT
+)
+RETURNS NVARCHAR(4000)
+AS
+BEGIN
+	DECLARE @Result NVARCHAR(4000)
+
+	SELECT @Result = ISNULL(@Result + ', $', '$') +
+	       CAST(i.Amount AS NVARCHAR) + ' after ' +
+	       CAST(ROUND(i.HoursToCollect / 8, 0) AS NVARCHAR) + ' days'
+	  FROM dbo.DefaultRecruiterCommissionItem AS i
+	 WHERE i.DefaultRecruiterCommissionHeaderId = @DefaultRecruiterCommissionHeaderId
+	ORDER BY i.HoursToCollect
+
+	RETURN @Result
+END
+
