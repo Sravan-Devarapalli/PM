8f5cb343194bf2d70c8fb64df717f2e020951bc5
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/BilledTime.table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[BilledTime] (
+    [PersonId]     INT             NOT NULL,
+    [MilestoneId]  INT             NOT NULL,
+    [BilledTimeId] INT             NOT NULL,
+    [BilledDate]   DATETIME        NOT NULL,
+    [Hours]        DECIMAL (18, 2) NOT NULL
+);
+
+
