eb5626e8cc64a00434f36f5728c9121377fbbba0
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetCFDivisions.proc.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[GetCFDivisions]
+AS
+BEGIN
+
+	SELECT	D.DivisionId,
+			D.DivisionCode,
+			D.DivisionName,
+			D.ParentId,
+			Ownr.DivisionCode AS ParentDivisionCode,
+			Ownr.DivisionName AS ParentDivisionName
+	FROM dbo.Division_CF D
+	LEFT JOIN dbo.Division_CF Ownr ON Ownr.DivisionId = D.ParentId
+
+END
