3dd4a25efbf0f20c6957d74a7df1d525b14a2a83
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ProjectStatusType.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// Determines the status of project
+    /// </summary>
+    [DataContract]
+    public enum ProjectStatusType
+    {
+        [EnumMember]
+        Inactive = 1,
+
+        [EnumMember]
+        Projected = 2,
+
+        [EnumMember]
+        Active = 3,
+
+        [EnumMember]
+        Completed = 4,
+
+        [EnumMember]
+        Experimental = 5,
+
+        [EnumMember]
+        Internal = 6,
+
+        [EnumMember]
+        Proposed = 7
+    }
+}
