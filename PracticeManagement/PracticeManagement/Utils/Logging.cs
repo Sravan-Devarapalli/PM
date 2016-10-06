10f4697774fa16c9ca8b086b52e2ad2d0e138e3a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Utils/Logging.cs
@@ -0,0 +1,40 @@
+﻿using System.ServiceModel;
+using PraticeManagement.ActivityLogService;
+using System.Web;
+
+namespace PraticeManagement.Utils
+{
+    public class Logging
+    {
+        /// <summary>
+        /// Writes error message to the AL
+        /// </summary>
+        /// <param name="excMsg"></param>
+        /// <param name="excSrc"></param>
+        /// <param name="innerExcMsg"></param>
+        /// <param name="innerExcSrc"></param>
+        /// <param name="srcUrl"></param>
+        /// <param name="srcQuery"></param>
+        /// <param name="userName"></param>
+        public static void LogErrorMessage(string excMsg, string excSrc, string innerExcMsg, string innerExcSrc, string srcUrl, string srcQuery, string userName)
+        {
+            string logText = string.Format(Constants.ActityLog.ErrorLogMessage,
+                userName, srcUrl, srcQuery,
+                HttpUtility.HtmlEncode(excMsg), excSrc, HttpUtility.HtmlEncode(innerExcMsg), innerExcSrc);
+
+            using (var serviceClient = new ActivityLogServiceClient())
+            {
+                try
+                {
+                    // Insert into the database
+                    serviceClient.ActivityLogInsert(Constants.ActityLog.ErrorMessageId, logText);
+                }
+                catch (CommunicationException)
+                {
+                    serviceClient.Abort();
+                }
+            }
+        }
+    }
+}
+
