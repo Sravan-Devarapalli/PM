fbf17891067a6d2620ef49cca51c03910d72f5c3
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/EmployeeManagementSystemDatabase/Schema Objects/Schemas/dbo/Tables/Practice.table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Practice](
+	[PracticeId] [int] NOT NULL IDENTITY(1,1),
+	[Description] [nvarchar](max) NULL,
+	[DisplayOrder] [tinyint] NULL,
+ CONSTRAINT [PK_Practice] PRIMARY KEY CLUSTERED 
+(
+	[PracticeId] ASC
+)
+)
