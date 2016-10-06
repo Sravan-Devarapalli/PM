dc622858cbf9644eb0539b50d9cfa0dfab7b9cab
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/ISDayLightSavingTime.function.sql
@@ -0,0 +1,31 @@
+﻿CREATE FUNCTION ISDayLightSavingTime
+(
+	@dateTime DATETIME
+)
+RETURNS BIT
+AS
+BEGIN
+
+	DECLARE @MarchFirst DATETIME ,
+			@DayOfTheWeekMarchFirst INT,
+			@DayOfTheWeekNovFirst INT,
+			@NovemberFirst DATETIME,
+			@MarchSecondSunday DATETIME,
+			@NovFirstSunday DATETIME
+		
+	SELECT @MarchFirst = CONVERT(NVARCHAR,YEAR(@dateTime)) +'-03-01' ,
+			@NovemberFirst =CONVERT(NVARCHAR,YEAR(@dateTime))+'-11-01'
+	SELECT @DayOfTheWeekMarchFirst =DATEPART(DW, @MarchFirst),
+			@DayOfTheWeekNovFirst = DATEPART(DW,@NovemberFirst)
+
+	SELECT   @MarchSecondSunday = CASE WHEN @DayOfTheWeekMarchFirst >1 THEN  @MarchFirst+15-@DayOfTheWeekMarchFirst ELSE @MarchFirst + 7 END,
+			@NovFirstSunday = CASE WHEN @DayOfTheWeekNovFirst >1 THEN @NovemberFirst +8 -@DayOfTheWeekNovFirst ELSE @DayOfTheWeekNovFirst END
+
+	IF @dateTime BETWEEN @MarchSecondSunday AND @NovFirstSunday - 1
+	BEGIN
+		RETURN 1
+	END
+
+	RETURN 0	
+	
+END	
