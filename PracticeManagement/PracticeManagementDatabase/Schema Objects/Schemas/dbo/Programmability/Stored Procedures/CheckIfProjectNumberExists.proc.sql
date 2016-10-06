cb652cf4ee181d382ea635f5de46789414c127ad
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CheckIfProjectNumberExists.proc.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[CheckIfProjectNumberExists]
+(
+	@ProjectNumber	NVARCHAR(MAX)
+)
+AS
+BEGIN
+
+	IF EXISTS(SELECT 1 FROM dbo.Project WHERE ProjectNumber = @ProjectNumber)
+	BEGIN
+			SELECT CONVERT(BIT,1) Result 
+	END
+	ELSE
+	BEGIN
+			SELECT CONVERT(BIT,0) Result
+	END
+
+END
