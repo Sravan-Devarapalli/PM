9cde6680db955a1d1143b6fbe59a85d63a504e3e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Timescale.table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Timescale] (
+    [TimescaleId]  INT          NOT NULL,
+    [Name]         VARCHAR (50) NOT NULL,
+    [DefaultTerms] INT          NULL,
+	IsContractType    BIT       NOT NULL,
+	TimescaleCode	VARCHAR(10) NULL
+);
+
+
