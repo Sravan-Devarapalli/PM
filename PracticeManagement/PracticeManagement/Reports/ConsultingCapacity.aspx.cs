c8e28e31f678fb60b3daf41fe9d5073cf42fb502
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Reports/ConsultingCapacity.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PraticeManagement.Reporting
+{
+    public partial class ConsultingCapacity : System.Web.UI.Page
+    {
+        public Controls.Reports.ConsultantsWeeklyReport ConsultantsControl
+        {
+            get
+            {
+                return repWeekly;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
