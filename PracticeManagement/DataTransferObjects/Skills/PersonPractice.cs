c74b85901b58d0103fd9de98c1af8329d52bc20a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Skills/PersonPractice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Skills
+{
+    [DataContract]
+    [Serializable]
+    public class PersonPractice
+    {
+        [DataMember]
+        public Person Person
+        {
+            set;
+            get;
+        }
+
+        [DataMember]
+        public Practice Pactice
+        {
+            set;
+            get;
+        }
+    }
+}
