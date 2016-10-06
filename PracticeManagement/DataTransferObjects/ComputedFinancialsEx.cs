9c3a43eca79a922027055378d2647402099483ac
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ComputedFinancialsEx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// Represents a results of rate computing with additional values.
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class ComputedFinancialsEx : ComputedFinancials
+    {
+        /// <summary>
+        /// Gets or sets a computed value of the loaded hourly rate (salary + overheads)
+        /// </summary>
+        [DataMember]
+        public PracticeManagementCurrency LoadedHourlyRate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public PracticeManagementCurrency SemiLoadedHourlyRate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public PracticeManagementCurrency SemiCOGS
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a computed Overheads.
+        /// </summary>
+        [DataMember]
+        public List<PersonOverhead> OverheadList
+        {
+            get;
+            set;
+        }
+    }
+}
