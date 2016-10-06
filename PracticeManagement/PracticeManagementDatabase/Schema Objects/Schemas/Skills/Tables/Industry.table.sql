9b079793eda4e42dbdabd3a8d0b692832e98abec
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/Industry.table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [Skills].[Industry](
+	[IndustryId] [int] NOT NULL IDENTITY(1,1),
+	[Description] [nvarchar](max) NULL,
+	[DisplayOrder] [tinyint] NULL,
+	[IsActive]		[bit] NOT NULL CONSTRAINT DF_Industry_IsActive DEFAULT(1),
+	[IsDeleted]		[bit] NOT NULL CONSTRAINT DF_Industry_IsDeleted DEFAULT(0),
+ CONSTRAINT [PK_Industry] PRIMARY KEY CLUSTERED 
+(
+	[IndustryId] ASC
+)
+)
