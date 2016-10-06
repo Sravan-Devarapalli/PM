9aac79df5b695d279b27bd0fff9c28b3a84b5291
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/RecruiterGet.proc.sql
@@ -0,0 +1,25 @@
+﻿-- =============================================
+-- Author:		Skip Sailors
+-- Create date: 5-10-2008
+-- Description:	Does a recruiter exist for a person?
+-- =============================================
+CREATE PROCEDURE [dbo].[RecruiterGet] 
+	@RecruiterId int,
+	@ShowAll bit = 0,
+	@HasRecruiter bit = 0 output
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	if @ShowAll = 1
+		select @HasRecruiter = count(*)
+			from Recruiter
+			where RecruiterId = @RecruiterId
+	else
+		select @HasRecruiter = count(*)
+			from Recruiter
+			where RecruiterId = @RecruiterId
+				and Inactive = 0
+	select @HasRecruiter
+END
+
