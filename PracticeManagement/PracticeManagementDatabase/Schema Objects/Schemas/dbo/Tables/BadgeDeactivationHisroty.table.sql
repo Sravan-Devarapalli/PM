9776fc69d56aa897ac8337d056634a242dcbb4eb
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/BadgeDeactivationHisroty.table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[BadgeDeactivationHisroty]
+(
+	PersonId				INT			NOT NULL,
+	DeactivatedDate			DATETIME	NULL,
+	OrganicBreakStartDate	DATETIME	NULL,
+	OrganicBreakEndDate		DATETIME	NULL,
+	ManualStartDate			DATETIME	NULL,
+	ManualEndDate			DATETIME	NULL,
+)
+
