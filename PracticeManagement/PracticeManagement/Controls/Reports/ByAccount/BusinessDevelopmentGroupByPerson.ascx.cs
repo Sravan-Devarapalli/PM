5b5d8259a88dec152fcb347ff0e327281ec6daad
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/Reports/ByAccount/BusinessDevelopmentGroupByPerson.ascx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PraticeManagement.Controls.Reports.ByAccount
+{
+    public partial class BusinessDevelopmentGroupByPerson : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void PopulateData(int accountId, string businessUnitIds, DateTime startDate, DateTime endDate)
+        {
+            var data = ServiceCallers.Custom.Report(r => r.AccountReportGroupByPerson(accountId, businessUnitIds, startDate, endDate));
+        }
+    }
+}
