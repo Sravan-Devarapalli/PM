2d042f8c754cf0caf745f0d1d961cfa27cf50ab4
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Lockout.table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Lockout]
+(
+	LockoutId			INT IDENTITY(1,1) NOT NULL,
+	LockoutPageId		INT	NOT NULL,
+	FunctionalityName	NVARCHAR(50) NOT NULL,
+	Lockout				BIT	NOT NULL,
+	LockoutDate			DATETIME NULL
+)
+
