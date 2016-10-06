c5118d44b453181fa8a34c2d45fd14a2d6fccd1a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GettingPMTime.function.sql
@@ -0,0 +1,27 @@
+﻿CREATE FUNCTION [dbo].[GettingPMTime]
+(
+	@utcDateTime DateTime 
+)
+RETURNS DATETIME
+AS
+BEGIN
+	
+	DECLARE @resultTime DATETIME
+	
+	DECLARE @currentTimeZone NVARCHAR(10)
+	SET @currentTimeZone = (SELECT Value FROM Settings WHERE SettingsKey = 'TimeZone')
+	
+	DECLARE @IsDayLightSavingsTimeEffect NVARCHAR(6)
+	SET @IsDayLightSavingsTimeEffect = (SELECT Value FROM Settings WHERE SettingsKey = 'IsDayLightSavingsTimeEffect')
+		
+	IF @currentTimeZone = '-08:00' AND dbo.ISDayLightSavingTime(@utcDateTime) = 1 AND @IsDayLightSavingsTimeEffect = 'true'
+	BEGIN
+		SET @resultTime = CONVERT(DATETIME, SWITCHOFFSET(CONVERT(DATETIMEOFFSET, @utcDateTime), '-07:00'))
+	END
+	ELSE
+	BEGIN
+		SET @resultTime = CONVERT(DATETIME, SWITCHOFFSET(CONVERT(DATETIMEOFFSET, @utcDateTime), @currentTimeZone))
+	END
+	
+	RETURN @resultTime
+END
