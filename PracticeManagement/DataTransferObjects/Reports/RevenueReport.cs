7bc2e53aa14094d87b71a95449fccecf02bf3de6
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Reports/RevenueReport.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Reports
+{
+    [DataContract]
+    [Serializable]
+    public class RevenueReport
+    {
+        [DataMember]
+        public decimal ActualRevenuePerHour
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public decimal TargetRevenuePerHour
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public decimal HoursUtilization
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public decimal TotalRevenuePerAnnual
+        {
+            get;
+            set;
+        }
+
+        public decimal RevenuePerFTE
+        {
+            get;
+            set;
+        }
+        public decimal FTE
+        {
+            get;
+            set;
+        }
+        public int ManagedServiceResources
+        {
+            get;
+            set;
+        }
+    }
+}
+
