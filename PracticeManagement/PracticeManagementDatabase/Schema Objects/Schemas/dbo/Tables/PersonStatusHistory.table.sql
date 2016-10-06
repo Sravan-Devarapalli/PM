09ff36faa1cf3a3955a5a17f5342fd1a86fd4554
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/PersonStatusHistory.table.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE dbo.PersonStatusHistory
+(
+	PersonId		INT NOT NULL,
+	PersonStatusId	INT NOT NULL,
+	StartDate		DATETIME NOT NULL,
+	EndDate			DATETIME NULL,
+	CONSTRAINT PK_PersonStatusHistory PRIMARY KEY CLUSTERED
+	(
+		PersonId ASC,	
+		PersonStatusId ASC,	
+		StartDate ASC
+	),
+	CONSTRAINT FK_PersonStatusHistory_PersonId FOREIGN KEY (PersonId)
+	REFERENCES dbo.Person(PersonId),
+	CONSTRAINT FK_PersonStatusHistory_StatusId FOREIGN KEY (PersonStatusId)
+	REFERENCES dbo.PersonStatus(PersonStatusId)
+)
