9bf21c971a985ab5fdb8b2ad59737f8c668192eb
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonOverheadByPerson.proc.sql
@@ -0,0 +1,37 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 5-28-2008
+-- Updated by:	Anatoliy Lokshin
+-- Update date: 7-22-2008
+-- Description:	Lists a person's overhead
+-- =============================================
+CREATE PROCEDURE [dbo].[PersonOverheadByPerson]
+(
+	@PersonId   INT,
+	@EffectiveDate DATETIME = null
+)
+AS
+	SET NOCOUNT ON
+
+	DECLARE @FutureDate DATETIME
+ 	SELECT @FutureDate = dbo.GetFutureDate()
+
+    IF @EffectiveDate IS null
+	BEGIN
+		SELECT @EffectiveDate = dbo.Today()
+	END
+
+	SELECT o.PersonId,
+	       o.Description,
+	       o.Rate,
+	       o.HoursToCollect,
+	       o.StartDate,
+	       o.EndDate,
+	       o.IsPercentage,
+	       o.OverheadRateTypeId,
+	       o.OverheadRateTypeName,
+	       o.BillRateMultiplier
+	  FROM dbo.v_PersonOverhead AS o
+	 WHERE o.PersonId = @PersonId
+	   AND o.StartDate <= @EffectiveDate AND ISNULL(o.EndDate, @FutureDate) > @EffectiveDate
+
