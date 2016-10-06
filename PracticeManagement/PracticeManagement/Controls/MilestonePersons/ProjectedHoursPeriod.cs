cd4276383088d07a4ed58e7f986b3ab0ea864208
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/MilestonePersons/ProjectedHoursPeriod.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DataTransferObjects;
+
+namespace PraticeManagement.Controls.MilestonePersons
+{
+    public class ProjectedHoursPeriod : DatePeriodBase
+    {
+        public ProjectedHoursPeriod(IActivityPeriod activityPeriod)
+            : base(activityPeriod)
+        {
+        }
+
+        public override IEnumerable<DatePoint> Period
+        {
+            get { return ActivityPeriod.ProjectedActivity; }
+        }
+
+        public double TotalProjectedHours
+        {
+            get
+            {
+                return Period.Sum(point => point.Value.HasValue && (!point.DayOff || (point.DayOff && !point.CompanyDayOff)) ? point.Value.Value : 0.0);
+            }
+        }
+    }
+}
+
