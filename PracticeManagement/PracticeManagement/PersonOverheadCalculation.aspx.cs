c325d881bcfd2294b43563d756bac1b3e30036be
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/PersonOverheadCalculation.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ServiceModel;
+using System.Web.UI.WebControls;
+using PraticeManagement.Controls;
+using PraticeManagement.OverheadService;
+
+namespace PraticeManagement
+{
+    public partial class PersonOverheadCalculation : PracticeManagementPageBase
+    {
+		private const string EditRecordCommand = "EditRecord";
+
+        protected override void Display()
+		{
+			using (OverheadServiceClient serviceClient = new OverheadServiceClient())
+			{
+				try
+				{
+					gvOverhead.DataSource = serviceClient.GetOverheadFixedRates(chbActive.Checked);
+					gvOverhead.DataBind();
+				}
+				catch (FaultException<ExceptionDetail>)
+				{
+					serviceClient.Abort();
+					throw;
+				}
+			}
+		}
+
+		protected void btnAddOverhead_Click(object sender, EventArgs e)
+		{
+			Redirect(Constants.ApplicationPages.OverheadDetail);
+		}
+
+		protected void gvOverhead_RowCommand(object sender, GridViewCommandEventArgs e)
+		{
+			if (e.CommandName == EditRecordCommand)
+			{
+				Redirect(string.Format(Constants.ApplicationPages.DetailRedirectFormat,
+					Constants.ApplicationPages.OverheadDetail,
+					e.CommandArgument));
+			}
+		}
+
+		protected void chbActive_CheckedChanged(object sender, EventArgs e)
+		{
+			Display();
+		}
+	}
+}
+
