b99e70aadd4144101f65fc1eefc8a67c752474b4
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Reports/ManagementMeetingReport.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Reports
+{
+    [DataContract]
+    [Serializable]
+    public class ManagementMeetingReport
+    {
+        [DataMember]
+        public Person Person
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public List<AvailableRange> Range
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Title Title
+        {
+            get;
+            set;
+        }
+
+        public decimal Average
+        {
+            get;
+            set;
+        }
+
+        public decimal Target
+        {
+            get;
+            set;
+        }
+    }
+}
+
