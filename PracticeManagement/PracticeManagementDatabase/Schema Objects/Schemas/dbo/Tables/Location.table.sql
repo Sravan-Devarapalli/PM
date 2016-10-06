42fc694f9b19abdc2a9be7789501df8327e25a6c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Location.table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Location]
+(
+	LocationId		INT				NOT NULL,
+	LocationCode	NVARCHAR(10)	NOT NULL,
+	LocationName	NVARCHAR(30)	NULL,
+	ParentId		INT				NULL,
+	Country			NVARCHAR(20)	NULL,
+	TimeZone		NVARCHAR(10)	NULL
+)
+
