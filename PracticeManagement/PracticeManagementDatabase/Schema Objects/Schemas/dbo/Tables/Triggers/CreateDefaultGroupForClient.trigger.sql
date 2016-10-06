d5b16b3722a76ed086b398b9e4c3b0f93273796c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Triggers/CreateDefaultGroupForClient.trigger.sql
@@ -0,0 +1,37 @@
+﻿CREATE TRIGGER CreateDefaultGroupForClient 
+   ON  dbo.Client 
+   AFTER INSERT
+AS 
+BEGIN
+	SET NOCOUNT ON;
+	INSERT INTO dbo.BusinessGroup (
+		ClientId,
+		[Name],
+		Code
+	)  ( 
+		SELECT i.ClientId, 'Default' AS [Name], 'BG0001' 
+		FROM inserted AS i
+	) 
+
+	INSERT INTO dbo.ProjectGroup (
+		ClientId,
+		[Name],
+		Code,
+		BusinessGroupId
+	)  ( 
+		SELECT i.ClientId, 'Default Group' AS [Name], 'B0001',(SELECT [BusinessGroupId] FROM BusinessGroup BG WHERE Code='BG0001' AND BG.ClientId=i.ClientId)
+		FROM inserted AS i
+	) 
+	INSERT INTO dbo.PricingList
+	(
+		ClientId,
+		[Name],
+		IsDefault
+	)  ( 
+		SELECT i.ClientId, 'Default' AS [Name],1
+		FROM inserted AS i
+	) 
+
+	
+END
+
