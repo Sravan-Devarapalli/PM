d14741732c738c12f3ea3413e21eb3c928f9905d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/PracticeLeadership.table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[PracticeLeadership]
+(
+	[PracticeLeadershipId]	INT IDENTITY(1,1) NOT NULL, 
+	[DivisionId]			INT	NOT NULL,
+	[PersonId]				INT NOT NULL
+	CONSTRAINT [PK_PracticeLeadership_PracticeLeadershipId]	PRIMARY KEY ([PracticeLeadershipId])
+)
+
