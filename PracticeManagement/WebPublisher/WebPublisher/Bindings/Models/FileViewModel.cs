41fd8ef24eebf38dcffbf5598f1e4da03221ed18
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/WebPublisher/WebPublisher/Bindings/Models/FileViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebPublisher.Bindings.Models
+{
+    public class FileViewModel : TreeViewItemViewModel
+    {
+        public string FilePath { get; set; }
+
+        public FileViewModel(string path, TreeViewItemViewModel parent)
+            : base(parent, false)
+        {
+            FilePath = path;
+            Name = System.IO.Path.GetFileName(FilePath);
+        }
+
+        public FileViewModel(string path)
+            : base(null, false)
+        {
+            FilePath = path;
+            Name = System.IO.Path.GetFileName(FilePath);
+        }
+
+        protected override string GetImageUri()
+        {
+            return base.GetImageUri(); 
+        }
+    }
+}
+
