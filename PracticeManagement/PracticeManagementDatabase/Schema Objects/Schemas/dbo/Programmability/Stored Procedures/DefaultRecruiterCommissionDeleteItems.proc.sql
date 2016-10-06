d7f209770f64be7b3cfae0f456dcd92068595c78
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DefaultRecruiterCommissionDeleteItems.proc.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[DefaultRecruiterCommissionDeleteItems]
+    (
+      @DefaultRecruiterCommissionHeaderId INT
+    )
+AS 
+    BEGIN 
+        SET NOCOUNT ON
+
+	-- Remove items from items table
+        DELETE  FROM dbo.DefaultRecruiterCommissionItem
+        WHERE   DefaultRecruiterCommissionHeaderId = @DefaultRecruiterCommissionHeaderId
+		
+    END
+
