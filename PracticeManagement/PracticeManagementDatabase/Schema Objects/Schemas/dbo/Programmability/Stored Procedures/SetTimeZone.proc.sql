927d51c2e0d23510811a83f6c5107502fe522dc2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SetTimeZone.proc.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[SetTimeZone]
+(
+@GMT NVARCHAR(10)
+)
+AS
+	IF EXISTS (SELECT id FROM TimeZones WHERE GMT = @GMT)
+	BEGIN
+		UPDATE TimeZones
+		SET IsActive = 0 -- Deactive previous Time zone.
+	
+		UPDATE TimeZones
+		SET IsActive = 1
+		WHERE GMT = @GMT -- Active selected Time zone.
+	END
