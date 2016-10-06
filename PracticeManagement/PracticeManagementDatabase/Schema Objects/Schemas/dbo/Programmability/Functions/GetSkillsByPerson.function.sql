ec446a0f4e574ce59f5112a0b24b013e0e8c056e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetSkillsByPerson.function.sql
@@ -0,0 +1,17 @@
+﻿CREATE FUNCTION [dbo].[GetSkillsByPerson]
+(
+	@PersonID	INT
+)
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN
+  
+	DECLARE @Temp NVARCHAR(MAX) = ''
+  
+	SELECT @Temp = @Temp + S.Description+ ', '
+	FROM Skills.PersonSkill PS
+	JOIN Skills.Skill S ON S.SkillId = PS.SkillId
+	WHERE PS.PersonId = @PersonId
+
+	RETURN @Temp
+END
