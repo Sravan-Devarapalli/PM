6ef3ce379c668bd2ebe2d6e4c7befc6039a088d5
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetLocations.proc.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[GetLocations]
+AS
+BEGIN
+
+	SELECT L.LocationId,L.LocationCode,L.LocationName,L.ParentId,P.LocationCode AS ParentLocationCode, L.TimeZone,L.Country
+	FROM dbo.Location L
+	LEFT JOIN dbo.Location P ON P.LocationId = L.ParentId
+	
+END
