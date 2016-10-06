6e997f99dad4b9e9930fd40c63a29f53224c9ddd
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/UserTemporaryCredentialsInsert.proc.sql
@@ -0,0 +1,52 @@
+﻿CREATE PROCEDURE [dbo].[UserTemporaryCredentialsInsert]
+(
+	@UserName		NVARCHAR (256),
+	@Password		NVARCHAR (128),
+	@PasswordFormat	INT,
+	@PasswordSalt	NVARCHAR (128)
+)
+AS
+BEGIN
+
+	DECLARE @LastCreatedDate DATETIME,
+			@CreatedDate	DATETIME
+
+	SELECT @CreatedDate = GETDATE()
+
+	SELECT @LastCreatedDate = [CreatedDate]
+	FROM [dbo].[UserTemporaryCredentials]
+	WHERE UserName = @UserName
+	
+	IF  @LastCreatedDate IS NULL
+	BEGIN
+		INSERT INTO [dbo].[UserTemporaryCredentials]
+           ([UserName]
+           ,[Password]
+           ,[PasswordFormat]
+           ,[PasswordSalt]
+           ,[CreatedDate])
+		 VALUES
+			   (@UserName
+			   ,@Password
+			   ,@PasswordFormat
+			   ,@PasswordSalt
+			   ,@CreatedDate)
+	
+
+	END
+	ELSE IF (@LastCreatedDate+1) < @CreatedDate
+	BEGIN
+		UPDATE [dbo].[UserTemporaryCredentials]
+		SET [Password] = @Password
+           ,[PasswordFormat]= @PasswordFormat
+           ,[PasswordSalt] = @PasswordSalt
+           ,[CreatedDate] = @CreatedDate
+        WHERE [UserName] = @UserName
+	END
+	ELSE
+	BEGIN
+		RETURN -1
+	END
+	
+	RETURN 0
+END
