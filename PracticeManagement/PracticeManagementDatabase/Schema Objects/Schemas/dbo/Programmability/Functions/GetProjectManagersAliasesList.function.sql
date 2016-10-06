06b37fcd1ea4274d5be50e6c64ce9eba358588d2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetProjectManagersAliasesList.function.sql
@@ -0,0 +1,19 @@
+﻿CREATE FUNCTION [dbo].[GetProjectManagersAliasesList]
+(
+	@ProjectId	INT
+)
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN
+	
+	DECLARE @Temp NVARCHAR(MAX) = ''
+  
+	SELECT @Temp = @Temp + Pers.Alias +','
+	FROM Project P
+	JOIN ProjectAccess PM ON PM.ProjectId = P.ProjectId
+	JOIN Person Pers ON Pers.Personid = PM.ProjectAccessId
+	WHERE P.ProjectId = @ProjectId
+
+	RETURN @Temp
+
+END
