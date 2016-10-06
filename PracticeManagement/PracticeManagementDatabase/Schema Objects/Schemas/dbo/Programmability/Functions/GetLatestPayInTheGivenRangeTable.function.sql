6eab5fb08052108980d57968c72fdbfea7d9dfa8
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetLatestPayInTheGivenRangeTable.function.sql
@@ -0,0 +1,17 @@
+﻿--Returns latest pay details in the given date range for the given person.
+--If the person doesn't have the any pay in the given range returns NO RECORDS.
+CREATE FUNCTION [dbo].[GetLatestPayInTheGivenRangeTable]
+(
+	@PersonId	INT,
+	@StartDate	DATETIME,
+	@EndDate	DATETIME
+)
+RETURNS TABLE
+AS
+	RETURN 
+	SELECT TOP 1 P.Person AS PersonId,P.TitleId,T.Title,P.StartDate,CASE WHEN P.EndDate IS NOT NULL THEN P.EndDate-1 ELSE NULL END EndDate
+	FROM dbo.Pay P 
+	INNER JOIN dbo.Title T ON T.TitleId = P.TitleId 
+	WHERE P.Person = @PersonId AND @StartDate <= P.EndDate-1 AND P.StartDate <= @EndDate
+	ORDER BY P.StartDate DESC
+
