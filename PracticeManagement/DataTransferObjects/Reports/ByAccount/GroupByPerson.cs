d0af878f2c91c4b51222473d2707010780d36d44
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Reports/ByAccount/GroupByPerson.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Reports.ByAccount
+{
+    [DataContract]
+    [Serializable]
+    public class GroupByPerson
+    {
+        [DataMember]
+        public Person Person
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public List<GroupByBusinessUnit> BusinessUnitLevelGroupedHoursList
+        {
+            get;
+            set;
+        }
+
+        public double TotalHours
+        {
+            get
+            {
+                return BusinessUnitLevelGroupedHoursList != null ? BusinessUnitLevelGroupedHoursList.Sum(bu => bu.TotalHours) : 0d;
+            }
+        }
+    }
+}
