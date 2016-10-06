d1187a5124855fbb14edd420bef1e48c65d831da
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_PersonProjectCommission.view.sql
@@ -0,0 +1,13 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 10-16-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	Selects persons commissions on the projects. Uses for filters.
+-- =============================================
+CREATE VIEW [dbo].[v_PersonProjectCommission]
+AS
+	SELECT c.ProjectId, c.PersonId, c.CommissionId, p.Alias, c.CommissionType
+	  FROM dbo.Commission AS c
+	       INNER JOIN dbo.Person AS p ON p.PersonId = c.PersonId
+
