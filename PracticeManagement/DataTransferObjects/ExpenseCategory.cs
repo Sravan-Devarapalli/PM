b49d397e42509bec4c6a0907df4eb88c1509faf8
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ExpenseCategory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// Represents an Expense Category entity
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class ExpenseCategory
+    {
+        /// <summary>
+        /// Gets or sets an ID of the Expense Category
+        /// </summary>
+        [DataMember]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a name of the Expense Category
+        /// </summary>
+        [DataMember]
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
