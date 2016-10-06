54d4e30c29d0f0916b8353c64cb0d722286edfb2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/MilestonePersons/MilestonePersonFinancials.ascx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Web.UI;
+using DataTransferObjects.Financials;
+using PraticeManagement.Security;
+
+namespace PraticeManagement.Controls.MilestonePersons
+{
+    public partial class MilestonePersonFinancials : UserControl
+    {
+        public MilestonePersonComputedFinancials Financials
+        {
+            set { DisplayPersonRate(value); }
+        }
+
+        public SeniorityAnalyzer SeniorityAnalyzer { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void DisplayPersonRate(MilestonePersonComputedFinancials financials)
+        {
+            if (financials != null)
+            {
+                lblClientDiscount.Text = financials.ProjectDiscount.ToString(Constants.Formatting.DoubleFormat);
+                lblGrossHourlyBillRate.Text = financials.GrossHourlyBillRate.ToString();
+                lblLoadedHourlyPay.Text =
+                    SeniorityAnalyzer.GreaterSeniorityExists
+                        ? Resources.Controls.HiddenCellText
+                        : financials.LoadedHourlyPayRate.ToString();
+                lblHoursInPeriod.Text = financials.HoursInPeriod.ToString();
+                lblProjectedRevenueContribution.Text = financials.Revenue.ToString();
+                lblProjectedCogs.Text = financials.Cogs.ToString(SeniorityAnalyzer.GreaterSeniorityExists);
+                lblProjectedMarginContribution.Text = financials.Margin.ToString(SeniorityAnalyzer.GreaterSeniorityExists);
+            }
+            else
+            {
+                lblGrossHourlyBillRate.Text = lblClientDiscount.Text = 
+                                              lblLoadedHourlyPay.Text =
+                                              lblProjectedRevenueContribution.Text =
+                                              lblProjectedMarginContribution.Text =
+                                              lblProjectedCogs.Text = lblHoursInPeriod.Text =
+                                                                      lblProjectedRevenueContribution.Text =
+                                                                      lblProjectedMarginContribution.Text =
+                                                                      Resources.Controls.UnavailableLabel;
+            }
+        }
+    }
+}
