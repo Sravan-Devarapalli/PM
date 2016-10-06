896be3002fb6b51da1bbf3f6cffae362e5a57697
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_Person_TerminationReasonId.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[Person]
+	ADD CONSTRAINT [FK_Person_TerminationReasonId] 
+	FOREIGN KEY (TerminationReasonId)
+	REFERENCES TerminationReasons (TerminationReasonId) ON DELETE NO ACTION ON UPDATE NO ACTION;
