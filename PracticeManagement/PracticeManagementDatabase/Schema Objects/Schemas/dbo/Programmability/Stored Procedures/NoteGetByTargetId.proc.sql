90c7388baa20e4b518c0ae3549e8dd3e25ee1578
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/NoteGetByTargetId.proc.sql
@@ -0,0 +1,17 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 10-06-2008
+-- =============================================
+CREATE PROCEDURE dbo.NoteGetByTargetId
+(
+	@TargetId   INT,
+	@NoteTargetId INT
+)
+AS
+	SET NOCOUNT ON
+
+	SELECT n.NoteId, n.TargetId, n.PersonId, n.CreateDate, n.NoteText, n.LastName, n.FirstName
+	  FROM dbo.v_Notes as n
+	 WHERE n.TargetId = @TargetId AND n.NoteTargetId = @NoteTargetId	
+	ORDER BY n.CreateDate DESC
+
