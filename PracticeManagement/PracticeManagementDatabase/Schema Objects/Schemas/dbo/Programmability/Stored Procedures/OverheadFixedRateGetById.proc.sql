ff7b1ebbfdae05ee1fc080e777222f340ec64222
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OverheadFixedRateGetById.proc.sql
@@ -0,0 +1,29 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 5-27-2008
+-- Updated by:	Anatoliy Lokshin
+-- Update date:	8-07-2008
+-- Description:	Retrieves the overhead with its details.
+-- =============================================
+CREATE PROCEDURE [dbo].[OverheadFixedRateGetById]
+(
+	@OverheadFixedRateId   INT
+)
+AS
+	SET NOCOUNT ON
+
+	SELECT r.OverheadFixedRateId,
+	       r.Description,
+	       r.Rate,
+	       r.StartDate,
+	       r.EndDate,
+	       r.Inactive,
+	       r.RateType AS OverheadRateTypeId,
+	       t.Name AS OverheadRateTypeName,
+	       t.IsPercentage,
+	       t.HoursToCollect,
+	       r.IsCogs
+	  FROM dbo.OverheadFixedRate AS r
+	  INNER JOIN dbo.OverheadRateType AS t ON r.RateType = t.OverheadRateTypeId
+	 WHERE r.OverheadFixedRateId = @OverheadFixedRateId AND r.IsMinimumLoadFactor = 0
+
