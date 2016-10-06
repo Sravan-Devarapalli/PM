b673a86500e5e65402d29be0c7c14feb8c379add
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Indexes/aspnet_Roles_index1.index.sql
@@ -0,0 +1,4 @@
+﻿CREATE UNIQUE CLUSTERED INDEX [aspnet_Roles_index1]
+    ON [dbo].[aspnet_Roles]([ApplicationId] ASC, [LoweredRoleName] ASC) WITH ( DROP_EXISTING = OFF, IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF, ONLINE = OFF, MAXDOP = 0);
+
+
