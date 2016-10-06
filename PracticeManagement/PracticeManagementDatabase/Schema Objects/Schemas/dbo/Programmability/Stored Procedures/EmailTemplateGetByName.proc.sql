4d63f08735b2ea81a11d4a99d5e47670a02e76a1
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/EmailTemplateGetByName.proc.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[EmailTemplateGetByName]
+(
+	@EmailTemplateName	NVARCHAR(50)
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	DECLARE @EmailTemplateNameLocal NVARCHAR(50)
+	SELECT @EmailTemplateNameLocal = @EmailTemplateName
+	SELECT 
+			et.EmailTemplateId
+			, et.EmailTemplateName
+			, et.EmailTemplateTo
+			, et.EmailTemplateCc
+			, et.EmailTemplateSubject
+			, et.EmailTemplateBody
+		FROM EmailTemplate AS et
+		WHERE ISNULL(et.Name,et.EmailTemplateName) = @EmailTemplateNameLocal
+END
+
