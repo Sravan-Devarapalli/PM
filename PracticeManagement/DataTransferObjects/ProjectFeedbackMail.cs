47a8c39b38a4af39f733290ed78f50f03dd39ec4
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ProjectFeedbackMail.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [Serializable]
+    [DataContract]
+    public class ProjectFeedbackMail
+    {
+        [DataMember]
+        public Project Project
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public List<ProjectFeedback> Resources
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string ProjectManagersAliasList
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string ClientDirectorAlias
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string ProjectOwnerAlias
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string SeniorManagerAlias
+        {
+            get;
+            set;
+        }
+    }
+}
+
