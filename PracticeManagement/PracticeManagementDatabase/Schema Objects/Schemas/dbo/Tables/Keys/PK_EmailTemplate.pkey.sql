0ae44810eb4166484ccf6bbb6f5271f1d687275a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/PK_EmailTemplate.pkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[EmailTemplate]
+    ADD CONSTRAINT [PK_EmailTemplate] PRIMARY KEY CLUSTERED ([EmailTemplateId] ASC) WITH (IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF);
+
+
