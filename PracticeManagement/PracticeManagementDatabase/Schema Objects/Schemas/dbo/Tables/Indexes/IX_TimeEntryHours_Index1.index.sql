e5498db5e16dc869815d12f8edb5b12ca35fd687
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Indexes/IX_TimeEntryHours_Index1.index.sql
@@ -0,0 +1,6 @@
+﻿CREATE NONCLUSTERED INDEX IX_TimeEntryHours_Index1 ON [dbo].[TimeEntryHours] 
+(
+	[TimeEntryId] ASC
+)
+INCLUDE ( [ActualHours],
+[IsChargeable]) WITH (SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
