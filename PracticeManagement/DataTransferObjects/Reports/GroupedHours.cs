2f61b2ee10301a8791ea2c7ba99a56244458def8
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Reports/GroupedHours.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Reports
+{
+    /// <summary>
+    /// Represents grouped TimeEntries between startdate and enddate.
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class GroupedHours
+    {
+        [DataMember]
+        public DateTime StartDate { get; set; }
+
+        [DataMember]
+        public DateTime EndDate { get; set; }
+
+        [DataMember]
+        public double BillabileTotal { get; set; }
+
+        [DataMember]
+        public double NonBillableTotal { get; set; }
+
+        public double CombinedTotal
+        {
+            get
+            {
+                return BillabileTotal + NonBillableTotal;
+            }
+        }
+
+        public void SetEnddate(string groupByCerteria)
+        {
+            if (StartDate == null || StartDate == DateTime.MinValue) return;
+            switch (groupByCerteria)
+            {
+                case "day":
+                    EndDate = StartDate;
+                    break;
+
+                case "week":
+                    EndDate = StartDate.AddDays(6);
+                    break;
+
+                case "month":
+                    EndDate = StartDate.AddMonths(1).AddDays(-StartDate.AddMonths(1).Day);
+                    break;
+
+                case "year":
+                    EndDate = StartDate.AddYears(1).AddDays(-StartDate.AddYears(1).DayOfYear);
+                    break;
+            }
+        }
+    }
+}
