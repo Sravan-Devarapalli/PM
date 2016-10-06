bb6365f07d735e90c9bd1fcd94216e09033ff2b9
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/IsPricingListInUse.function.sql
@@ -0,0 +1,22 @@
+﻿CREATE FUNCTION [dbo].[IsPricingListInUse]
+(
+	@PricingListId INT,
+	@IsDefault BIT
+)
+RETURNS BIT
+AS
+BEGIN
+	IF(@IsDefault = 1)
+	BEGIN
+		RETURN 1
+	END
+	IF EXISTS(SELECT 1 FROM dbo.Project WHERE PricingListId = @PricingListId)
+	BEGIN
+		RETURN 1
+	END
+	IF EXISTS(SELECT 1 FROM dbo.Opportunity WHERE PricingListId = @PricingListId)
+	BEGIN
+		RETURN 1
+	END
+		RETURN 0
+END
