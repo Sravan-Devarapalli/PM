0b8a9ce984895a3f49b66b0b4baa187a09dfd3f7
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Indexes/IX_MilestonePersonEntry_index2.index.sql
@@ -0,0 +1,10 @@
+﻿CREATE NONCLUSTERED INDEX [IX_MilestonePersonEntry_index2] ON [dbo].[MilestonePersonEntry] 
+(
+	[MilestonePersonId] ASC,
+	[StartDate] ASC,
+	[EndDate] ASC,
+	[HoursPerDay] ASC,
+	[Amount] ASC,
+	[Id] ASC
+)WITH (SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+
