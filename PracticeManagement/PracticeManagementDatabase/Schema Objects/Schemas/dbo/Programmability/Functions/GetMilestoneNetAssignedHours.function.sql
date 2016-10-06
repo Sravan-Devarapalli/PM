f2eec57b73bf4bb8b0045309763574008850b8ab
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetMilestoneNetAssignedHours.function.sql
@@ -0,0 +1,28 @@
+﻿-- =============================================
+-- Author:		Thulasiram
+-- Create date: 07-13-2012
+-- Update by:	
+-- Update date:	
+-- Description:	Gives the sum of person work hours in the milestone without vacation days.
+-- =============================================
+CREATE FUNCTION GetMilestoneNetAssignedHours(@MilestoneId INT)
+RETURNS @Result TABLE 
+(
+	TotalHours DECIMAL(15,2)
+)
+AS
+BEGIN
+	
+	INSERT INTO @Result
+	SELECT SUM(MPE.HoursPerDay)
+	FROM   dbo.MilestonePerson AS MP  
+	INNER JOIN dbo.MilestonePersonEntry AS  MPE ON MP.MileStonePersonId = MPE.MileStonePersonId
+	INNER JOIN dbo.PersonCalendarAuto AS pcal 	ON pcal.Date BETWEEN mpe.Startdate AND mpe.EndDate 
+										   AND pcal.DayOff=0 AND pcal.PersonId = mp.PersonId 
+	WHERE  mp.MileStoneId = @MilestoneId
+	GROUP BY MP.MilestoneId
+
+	RETURN
+END
+
+GO
