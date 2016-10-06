32b3180120c6e9eb8f473b01ffdf09ac741d28dc
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MilestoneUpdateShortDetails.proc.sql
@@ -0,0 +1,28 @@
+﻿-- =============================================
+-- Author:		Srinivas.M
+-- Create date: 06-05-2012
+-- Updated By:	
+-- Updated Date: 
+-- Description:	Update some details Milestone.
+-- =============================================
+CREATE PROCEDURE [dbo].[MilestoneUpdateShortDetails]
+(
+	@MilestoneId              INT,
+	@Description              NVARCHAR(255),
+	@UserLogin                NVARCHAR(255)
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	-- Start logging session
+	EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
+
+	-- Change the milestone
+	UPDATE dbo.Milestone
+	   SET [Description] = @Description
+	 WHERE MilestoneId = @MilestoneId
+
+	-- End logging session
+	EXEC dbo.SessionLogUnprepare
+END
