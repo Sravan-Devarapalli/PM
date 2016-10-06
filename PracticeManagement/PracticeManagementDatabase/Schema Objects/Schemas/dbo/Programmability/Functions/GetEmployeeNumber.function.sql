3a68b76e5a505b6e71f0ace70f20b9c12332537b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetEmployeeNumber.function.sql
@@ -0,0 +1,25 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2010-07-02
+-- Description:	Gets Employee number
+-- =============================================
+CREATE FUNCTION dbo.GetEmployeeNumber
+(
+	@StartDate           DATETIME,
+	@EndDate             DATETIME
+)
+RETURNS int
+AS
+BEGIN
+	DECLARE @Num int
+
+	SELECT @Num = COUNT(DISTINCT pe.PersonId) FROM dbo.Person pe 
+	INNER JOIN dbo.Pay AS pa ON pe.PersonId = pa.Person 
+	WHERE ((@StartDate >= pa.StartDate AND @StartDate <= pa.EndDate) OR
+		(@EndDate IS NULL OR @EndDate <= pa.EndDate)) AND
+		(pa.Timescale = 1 OR pa.Timescale = 2) AND
+		(pe.PersonStatusId not in (3, 4))
+
+	RETURN @Num
+END
+
