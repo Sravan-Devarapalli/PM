cc84912d45fb4cbcb2129bec1e75afa7339242b4
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/CornerStone/DivisionCF.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.CornerStone
+{
+    [DataContract]
+    [Serializable]
+    public class DivisionCF
+    {
+        [DataMember]
+        public int DivisionId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string DivisionCode
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string DivisionName
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DivisionCF Parent
+        {
+            get;
+            set;
+        }
+    }
+}
+
