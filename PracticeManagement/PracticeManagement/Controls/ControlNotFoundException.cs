0af5b446a24b137e937435cae6c427da8fc73a78
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/ControlNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PraticeManagement.Controls
+{
+	/// <summary>
+	/// Provides an exception for the VisibilitySwitch class.
+	/// </summary>
+	public class ControlNotFoundException : Exception
+	{
+		private const string MessageFormat = "The control {0} not fount in the current context.";
+
+		public ControlNotFoundException(string controlId)
+			: base(string.Format(MessageFormat, controlId))
+		{
+		}
+	}
+}
+
