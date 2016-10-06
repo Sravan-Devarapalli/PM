c3527115c5cb065e002f91af205ab8e2c5e09360
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/Generic/Filtering/OpportunityFilterObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PraticeManagement.Controls.Generic.Filtering
+{
+    [Serializable]
+    public class OpportunityFilterObject
+    {
+        public bool ActiveClientsOnly { get; set; }
+        public string SearchText { get; set; }
+        public int? ClientId { get; set; }
+        public int? SalespersonId { get; set; }
+
+        public void ResetProperties()
+        {
+            ActiveClientsOnly = true;
+            ClientId = null;
+            SalespersonId = null;
+            SearchText = string.Empty;
+        }
+
+        public OpportunityFilterObject()
+        {
+            ResetProperties();
+        }
+    }
+}
+
