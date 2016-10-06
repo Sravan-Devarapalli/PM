bec1d5848fa1d1d6c546e44ea5a6aedf7a80d7fa
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/EmployeeManagementSystemDatabase/Schema Objects/Schemas/dbo/Tables/Constraints/CKPersonSkill_YearsExperience.chkconst.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[PersonSkill]  WITH CHECK ADD  CONSTRAINT [CKPersonSkill_YearsExperience] CHECK  (([YearsExperience]>=(1) AND [YearsExperience]<=(40)))
+GO
+ALTER TABLE [dbo].[PersonSkill] CHECK CONSTRAINT [CKPersonSkill_YearsExperience]
+GO
