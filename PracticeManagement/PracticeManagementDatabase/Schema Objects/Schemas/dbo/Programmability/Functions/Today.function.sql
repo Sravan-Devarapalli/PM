4cba2de42d5c8d33e0c1ae87d1676bcb067bc33e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/Today.function.sql
@@ -0,0 +1,20 @@
+﻿--The source is 
+-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 5-26-2008
+-- Description:	Gets a current date without the time part.
+-- =============================================
+CREATE FUNCTION [dbo].[Today]()
+RETURNS DATETIME
+AS
+BEGIN
+	DECLARE @Today DATETIME
+	SET @Today = GETDATE()
+	SET @Today =
+		CAST(CAST(YEAR(@Today) AS VARCHAR) + '-' +
+			CAST(MONTH(@Today) AS VARCHAR) + '-' +
+			CAST(DAY(@Today) AS VARCHAR) AS DATETIME)
+
+	RETURN @Today
+END
+
