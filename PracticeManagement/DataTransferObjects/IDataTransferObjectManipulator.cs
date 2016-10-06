3214115e31638bf6e490d7d83371e2cf49690a82
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/IDataTransferObjectManipulator.cs
@@ -0,0 +1,34 @@
+﻿namespace DataTransferObjects
+{
+    /// <summary>
+    /// Provides basic operations for given entity
+    /// </summary>
+    /// <typeparam name="TE"></typeparam>
+    public interface IDataTransferObjectManipulator<TE> where TE : class, IIdNameObject, new()
+    {
+        /// <summary>
+        /// Get entity by Id
+        /// </summary>
+        /// <param name="id">Id of the entity</param>
+        /// <returns>Entity instance</returns>
+        TE GetById(int id);
+
+        /// <summary>
+        /// Insert entity to the database
+        /// </summary>
+        /// <param name="entity">Entity instance</param>
+        /// <returns>Id of the inserted entity</returns>
+        int Add(TE entity);
+
+        /// <summary>
+        /// Remove entity from the database
+        /// </summary>
+        /// <param name="entity">Entity instance</param>
+        void Remove(TE entity);
+
+        /// <summary>
+        /// Update entity in the database
+        /// </summary>
+        void Update(TE entity);
+    }
+}
