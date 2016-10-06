5cda80780dab72d45217bcf42c147b607472f7ad
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/EmployeeManagementSystemDatabase/Schema Objects/Schemas/dbo/Tables/SkillType.table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SkillType](
+	[SkillTypeId] [int] NOT NULL IDENTITY(1,1),
+	[Description] [nvarchar](max) NULL,
+	[DisplayOrder] [tinyint] NULL,
+ CONSTRAINT [PK_SkillType] PRIMARY KEY CLUSTERED 
+(
+	[SkillTypeId] ASC
+)
+)
