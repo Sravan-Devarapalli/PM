7c74522f7b6e3b23866c38aa73c20082edb57c9f
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Utils/Excel/RowStyles.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NPOI.SS.UserModel;
+
+namespace PraticeManagement.Utils.Excel
+{
+    public class RowStyles
+    {
+        public IWorkbook parentWorkbook;
+        public CellStyles[] cellStyles;
+        public short Height = 300;
+
+        public RowStyles(CellStyles[] cellStyles)
+        {
+            this.cellStyles = cellStyles;
+        }
+
+        public void ApplyRowStyles(IRow row, List<ICellStyle> allCellStyles, Dictionary<string, short> allDataFormats)
+        {
+            if (parentWorkbook == null || cellStyles == null || row == null) return;
+            row.Height = Height;
+            int i = 0;
+            foreach (ICell cell in row.Cells)
+            {
+                cellStyles[i].parentWorkbook = parentWorkbook;
+                cellStyles[i].ApplyStyles(cell, allCellStyles, allDataFormats);
+                if (i < cellStyles.Length - 1)
+                {
+                    i++;
+                }
+            }
+        }
+    }
+}
