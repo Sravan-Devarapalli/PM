260bf7f3d576e4ac63a96f298f7050cabbe38a61
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectSetStatus.proc.sql
@@ -0,0 +1,14 @@
+﻿----##############################################################################
+---- CREATE NEW STORED PROCEDURES
+----##############################################################################
+CREATE PROCEDURE [dbo].[ProjectSetStatus] 
+ @ProjectID int,
+ @ProjectStatusId int
+AS
+BEGIN
+ SET NOCOUNT ON;
+  
+ UPDATE Project
+  SET ProjectStatusId = @ProjectStatusId
+  WHERE ProjectID = @ProjectID
+END
