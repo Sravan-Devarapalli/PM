703bc4611ae2401f6f01c375c918d390ae933ae4
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Skills/PersonContactInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Skills
+{
+    [DataContract]
+    [Serializable]
+    public class PersonContactInfo
+    {
+        [DataMember]
+        public Person Person
+        {
+            set;
+            get;
+        }
+
+        [DataMember]
+        public string Info
+        {
+            set;
+            get;
+        }
+
+        [DataMember]
+        public int? DisplayOrder
+        {
+            set;
+            get;
+        }
+
+        [DataMember]
+        public DateTime ModificationDate
+        {
+            set;
+            get;
+        }
+
+        [DataMember]
+        public ContactInfoType ContactInfoType
+        {
+            set;
+            get;
+        }
+    }
+}
