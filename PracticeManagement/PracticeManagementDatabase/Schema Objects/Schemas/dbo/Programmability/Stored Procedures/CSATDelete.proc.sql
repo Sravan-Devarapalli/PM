49091b862eac69c7af2c299c8ce79e065c8f9968
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CSATDelete.proc.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[CSATDelete]
+(
+	@ProjectCSATId		INT, 
+	@UserLogin      NVARCHAR(255)
+)
+AS
+BEGIN
+	-- Start logging session
+	EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
+
+	DELETE dbo.ProjectCSAT 
+	WHERE [CSATId] = @ProjectCSATId
+
+	-- End logging session
+	EXEC dbo.SessionLogUnprepare
+END
