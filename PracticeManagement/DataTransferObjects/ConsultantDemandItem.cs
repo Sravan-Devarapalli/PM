6cdc43c8bcab0b6d2dc5bf97191a2fa72e6979da
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ConsultantDemandItem.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class ConsultantDemandItem
+    {
+        [DataMember]
+        public Person Consultant
+        {
+            set;
+            get;
+        }
+
+        [DataMember]
+        public string QuantityString
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Client Client
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int ObjectId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string ObjectName
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int? LinkedObjectId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string LinkedObjectNumber
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string ObjectNumber
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int ObjectType
+        {
+            set;
+            get;
+        }
+
+        [DataMember]
+        public int ObjectStatusId
+        {
+            set;
+            get;
+        }
+
+        [DataMember]
+        public DateTime StartDate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime EndDate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string OpportunintyDescription
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string ProjectDescription
+        {
+            get;
+            set;
+        }
+    }
+}
