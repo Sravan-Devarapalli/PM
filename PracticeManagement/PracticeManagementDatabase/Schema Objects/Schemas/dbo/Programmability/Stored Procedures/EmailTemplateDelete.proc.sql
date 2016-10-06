232fd2b956912c12f9762c38233fc80dabff8988
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/EmailTemplateDelete.proc.sql
@@ -0,0 +1,17 @@
+﻿
+-- =============================================
+-- Author:		Artem Dolya
+-- Create date: 2010-05-13
+-- Description:	Inserts new email template record
+-- =============================================
+CREATE PROCEDURE dbo.EmailTemplateDelete
+	@EmailTemplateId int
+AS 
+    BEGIN
+        SET NOCOUNT ON ;
+	
+		DELETE dbo.EmailTemplate
+		 WHERE EmailTemplateId = @EmailTemplateId
+      
+    END
+
