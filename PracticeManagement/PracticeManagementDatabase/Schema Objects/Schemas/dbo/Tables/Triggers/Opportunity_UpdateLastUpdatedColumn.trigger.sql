807ee475fa29ae9df2fe5c02c6f079fd40057474
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Triggers/Opportunity_UpdateLastUpdatedColumn.trigger.sql
@@ -0,0 +1,21 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2010-06-23
+-- Description:	Updates LastUpdated column
+-- =============================================
+--CREATE TRIGGER Opportunity_UpdateLastUpdatedColumn 
+--   ON  dbo.Opportunity 
+--   AFTER UPDATE
+--AS 
+--BEGIN
+--	SET NOCOUNT ON;
+
+--	Update Opportunity 
+--	set Opportunity.LastUpdated = getdate()
+--	From Opportunity inner join inserted on inserted.OpportunityId = Opportunity.OpportunityId
+--END
+
+GO
+--EXECUTE sp_settriggerorder @triggername = N'[dbo].[Opportunity_UpdateLastUpdatedColumn]', @order = N'first', @stmttype = N'update';
+
+
