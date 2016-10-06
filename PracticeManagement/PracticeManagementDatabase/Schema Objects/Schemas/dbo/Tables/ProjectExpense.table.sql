97ff746448e25e6b29e3fc55a289ca1539032ecf
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ProjectExpense.table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[ProjectExpense] (
+    [Id]            INT             IDENTITY (1, 1) NOT NULL,
+    [Name]          NVARCHAR (50)   NOT NULL,
+    [Amount]        DECIMAL (18, 2) NOT NULL,
+    [Reimbursement] DECIMAL (18, 2) NOT NULL,
+    [ProjectId]		INT             NOT NULL,
+	[StartDate]		DATETIME		NOT NULL,
+	[EndDate]		DATETIME		NOT NULL,
+	[MilestoneId]	INT				NULL,
+	[ExpenseTypeId] INT             NOT NULL,
+	[ExpectedAmount]DECIMAL (18, 2) NOT NULL
+);
+
