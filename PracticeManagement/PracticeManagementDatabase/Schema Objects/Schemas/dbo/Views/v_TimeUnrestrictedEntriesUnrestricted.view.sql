4de313ab8b141cca84b7d0ddd4b9799a121b8ae7
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_TimeUnrestrictedEntriesUnrestricted.view.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW [dbo].[v_TimeUnrestrictedEntriesUnrestricted]
+AS
+SELECT  DISTINCT   te.TimeEntryId, te.EntryDate, te.ModifiedDate, te.MilestonePersonId, te.ActualHours, te.ForecastedHours, te.TimeTypeId, te.ModifiedBy, te.Note, 
+                      te.IsReviewed, te.IsChargeable, te.MilestoneDate, m.Description AS 'MilestoneName', mpe.StartDate, mpe.EndDate, 
+                      mpe.HoursPerDay, m.MilestoneId
+FROM         dbo.TimeEntries AS te INNER JOIN
+                      dbo.MilestonePerson AS mp ON mp.MilestonePersonId = te.MilestonePersonId LEFT JOIN
+                      dbo.MilestonePersonEntry AS mpe ON mp.MilestonePersonId = mpe.MilestonePersonId INNER JOIN
+                      dbo.Milestone AS m ON m.MilestoneId = mp.MilestoneId 
