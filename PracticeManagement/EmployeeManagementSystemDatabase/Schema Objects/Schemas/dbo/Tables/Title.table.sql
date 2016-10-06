1fccdb8d3060524e2b8370c0d437a0528c83fa8b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/EmployeeManagementSystemDatabase/Schema Objects/Schemas/dbo/Tables/Title.table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Title](
+	[TitleId] INT NOT NULL IDENTITY(1,1),
+	[Description] [nvarchar](max) NULL,
+	[DisplayOrder] [tinyint] NULL,
+ CONSTRAINT [PK_Title] PRIMARY KEY CLUSTERED 
+(
+	[TitleId] ASC
+)
+)
