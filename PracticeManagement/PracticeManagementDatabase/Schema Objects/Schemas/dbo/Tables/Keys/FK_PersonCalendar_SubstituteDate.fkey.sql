9f2dd66e22f3b5c0295f84754e3f594845e71f3a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_PersonCalendar_SubstituteDate.fkey.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE dbo.PersonCalendar
+ADD CONSTRAINT [FK_PersonCalendar_SubstituteDate] FOREIGN KEY (SubstituteDate) 
+REFERENCES [dbo].[Calendar] ([Date]) ON DELETE NO ACTION ON UPDATE NO ACTION;
