a8793b590b5580ab38bd840b214bfb6f53b7ea9a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PayGetHistoryByPerson.proc.sql
@@ -0,0 +1,43 @@
+﻿--The source is 
+-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 6-23-2008
+-- Update by:	Anatoliy Lokshin
+-- Update date:	6-24-2008
+-- Description:	Retrives a payment histiry for the specified person.
+-- =============================================
+CREATE PROCEDURE [dbo].[PayGetHistoryByPerson]
+(
+	@PersonId   INT
+)
+AS
+	SET NOCOUNT ON
+
+	SELECT p.PersonId,
+	       p.StartDate,
+	       p.EndDate,
+	       p.Amount,
+	       p.Timescale,
+	       p.AmountHourly,
+	       p.VacationDays,
+	       p.BonusAmount,
+	       p.BonusHoursToCollect,
+	       p.IsYearBonus,
+	       p.TimescaleName,
+		   p.PracticeId,
+		   pr.Name PracticeName,
+		   Tt.TitleId,
+		   Tt.Title,
+		   p.SLTApproval,
+		   p.SLTPTOApproval,
+		   p.DivisionId,
+		   d.DivisionName,
+		   p.VendorId,
+		   p.VendorName
+	  FROM dbo.v_Pay AS p
+	  LEFT JOIN dbo.Title Tt ON Tt.TitleId = P.TitleId
+	  LEFT JOIN dbo.Practice pr on pr.PracticeId = p.PracticeId
+	  LEFT JOIN dbo.PersonDivision d ON d.DivisionId=p.DivisionId
+	 WHERE p.PersonId = @PersonId
+	ORDER BY p.StartDate
+
