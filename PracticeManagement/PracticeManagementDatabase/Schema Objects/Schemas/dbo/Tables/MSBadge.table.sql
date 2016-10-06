7700e0d4444b02350e1a349fbc648b4406071afd
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/MSBadge.table.sql
@@ -0,0 +1,32 @@
+﻿CREATE TABLE [dbo].[MSBadge]
+(
+	PersonId			INT			NOT NULL,
+	BadgeStartDate		DATETIME	NULL,
+	BadgeEndDate		DATETIME	NULL,
+	PlannedEndDate		DATETIME	NULL,
+	BreakStartDate		DATETIME	NULL,
+	BreakEndDate		DATETIME	NULL,
+	IsBlocked			BIT			NOT NULL,
+	BlockStartDate		DATETIME	NULL,
+	BlockEndDate		DATETIME	NULL,
+	IsPreviousBadge		BIT			NOT NULL,
+	PreviousBadgeAlias	NVARCHAR(20) NULL,
+	LastBadgeStartDate	DATETIME	NULL,
+	LastBadgeEndDate	DATETIME	NULL,
+	IsException			BIT			NOT NULL,
+	ExceptionStartDate	DATETIME	NULL,
+	ExceptionEndDate	DATETIME	NULL,
+	BadgeStartDateSource NVARCHAR(30) NULL,
+	PlannedEndDateSource NVARCHAR(30) NULL,
+	BadgeEndDateSource NVARCHAR(30) NULL,
+	DeactivatedDate		DATETIME	NULL,
+	OrganicBreakStartDate	DATETIME	NULL,
+	OrganicBreakEndDate		DATETIME	NULL,
+	ExcludeInReports	BIT			NULL,
+	ManualStartDate		DATETIME	NULL,
+	ManualEndDate		DATETIME	NULL,
+	ManualBreakStart	DATETIME	NULL,
+	ManualBreakEnd		DATETIME	NULL,
+	ManageServiceContract 	BIT		NOT NULL CONSTRAINT DF_MSBadge_ManageServiceContract DEFAULT(0)
+)
+
