bb4e304655d4a8bef3158e54a0857bb02366d904
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/NotesClone.proc.sql
@@ -0,0 +1,22 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2010-07-23
+-- =============================================
+CREATE PROCEDURE NotesClone
+	@OldTargetId INT, 
+	@NewTargetId INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO [dbo].[Note]
+           ([TargetId]
+           ,[PersonId]
+           ,[CreateDate]
+           ,[NoteText]
+           ,[NoteTargetId])
+     select @NewTargetId, PersonId, CreateDate, NoteText, NoteTargetId
+	 from Note
+	 where TargetId = @OldTargetId
+END
+
