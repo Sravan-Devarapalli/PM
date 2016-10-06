2588b5913f19dbdac546fbae9df5d44d83908dac
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CheckIfPersonIsRestrictedByProjectId.proc.sql
@@ -0,0 +1,29 @@
+﻿CREATE PROCEDURE [dbo].[CheckIfPersonIsRestrictedByProjectId]
+(
+	@ProjectId		INT,
+	@PersonId		INT,
+	@ChargeDate		DATETIME
+)
+AS
+BEGIN
+
+	IF EXISTS(	SELECT 1
+				FROM dbo.MilestonePersonEntry MPE 
+				JOIN dbo.MilestonePerson MP ON MP.MilestonePersonId = MPE.MilestonePersonId
+				JOIN dbo.Milestone M ON M.MilestoneId = MP.MilestoneId
+				JOIN dbo.Project P ON P.ProjectId = M.ProjectId
+				WHERE M.ProjectId = @ProjectId 
+					  AND P.ClientId = 2 --Microsoft Account
+					  AND MPE.IsBadgeRequired = 1 AND MPE.IsApproved = 0
+					  AND MP.PersonId = @PersonId
+					  AND (@ChargeDate BETWEEN MPE.BadgeStartDate AND MPE.BadgeEndDate)
+			 )
+	BEGIN
+			SELECT CONVERT(BIT,1) Result 
+	END
+	ELSE
+	BEGIN
+			SELECT CONVERT(BIT,0) Result
+	END
+
+END
