f1a66d8bb6b380f2bda61efddf4e28df218cb654
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PricingListUpdate.proc.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].PricingListUpdate
+(
+	  @PricingListId	INT,
+	  @Name	    	    NVARCHAR(200),
+  	  @IsActive	    	    BIT,
+	  @UserLogin          NVARCHAR(255)
+)
+AS
+BEGIN
+	-- Start logging session
+	EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
+
+	UPDATE PricingList
+	SET Name = @Name,
+	IsActive = @IsActive
+	WHERE PricingListId = @PricingListId
+
+	-- End logging session
+	EXEC dbo.SessionLogUnprepare
+END
