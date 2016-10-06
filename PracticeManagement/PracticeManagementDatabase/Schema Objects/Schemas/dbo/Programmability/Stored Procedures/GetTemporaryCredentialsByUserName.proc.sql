f3a5510119b8e95dc8682dbe89194000f73389fb
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetTemporaryCredentialsByUserName.proc.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[GetTemporaryCredentialsByUserName]
+(
+	@UserName		NVARCHAR (256)
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	DECLARE  @Now	DATETIME
+	SELECT @Now = GETDATE()
+
+	DELETE FROM  [dbo].[UserTemporaryCredentials]
+	WHERE UserName = @UserName
+		  AND ([CreatedDate]+1) <= @Now
+
+	SELECT [UserName],[Password],[PasswordSalt]
+	FROM [dbo].[UserTemporaryCredentials]
+	WHERE UserName = @UserName
+		  AND ([CreatedDate]+1) > @Now
+
+	
+END
