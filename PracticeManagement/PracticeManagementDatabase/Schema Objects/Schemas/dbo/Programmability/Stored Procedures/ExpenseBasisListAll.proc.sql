fa467d99e85bd4a0d90381e45264773ab5f41107
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ExpenseBasisListAll.proc.sql
@@ -0,0 +1,15 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 7-24-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	Retrieves the list of the Expense Bases
+-- =============================================
+CREATE PROCEDURE [dbo].[ExpenseBasisListAll]
+AS
+	SET NOCOUNT ON
+
+	SELECT b.ExpenseBasisId, Name
+	  FROM dbo.ExpenseBasis AS b
+	ORDER BY b.Name
+
