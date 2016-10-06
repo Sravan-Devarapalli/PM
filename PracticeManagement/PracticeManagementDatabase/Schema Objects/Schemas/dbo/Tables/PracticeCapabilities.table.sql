be3f29da1768641bf304afa5b69960c9a4a028d2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/PracticeCapabilities.table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[PracticeCapabilities]
+(
+	[CapabilityId]		INT	IDENTITY (1, 1) NOT NULL,
+	[PracticeId]		INT					NOT NULL,
+    [CapabilityName]	VARCHAR (100)		NOT NULL,
+	[IsActive]          BIT					NOT NULL CONSTRAINT DF_PracticeCapabilities_IsActive DEFAULT(0),
+	CONSTRAINT PK_PracticeCapabilities_CapabilityId PRIMARY KEY CLUSTERED([CapabilityId]),
+	CONSTRAINT [UQ_PracticeCapabilities_CapabilityName] UNIQUE NONCLUSTERED([CapabilityName]) ON [PRIMARY]
+)
+
