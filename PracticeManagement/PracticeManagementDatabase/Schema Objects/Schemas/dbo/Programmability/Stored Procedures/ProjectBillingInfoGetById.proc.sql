04169b71054995f04564f083dba5b70f48b270b0
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectBillingInfoGetById.proc.sql
@@ -0,0 +1,28 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 10-10-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	Retrives the billing info for the specified project.
+-- =============================================
+CREATE PROCEDURE [dbo].[ProjectBillingInfoGetById]
+(
+	@ProjectId   INT
+)
+AS
+	SET NOCOUNT ON
+
+	SELECT bi.ProjectId,
+	       bi.BillingContact,
+	       bi.BillingPhone,
+	       bi.BillingEmail,
+	       bi.BillingType,
+	       bi.BillingAddress1,
+	       bi.BillingAddress2,
+	       bi.BillingCity,
+	       bi.BillingState,
+	       bi.BillingZip,
+	       bi.PurchaseOrder
+	  FROM dbo.ProjectBillingInfo AS bi
+	 WHERE bi.ProjectId = @ProjectId
+
