ac1010225b97531addf09b6cfd358623480919d6
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Quadruple.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// Provides a basic utility class that is used to store 4 typed objects
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class Quadruple<A, B, C, D>
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the First object.
+        /// </summary>
+        [DataMember]
+        public A First
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the Second object.
+        /// </summary>
+        [DataMember]
+        public B Second
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the Third object.
+        /// </summary>
+        [DataMember]
+        public C Third
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the Fourth object.
+        /// </summary>
+        [DataMember]
+        public D Fourth
+        {
+            get;
+            set;
+        }
+
+        #endregion Properties
+
+        /// <summary>
+        /// Init constructor of Quadruple.
+        /// </summary>
+        public Quadruple(A first, B second, C third, D fourth)
+        {
+            First = first;
+            Second = second;
+            Third = third;
+            Fourth = fourth;
+        }
+    }
+}
