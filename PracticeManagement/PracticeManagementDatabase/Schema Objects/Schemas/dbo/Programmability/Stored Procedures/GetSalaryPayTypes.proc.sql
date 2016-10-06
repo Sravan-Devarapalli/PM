de02cdcf778e51c4f4262a9fc7b980cd8e32d795
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetSalaryPayTypes.proc.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[GetSalaryPayTypes]
+AS
+BEGIN
+
+	SELECT	TimescaleId,
+			Name AS Timescale,
+			TimescaleCode
+	FROM dbo.Timescale
+	WHERE	IsContractType = 0
+
+	UNION ALL
+
+	SELECT -1,'1099-Hourly','1099'
+
+END
