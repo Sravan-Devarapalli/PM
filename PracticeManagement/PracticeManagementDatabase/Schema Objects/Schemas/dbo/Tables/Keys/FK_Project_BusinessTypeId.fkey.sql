14ed58fffb9da712ea79b8b39a27a9d49bf17877
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_Project_BusinessTypeId.fkey.sql
@@ -0,0 +1,2 @@
+﻿ALTER TABLE [dbo].[Project]
+    ADD CONSTRAINT [FK_Project_BusinessTypeId] FOREIGN KEY ([BusinessTypeId]) REFERENCES [dbo].[BusinessType] ([BusinessTypeId]) ON DELETE NO ACTION ON UPDATE NO ACTION;
