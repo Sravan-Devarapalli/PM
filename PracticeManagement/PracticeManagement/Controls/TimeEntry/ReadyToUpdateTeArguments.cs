57585770ac3f31e09ed8ca4ed94d84e02fa551b4
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/TimeEntry/ReadyToUpdateTeArguments.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DataTransferObjects.TimeEntry;
+
+namespace PraticeManagement.Controls.TimeEntry
+{
+    public class ReadyToUpdateTeArguments : EventArgs
+    {
+        public TimeEntryRecord TimeEntry { get; set; }
+
+        /// <summary>
+        /// Init constructor of ReadyToUpdateTeArguments.
+        /// </summary>
+        public ReadyToUpdateTeArguments(DataTransferObjects.TimeEntry.TimeEntryRecord timeEntry)
+        {
+            this.TimeEntry = timeEntry;
+        }
+    }
+}
+
