f76f9644ada6f6235c1d86a43116bcc0566153e5
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/IsPersonHaveActiveStatusDuringThisPeriod.proc.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[IsPersonHaveActiveStatusDuringThisPeriod]
+(
+	@PersonId           INT,
+	@StartDate          DATETIME,
+	@EndDate            DATETIME = NULL
+)	
+AS
+BEGIN
+
+	DECLARE @IsPersonHasActiveStatus BIT,
+			@FutureDate	DATETIME
+
+	SELECT  @IsPersonHasActiveStatus = 0,
+			@FutureDate = dbo.GetFutureDate()
+
+	SELECT @IsPersonHasActiveStatus = 1
+	FROM dbo.v_PersonHistory AS p
+	INNER JOIN dbo.PersonStatusHistory PSH ON  PSH.PersonId = p.PersonId  AND P.PersonId = @PersonId AND PSH.PersonStatusId IN (1,5) -- ACTIVE And TerminationPending Status
+	WHERE  P.HireDate <= ISNULL(@EndDate,@FutureDate) AND ISNULL(P.TerminationDate,@FutureDate)  >= @StartDate AND 
+		PSH.StartDate <= ISNULL(@EndDate,@FutureDate) AND ISNULL(PSH.EndDate,@FutureDate)  >= @StartDate
+
+    SELECT @IsPersonHasActiveStatus 
+END
