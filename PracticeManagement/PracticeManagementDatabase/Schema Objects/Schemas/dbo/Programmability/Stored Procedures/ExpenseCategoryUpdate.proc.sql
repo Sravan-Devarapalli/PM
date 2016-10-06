c6aafac5e5b19a018a33c39956eb4f0f3f788fd3
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ExpenseCategoryUpdate.proc.sql
@@ -0,0 +1,21 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 7-22-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	Updates an Expense Category
+-- =============================================
+CREATE PROCEDURE [dbo].[ExpenseCategoryUpdate]
+(
+	@ExpenseCategoryId   INT,
+	@Name                NVARCHAR(25)
+)
+AS
+	SET NOCOUNT ON
+
+	UPDATE dbo.ExpenseCategory
+	   SET Name = @Name
+	 WHERE ExpenseCategoryId = @ExpenseCategoryId
+	 
+
+
