56c22c73e721d86daac8529bb8be8c136e6e6b8a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Reports/GroupByDate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataTransferObjects.Reports
+{
+    public class GroupByDate
+    {
+        public DateTime Date
+        {
+            get;
+            set;
+        }
+
+        public List<GroupByClientAndProject> ProjectTotalHours
+        {
+            get;
+            set;
+        }
+
+        public double TotalHours
+        {
+            get
+            {
+                return ProjectTotalHours != null ? ProjectTotalHours.Sum(p => p.TotalHours) : 0;
+            }
+        }
+    }
+}
