985a27f45bde9438c607057c96baca3de6e7613b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Revenue.table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Revenue]
+(
+	[RevenueTypeId]   INT             IDENTITY (1, 1) NOT NULL,
+	[RevenueName]     NVARCHAR (50)   NOT NULL,
+	[IsDefault]	      BIT			  NOT NULL CONSTRAINT DF_Revenue_IsDefault DEFAULT 0,
+	CONSTRAINT [PK_Revenue_RevenueTypeID]	PRIMARY KEY ([RevenueTypeId])
+)
+
