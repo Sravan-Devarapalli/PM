2fea9f9aab6fb825c920e98bf924e3757740cd3b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/IsUserIsProjectOwner.proc.sql
@@ -0,0 +1,33 @@
+﻿-- =============================================
+-- Author:		Srinivas.M
+-- Create date: 06-05-2012
+-- Updated By:	
+-- Updated Date: 
+-- Description:	Return whether user is Project Owner or not.
+-- =============================================
+CREATE PROCEDURE [dbo].[IsUserIsProjectOwner]
+(
+	@UserLogin NVARCHAR(100),
+	@Id INT
+)
+AS
+BEGIN
+	
+	DECLARE @PersonId INT
+	
+	SELECT @PersonId = PersonId
+	FROM Person
+	WHERE Alias = @UserLogin
+
+	IF EXISTS (SELECT *
+				FROM Project P
+				WHERE ProjectManagerId = @PersonId)
+	BEGIN
+		SELECT 'True'
+	END
+	ELSE
+	BEGIN
+		SELECT 'False'
+	END
+
+END
