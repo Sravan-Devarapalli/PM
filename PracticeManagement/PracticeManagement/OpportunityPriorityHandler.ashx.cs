67f3b2b31d6c9fb69256017cb6f599e0e04f144e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/OpportunityPriorityHandler.ashx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PraticeManagement
+{
+    /// <summary>
+    /// Summary description for OpportunityPriorityHandler
+    /// </summary>
+    public class OpportunityPriorityHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            var opportunityId = Convert.ToInt32(context.Request.QueryString["OpportunityID"]);
+            var priorityId = Convert.ToInt32(context.Request.QueryString["PriorityID"]);
+            ServiceCallers.Custom.Opportunity(os => os.UpdatePriorityIdForOpportunity(opportunityId, priorityId,context.User.Identity.Name));
+            context.Response.Write("Save Completed.");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
