c0c6ec92e1e091610d2ecc2ac224f9d2f4d360b7
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Security/SeniorityAnalyzer.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Web.Security;
+using DataTransferObjects;
+
+namespace PraticeManagement.Security
+{
+    public class SeniorityAnalyzer
+    {
+        private readonly bool isAdminOrSales;
+        private readonly Person currentPerson;
+
+        /// <summary>
+        /// Init constructor of SeniorityAnalyzer.
+        /// </summary>
+        public SeniorityAnalyzer(Person currentPerson)
+        {
+            this.currentPerson = currentPerson;
+
+            GreaterSeniorityExists = false;
+
+            isAdminOrSales =
+               Roles.IsUserInRole(DataTransferObjects.Constants.RoleNames.AdministratorRoleName) ||
+               Roles.IsUserInRole(DataTransferObjects.Constants.RoleNames.SalespersonRoleName);
+        }
+
+        public bool GreaterSeniorityExists { get; private set; }
+
+        public bool OneWithGreaterSeniorityExists(IEnumerable<Person> persons)
+        {
+            if (isAdminOrSales)
+                return false;
+
+            foreach (Person person in persons)
+                if (IsOtherGreater(person))
+                    return true;
+
+            return false;
+        }
+
+        public bool OneWithGreaterSeniorityExists(List<MilestonePerson> persons)
+        {
+            if (isAdminOrSales)
+                return false;
+
+            foreach (MilestonePerson person in persons)
+                if (IsOtherGreater(person.Person))
+                    return true;
+
+            return false;
+        }
+
+        public bool IsOtherGreater(Person other)
+        {
+            if (isAdminOrSales || currentPerson.Id == other.Id)
+                return false;
+
+            Seniority otherSeniority = other.Seniority;
+            if (otherSeniority == null)
+                return false;
+
+            bool otherHasGreaterSeniority = currentPerson.Seniority.OtherHasGreaterOrEqualSeniority(otherSeniority);
+            if (otherHasGreaterSeniority)
+                GreaterSeniorityExists = true;
+
+            return otherHasGreaterSeniority;
+        }
+
+        public bool IsOtherGreater(int seniorityId)
+        {
+            return IsOtherGreater(CreateSimplePerson(seniorityId));
+        }
+
+        /// <summary>
+        /// Creates person with just seniority value
+        /// </summary>
+        /// <param name="seniorityId"></param>
+        /// <returns></returns>
+        private static Person CreateSimplePerson(int seniorityId)
+        {
+            Person other = new Person();
+            other.Seniority = new Seniority();
+            other.Seniority.Id = seniorityId;
+            return other;
+        }
+    }
+}
