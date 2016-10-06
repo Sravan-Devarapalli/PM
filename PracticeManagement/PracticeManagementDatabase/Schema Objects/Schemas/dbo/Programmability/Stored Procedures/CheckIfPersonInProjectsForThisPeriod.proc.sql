61774e653ded2c3ee01143b8a511c43ed5faa5ad
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CheckIfPersonInProjectsForThisPeriod.proc.sql
@@ -0,0 +1,40 @@
+﻿CREATE PROCEDURE [dbo].[CheckIfPersonInProjectsForThisPeriod]
+(
+	@ModifiedEndDate	DATETIME, --LESS THAN OLDENDDATE
+	@OldEndDate			DATETIME,
+	@ModifiedStartDate	DATETIME, -- GREATER THAN OLDSTARTDATE
+	@OldStartDate		DATETIME,
+	@PersonId			INT
+)
+AS
+BEGIN
+	
+	DECLARE @ProjectsExists	BIT = 0
+
+	IF(@ModifiedEndDate IS NOT NULL)
+	BEGIN
+		
+		SELECT @ProjectsExists = 1 
+		FROM dbo.MilestonePersonEntry MPE 
+		INNER JOIN dbo.MilestonePerson MP ON MP.MilestonePersonId = MPE.MilestonePersonId
+		INNER JOIN dbo.Milestone M ON M.MilestoneId = MP.MilestoneId
+		INNER JOIN dbo.Project P ON P.ProjectId = M.ProjectId
+		WHERE MP.PersonId = @PersonId AND P.ProjectStatusId IN (1,2,3,4) AND MPE.IsBadgeRequired = 1 AND @ModifiedEndDate <= MPE.BadgeEndDate AND MPE.BadgeStartDate <= @OldEndDate
+
+	END
+
+	IF(@ProjectsExists = 0 AND @ModifiedStartDate IS NOT NULL)
+	BEGIN
+
+		SELECT @ProjectsExists = 1 
+		FROM dbo.MilestonePersonEntry MPE 
+		INNER JOIN dbo.MilestonePerson MP ON MP.MilestonePersonId = MPE.MilestonePersonId
+		INNER JOIN dbo.Milestone M ON M.MilestoneId = MP.MilestoneId
+		INNER JOIN dbo.Project P ON P.ProjectId = M.ProjectId
+		WHERE MP.PersonId = @PersonId AND P.ProjectStatusId IN (1,2,3,4) AND MPE.IsBadgeRequired = 1 AND @OldStartDate <= MPE.BadgeEndDate AND MPE.BadgeStartDate <= @ModifiedStartDate
+
+	END
+
+	SELECT @ProjectsExists
+
+END
