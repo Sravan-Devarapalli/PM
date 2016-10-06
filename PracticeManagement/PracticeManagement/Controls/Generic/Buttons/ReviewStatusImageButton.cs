e8fbdceae479b9856f362185567daf52afee3c72
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/Generic/Buttons/ReviewStatusImageButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using DataTransferObjects.TimeEntry;
+
+namespace PraticeManagement.Controls.Generic.Buttons
+{
+    public class ReviewStatusImageButton : MultistateImageButton<ReviewStatus>
+    {
+        private static readonly Dictionary<ReviewStatus, KeyValuePair<string, string>> Filenames;
+
+        static ReviewStatusImageButton()
+        {
+            Filenames = new Dictionary<ReviewStatus, KeyValuePair<string, string>>
+                            {
+                                {ReviewStatus.Pending, new KeyValuePair<string, string>(
+                                                        "~\\Images\\pending_16.png", "Pending")},
+                                {ReviewStatus.Declined, new KeyValuePair<string, string>(
+                                                        "~\\Images\\declined_16.png", "Declined")},
+                                {ReviewStatus.Approved, new KeyValuePair<string, string>(
+                                                        "~\\Images\\accepted_16.png", "Approved")}
+                            };
+        }
+
+        #region Overrides of MultistateImageButton<ReviewStatus>
+
+        protected override Dictionary<ReviewStatus, KeyValuePair<string, string>> FilenameMapping
+        {
+            get { return Filenames; }
+        }
+
+        public override string EntityName
+        {
+            get { return Constants.EntityNames.ReviewStatusEntity; }
+        }
+
+        #endregion
+    }
+}
+
+
+
