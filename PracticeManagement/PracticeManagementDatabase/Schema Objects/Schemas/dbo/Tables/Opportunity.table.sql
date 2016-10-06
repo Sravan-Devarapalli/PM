cb41ca771fc3c7545b86497677f87ed574d5e565
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Opportunity.table.sql
@@ -0,0 +1,32 @@
+﻿CREATE TABLE [dbo].[Opportunity] (
+    [OpportunityId]       INT            IDENTITY (1, 1) NOT NULL,
+    [Name]                NVARCHAR (50)  NOT NULL,
+    [ClientId]            INT            NOT NULL,
+    [SalespersonId]       INT            NULL,
+    [OpportunityStatusId] INT            NOT NULL,
+    [Priority]            CHAR (1)       NULL,
+    [ProjectedStartDate]  DATETIME       NULL,
+    [ProjectedEndDate]    DATETIME       NULL,
+    [OpportunityNumber]   NVARCHAR (12)  NOT NULL,
+    [Description]         NVARCHAR (MAX) NULL,
+    [PracticeId]          INT            NOT NULL,
+    [BuyerName]           NVARCHAR (100) NULL,
+    [CreateDate]          DATETIME       NOT NULL,
+    [Pipeline]            NVARCHAR (512) NULL,
+    [Proposed]            NVARCHAR (512) NULL,
+    [SendOut]             NVARCHAR (512) NULL,
+    [ProjectId]           INT            NULL,
+    [OpportunityIndex]    INT            NULL,
+    [RevenueType]         INT            NOT NULL,
+    [OwnerId]             INT            NULL,
+    [GroupId]             INT            NULL,
+    [LastUpdated]         DATETIME       NOT NULL,
+	[EstimatedRevenue]    DECIMAL(18, 2) NULL,
+	[OutSideResources]	  NVARCHAR(4000) NULL,
+	PriorityId			  INT NOT NULL,
+	[CloseDate]			  DATETIME       NULL,
+	[PricingListId]       INT			 NULL,
+	[BusinessTypeId]	  INT	    	 NULL
+);
+ 
+
