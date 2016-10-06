e0558168e2a3c0a8668c2f9f3de7eb1632081c8f
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Timezone.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// Represents a TimeZone entity
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class Timezone
+    {
+        /// <summary>
+        /// Gets or sets a <see cref="TimeZone"/> Id value.
+        /// </summary>
+        [DataMember]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a  <see cref="TimeZone"/> GMT value.
+        /// </summary>
+        [DataMember]
+        public string GMT
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets GMTName <see cref="TimeZone"/>.
+        /// </summary>
+        [DataMember]
+        public string GMTName
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a <see cref="TimeZone"/> IsActive value.
+        /// </summary>
+        [DataMember]
+        public bool IsActive
+        {
+            get;
+            set;
+        }
+    }
+}
