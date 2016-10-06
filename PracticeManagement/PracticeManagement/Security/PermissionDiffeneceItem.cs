4a85de9781d0a0e07ccaa2cb5ecd1cf11ae87c85
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Security/PermissionDiffeneceItem.cs
@@ -0,0 +1,11 @@
+﻿namespace PraticeManagement.Security
+{
+    public class PermissionDiffeneceItem
+    {
+        public string Title { get; set; }
+        public bool Old { get; set; }
+        public bool New { get; set; }
+        public bool IsDifferent { get { return Old != New; } }
+    }
+}
+
