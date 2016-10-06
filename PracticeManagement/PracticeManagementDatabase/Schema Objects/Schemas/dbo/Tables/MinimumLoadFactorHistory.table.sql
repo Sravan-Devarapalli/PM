cf0c9619fb43213692764bafbd46643721487c5e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/MinimumLoadFactorHistory.table.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[MinimumLoadFactorHistory](
+	[OverheadFixedRateId] [int] NOT NULL,
+	[TimescaleId] [int] NOT NULL,
+	[Rate] [decimal](18, 5) NULL,
+	[StartDate]	DATETIME NOT NULL,
+	[EndDate]	DATETIME NULL,
+ CONSTRAINT [PK_MinimumLoadFactorHistory] PRIMARY KEY CLUSTERED 
+(
+	[OverheadFixedRateId] ASC,
+	[TimescaleId] ASC,
+	[StartDate] ASC
+)
+)
+
