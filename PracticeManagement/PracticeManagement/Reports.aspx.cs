044785e8705cb16a0267b849b6b72340a1ee6b2e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Reports.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Security;
+
+namespace PraticeManagement
+{
+	public partial class Reports : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+           
+        }
+	}
+}
+
