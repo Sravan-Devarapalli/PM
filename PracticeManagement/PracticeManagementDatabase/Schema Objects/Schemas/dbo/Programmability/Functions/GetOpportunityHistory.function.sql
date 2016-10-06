9ec9ad0a0b4b782b6774b9daa6f39096f3eaf1f4
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetOpportunityHistory.function.sql
@@ -0,0 +1,24 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 28 Sept. 2009
+-- Description:	Converts opportunity history rows into string
+-- =============================================
+CREATE FUNCTION dbo.GetOpportunityHistory 
+(
+	@optId int
+)
+RETURNS VARCHAR(5000)
+AS
+BEGIN
+	DECLARE @history VARCHAR(5000)
+	SET @history = ''
+
+	SELECT @history = @history + [Status] + ' ' + CONVERT(VARCHAR(20), TransitionDate, 100) + ' by ' + Responsible + ', Note: ' + NoteText + '. | '
+	FROM v_OpportunityHistory
+	WHERE OpportunityId = @optId
+	ORDER BY TransitionDate
+
+	RETURN @history
+
+END
+
