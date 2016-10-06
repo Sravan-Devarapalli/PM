529f220570cd9665d15286afe1d8dc47755525eb
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetNumberHolidayDaysWithWeekends.function.sql
@@ -0,0 +1,25 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2009-10-29
+-- Description:	Gets number of holiday days for the person
+-- =============================================
+CREATE FUNCTION [dbo].[GetNumberHolidayDaysWithWeekends]
+(
+	@PersonId INT,
+	@startDate DATETIME,
+	@endDate DATETIME
+)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @res INT
+
+	SELECT @res = COUNT(*) 
+	FROM dbo.v_PersonCalendar
+	WHERE Date BETWEEN @startDate AND @endDate AND 
+		  @PersonId = PersonId AND 
+		  DayOff = 1 AND CompanyDayOff = 1
+
+	RETURN @res
+END
+
