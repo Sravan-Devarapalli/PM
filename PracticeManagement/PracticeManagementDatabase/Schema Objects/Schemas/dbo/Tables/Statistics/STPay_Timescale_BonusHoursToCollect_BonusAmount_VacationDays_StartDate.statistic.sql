cb7a2828b3101c0e5883a26a68933c045fa5d038
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Statistics/STPay_Timescale_BonusHoursToCollect_BonusAmount_VacationDays_StartDate.statistic.sql
@@ -0,0 +1,3 @@
+﻿CREATE STATISTICS STPay_Timescale_BonusHoursToCollect_BonusAmount_VacationDays_StartDate 
+	ON [dbo].[Pay]([Timescale], [BonusHoursToCollect], [BonusAmount], [VacationDays], [StartDate])
+
