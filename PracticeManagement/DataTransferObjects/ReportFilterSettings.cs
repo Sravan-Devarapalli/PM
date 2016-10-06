407cf3f92c23479d639aec9ee83f60b9d0f0dfc0
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ReportFilterSettings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTransferObjects
+{
+    [Serializable]
+    public class ReportFilterSettings
+    {
+        public List<int> ClientIds { get; set; }
+        public List<int> BusinessUnitIds { get; set; }
+        public List<int> PayTypeIds { get; set; }
+        public List<int> ProjectAccessPeopleIds { get; set; }
+        public List<int> PersonStatusIds { get; set; }
+        public List<int> PracticeIds { get; set; }
+        public List<int> ProjectStatusIds { get; set; }
+        public List<int> SalesPersonIds { get; set; }
+        public string ReportPeriod { get; set; }
+        public DateTime? ReportStartDate { get; set; }
+        public DateTime? ReportEndDate { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsInactive { get; set; }
+        public bool IsInternal { get; set; }
+        public bool IsProposed { get; set; }
+        public bool IsProjected { get; set; }
+        public bool IsCompleted { get; set; }
+        public bool IsExperimental { get; set; }
+        public bool ExcludeInternalPractices { get; set; }
+
+    }
+}
+
