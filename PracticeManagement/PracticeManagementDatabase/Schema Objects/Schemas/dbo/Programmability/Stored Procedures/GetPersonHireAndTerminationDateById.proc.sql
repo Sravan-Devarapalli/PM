04614db09e6d1c34b9d9e10ba5318767b8ade219
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetPersonHireAndTerminationDateById.proc.sql
@@ -0,0 +1,23 @@
+﻿-- =============================================
+-- Author:		Shwetha
+-- Create date: 10-07-2012
+-- Last Updated by:	
+-- Last Update date:
+-- Read Hire Date and Termination Date for a person.	
+-- =============================================
+CREATE PROCEDURE [dbo].[GetPersonHireAndTerminationDateById]
+(
+	@PersonId	INT
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT p.HireDate,
+	       p.TerminationDate
+	  FROM dbo.Person AS p
+	 WHERE p.PersonId = @PersonId
+END
+
+GO
+
