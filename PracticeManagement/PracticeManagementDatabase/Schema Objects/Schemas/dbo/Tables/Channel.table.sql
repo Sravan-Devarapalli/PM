748d3170ddd5e9c42d9afd93c70710b66b2e56d4
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Channel.table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Channel]
+(
+	[ChannelId]    INT             IDENTITY (1, 1) NOT NULL,
+	[ChannelName]  NVARCHAR (50)   NOT NULL,
+	[IsDefault]	   BIT			   NOT NULL CONSTRAINT DF_Channel_IsDefault DEFAULT 0,
+	[IsSubChannelNamePicker]	   BIT			   NOT NULL CONSTRAINT DF_Channel_IsSubChannelNamePicker DEFAULT 0,
+	[IsSubChannelVendor]			BIT				NOT NULL CONSTRAINT DF_Channel_IsSubChannelVendor DEFAULT 0,
+	CONSTRAINT [PK_Channel_ChannelId]	PRIMARY KEY ([ChannelId])
+)
+
