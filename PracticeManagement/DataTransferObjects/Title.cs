597fb426319f2c43b254cc0f5dbce4f4977295dd
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Title.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace DataTransferObjects
+{
+    [Serializable]
+    [DataContract]
+    public class Title
+    {
+        [DataMember]
+        public int TitleId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string TitleName
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public TitleType TitleType
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int SortOrder
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int PTOAccrual
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int? MinimumSalary
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int? MaximumSalary
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public bool InUse
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Title Parent
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int PositionId
+        {
+            get;
+            set;
+        }
+
+        public string HtmlEncodedTitleName
+        {
+            get
+            {
+                return HttpUtility.HtmlEncode(TitleName);
+            }
+        }
+    }
+}
+
