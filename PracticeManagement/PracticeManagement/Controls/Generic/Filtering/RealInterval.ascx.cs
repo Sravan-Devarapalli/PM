59a0e0b8ac0473a4a27fe37ceba14722f9a853ec
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/Generic/Filtering/RealInterval.ascx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Web.UI.WebControls;
+
+namespace PraticeManagement.Controls.Generic.Filtering
+{
+    public partial class RealInterval : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public double MinValue
+        {
+            get
+            {
+                return ParseDouble(tbFrom, 0.0);
+            }
+            set
+            {
+                tbFrom.Text = value.ToString();
+            }
+        }
+
+        public double MaxValue
+        {
+            get
+            {
+                return ParseDouble(tbTo, 24.0);
+            }
+            set
+            {
+                tbTo.Text = value.ToString();
+            }
+        }
+
+        private static double ParseDouble(TextBox sourceTextBox, double defaultValue)
+        {
+            try
+            {
+                return Convert.ToDouble(sourceTextBox.Text);
+            }
+            catch
+            {
+
+                return defaultValue;
+            }
+        }
+
+        public void Reset()
+        {
+            tbTo.Text = "24.0";
+            tbFrom.Text = "0.0";
+        }
+    }
+}
+
