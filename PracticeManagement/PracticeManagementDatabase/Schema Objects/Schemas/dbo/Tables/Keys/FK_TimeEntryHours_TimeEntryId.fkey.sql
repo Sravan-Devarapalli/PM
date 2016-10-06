762827480c7a697c38c9719e78e96f78fe85dc7c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_TimeEntryHours_TimeEntryId.fkey.sql
@@ -0,0 +1,5 @@
+﻿ALTER TABLE [dbo].[TimeEntryHours]
+	ADD CONSTRAINT [FK_TimeEntryHours_TimeEntryId] 
+	FOREIGN KEY (TimeEntryId)
+	REFERENCES TimeEntry (TimeEntryId)  ON DELETE NO ACTION ON UPDATE NO ACTION;
+
