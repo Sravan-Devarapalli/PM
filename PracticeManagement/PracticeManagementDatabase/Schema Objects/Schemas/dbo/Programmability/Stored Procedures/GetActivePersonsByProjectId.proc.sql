0433a30ba45e605ae3387ab6383955a102e7d35c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetActivePersonsByProjectId.proc.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [dbo].[GetActivePersonsByProjectId]
+(
+	@ProjectId	INT
+)
+AS
+BEGIN
+  
+  DECLARE	@W2SalaryId			INT,
+			@W2HourlyId			INT,
+			@ProjectStartDate	DATETIME,
+			@ProjectEndDate		DATETIME
+
+  SELECT	@W2SalaryId = TimescaleId FROM dbo.Timescale WHERE Name = 'W2-Salary'
+  SELECT	@W2HourlyId = TimescaleId FROM dbo.Timescale WHERE Name = 'W2-Hourly'
+  SELECT    @ProjectStartDate = StartDate,@ProjectEndDate = EndDate FROM dbo.Project WHERE ProjectId = @ProjectId
+   
+  SELECT	DISTINCT P.PersonId,
+			P.FirstName,
+			P.LastName
+  FROM		dbo.Person P 
+  INNER JOIN v_Pay pay ON pay.PersonId = P.PersonId AND  (@ProjectStartDate < pay.EndDateOrig) AND (pay.StartDate <= @ProjectEndDate)
+  INNER JOIN v_DivisionHistory DH ON DH.PersonId = P.PersonId AND (DH.EndDate IS NULL OR @ProjectStartDate < DH.EndDate) AND (DH.StartDate <= @ProjectEndDate) AND ISNULL(DH.DivisionId,0) IN (SELECT DivisionId FROM dbo.PersonDivision WHERE DivisionName IN ('Consulting','Business Development','Technology Consulting','Business Consulting','Director'))
+  WHERE	    P.IsStrawman = 0 --Active and Terminated Status
+			AND pay.Timescale IN (@W2SalaryId,@W2HourlyId) 
+END
+
