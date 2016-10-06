c4deb1a1df8b88a17cdbb8d6618c45a115cef987
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Config/Branding.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Drawing;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PraticeManagement.Configuration;
+using PraticeManagement.Controls;
+
+namespace PraticeManagement.Config
+{
+    public partial class Branding : PracticeManagementPageBase
+    {
+        protected override void Display()
+        {
+        }
+    }
+}
+
