9cdc13d5c8c9b1aad39ce5e46efdbb43514965e4
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/WeekPaidOption.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// Represents a Week Paid Option database entity
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class WeekPaidOption
+    {
+        /// <summary>
+        /// Gets or sets an ID of the Week Paid Option.
+        /// </summary>
+        [DataMember]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a name of the Week Paid Option.
+        /// </summary>
+        [DataMember]
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
