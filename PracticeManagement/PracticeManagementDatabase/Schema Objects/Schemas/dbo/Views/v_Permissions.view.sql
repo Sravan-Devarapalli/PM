07d7c436aa50d9a37b409de788c0bd0f1832e378
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_Permissions.view.sql
@@ -0,0 +1,6 @@
+﻿CREATE view dbo.v_Permissions
+as
+select perm.PersonID, perm.TargetId, targ.* 
+from dbo.permission as perm
+left join dbo.permissiontarget as targ on perm.targettype = targ.PermissionTypeID
+
