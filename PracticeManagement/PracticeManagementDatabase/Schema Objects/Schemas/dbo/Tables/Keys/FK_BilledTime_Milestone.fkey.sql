1b98cd1ac058777a700907f4ebce5fe7ea664463
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_BilledTime_Milestone.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[BilledTime]
+    ADD CONSTRAINT [FK_BilledTime_Milestone] FOREIGN KEY ([MilestoneId]) REFERENCES [dbo].[Milestone] ([MilestoneId]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
