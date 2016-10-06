65ad60b44a70678996d4b15ddf7f3c6d31db5f50
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/TimeEntry/WeekChangedEventArguments.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PraticeManagement.Controls.TimeEntry
+{
+    public class WeekChangedEventArgs : EventArgs
+    {
+        private DateTime startDate;
+        private DateTime endDate;
+
+        /// <summary>
+        /// Init constructor of WeekChangedEventArguments.
+        /// </summary>
+        public WeekChangedEventArgs(System.DateTime startDate, System.DateTime endDate)
+        {
+            this.startDate = startDate;
+            this.endDate = endDate;
+        }
+
+
+        #region Properties
+        public System.DateTime StartDate
+        {
+            get
+            {
+                return this.startDate;
+            }
+        }
+
+        public System.DateTime EndDate
+        {
+            get
+            {
+                return this.endDate;
+            }
+        }
+        #endregion
+    }
+}
+
