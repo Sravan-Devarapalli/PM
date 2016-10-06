b83ad5df55daedb0c60d40a7b0a1435542ac846d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/OverheadRateType.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// Represents the OverheadRateType entity.
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class OverheadRateType
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets an ID of the <see cref="OverheadRateType"/> entity.
+        /// </summary>
+        [DataMember]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a name of the <see cref="OverheadRateType"/> entity.
+        /// </summary>
+        [DataMember]
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets if the <see cref="OverheadRateType"/> entity is for a percentage rate.
+        /// </summary>
+        [DataMember]
+        public bool IsPercentage
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a number of the hours the <see cref="OverheadRateType"/> entity is for.
+        /// </summary>
+        [DataMember]
+        public int HoursToCollect
+        {
+            get;
+            set;
+        }
+
+        #endregion Properties
+    }
+}
