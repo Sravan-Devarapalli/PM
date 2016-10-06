c1ada41bca4fe65ac2a3a213be73651c585e1f0b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ProjectTimeType.table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ProjectTimeType]
+(
+	[ProjectId]			INT NOT NULL,
+	[TimeTypeId]        INT NOT NULL,
+	[IsAllowedToShow]   BIT NOT NULL,
+	CONSTRAINT PK_ProjectTimeType_ProjectIdAndTimeTypeId PRIMARY KEY CLUSTERED ([ProjectId] ASC,[TimeTypeId] ASC)
+);
+
