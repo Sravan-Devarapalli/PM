1504568e414bb8d78a558ec7314fbd4880eb6e34
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MilestonePersonEntryListByMilestonePersonId.proc.sql
@@ -0,0 +1,29 @@
+﻿
+CREATE PROCEDURE dbo.MilestonePersonEntryListByMilestonePersonId
+(
+	@MilestonePersonId   INT
+)
+AS
+	SET NOCOUNT ON
+
+	SELECT mp.MilestonePersonId,
+		   mp.SeniorityId,
+           mp.PersonId,
+	       mp.StartDate,
+	       mp.EndDate,
+	       mp.PersonRoleId,
+	       mp.Amount,
+	       mp.HoursPerDay,
+	       mp.RoleName,
+		   mp.VacationDays,	
+	       mp.ExpectedHoursWithVacationDays,
+		   mp.Location,
+		   mp.LastName,
+		   mp.FirstName,
+		   mp.EntryId
+	  FROM dbo.v_MilestonePerson AS mp
+	 WHERE mp.MilestonePersonId = @MilestonePersonId
+
+	 SELECT  personid,Date,ActualHours 
+	 FROM dbo.[GetPersonTimeoffValuesByMilestoneId](NULL,NULL,@MilestonePersonId,NULL)
+
