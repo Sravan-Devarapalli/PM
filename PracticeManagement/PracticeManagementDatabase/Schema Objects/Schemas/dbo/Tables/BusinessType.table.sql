e7df1a5c3a9191020dbe45ef0ce9ee3e33c3ad4c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/BusinessType.table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[BusinessType]
+(
+	[BusinessTypeId]       INT            NOT NULL,
+	[Name]                   NVARCHAR (100)  NULL,       
+	PRIMARY KEY CLUSTERED ([BusinessTypeId] ASC) WITH (IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF)
+)
+
