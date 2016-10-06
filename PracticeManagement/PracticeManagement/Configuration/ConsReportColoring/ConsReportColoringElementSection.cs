32ae002343d62a4e57ca520eb3007d335f882285
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Configuration/ConsReportColoring/ConsReportColoringElementSection.cs
@@ -0,0 +1,179 @@
+﻿using System.Configuration;
+using System.Drawing;
+
+namespace PraticeManagement.Configuration.ConsReportColoring
+{
+    public class ConsReportColoringElementSection : ConfigurationSection
+    {
+        #region Constants
+
+        private const string COLORS_PROPERTY = "colors";
+        private const string Investment_COLORS_PROPERTY = "investmentResourceColors";
+        private const string COLORING_SECTION_NAME = "practiceManagement/consReportColoring";
+        private const string DEFAULT_COLOR_PROPERTY = "defaultColorValue";
+        private const string VACATION_COLOR_PROPERTY = "vacationColorValue";
+        private const string COMPANYHOLIDAY_COLOR_PROPERTY = "companyHolidayColorValue";
+        private const string HIRED_COLOR_PROPERTY = "hiredColorValue";       
+        private const string MILESTONE_COLOR_PROPERTY = "milestonesColorValue";
+        private const string OPPORTUNITY_COLOR_PROPERTY = "opportunityColorValue";
+        private const string VACATION_TITLE_PROPERTY = "vacationTitle";
+        private const string COMPANYHOLIDAYS_TITLE_PROPERTY = "companyHolidaysTitle";
+        private const string SEPARATOR_PROPERTY = "separator";
+
+        #endregion
+
+        public static ConsReportColoringElementSection ColorSettings
+        {
+            get
+            {
+                return ConfigurationManager.GetSection(COLORING_SECTION_NAME) as ConsReportColoringElementSection;
+            }
+        }
+
+        [ConfigurationProperty(COLORS_PROPERTY, IsDefaultCollection=true)]
+        public ConsReportColoringElementCollection Colors
+        {
+            get
+            {
+                return this[COLORS_PROPERTY] as ConsReportColoringElementCollection;
+            }
+        }
+
+        [ConfigurationProperty(Investment_COLORS_PROPERTY, IsDefaultCollection = true)]
+        public ConsReportColoringElementCollection InvestmentResourceColors
+        {
+            get
+            {
+                return this[Investment_COLORS_PROPERTY] as ConsReportColoringElementCollection;
+            }
+        }
+
+        [ConfigurationProperty(DEFAULT_COLOR_PROPERTY)]
+        protected string DefaultColorValue
+        {
+            get
+            {
+                return this[DEFAULT_COLOR_PROPERTY] as string;
+            }
+        }
+
+        [ConfigurationProperty(VACATION_TITLE_PROPERTY)]
+        public string VacationTitle
+        {
+            get
+            {
+                return this[VACATION_TITLE_PROPERTY] as string;
+            }
+        }
+
+        [ConfigurationProperty(COMPANYHOLIDAYS_TITLE_PROPERTY)]
+        public string CompanyHolidaysTitle
+        {
+            get
+            {
+                return this[COMPANYHOLIDAYS_TITLE_PROPERTY] as string;
+            }
+        }
+
+        [ConfigurationProperty(VACATION_COLOR_PROPERTY)]
+        protected string VacationColorValue
+        {
+            get
+            {
+                return this[VACATION_COLOR_PROPERTY] as string;
+            }
+        }
+
+        [ConfigurationProperty(COMPANYHOLIDAY_COLOR_PROPERTY)]
+        protected string CompanyHolidayColorValue
+        {
+            get
+            {
+                return this[COMPANYHOLIDAY_COLOR_PROPERTY] as string;
+            }
+        }
+
+        
+        [ConfigurationProperty(HIRED_COLOR_PROPERTY)]
+        protected string HiredColorValue
+        {
+            get
+            {
+                return this[HIRED_COLOR_PROPERTY] as string;
+            }
+        }
+
+        [ConfigurationProperty(MILESTONE_COLOR_PROPERTY)]
+        protected string MilestoneColorValue
+        {
+            get
+            {
+                return this[MILESTONE_COLOR_PROPERTY] as string;
+            }
+        }
+
+        [ConfigurationProperty(OPPORTUNITY_COLOR_PROPERTY)]
+        protected string OpportunityColorValue
+        {
+            get
+            {
+                return this[OPPORTUNITY_COLOR_PROPERTY] as string;
+            }
+        }
+
+        [ConfigurationProperty(SEPARATOR_PROPERTY)]
+        public string Separator
+        {
+            get
+            {
+                return this[SEPARATOR_PROPERTY] as string;
+            }
+        }
+
+        public Color VacationColor
+        {
+            get
+            {
+                return Utils.Coloring.StringToColor(
+                    VacationColorValue, Separator);
+            }
+        }
+
+        public Color CompanyHolidayColor
+        {
+            get
+            {
+                return Utils.Coloring.StringToColor(
+                    CompanyHolidayColorValue, Separator);
+            }
+        }
+
+        public Color HiredColor
+        {
+            get
+            {
+                return Utils.Coloring.StringToColor(
+                    HiredColorValue, Separator);
+            }
+        }
+
+        public Color MilestoneColor
+        {
+            get
+            {
+                return Utils.Coloring.StringToColor(
+                    MilestoneColorValue, Separator);
+            }
+        }
+
+        public Color OpportunityColor
+        {
+            get
+            {
+                return Utils.Coloring.StringToColor(
+                    OpportunityColorValue, Separator);
+            }
+        }
+    }
+}
+
