de648aabba8c6d8604ae51d7c5b0a318cbf03bf2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/IsOpportunityHaveTeamStructure.proc.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[IsOpportunityHaveTeamStructure]
+@OpportunityId INT	
+AS
+BEGIN
+	SELECT COUNT(*)
+	FROM  OpportunityPersons AS o 
+	WHERE o.OpportunityId = @OpportunityId
+END
+
