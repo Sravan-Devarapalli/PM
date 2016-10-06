34d3cf7c3ad6719f953d54a0fd5fb9d82c1988d6
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ClientMarginColorInfo.table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[ClientMarginColorInfo]
+(
+	Id              INT IDENTITY(1,1) NOT NULL,
+	ClientId		INT NOT NULL,
+	ColorId		    INT NOT NULL,
+	StartRange		INT NOT NULL,
+	EndRange		INT NOT NULL
+	
+	CONSTRAINT PK_ClientMarginColorInfo_Id PRIMARY KEY CLUSTERED(Id),
+    CONSTRAINT FK_ClientMarginColorInfo_ClientId FOREIGN KEY(ClientId) REFERENCES  dbo.Client(ClientId),
+	CONSTRAINT FK_ClientMarginColorInfo_ColorId FOREIGN KEY(ColorId) REFERENCES  dbo.Color([Id])
+); 
