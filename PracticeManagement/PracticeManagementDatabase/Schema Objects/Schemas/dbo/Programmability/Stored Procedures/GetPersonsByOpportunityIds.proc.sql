933d4ff8550421c603ad8410634192e94b7252da
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetPersonsByOpportunityIds.proc.sql
@@ -0,0 +1,40 @@
+﻿CREATE PROCEDURE dbo.GetPersonsByOpportunityIds
+(
+	@OpportunityIds NVARCHAR(MAX)
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	DECLARE @OpportunityIdsLocal NVARCHAR(MAX)
+	SELECT @OpportunityIdsLocal = @OpportunityIds
+	
+	DECLARE @OpportunityIdTable TABLE
+	(
+		OpportunityId INT
+	)
+	
+	INSERT INTO @OpportunityIdTable
+	SELECT ResultId 
+	FROM [dbo].[ConvertStringListIntoTable] (@OpportunityIdsLocal)
+	
+	SELECT   op.PersonId
+				,ISNULL(p.PreferredFirstName,p.FirstName) AS FirstName
+				,p.LastName,
+				op.OpportunityId,
+				op.OpportunityPersonTypeId,
+				op.RelationTypeId,
+				op.Quantity,
+				op.NeedBy,
+				p.PersonStatusId 
+		FROM dbo.OpportunityPersons op
+		INNER JOIN dbo.Person p ON p.PersonId = op.PersonId
+		INNER JOIN @OpportunityIdTable O ON O.OpportunityId = op.OpportunityId
+		WHERE (
+				p.PersonStatusId IN(1,3,5)
+				OR
+				p.IsStrawman = 1
+			)
+		ORDER BY op.OpportunityId
+END
+
