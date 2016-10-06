76a8c56ca2b1954ba10f85bec441a43f2cb1ecda
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Filters/TerminationReportFilters.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTransferObjects.Filters
+{
+    [Serializable]
+    public class TerminationReportFilters
+    {
+        public DateTime? ReportStartDate
+        {
+            get;
+            set;
+        }
+
+        public DateTime? ReportEndDate
+        {
+            get;
+            set;
+        }
+
+        public string ReportPeriod
+        {
+            get;
+            set;
+        }
+
+        public string PracticeIds
+        {
+            get;
+            set;
+        }
+
+        public bool ExcludeInternalPractices
+        {
+            get;
+            set;
+        }
+
+        public string PayTypeIds
+        {
+            get;
+            set;
+        }
+
+
+        public string TitleIds
+        {
+            get;
+            set;
+        }
+
+        public string TerminationReasonIds
+        {
+            get;
+            set;
+        }
+
+    }
+}
+
