8e9bc3c3adb6d3a8ee8aa8158e315a8ce5245729
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementService/IEMailDeliveryProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PracticeManagement
+{
+    public interface IEMailDeliveryProvider
+    {
+        void SendMail(List<string> to, List<string> cc, string from, string subject, string body);
+    }
+}
+
