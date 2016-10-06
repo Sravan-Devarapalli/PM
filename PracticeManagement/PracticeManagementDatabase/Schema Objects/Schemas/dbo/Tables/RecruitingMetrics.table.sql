a50a55e64ad280c701be7f264954ab67f59b7dba
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/RecruitingMetrics.table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[RecruitingMetrics]
+(
+    [RecruitingMetricsId]		INT			 IDENTITY (1, 1) NOT NULL,
+    [Name]						NVARCHAR(50) NOT NULL,
+	[RecruitingMetricsTypeId]	INT			 NOT NULL,
+	[SortOrder]					INT			 NOT NULL
+)
+
