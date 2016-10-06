d78e6bb5b2e11963762067a059f47929f9a315de
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/OpportunityPriorities.table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE dbo.OpportunityPriorities (
+    Id                   INT IDENTITY(1,1) NOT NULL,
+    Priority			 NVARCHAR(255) NOT NULL,
+    Description          NVARCHAR(MAX) NULL,
+	sortOrder			 INT	,
+	[IsInserted]        bit NOT NULL,
+	DisplayName			NVARCHAR(15) NULL
+   CONSTRAINT PK_OpportunityPriorities_Id PRIMARY KEY CLUSTERED(Id)
+);
