20cb6e6a6064c05b30799659106f876ee2e79ebc
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/UserCredentials.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class UserCredentials
+    {
+        [DataMember]
+        public string UserName
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string PasswordSalt
+        {
+            get;
+            set;
+        }
+    }
+}
