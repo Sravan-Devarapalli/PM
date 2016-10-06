e6689fe446c83d740db4983c79ca3c6fdd3d87bd
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Settings.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using DataTransferObjects.Utils;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace DataTransferObjects
+{
+    public class Settings
+    {
+        #region Constants
+
+        private const string SenioritySeparationRangeValue = "senioritySeparationRangeValue";
+
+        #endregion Constants
+
+        #region Properties
+
+        public static int SenioritySeparationRange
+        {
+            get
+            {
+                return GetSenioritySeparationRange(SenioritySeparationRangeValue);
+            }
+        }
+
+        #endregion Properties
+
+        public static int GetSenioritySeparationRange(string key)
+        {
+            if (!IsAzureWebRole())
+            {
+                return Generic.GetIntConfiguration(SenioritySeparationRangeValue);
+            }
+            try
+            {
+                return Convert.ToInt32(RoleEnvironment.GetConfigurationSettingValue(key));
+            }
+            catch
+            {
+                return Generic.GetIntConfiguration(SenioritySeparationRangeValue);
+            }
+        }
+
+        private static Boolean IsAzureWebRole()
+        {
+            try
+            {
+                return RoleEnvironment.IsAvailable;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
