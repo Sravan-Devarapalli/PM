967b0961cf96752809f7547953655f7cbd53ea49
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Financials/QuarterRange.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTransferObjects.Financials
+{
+    public class QuarterRange
+    {
+        public RangeType QuarterRangeType { get; set; }
+
+        public int ProjectId { get; set; }
+
+        public List<ComputedFinancials> FinancialsList { get; set; }
+
+        public ComputedFinancials getSummedComputedFinancials()
+        {
+            var finalComputedFinancials = new ComputedFinancials();
+            finalComputedFinancials.FinancialDate = QuarterRangeType.StartDate;
+            finalComputedFinancials.FinancialRange = QuarterRangeType;
+            finalComputedFinancials.Revenue = FinancialsList.Sum(p=>p.Revenue);
+            finalComputedFinancials.GrossMargin = FinancialsList.Sum(p=>p.GrossMargin);
+            finalComputedFinancials.ActualRevenue = FinancialsList.Sum(p=>p.ActualRevenue);
+            finalComputedFinancials.ActualGrossMargin = FinancialsList.Sum(p => p.ActualGrossMargin);
+            finalComputedFinancials.FinancialRange = QuarterRangeType;
+            return finalComputedFinancials;
+        }
+
+    }
+}
+
