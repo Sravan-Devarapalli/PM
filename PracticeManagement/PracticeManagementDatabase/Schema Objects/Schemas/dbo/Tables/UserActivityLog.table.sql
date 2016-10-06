32e00610759c0a3dda8e37be6eaa0b6ae13d66ef
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/UserActivityLog.table.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[UserActivityLog] (
+    [ActivityID]      INT            IDENTITY (1, 1) NOT NULL,
+    [ActivityTypeID]  INT            NOT NULL,
+    [SessionID]       INT            NOT NULL,
+    [LogDate]         DATETIME       NOT NULL,
+    [SystemUser]      NVARCHAR (255) NOT NULL,
+    [Workstation]     NVARCHAR (128) NULL,
+    [ApplicationName] NVARCHAR (128) NULL,
+    [UserLogin]       NVARCHAR (255) NULL,
+    [PersonID]        INT            NULL,
+    [LastName]        NVARCHAR (100) NULL,
+    [FirstName]       NVARCHAR (100) NULL,
+    [LogData]         XML            NULL,
+	[Data]			  NVARCHAR(MAX)  NULL
+);
+
+
