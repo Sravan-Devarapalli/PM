005baa42f206c1d158fe48185d042f52c8f4284c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementService/IOverheadService.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+using DataTransferObjects;
+
+namespace PracticeManagementService
+{
+    [ServiceContract]
+    public interface IOverheadService
+    {
+        /// <summary>
+        /// Retrives the list of <see cref="OverheadRateType"/> objects.
+        /// </summary>
+        /// <returns>The list of the <see cref="OverheadRateType"/> objects.</returns>
+        [OperationContract]
+        List<OverheadRateType> GetRateTypes();
+
+        /// <summary>
+        /// Retrieves an overhead rate type info by the specified ID.
+        /// </summary>
+        /// <param name="overheadRateTypeId">An ID of the record to be reatrieved from.</param>
+        /// <returns>The <see cref="OverheadRateType"/> object if found and null otherwise.</returns>
+        [OperationContract]
+        OverheadRateType GetRateTypeDetail(int overheadRateTypeId);
+
+        /// <summary>
+        /// Retrieves the list of the <see cref="OverheadFixedRate"/> objects.
+        /// </summary>
+        /// <returns>The list of the <see cref="OverheadFixedRate"/> objects.</returns>
+        [OperationContract]
+        List<OverheadFixedRate> GetOverheadFixedRates(bool activeOnly);
+
+        /// <summary>
+        /// Retrieves the <see cref="OverheadFixedRate"/> object with the specified ID.
+        /// </summary>
+        /// <param name="overheadId">The ID of the <see cref="OverheadFixedRate"/> to be retrived.</param>
+        /// <returns>The <see cref="OverheadFixedRate"/> object if found any or null otherwise.</returns>
+        [OperationContract]
+        OverheadFixedRate GetOverheadFixedRateDetail(int overheadId);
+
+        /// <summary>
+        /// Saves the <see cref="OverheadFixedRate"/> data into the database.
+        /// </summary>
+        /// <param name="overhead">The <see cref="OverheadFixedRate"/> data to be saved to.</param>
+        [OperationContract]
+        int? SaveOverheadFixedRateDetail(OverheadFixedRate overhead);
+
+        /// <summary>
+        /// Deactivates an overhead with the specified ID.
+        /// </summary>
+        /// <param name="overheadId">The ID of the overhead to be deactivated.</param>
+        [OperationContract]
+        void OverheadFixedRateInactivate(int overheadId);
+
+        /// <summary>
+        /// Activates an overhead with the specified ID.
+        /// </summary>
+        /// <param name="overheadId">The ID of the overhead to be activated.</param>
+        [OperationContract]
+        void OverheadFixedRateReactivate(int overheadId);
+
+        /// <summary>
+        /// Gets the Rate for each time scale type of a Overhead specified by description.
+        /// </summary>
+        /// <param name="OverHeadName"></param>
+        /// <param name="?"></param>
+        /// <returns></returns>
+        [OperationContract]
+        Dictionary<int, decimal> GetMinimumLoadFactorOverheadMultipliers(string OverHeadName, ref bool isInactive);
+
+        [OperationContract]
+        void UpdateMinimumLoadFactorHistory(int timeScaleId, decimal rate);
+
+        [OperationContract]
+        void UpdateMinimumLoadFactorStatus(bool inActive);
+
+        [OperationContract]
+        List<OverHeadHistory> GetOverheadHistory();
+    }
+}
