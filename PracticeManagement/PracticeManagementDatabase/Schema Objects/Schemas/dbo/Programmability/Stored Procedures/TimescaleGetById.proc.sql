873abf8ae4c93666a29246839de5ea3aab1ec1d8
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimescaleGetById.proc.sql
@@ -0,0 +1,19 @@
+﻿--The source is 
+-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 7-3-2008
+-- Updated by:	
+-- Update date: 
+-- Description:	Retrives a timescale by its ID.
+-- =============================================
+CREATE PROCEDURE [dbo].[TimescaleGetById]
+(
+	@TimescaleId   INT
+)
+AS
+	SET NOCOUNT ON
+
+	SELECT t.TimescaleId, t.Name
+	  FROM dbo.Timescale AS t
+	 WHERE t.TimescaleId = @TimescaleId
+
