96339b87df07e019815c2a8afffb31fbbb296909
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetClientMarginColorInfo.proc.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[GetClientMarginColorInfo]
+(
+	@ClientId INT
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	SELECT cmci.[ColorId]
+		  ,cmci.[StartRange]
+		  ,cmci.[EndRange]
+		  ,color.Value
+		  ,color.Description
+	  FROM [dbo].[ClientMarginColorInfo] AS cmci 
+	  INNER JOIN dbo.Color color ON cmci.[ColorId] = color.Id
+	  WHERE cmci.ClientId =@ClientId
+	  ORDER BY cmci.StartRange
+END
+GO
