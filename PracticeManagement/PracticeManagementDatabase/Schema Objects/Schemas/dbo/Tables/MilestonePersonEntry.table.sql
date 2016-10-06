18bb822a3c293ea9c571c58bac28bd8417a00062
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/MilestonePersonEntry.table.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[MilestonePersonEntry] (
+    [Id]                INT IDENTITY(1,1) NOT NULL,
+    [MilestonePersonId] INT             NOT NULL,
+    [StartDate]         DATETIME        NOT NULL,
+    [EndDate]           DATETIME        NULL,
+    [PersonRoleId]      INT             NULL,
+    [Amount]            DECIMAL (18, 2) NULL,
+	[Location]			NVARCHAR(20)    NULL,
+    [HoursPerDay]       DECIMAL (4, 2)  NOT NULL,
+	IsBadgeRequired		BIT				NULL,
+	BadgeStartDate		DATETIME		NULL,
+	BadgeEndDate		DATETIME		NULL,
+	IsBadgeException	BIT				NULL,
+	IsApproved			BIT				NULL,
+	BadgeRequestDate	DATETIME		NULL,
+	Requester			INT				NULL
+);
+
+ 
+
