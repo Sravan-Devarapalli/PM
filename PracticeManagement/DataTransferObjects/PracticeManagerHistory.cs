5189d5351f7685131276e91b0624546fa1f79ce0
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/PracticeManagerHistory.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class PracticeManagerHistory
+    {
+        [DataMember]
+        public int PracticeId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int PracticeManagerId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string PracticeManagerLastName
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string PracticeManagerFirstName
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Date from which the practice manager owned up the practice.
+        /// </summary>
+        [DataMember]
+        public DateTime StartDate
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// End Date, the practice manager owned up the practice.
+        /// </summary>
+        [DataMember]
+        public DateTime EndDate
+        {
+            get;
+            set;
+        }
+    }
+}
