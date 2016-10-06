afa1dee672fb49e841e7d687e6bf321afe97545c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetProjectAttachments.proc.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE GetProjectAttachments
+(
+	@ProjectId INT
+)
+AS
+BEGIN
+	SELECT Id,
+			FileName,
+			DATALENGTH(AttachmentData) AS AttachmentSize,
+			UploadedDate,
+			PA.CategoryId,
+			P.PersonId,
+			P.LastName,
+			P.FirstName
+	FROM dbo.ProjectAttachment PA
+	LEFT JOIN dbo.Person P ON PA.ModifiedBy = P.PersonId
+	WHERE ProjectId = @ProjectId
+END
