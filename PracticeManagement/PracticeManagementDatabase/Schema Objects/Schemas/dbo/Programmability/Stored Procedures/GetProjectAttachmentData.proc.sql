6b7ba18a9f8e40d0a4e07e02d0abb820e346764b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetProjectAttachmentData.proc.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE dbo.GetProjectAttachmentData
+(
+	@ProjectId	         INT,
+	@AttachmentId		 INT
+)
+AS
+BEGIN
+	SELECT TOP(1) pa.AttachmentData 
+	FROM dbo.ProjectAttachment AS pa
+	WHERE pa.ProjectId = @ProjectId
+		AND pa.Id = @AttachmentId
+END
+
