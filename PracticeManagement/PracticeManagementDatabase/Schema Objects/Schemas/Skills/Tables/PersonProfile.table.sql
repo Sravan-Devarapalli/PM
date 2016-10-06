52d93513d817b8db2801be270974777dc79f904a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/PersonProfile.table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [Skills].[PersonProfile]
+(
+	Id				  INT   IDENTITY (1, 1) NOT NULL,
+	PersonId          INT					NOT NULL,
+	ProfileName		  NVARCHAR (50)			NOT NULL,
+	ProfileUrl		  NVARCHAR (MAX)		NOT	NULL,
+	IsDefault		  BIT					NOT NULL CONSTRAINT DF_PersonProfile_IsDefault DEFAULT 0,
+	ModifiedBy        INT					NOT NULL,
+	CreatedDate	      DATETIME				NOT	NULL,
+	ModifiedDate	  DATETIME				NOT	NULL,
+	CONSTRAINT PK_PersonProfile_Id PRIMARY KEY CLUSTERED (Id ASC)
+)
+
