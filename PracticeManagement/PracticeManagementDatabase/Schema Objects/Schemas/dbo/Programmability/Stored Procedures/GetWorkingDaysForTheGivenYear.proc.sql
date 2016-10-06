a67fb1a9e5e90adcdd60bfd73872ab855647d056
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetWorkingDaysForTheGivenYear.proc.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[GetWorkingDaysForTheGivenYear]
+(
+	@Year    INT
+)
+AS
+	SET NOCOUNT ON
+	DECLARE @DaysInYear INT = 260
+	SELECT @DaysInYear = C.DaysInYear FROM dbo.Calendar C WHERE C.Year = @Year
+
+	SELECT @DaysInYear AS DaysInYear
