630654e09c0e947167a5358240b515eda4889cf8
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetAllPayTypes.proc.sql
@@ -0,0 +1,13 @@
+﻿-- =============================================
+-- Author:		Sainath CH
+-- Create date: 03-13-2012
+-- Description: Get ALL Pay Types
+-- =============================================
+CREATE PROCEDURE [dbo].[GetAllPayTypes]
+AS
+BEGIN
+	SELECT  t.TimescaleId ,
+			t.Name AS Timescale
+	FROM dbo.Timescale t
+END
+
