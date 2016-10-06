eae30b1987ebc58cc29bb661ffee976586381b94
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ContextObjects/ProjectCloningContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.ContextObjects
+{
+    /// <summary>
+    /// Pepresents project cloning context
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    [DebuggerDisplay("ProjectCloningContext: ")]
+    public class ProjectCloningContext
+    {
+        /// <summary>
+        /// Project to be cloned
+        /// </summary>
+        [DataMember]
+        public Project Project { get; set; }
+
+        /// <summary>
+        /// Whether to clone milestones too
+        /// </summary>
+        [DataMember]
+        public bool CloneMilestones { get; set; }
+
+        /// <summary>
+        /// Whether to clone commissions too
+        /// </summary>
+        [DataMember]
+        public bool CloneCommissions { get; set; }
+
+        /// <summary>
+        /// Project Status
+        /// </summary>
+        [DataMember]
+        public ProjectStatus ProjectStatus { get; set; }
+    }
+}
