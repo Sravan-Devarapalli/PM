756f7533d20f9b7a884e5dae4df1412feed141f5
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Scripts/ScrollinDropDown.yui.js
@@ -0,0 +1,84 @@
+﻿function scrollingDropdown_onclick(control, type, pluralform, pluralformType, maxNoOfCharacters) {
+    var temp = 0;
+    var text = "";
+    if (pluralform == undefined || pluralform == null || pluralform == '') {
+        pluralform = "s";
+    }
+    if (maxNoOfCharacters == undefined || maxNoOfCharacters == null || maxNoOfCharacters == '' || isNaN(maxNoOfCharacters)) {
+        maxNoOfCharacters = 33;
+    }
+    var scrollingDropdownList = document.getElementById(control.toString());
+    var arrayOfCheckBoxes = scrollingDropdownList.getElementsByTagName("input");
+    if (arrayOfCheckBoxes.length == 1 && arrayOfCheckBoxes[0].disabled) {
+        text = "No " + type.toString() + "s to select.";
+    }
+    else {
+        for (var i = 0; i < arrayOfCheckBoxes.length; i++) {
+            if (arrayOfCheckBoxes[i].checked) {
+                temp++;
+                text = arrayOfCheckBoxes[i].parentNode.childNodes[1].innerHTML;
+            }
+            if (temp > 1) {
+                if (pluralformType == undefined || pluralformType == null || pluralformType == '') {
+                    pluralformType = type.toString() + pluralform;
+                }
+                text = "Multiple " + pluralformType + " selected";
+
+            }
+            if (arrayOfCheckBoxes[0].checked) {
+                text = arrayOfCheckBoxes[0].parentNode.childNodes[1].innerHTML;
+            }
+            if (temp === 0) {
+                text = "-- Select " + type.toString() + "("+pluralform +") --";
+            }
+        }
+        var fulltext = text;
+        text = DecodeString(text);
+        var isLengthExceded = (text.length > maxNoOfCharacters);
+        scrollingDropdownList.parentNode.children[1].children[0].firstChild.nodeValue = isLengthExceded ? text.substr(0, maxNoOfCharacters-2) + ".." : text;
+        scrollingDropdownList.parentNode.children[1].children[0].firstChild.parentNode.attributes['title'].nodeValue = isLengthExceded ? fulltext : '';
+    }
+}
+
+function scrolling_onclick(control, type, pluralform, pluralformType, maxNoOfCharacters, firstWord, secondWord) {
+    var temp = 0;
+    var text = "";
+    if (pluralform == undefined || pluralform == null || pluralform == '') {
+        pluralform = "s";
+    }
+    if (maxNoOfCharacters == undefined || maxNoOfCharacters == null || maxNoOfCharacters == '' || isNaN(maxNoOfCharacters)) {
+        maxNoOfCharacters = 33;
+    }
+    var scrollingDropdownList = document.getElementById(control.toString());
+    var arrayOfCheckBoxes = scrollingDropdownList.getElementsByTagName("input");
+    if (arrayOfCheckBoxes.length == 1 && arrayOfCheckBoxes[0].disabled) {
+        text = "No " + type.toString() + "s to select.";
+    }
+    else {
+        for (var i = 0; i < arrayOfCheckBoxes.length; i++) {
+            if (arrayOfCheckBoxes[i].checked) {
+                temp++;
+                text = arrayOfCheckBoxes[i].parentNode.childNodes[1].innerHTML;
+            }
+            if (temp > 1) {
+                if (pluralformType == undefined || pluralformType == null || pluralformType == '') {
+                    pluralformType = type.toString() + pluralform;
+                }
+                text = "Multiple " + pluralformType + " selected";
+
+            }
+            if (arrayOfCheckBoxes[0].checked) {
+                text = arrayOfCheckBoxes[0].parentNode.childNodes[1].innerHTML;
+            }
+            if (temp === 0) {
+                text = "-- Select " + firstWord.toString() + "(" + pluralform + ") " + secondWord.toString() + "--";
+            }
+        }
+        var fulltext = text;
+        text = DecodeString(text);
+        var isLengthExceded = (text.length > maxNoOfCharacters);
+        scrollingDropdownList.parentNode.children[1].children[0].firstChild.nodeValue = isLengthExceded ? text.substr(0, maxNoOfCharacters - 2) + ".." : text;
+        scrollingDropdownList.parentNode.children[1].children[0].firstChild.parentNode.attributes['title'].nodeValue = isLengthExceded ? fulltext : '';
+    }
+}
+
