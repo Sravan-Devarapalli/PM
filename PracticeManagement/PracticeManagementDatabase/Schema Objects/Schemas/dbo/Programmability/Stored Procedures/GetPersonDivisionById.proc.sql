5de16d6f0179b9fee5553f0dc56c1e84f9e8b596
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetPersonDivisionById.proc.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[GetPersonDivisionById]
+(
+@DivisionId INT
+)
+	
+AS
+BEGIN
+	SET NOCOUNT ON	
+	SELECT Pd.DivisionId,
+		   Pd.DivisionName,
+	       Pd.Inactive,
+	       pd.ShowSetPracticeOwnerLink
+	FROM   PersonDivision Pd 
+	WHERE  Pd.DivisionId=@DivisionId
+END
