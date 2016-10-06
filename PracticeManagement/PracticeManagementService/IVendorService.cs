7b29cfcea8a15ffc12961f3bbc1b7addea86c36c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementService/IVendorService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using DataTransferObjects;
+
+namespace PracticeManagementService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IVendorService" in both code and config file together.
+    [ServiceContract]
+    public interface IVendorService
+    {
+        [OperationContract]
+        List<VendorType> GetListOfVendorTypes();
+
+        [OperationContract]
+        List<Vendor> GetAllActiveVendors();
+
+        [OperationContract]
+        List<Vendor> GetListOfVendorsWithFilters(bool active, bool inactive, string vendorTypes, string looked);
+
+        [OperationContract]
+        Vendor GetVendorById(int vendorId);
+
+        [OperationContract]
+        int SaveVendorDetail(Vendor vendor, string userName);
+
+        [OperationContract]
+        void VendorValidations(Vendor vendor);
+
+        [OperationContract]
+        List<VendorAttachment> GetVendorAttachments(int vendorId);
+
+        [OperationContract]
+        void SaveVendorAttachmentData(VendorAttachment attachment, int vendorId, string userName);
+
+        [OperationContract]
+        void DeleteVendorAttachmentById(int? attachmentId, int vendorId, string userName);
+
+        [OperationContract]
+        byte[] GetVendorAttachmentData(int vendorId, int attachmentId);
+
+        [OperationContract]
+        List<Project> ProjectListByVendor(int vendorId);
+
+        [OperationContract]
+        List<Person> PersonListByVendor(int vendorId);
+    }
+}
+
