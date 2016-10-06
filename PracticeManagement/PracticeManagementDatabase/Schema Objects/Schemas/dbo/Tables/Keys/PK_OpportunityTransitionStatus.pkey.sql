34143c619b97051e561d6a07a2eed9d7e3f6655c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/PK_OpportunityTransitionStatus.pkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[OpportunityTransitionStatus]
+    ADD CONSTRAINT [PK_OpportunityTransitionStatus] PRIMARY KEY CLUSTERED ([OpportunityTransitionStatusId] ASC) WITH (IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF);
+
+
