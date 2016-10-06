07567b1c4a80625a8f7c018723cdc1e1081df2bf
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Reports/TimeEntryAudit.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using DataTransferObjects.TimeEntry;
+
+namespace DataTransferObjects.Reports
+{
+    [DataContract]
+    [Serializable]
+    public class TimeEntryAudit
+    {
+        [DataMember]
+        public Person Person
+        {
+            get;
+            set;
+        }
+
+
+        [DataMember]
+        public DateTime AffectedDate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime ModifiedDate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public ChargeCode ChargeCode
+        {
+            get;
+            set;
+        }
+
+
+        [DataMember]
+        public double OldHours
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public double ActualHours
+        {
+            get;
+            set;
+        }
+
+        public double NetChange
+        {
+            get
+            {
+                return OldHours - ActualHours;
+            }
+        }
+
+    }
+}
+
