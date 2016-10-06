96f5333885207e87b83ce4bdf42081ccdbcb9d8d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Reports/ConsultantUtilTimeLineReport.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PraticeManagement.Reporting
+{
+    public partial class ConsultantUtilTimeLineReport : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int refreshValue = 3600;
+            int result;
+
+            if (Request.QueryString["Refresh"] != null)
+            {                
+                if (int.TryParse(Request.QueryString["Refresh"].ToString() ,out result))
+                {
+                    refreshValue = result * 60;
+                }                
+            }
+
+            this.Response.AppendHeader("Refresh", refreshValue.ToString());
+
+        }
+    }
+}
+
