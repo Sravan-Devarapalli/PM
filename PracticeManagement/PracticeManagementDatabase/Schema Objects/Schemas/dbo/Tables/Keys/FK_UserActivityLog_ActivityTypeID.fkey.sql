fe12324c51c50c08c3b3e557da49a15b0796f9fa
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_UserActivityLog_ActivityTypeID.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[UserActivityLog]
+    ADD CONSTRAINT [FK_UserActivityLog_ActivityTypeID] FOREIGN KEY ([ActivityTypeID]) REFERENCES [dbo].[UserActivityType] ([ActivityTypeID]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
