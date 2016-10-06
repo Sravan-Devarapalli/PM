c6ec74259f4d5d72b54e138916e97927a66e334d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DefaultRecruiterCommissionItemList.proc.sql
@@ -0,0 +1,19 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 9-02-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	Selects a list of the default recruiter commission items for the specified commission
+-- =============================================
+CREATE PROCEDURE [dbo].[DefaultRecruiterCommissionItemList]
+(
+	@DefaultRecruiterCommissionHeaderId   INT
+)
+AS
+	SET NOCOUNT ON
+
+	SELECT i.DefaultRecruiterCommissionHeaderId, i.HoursToCollect, i.Amount
+	  FROM dbo.DefaultRecruiterCommissionItem AS i
+	 WHERE i.DefaultRecruiterCommissionHeaderId = @DefaultRecruiterCommissionHeaderId
+	ORDER BY i.HoursToCollect
+
