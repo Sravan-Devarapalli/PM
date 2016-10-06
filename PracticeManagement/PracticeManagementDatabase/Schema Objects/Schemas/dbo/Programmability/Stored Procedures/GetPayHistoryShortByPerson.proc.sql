20de72c97cfe70a11fc46cfc2e8c10e0d02f0142
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetPayHistoryShortByPerson.proc.sql
@@ -0,0 +1,23 @@
+﻿-- =============================================
+-- Author:		ThulasiRam.P
+-- Create date: 05-10-2012
+-- Description:	Retrieves a payment history for the specified person.
+-- =============================================
+CREATE PROCEDURE [dbo].[GetPayHistoryShortByPerson]
+(
+	@PersonId   INT
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT P.StartDate,
+	       CASE WHEN P.EndDate IS NULL 
+		   THEN NULL ELSE  P.EndDate- 1 END AS [EndDate],
+	       P.Timescale
+	  FROM dbo.Pay AS P
+	  WHERE p.Person = @PersonId
+	  ORDER BY p.StartDate
+
+END
+
