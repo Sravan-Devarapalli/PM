79abdfd64f677d75176b0f2d7164135710f122c0
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Reports/WorkTypeLevelGroupedHours.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.Serialization;
+using DataTransferObjects.TimeEntry;
+
+namespace DataTransferObjects.Reports
+{
+    [DataContract]
+    [Serializable]
+    public class WorkTypeLevelGroupedHours
+    {
+        [DataMember]
+        public TimeTypeRecord WorkType
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public double BillableHours
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public double NonBillableHours
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public double ForecastedHours
+        {
+            get;
+            set;
+        }
+
+        public double TotalHours
+        {
+            get
+            {
+                return BillableHours + NonBillableHours;
+            }
+        }
+
+        public int WorkTypeTotalHoursPercent
+        {
+            get;
+            set;
+        }
+    }
+}
