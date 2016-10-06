5627ca46f220d3418cc2f0344045f6162aa77520
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/PersonQualification.table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [Skills].[PersonQualification](
+	[QualificationTypeId] int NOT NULL ,
+	[PersonId] [int] NOT NULL,
+	[Info] [nvarchar](max) NULL,
+	[DisplayOrder] [tinyint] NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_PersonQualification] PRIMARY KEY CLUSTERED 
+(
+	[QualificationTypeId] ASC,
+	[PersonId] ASC
+)
+)
