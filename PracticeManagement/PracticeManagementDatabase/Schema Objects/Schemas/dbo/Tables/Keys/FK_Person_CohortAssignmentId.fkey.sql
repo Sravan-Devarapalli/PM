9b50337ef383c8bad1c6f69c75b4be41b94b85dc
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_Person_CohortAssignmentId.fkey.sql
@@ -0,0 +1,6 @@
+﻿ALTER TABLE [dbo].[Person]
+	ADD CONSTRAINT [FK_Person_CohortAssignmentId] 
+	FOREIGN KEY (CohortAssignmentId)
+	REFERENCES [CohortAssignment] ([CohortAssignmentId]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
