9784e623de34bf2ecf9a37169ee3485b5dd4d46e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/NoteInsert.proc.sql
@@ -0,0 +1,39 @@
+﻿CREATE PROCEDURE [dbo].[NoteInsert]
+(
+	@TargetId		INT,
+	@NoteTargetId	INT,
+	@PersonId		INT,
+	@NoteText		NVARCHAR(MAX),
+	@NoteId         INT OUT 
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+	DECLARE @UserLogin NVARCHAR(255)
+
+	SELECT @UserLogin = Alias FROM dbo.Person WHERE PersonId = @PersonId
+
+	-- Start logging session
+	EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin 
+
+	INSERT INTO dbo.Note
+	            (
+	              TargetId, 
+	              NoteTargetId, 
+	              PersonId, 
+	              NoteText
+	             )
+	VALUES       (
+				  @TargetId, 
+				  @NoteTargetId, 
+				  @PersonId, 
+				  @NoteText
+				  )
+    SET @NoteId = SCOPE_IDENTITY()
+
+	-- End logging session
+	EXEC dbo.SessionLogUnprepare
+    
+	SELECT @NoteId
+
+END
