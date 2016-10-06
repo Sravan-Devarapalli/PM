bf91e42d88056d6b617a0e8ac5972f1184abf6ae
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/PersonPassword.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class PersonPassword
+    {
+        [DataMember]
+        public string PersonId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string Password
+        {
+            get;
+            set;
+        }
+    }
+}
+
