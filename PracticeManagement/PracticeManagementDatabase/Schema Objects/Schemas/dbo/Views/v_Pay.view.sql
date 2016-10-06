64984521dc220c68ddcb002b88557585c753e8db
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_Pay.view.sql
@@ -0,0 +1,44 @@
+﻿--The source is 
+-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 6-24-2008
+-- Updated by:	
+-- Update date: 
+-- Description:	Lists the payments
+-- =============================================
+CREATE VIEW [dbo].[v_Pay]
+AS
+	SELECT p.Person AS PersonId,
+	       p.StartDate,
+	       CASE
+	           WHEN p.EndDate < FT.FutureDate
+	           THEN p.EndDate
+	           ELSE CAST(NULL AS DATETIME)
+	       END AS EndDate,
+	       p.EndDate AS EndDateOrig,
+	       p.Amount,
+	       p.Timescale,
+	       CAST(CASE p.Timescale
+	                WHEN 2 THEN p.Amount / WHY.HoursInYear
+	                ELSE p.Amount
+	            END AS DECIMAL(18,2)) AS AmountHourly,
+	       p.VacationDays,
+	       p.BonusAmount,
+	       p.BonusHoursToCollect,
+	       CAST(CASE p.BonusHoursToCollect WHEN HPY.HoursPerYear THEN 1 ELSE 0 END AS BIT) AS IsYearBonus,
+	       t.Name AS TimescaleName,
+		   p.PracticeId,
+		   p.TitleId,
+		   p.SLTApproval,
+		   p.SLTPTOApproval,
+		   p.DivisionId,
+		   v.Id as 'VendorId',
+		   v.Name as 'VendorName'
+	  FROM dbo.Pay AS p
+		   INNER JOIN dbo.GetFutureDateTable() FT ON 1 = 1
+		   INNER JOIN dbo.[BonusHoursPerYearTable]() HPY ON 1 = 1
+		   INNER JOIN  dbo.V_WorkinHoursByYear WHY ON WHY.Year = YEAR(dbo.GettingPMTime(GETUTCDATE()))
+	       INNER JOIN dbo.Timescale AS t ON p.Timescale = t.TimescaleId
+		   LEFT JOIN dbo.Vendor AS v on v.Id=p.VendorId
+
+
