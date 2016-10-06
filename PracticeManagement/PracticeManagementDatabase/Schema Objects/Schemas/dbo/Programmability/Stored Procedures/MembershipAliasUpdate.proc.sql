d98f7a74699d271800e39e645cd4849e5e047ee9
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MembershipAliasUpdate.proc.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE dbo.MembershipAliasUpdate
+(
+	@OldAlias	NVARCHAR(256),
+	@NewAlias	NVARCHAR(256)
+)
+AS
+BEGIN
+
+	DECLARE @UserId  uniqueidentifier
+	SELECT @UserId = UserId
+	FROM aspnet_Users
+	WHERE UserName = @OldAlias
+	
+	UPDATE 	aspnet_Users
+	SET UserName = @NewAlias,
+		LoweredUserName = LOWER(@NewAlias)
+	WHERE UserId = @UserId
+	
+	UPDATE aspnet_Membership
+	SET Email = @NewAlias,
+		LoweredEmail = LOWER(@NewAlias)
+	WHERE UserId = @UserId
+
+END
