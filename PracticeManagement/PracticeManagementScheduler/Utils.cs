5cae2a92145f6928f12c93c889935084c9c5b75d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementScheduler/Utils.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using DataTransferObjects;
+using DataTransferObjects.ContextObjects;
+
+namespace PracticeManagementScheduler
+{
+    public class Utils
+    {
+        private const string TokenFormat = "<{0}>";
+
+        public static EmailData GetEmailData(EmailContext emailContext)
+        {
+            return ServiceCallers.Configuration(c => c.GetEmailData(emailContext));
+        }
+
+        public static string ReplaceValues(IEnumerable<string> tokens, DataRow row, string source)
+        {
+            foreach (var token in tokens)
+            {
+                var rowValue = row[token].ToString();
+                source = source.Replace(String.Format(TokenFormat, token), rowValue);
+            }
+
+            return source;
+        }
+
+        public static IEnumerable<Email> PrepareEmails(EmailData emailData)
+        {
+            if (!emailData.IsEmpty)
+            {
+                var headers = emailData.DataHeaders;
+
+                foreach (DataRow row in emailData.Data.Tables[0].Rows)
+                {
+                    var body = ReplaceValues(headers, row, emailData.EmailTemplate.Body);
+                    var subj = ReplaceValues(headers, row, emailData.EmailTemplate.Subject);
+
+                    yield return new Email
+                                     {
+                                         Body = body, 
+                                         Subject = subj
+                                     };
+                }
+            }
+        }
+    }
+}
+
