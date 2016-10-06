2d7ce00698293e6a5ef3426094df45a0e8a16181
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Configuration/TermsConfigurationSection.cs
@@ -0,0 +1,72 @@
+﻿using System.Configuration;
+
+namespace PraticeManagement.Configuration
+{
+	public class TermsConfigurationSection : ConfigurationSection
+	{
+		#region Constants
+
+		private const string TermsPropertyName = "terms";
+		private const string TermsSectionName = "practiceManagement/terms";
+
+		#endregion
+
+		#region Fields
+
+		private static ConfigurationProperty termsProperty =
+			new ConfigurationProperty(TermsPropertyName,
+				typeof(TermConfigurationElementCollection),
+				null,
+				ConfigurationPropertyOptions.IsDefaultCollection);
+		private static ConfigurationPropertyCollection propertiesValue;
+
+		#endregion
+
+		#region Properties
+
+		protected override ConfigurationPropertyCollection Properties
+		{
+			get
+			{
+				return propertiesValue;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets a collection of the <see cref="TermConfigurationElement"/> objects.
+		/// </summary>
+		[ConfigurationProperty(TermsPropertyName, IsDefaultCollection = true)]
+		public TermConfigurationElementCollection Terms
+		{
+			get
+			{
+				return (TermConfigurationElementCollection)this[TermsPropertyName];
+			}
+			set
+			{
+				this[TermsPropertyName] = value;
+			}
+		}
+
+		public static TermsConfigurationSection Current
+		{
+			get
+			{
+				return ConfigurationManager.GetSection(TermsSectionName) as TermsConfigurationSection;
+			}
+		}
+
+		#endregion
+
+		#region Construction
+
+		static TermsConfigurationSection()
+		{
+			propertiesValue = new ConfigurationPropertyCollection();
+			propertiesValue.Add(termsProperty);
+		}
+
+		#endregion
+	}
+}
+
