f46760981f1d73289bae495917a6084bffb1a4f9
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetSickLeaveTimeTypeId.function.sql
@@ -0,0 +1,12 @@
+﻿CREATE FUNCTION [dbo].[GetSickLeaveTimeTypeId]
+(
+)
+RETURNS INT
+AS
+BEGIN
+
+	RETURN (SELECT tt.TimeTypeId 
+			FROM TimeType tt 
+			WHERE tt.Code = 'W9311' /* Here 'W9311' is code of Sick/Safe Leave Work Type.*/
+			)
+END
