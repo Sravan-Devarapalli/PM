16cc5922029178392e525b5fe2fa3904c2f583c3
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimeEntryHoursByPersonProject.proc.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE TimeEntryHoursByPersonProject
+	@PersonId	INT,
+	@StartDate	datetime = NULL,
+	@EndDate	datetime = NULL
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+ 	select te.MilestoneDate, te.ProjectId as [Id], te.ProjectName as [Name], SUM(te.ActualHours) as ActualHours
+	from v_TimeEntries as te
+	where te.MilestoneDate between @StartDate and @EndDate
+			and te.PersonId = @PersonId
+	group by te.MilestoneDate, te.ProjectId, te.ProjectName
+	order by te.ProjectId
+END
+
