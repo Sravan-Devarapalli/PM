511cdc6fc0ed6fed4fb89d7058c13693aeaec3dc
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectBillingInfoDelete.proc.sql
@@ -0,0 +1,22 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 10-13-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	Deletes the billing info for the project.
+-- =============================================
+CREATE PROCEDURE [dbo].[ProjectBillingInfoDelete]
+(
+	@ProjectId         INT
+)
+AS
+	SET NOCOUNT ON
+
+	DELETE
+	  FROM dbo.ProjectBillingInfoNote
+	 WHERE ProjectId = @ProjectId
+
+	DELETE
+	  FROM dbo.ProjectBillingInfo
+	 WHERE ProjectId = @ProjectId
+
