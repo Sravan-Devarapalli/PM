71f4d4b8b696eaef208a28cd484d88cf6d3cb894
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/PersonPicture.ashx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PraticeManagement.Controls
+{
+    public class PersonPicture : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            int personId = int.Parse(context.Request.QueryString["PersonId"]);
+            PraticeManagement.AttachmentService.AttachmentService svc = PraticeManagement.Utils.WCFClientUtility.GetAttachmentService();
+
+            byte[] data = svc.GetPersonPicture(personId);
+            context.Response.OutputStream.Write(data, 0, data.Length);
+            
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
