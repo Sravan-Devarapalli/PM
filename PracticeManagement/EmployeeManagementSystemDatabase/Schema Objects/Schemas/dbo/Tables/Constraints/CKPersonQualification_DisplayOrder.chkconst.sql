ee43b49eb5d748f1dd939af9980d8ff7c32461c9
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/EmployeeManagementSystemDatabase/Schema Objects/Schemas/dbo/Tables/Constraints/CKPersonQualification_DisplayOrder.chkconst.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[PersonQualification]  WITH CHECK ADD  CONSTRAINT [CKPersonQualification_DisplayOrder] CHECK  (([DisplayOrder]>=(1) AND [DisplayOrder]<=(20)))
+GO
+ALTER TABLE [dbo].[PersonQualification] CHECK CONSTRAINT [CKPersonQualification_DisplayOrder]
+GO
