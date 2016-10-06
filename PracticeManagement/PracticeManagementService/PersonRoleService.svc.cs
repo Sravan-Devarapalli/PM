ae582c461370c52628a1f2f9a73122da66f58ba2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementService/PersonRoleService.svc.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel.Activation;
+using DataAccess;
+using DataTransferObjects;
+
+namespace PracticeManagementService
+{
+    // NOTE: If you change the class name "PersonRoleService" here, you must also update the reference to "PersonRoleService" in Web.config.
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class PersonRoleService : IPersonRoleService
+    {
+        /// <summary>
+        /// Retrives a list of the avalable role for the person.
+        /// </summary>
+        /// <returns>The list of <see cref="PersonRole"/> objects.</returns>
+        public List<PersonRole> GetPersonRoles()
+        {
+            return PersonRoleDAL.PersonRoleListAll();
+        }
+    }
+}
