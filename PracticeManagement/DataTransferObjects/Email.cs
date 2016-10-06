c33f590af188a7e0ea5485fed488aaeadea30f6f
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Email.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace DataTransferObjects
+{
+    public class Email
+    {
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+
+        public List<string> To { get; private set; }
+
+        public List<string> Cc { get; private set; }
+
+        public Email()
+        {
+            To = new List<string>();
+            Cc = new List<string>();
+        }
+    }
+}
