213488458f9b9c01a43efdcd385c5720ea9014e4
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/Generic/AjaxValidationSummary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+using System.Web.UI;
+
+namespace PraticeManagement.Controls.Generic
+{
+    public class AjaxValidationSummary : ValidationSummary
+    {
+        protected override void OnPreRender(EventArgs e)
+        {
+            base.OnPreRender(e);
+            ScriptManager.RegisterStartupScript(this.Page, this.Page.GetType(), this.ClientID + "AjaxValidationSummary", ";", true);
+        }
+
+    }
+}
+
+
+
