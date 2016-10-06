12940697679ec1dcc6296843e3e72cf031ea85ce
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SaveSettingsKeyValuePairs.proc.sql
@@ -0,0 +1,33 @@
+﻿CREATE PROCEDURE dbo.SaveSettingsKeyValuePairs
+(
+	@TypeId   INT,
+	@SettingsKey NVARCHAR(255),
+	@Value      NVARCHAR(MAX)
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+	IF NOT EXISTS(SELECT TOP 1 1 
+				  FROM [dbo].[Settings]
+				  WHERE @SettingsKey = SettingsKey 
+				  AND TypeId = @TypeId 
+				 )
+	BEGIN
+		INSERT INTO [dbo].[Settings]
+					([TypeId]
+					,[SettingsKey]
+					,[Value])
+		 VALUES
+					(@TypeId
+					,@SettingsKey
+					,@Value)
+           
+	END
+	ELSE 
+	BEGIN
+		UPDATE [dbo].[Settings]
+		SET  Value = @Value
+		WHERE SettingsKey = @SettingsKey  AND TypeId = @TypeId
+	END
+END
+           
