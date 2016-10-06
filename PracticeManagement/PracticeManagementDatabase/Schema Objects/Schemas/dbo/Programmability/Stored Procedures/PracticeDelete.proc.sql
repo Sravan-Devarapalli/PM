dd081eef58b6531b6da91d48266686c7f6916f9c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PracticeDelete.proc.sql
@@ -0,0 +1,33 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2010-02-26
+-- Description:	Practices routines
+-- =============================================
+CREATE PROCEDURE dbo.PracticeDelete 
+	@PracticeId INT	,
+	@UserLogin NVARCHAR(MAX)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	EXEC SessionLogPrepare @UserLogin = @UserLogin
+
+	DELETE FROM dbo.PracticeStatusHistory
+	WHERE PracticeId = @PracticeId
+
+	DELETE FROM dbo.PracticeManagerHistory
+	WHERE PracticeId = @PracticeId
+
+	 DELETE FROM DivisionPracticeArea
+	 WHERE PracticeId=@PracticeId
+
+	 DELETE FROM dbo.ProjectDivisionPracticeMapping
+	 WHERE PracticeId=@PracticeId
+
+	DELETE FROM Practice 
+	 WHERE PracticeId = @PracticeId
+
+	EXEC dbo.SessionLogUnprepare
+ 
+ END
+
