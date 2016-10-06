dfa5356df6355437e276e92191640a0f59b70fb3
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_Attribution_AttributionRecordId.fkey.sql
@@ -0,0 +1,6 @@
+﻿ALTER TABLE [dbo].[Attribution]
+	ADD CONSTRAINT [FK_Attribution_AttributionRecordId] 
+	FOREIGN KEY (AttributionRecordTypeId)
+	REFERENCES dbo.AttributionRecordTypes (AttributionRecordId)	 ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
