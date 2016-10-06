e40bbd1e9551efedff4f7021e3530354e68645d7
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/TimeEntry/TimeTypeRecord.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.TimeEntry
+{
+    /// <summary>
+    /// Time type
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    [DebuggerDisplay("TimeType: Id = {Id}, Time type = {Name}, In use = {InUse}, Is default = {IsDefault}")]
+    public class TimeTypeRecord : IEquatable<TimeTypeRecord>
+    {
+        #region Properties
+
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Code { get; set; }
+
+        [DataMember]
+        public bool InUse { get; set; }
+
+        [DataMember]
+        public bool InFutureUse { get; set; }
+
+        [DataMember]
+        public bool IsDefault { get; set; }
+
+        [DataMember]
+        public bool IsSystemTimeType
+        {
+            get
+            {
+                return !IsAllowedToEdit;
+            }
+            set
+            {
+                IsAllowedToEdit = !value;
+            }
+        }
+
+        [DataMember]
+        public bool IsAllowedToEdit { get; set; }
+
+        [DataMember]
+        public bool IsInternal { get; set; }
+
+        [DataMember]
+        public bool IsAdministrative { get; set; }
+
+        [DataMember]
+        public bool IsActive { get; set; }
+
+        [DataMember]
+        public bool IsORTTimeType { get; set; }//ORT:- "Other Reportable Time" work type.
+
+        [DataMember]
+        public bool IsUnpaidTimeType { get; set; }
+
+        public bool IsPTOTimeType { get; set; }
+
+        public bool IsHolidayTimeType { get; set; }
+
+        public bool IsJuryDutyTimeType { get; set; }
+
+        public bool IsSickLeaveTimeType { get; set; }
+
+        [DataMember]
+        public bool IsBereavementTimeType { get; set; }
+
+        [DataMember]
+        public bool IsW2HourlyAllowed { get; set; }
+
+        [DataMember]
+        public bool IsW2SalaryAllowed { get; set; }
+
+        [DataMember]
+        public string Category
+        {
+            get;
+            set;
+        }
+
+        #endregion Properties
+
+        #region Constructors
+
+        public TimeTypeRecord()
+        {
+        }
+
+        /// <summary>
+        /// Init constructor of TimeTypeRecord.
+        /// </summary>
+        public TimeTypeRecord(int id)
+        {
+            Id = id;
+        }
+
+        /// <summary>
+        /// Init constructor of TimeTypeRecord.
+        /// </summary>
+        public TimeTypeRecord(string id)
+            : this(Convert.ToInt32(id))
+        {
+        }
+
+        #endregion Constructors
+
+        #region IEquatable<TimeTypeRecord> Members
+
+        public bool Equals(TimeTypeRecord other)
+        {
+            if (other == null)
+                return false;
+
+            return Id == other.Id;
+        }
+
+        #endregion IEquatable<TimeTypeRecord> Members
+    }
+}
