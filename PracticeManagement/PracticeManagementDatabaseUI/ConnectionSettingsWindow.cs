743ac89e3a311a5ec122ec397d20850d08d63ab2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabaseUI/ConnectionSettingsWindow.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PracticeManagementDatabaseUI
+{
+    public partial class ConnectionSettingsWindow : Form
+    {
+        public string ServerName
+        {
+            get
+            {
+                return tbServerName.Text;
+            }
+        }
+
+        public string DatabaseName
+        {
+            get
+            {
+                return tbDatabaseName.Text;
+            }
+        }
+
+        public ConnectionSettingsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTestConnection_Click(object sender, EventArgs e)
+        {
+            var conn = new ConnectionProperties
+                    {
+                        ServerName = ServerName,
+                        DatabaseName = DatabaseName
+                    };
+
+            if (Utils.IsConnectionValid(conn))
+            {
+                MessageBox.Show(
+                    PracticeManagementDatabaseUI.Properties.Resources.ConnectionIsValid,
+                    Properties.Resources.ConnectionTest);
+
+                btnDeploy.Enabled = true;
+            }
+            else
+                MessageBox.Show(
+                    PracticeManagementDatabaseUI.Properties.Resources.ConnectionFailed,
+                    Properties.Resources.ConnectionTest,
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+}
+
