6aa802fb0bf8e4a6bdb22b36c1f547533b3e029d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/OpportunitySearch.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PraticeManagement.OpportunityService;
+using PraticeManagement.Controls;
+using DataTransferObjects;
+
+namespace PraticeManagement
+{
+    public partial class OpportunitySearch : PracticeManagementPageBase
+    {
+
+        private List<Opportunity> Opportunities
+        {
+            get
+            {
+                if (ViewState["OpportunitiesLooked"] != null)
+                    return ViewState["OpportunitiesLooked"] as List<Opportunity>;
+                else
+                    return null;
+            }
+            set
+            {
+                ViewState["OpportunitiesLooked"] = value;
+            }
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            DisplaySearch();
+        }
+
+        protected void ucOpportunityList_OnFilterOptionsChanged(object sender, EventArgs e)
+        {
+            var opportunities = DataHelper.SortOppotunities(Opportunities.AsQueryable().ToArray());
+            ucOpportunityList.DataBindLookedOpportunities(opportunities.ToArray());
+        }
+
+        private void DisplaySearch()
+        {
+            Page.Validate();
+            if (Page.IsValid)
+            {
+                var opportunities = DataHelper.GetLookedOpportunities(txtSearchText.Text, DataHelper.CurrentPerson.Id.Value);
+                Opportunities = opportunities.ToList();
+                ucOpportunityList.DataBindLookedOpportunities(opportunities.AsQueryable().ToArray());
+            }
+        }
+
+        protected override void Display()
+        {
+            if (PreviousPage != null)
+            {
+                txtSearchText.Text = PreviousPage.SearchText;
+                DisplaySearch();
+            }
+        }
+    }
+}
+
