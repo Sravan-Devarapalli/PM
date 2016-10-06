ba4ddc3f7b9b5b44a6000a20876de4facd9aae98
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/dbo.GetAvgUtilization.function.sql
@@ -0,0 +1,39 @@
+﻿CREATE FUNCTION dbo.GetAvgUtilization
+(
+	@PersonId INT,
+	@StartDate DATETIME,
+	@DaysForward INT = 184,
+	@ActiveProjects BIT = 1,
+	@ProjectedProjects BIT = 1,
+	@ExperimentalProjects BIT = 1,
+	@ProposedProjects BIT=1,
+	@InternalProjects	BIT = 1  
+)
+RETURNS INT
+AS
+BEGIN
+	  
+    
+    DECLARE @wUtil INT
+    DECLARE @av INT 
+	DECLARE @EndRange DATETIME
+	
+	SET @EndRange = DATEADD(dd , @DaysForward, @StartDate) - 1
+    
+    -- Iterate through all days
+
+		BEGIN
+			SET @av = dbo.GetNumberAvaliableHours(@PersonId, @StartDate, @EndRange, @ActiveProjects, @ProjectedProjects, @ExperimentalProjects ,@ProposedProjects,@InternalProjects)
+			
+			IF (@av = 0 OR @av IS NULL)
+				SET @wUtil = 0
+			ELSE 		
+				SET @wUtil = CEILING(
+					100*ISNULL(dbo.GetNumberProjectedHours(@PersonId, @StartDate, @EndRange, @ActiveProjects, @ProjectedProjects, @ExperimentalProjects,@ProposedProjects, @InternalProjects), 0) / 
+						@av)
+		END 
+ 
+   return @wUtil
+
+END
+
