0d721aad839e4e32749dc19da75213569d61a7a5
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CheckIfRangeWithinHireAndTermination.proc.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[CheckIfRangeWithinHireAndTermination]
+	(
+		@PersonId	INT,
+		@StartDate	DATETIME,
+		@EndDate	DATETIME
+	)
+AS
+BEGIN
+
+	DECLARE @ValidRange BIT
+
+	IF EXISTS(
+				SELECT	1
+				FROM	v_PersonHistory PH 
+				WHERE	PH.PersonId = @PersonId AND (PH.HireDate <= @StartDate AND (PH.TerminationDate IS NULL OR PH.TerminationDate >= @EndDate OR PH.PersonStatusId <> 2))
+			)
+		SET @ValidRange  = 1
+	ELSE
+		SET @ValidRange = 0
+
+	SELECT @ValidRange
+
+END
