60a0b26343ecc532ca3c6f309df23474ddefc64d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Practice.table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Practice] (
+    [PracticeId]        INT           IDENTITY (1, 1) NOT NULL,
+    [Name]              VARCHAR (100) NOT NULL,
+    [PracticeManagerId] INT           NULL,
+    [IsActive]          BIT           NOT NULL,
+    [IsCompanyInternal] BIT           NOT NULL,
+	[Abbreviation]      NVARCHAR (100)  NULL,
+	IsNotesRequired     BIT   CONSTRAINT Df_Practice_IsNotesRequired DEFAULT (1) NOT NULL,
+	[ShowInUtilizationReport]	BIT NOT NULL CONSTRAINT DF_Practice_ShowInUtilizationReport DEFAULT(1)
+);
+
+
