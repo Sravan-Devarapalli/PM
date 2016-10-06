ed0470661b28e792a2fa59092d87724dd2af857b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/WeekPaidOptionListAll.proc.sql
@@ -0,0 +1,15 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 7-24-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	Retrieves the list of the Week Paid Options
+-- =============================================
+CREATE PROCEDURE [dbo].[WeekPaidOptionListAll]
+AS
+	SET NOCOUNT ON
+
+	SELECT WeekPaidOptionId, Name
+	  FROM dbo.WeekPaidOption
+	ORDER BY WeekPaidOptionId
+
