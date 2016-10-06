26e3e7f32136305608b703b0f8257ab86bbb8923
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetNoteRequiredDetailsForSelectedDateRange.proc.sql
@@ -0,0 +1,27 @@
+﻿CREATE PROCEDURE [dbo].[GetNoteRequiredDetailsForSelectedDateRange]
+	@StartDate DATETIME, 
+	@EndDate DATETIME,
+	@PersonId INT
+AS
+BEGIN
+SET NOCOUNT ON;
+
+	;WITH DateList AS (   
+				SELECT @StartDate AS 'Date'
+				UNION ALL  
+				SELECT DATEADD(dd, 1, [Date])  
+				FROM DateList s  
+				WHERE DATEADD(dd, 1, [Date]) <= @EndDate
+				)
+
+		
+		SELECT d.Date,COALESCE(P2.IsNotesRequired,P1.IsNotesRequired,1) IsNotesRequired
+		FROM DateList as d
+		CROSS JOIN  Person as p
+		LEFT JOIN Practice as P1 on  P1.PracticeId = p.DefaultPractice
+		LEFT JOIN  dbo.Pay  on  d.[Date]  BETWEEN pay.StartDate AND pay.EndDate-1 AND pay.Person = p.PersonId
+		LEFT JOIN Practice as P2 on  P2.PracticeId = pay.PracticeId
+		WHERE d.Date is not null and P.PersonId = @PersonId
+
+END
+	
