43a23b7417f17780fc66f97f8f1324292dc0ad3c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/aspnet_Paths.table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[aspnet_Paths] (
+    [ApplicationId] UNIQUEIDENTIFIER NOT NULL,
+    [PathId]        UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
+    [Path]          NVARCHAR (256)   NOT NULL,
+    [LoweredPath]   NVARCHAR (256)   NOT NULL,
+    PRIMARY KEY NONCLUSTERED ([PathId] ASC) WITH (IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF) ON [PRIMARY],
+    FOREIGN KEY ([ApplicationId]) REFERENCES [dbo].[aspnet_Applications] ([ApplicationId]) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    FOREIGN KEY ([ApplicationId]) REFERENCES [dbo].[aspnet_Applications] ([ApplicationId]) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+
