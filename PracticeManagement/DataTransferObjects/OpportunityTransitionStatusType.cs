63a1f22ffd471bbe302e16f4c9471c7d9a403a10
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/OpportunityTransitionStatusType.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// Determines the status for the <see cref="OpportunityTransition"/>.
+    /// </summary>
+    [DataContract]
+    public enum OpportunityTransitionStatusType
+    {
+        [EnumMember]
+        All = 0,
+
+        [EnumMember]
+        Created = 1,
+
+        [EnumMember]
+        Notes = 2,
+
+        [EnumMember]
+        Proposed = 3,
+
+        [EnumMember]
+        Presented = 4,
+
+        [EnumMember]
+        SendOut = 5,
+
+        [EnumMember]
+        Pipeline = 6,
+
+        [EnumMember]
+        Lost = 7
+    }
+}
