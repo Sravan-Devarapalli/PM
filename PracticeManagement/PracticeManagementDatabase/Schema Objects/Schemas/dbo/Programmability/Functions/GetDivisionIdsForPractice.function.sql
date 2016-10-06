acf7c859e22a21c56739b158632eee5dccc3d566
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetDivisionIdsForPractice.function.sql
@@ -0,0 +1,29 @@
+﻿CREATE FUNCTION [dbo].[GetDivisionIdsForPractice]
+(
+	@PracticeId INT,
+	@IsProject INT
+)
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN
+  
+	DECLARE @Temp NVARCHAR(MAX) = ''
+
+  IF @IsProject=0 --person division mappings
+  BEGIN
+	SELECT @Temp = @Temp + CAST(DPA.DivisionId AS NVARCHAR(5))+','
+	FROM DivisionPracticeArea DPA
+	WHERE DPA.PracticeId=@PracticeId
+  END
+
+  ELSE --project division mappings
+  BEGIN 
+	SELECT @Temp = @Temp + CAST(DPA.ProjectDivisionId AS NVARCHAR(5))+','
+	FROM ProjectDivisionPracticeMapping DPA
+	WHERE DPA.PracticeId=@PracticeId
+  END
+
+	RETURN @Temp
+END
+GO
+
