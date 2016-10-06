07a0be63474fe132deff42226d0db3b148ce7009
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CheckIfPersonIsOwnerForDivisionAndOrPractice.proc.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[CheckIfPersonIsOwnerForDivisionAndOrPractice]
+	@PersonId INT 
+AS
+BEGIN
+	SELECT PD.DivisionName AS 'TargetName',
+			CAST(1 AS BIT) 'IsDivisionOwner'
+	FROM dbo.PersonDivision PD
+	WHERE PD.DivisionOwnerId=@PersonId
+	UNION ALL
+	SELECT P.Name AS 'TargetName',
+			CAST(0 AS BIT) AS 'IsDivisionOwner'
+	FROM dbo.Practice P
+	WHERE p.PracticeManagerId=@PersonId
+END	
