6cdb2f3944e80add3cee5da95abdbf3fe6e3cb97
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/Generic/FormattedLabel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Web.UI.WebControls;
+
+namespace PraticeManagement.Controls.Generic
+{
+    public class FormattedLabel : Label
+    {
+        private const string DATA_FORMAT_STRING_VIEWSTATE = "FormattedLabelViewState";
+
+        public DateTime DateText
+        {
+            get
+            {
+                return Utils.Generic.ParseDate(base.Text);
+            }
+            set
+            {
+                base.Text = value.ToString(DataFormatString);
+            }
+        }
+
+        public double DoubleText
+        {
+            get
+            {
+                return Convert.ToDouble(base.Text);
+            }
+
+            set
+            {
+                base.Text = value.ToString(Constants.Formatting.DoubleFormat);
+            }
+        }
+
+        public string DataFormatString
+        {
+            get
+            {
+                string dfs = ViewState[DATA_FORMAT_STRING_VIEWSTATE] as string;
+                return dfs ?? string.Empty;
+            }
+
+            set
+            {
+                ViewState[DATA_FORMAT_STRING_VIEWSTATE] = value;
+            }
+        }
+    }
+}
