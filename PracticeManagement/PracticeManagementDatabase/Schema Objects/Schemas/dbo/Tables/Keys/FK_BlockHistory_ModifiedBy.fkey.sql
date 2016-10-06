12599cd96f23bf8f3e69c59aae6b245317f7feeb
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_BlockHistory_ModifiedBy.fkey.sql
@@ -0,0 +1,6 @@
+﻿ALTER TABLE [dbo].[BlockHistory]
+	ADD CONSTRAINT [FK_BlockHistory_ModifiedBy] 
+	FOREIGN KEY (ModifiedBy)
+	REFERENCES dbo.Person (PersonId) ON DELETE NO ACTION ON UPDATE NO ACTION;	
+
+
