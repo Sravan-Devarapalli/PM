e29917c7490dda56df1a9b3c69cac136ac183f70
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Config/GeneralConfiguration.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Web.UI.WebControls;
+using DataTransferObjects.TimeEntry;
+using PraticeManagement.Utils;
+using System.Web;
+using PraticeManagement.TimeEntryService;
+using PraticeManagement.Controls;
+using System.Web.Security;
+
+namespace PraticeManagement.Config
+{
+    public partial class GeneralConfiguration : PracticeManagementPageBase
+    {
+        #region "Declarations"
+
+        private bool userIsAdministrator;
+        private bool userIsSalesperson;
+        private bool userIsRecruiter;
+        private bool userIsHR;
+
+        #endregion "Declarations"
+
+        protected override void Display()
+        {
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            VerifyPrivileges();
+        }
+
+        private void VerifyPrivileges()
+        {
+            userIsAdministrator =
+                Roles.IsUserInRole(DataTransferObjects.Constants.RoleNames.AdministratorRoleName);
+            userIsSalesperson =
+                Roles.IsUserInRole(DataTransferObjects.Constants.RoleNames.SalespersonRoleName);
+            userIsRecruiter =
+                Roles.IsUserInRole(DataTransferObjects.Constants.RoleNames.RecruiterRoleName);
+            userIsHR =
+                Roles.IsUserInRole(DataTransferObjects.Constants.RoleNames.HRRoleName);
+            if (userIsAdministrator)
+            {
+                tpnlBranding.Visible = true;
+                tpnlDefaultManager.Visible = true;
+                tpnlPractices.Visible = true;
+                tpnlOpportunityPriorities.Visible = true;
+            }
+            if (userIsAdministrator || userIsSalesperson)
+            {
+                tpnlClientList.Visible = true;
+            }
+            if (userIsAdministrator || userIsRecruiter || userIsHR)
+            {
+                tpnlPersons.Visible = true;
+            }
+        }
+
+    }
+}
+
+
+
