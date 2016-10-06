e003e6137e979baadca0481eef3f88affc6d09ea
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/PersonSeniorityType.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// Determines the Seniority of person
+    /// </summary>
+    [DataContract]
+    public enum PersonSeniorityType
+    {
+        [EnumMember]
+        Admin = 1,
+
+        [EnumMember]
+        Partner = 15,
+
+        [EnumMember]
+        VP = 25,
+
+        [EnumMember]
+        Director = 35,
+
+        [EnumMember]
+        PracticeManager = 45,
+
+        [EnumMember]
+        SeniorManager = 55,
+
+        [EnumMember]
+        Manager = 65,
+
+        [EnumMember]
+        SeniorConsultant = 75,
+
+        [EnumMember]
+        Consultant = 85,
+
+        [EnumMember]
+        Analyst = 95,
+
+        [EnumMember]
+        Intern = 105
+    }
+}
