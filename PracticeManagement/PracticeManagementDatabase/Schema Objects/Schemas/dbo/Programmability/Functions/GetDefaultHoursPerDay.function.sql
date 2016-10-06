4b8b9cfac43e1bbabe83636db325fda23af3d3a1
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetDefaultHoursPerDay.function.sql
@@ -0,0 +1,24 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2009-10-29
+-- Description:	Gets default hours per day for given date
+-- =============================================
+CREATE FUNCTION GetDefaultHoursPerDay
+(
+	@PersonId INT,
+	@Date DATETIME
+)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @res INT,@FutureDate DATETIME
+	SET @FutureDate = dbo.GetFutureDate()
+
+	SELECT @res = SUM(DefaultHoursPerDay) 
+	FROM dbo.v_Pay
+	WHERE @Date BETWEEN StartDate AND ISNULL(EndDate, @FutureDate) AND 
+		  @PersonId = PersonId
+
+	RETURN ISNULL(@res, 8)
+END
+
