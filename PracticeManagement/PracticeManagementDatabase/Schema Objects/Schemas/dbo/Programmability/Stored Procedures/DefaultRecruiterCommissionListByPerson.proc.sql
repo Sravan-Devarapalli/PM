2620d766bdb079ea17f8a03769f7e9fb31227c6d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DefaultRecruiterCommissionListByPerson.proc.sql
@@ -0,0 +1,24 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 9-02-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	Selects a list of the default recruiter commissions for the specified person
+-- =============================================
+CREATE PROCEDURE [dbo].[DefaultRecruiterCommissionListByPerson]
+(
+	@PersonId   INT
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+	DECLARE @FutureDate DATETIME
+	SET @FutureDate = dbo.GetFutureDate()
+
+	SELECT h.DefaultRecruiterCommissionHeaderId, h.PersonId, h.StartDate,
+	       CASE h.EndDate WHEN @FutureDate THEN CAST(NULL AS DATETIME) ELSE h.EndDate END AS EndDate,
+	       dbo.MakeDefaultRecruiterCommissionText(h.defaultrecruitercommissionheaderid) AS TextLine
+	  FROM dbo.DefaultRecruiterCommissionHeader as h
+	 WHERE h.PersonId = @PersonId
+	ORDER BY h.StartDate
+END
