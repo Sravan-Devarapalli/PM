dedeb6d7e0b0d14e8239b3970bb41729ef89256d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OpportunityPersonDelete.proc.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[OpportunityPersonDelete]
+(
+	@OpportunityId INT,
+	@PersonIdList  NVARCHAR(MAX)
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DELETE dbo.OpportunityPersons
+	WHERE OpportunityId = @OpportunityId 
+			AND PersonId IN(
+							SELECT ResultId 
+							FROM [dbo].[ConvertStringListIntoTable] (@PersonIdList)
+							)
+END
