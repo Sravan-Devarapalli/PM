21f2e88df9171b1ecff560867fb155b6e011f26e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/UpdatePersonDivision.proc.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[UpdatePersonDivision]
+	@DivisionId INT, 
+	@DivisionOwnerId INT
+AS
+BEGIN
+		UPDATE dbo.PersonDivision
+		SET DivisionOwnerId = @DivisionOwnerId
+		WHERE DivisionId=@DivisionId
+END
