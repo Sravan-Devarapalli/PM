0f412b24a6d3367bf5fda4c738832c37012ff4e0
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_UsersInRoles.view.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW dbo.v_UsersInRoles
+AS 
+SELECT p.PersonId, p.LastName, p.FirstName, u.UserName, r.RoleName, p.PersonStatusId, st.Name as 'StatusName'
+FROM dbo.aspnet_Users AS u
+INNER JOIN dbo.Person AS p ON u.UserName = p.Alias
+INNER JOIN dbo.aspnet_UsersInRoles AS ur ON u.UserId = ur.UserId
+INNER JOIN dbo.aspnet_Roles AS r ON ur.RoleId = r.RoleId
+inner join dbo.PersonStatus as st on st.PersonStatusId = p.PersonStatusId
+--WHERE p.PersonStatusId = 1
+
