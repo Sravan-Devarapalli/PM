dcba8b3bee8ed6f6c91a8207e991a7d9b46a7019
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/SettingsType.table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE dbo.SettingsType
+(
+	[TypeId]   INT IDENTITY(1,1) NOT NULL,
+	[Description] NVARCHAR(255) NOT NULL,
+	CONSTRAINT PK_SettingsType_TypeId PRIMARY KEY CLUSTERED([TypeId])
+); 
