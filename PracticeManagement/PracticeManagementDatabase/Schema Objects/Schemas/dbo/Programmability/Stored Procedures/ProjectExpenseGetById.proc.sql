8b9e88380ccc43ca5686e94ed9e5ecb27de42954
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectExpenseGetById.proc.sql
@@ -0,0 +1,13 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2010-04-20
+-- =============================================
+CREATE PROCEDURE dbo.ProjectExpenseGetById 
+    @ExpenseId int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	--SELECT * from v_ProjectExpenses where ExpenseId = @ExpenseId
+END
+
