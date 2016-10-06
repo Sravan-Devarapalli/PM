8fa7be61ce8d0161e04dc8ecfba2d8d10901fc49
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/TimeEntry/TimeEntryBarHeader.ascx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PraticeManagement.Controls.TimeEntry
+{
+    public partial class TimeEntryBarHeader : System.Web.UI.UserControl
+    {
+        public IEnumerable<DateTime> SelectedDates { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void UpdateHeader()
+        {
+            repEntries.DataSource = SelectedDates;
+            repEntries.DataBind();
+        }
+    }
+}
