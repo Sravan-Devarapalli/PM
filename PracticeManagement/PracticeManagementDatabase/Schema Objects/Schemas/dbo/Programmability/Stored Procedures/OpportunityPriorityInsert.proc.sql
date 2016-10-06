362e888150b469593efbf386bf0f884fc075c24b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OpportunityPriorityInsert.proc.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[OpportunityPriorityInsert]
+@PriorityId INT,
+@Description NVARCHAR(255),
+@DisplayName NVARCHAR(15)
+AS
+BEGIN
+	SET NOCOUNT ON
+	UPDATE [dbo].[OpportunityPriorities]
+   SET 
+       [Description] =@Description      
+      ,[IsInserted] = 1
+	  ,[DisplayName] = @DisplayName
+ WHERE id = @PriorityId
+END
+GO
