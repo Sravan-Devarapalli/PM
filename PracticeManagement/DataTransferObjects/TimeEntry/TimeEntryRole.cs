961d87bf939ded3ab2c966e0bd4b573d724291db
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/TimeEntry/TimeEntryRole.cs
@@ -0,0 +1,23 @@
+﻿namespace DataTransferObjects.TimeEntry
+{
+    /// <summary>
+    /// Time Entry Roles
+    /// </summary>
+    public enum TimeEntryRole
+    {
+        /// <summary>
+        /// Standard User
+        /// </summary>
+        StandardUser,
+
+        /// <summary>
+        /// Management User
+        /// </summary>
+        ManagementUser,
+
+        /// <summary>
+        /// Accounting Administrator
+        /// </summary>
+        AccountingAdministrator
+    }
+}
