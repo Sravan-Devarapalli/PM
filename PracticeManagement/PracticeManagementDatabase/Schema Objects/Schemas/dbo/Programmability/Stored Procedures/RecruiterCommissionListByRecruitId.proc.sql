2835bc2408837e980ef29acbd57a318984b9a586
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/RecruiterCommissionListByRecruitId.proc.sql
@@ -0,0 +1,21 @@
+﻿--The source is 
+-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 5-21-2008
+-- Description:	List recruiter commission by the given recruit
+-- =============================================
+CREATE PROCEDURE [dbo].[RecruiterCommissionListByRecruitId]
+(
+	@RecruitId   INT
+)
+AS
+	SET NOCOUNT ON
+
+	SELECT c.RecruiterId,
+	       c.RecruitId,
+	       c.HoursToCollect,
+	       c.Amount
+	  FROM dbo.RecruiterCommission AS c
+	 WHERE c.RecruitId = @RecruitId
+	ORDER BY c.HoursToCollect
+
