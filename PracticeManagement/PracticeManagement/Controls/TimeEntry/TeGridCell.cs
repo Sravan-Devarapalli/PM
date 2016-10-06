46d6c16cbf45ab724edc18c728c42cefa76bff39
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/TimeEntry/TeGridCell.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using DataTransferObjects;
+using DataTransferObjects.TimeEntry;
+
+namespace PraticeManagement.Controls.TimeEntry
+{
+    /// <summary>
+    /// 	Object behind SingleTimeEntry.ascx control
+    /// </summary>
+    [DebuggerDisplay("TeGridCell; Date = {Date}, TimeEntry = {TimeEntry}")]
+    public class TeGridCell
+    {
+        #region Properties
+
+        public CalendarItem Day { get; set; }
+        public TimeEntryRecord TimeEntry { get; set; }
+        public MilestonePersonEntry MilestoneBehind { get; set; }
+
+        #endregion
+    }
+}
