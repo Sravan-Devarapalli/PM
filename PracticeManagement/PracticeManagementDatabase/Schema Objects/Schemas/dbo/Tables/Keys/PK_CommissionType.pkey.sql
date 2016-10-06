2d055255933df8ad7bf93f2041facd594a5eb998
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/PK_CommissionType.pkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[CommissionType]
+    ADD CONSTRAINT [PK_CommissionType] PRIMARY KEY CLUSTERED ([CommissionTypeId] ASC) WITH (IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF);
+
+
