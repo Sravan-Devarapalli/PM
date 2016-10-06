9705447928be7e9e50c7a7c0c77fadbe9ab5513a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_MilestoneLight.view.sql
@@ -0,0 +1,17 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 8-13-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	List milestones
+-- =============================================
+CREATE VIEW [dbo].[v_MilestoneLight]
+AS
+	SELECT m.MilestoneId,
+	       m.ProjectId,
+	       m.Amount,
+	       m.StartDate,
+	       m.ProjectedDeliveryDate,
+	       m.IsHourlyAmount
+	  FROM dbo.Milestone AS m
+
