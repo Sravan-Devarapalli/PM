dbae131af529073be9b42ab8fa5edbc8b47bc027
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/EmployeeManagementSystemDatabase/Schema Objects/Schemas/dbo/Tables/Person.table.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Person](
+	[PersonId] [bigint] IDENTITY(1,1) NOT NULL,
+	[Alias] [nvarchar](100) NOT NULL,
+	[FirstName] [nvarchar](100) NOT NULL,
+	[MiddleName] [nvarchar](100) NULL,
+	[LastName] [nvarchar](100) NOT NULL,
+	[IsManager] [bit] NOT NULL,
+	[TitleId] [int] NULL,
+	[ManagerId] [bigint] NULL,
+	[ImageUrl] [nvarchar](max) NULL,
+	[ModificationDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_Person] PRIMARY KEY CLUSTERED 
+(
+	[PersonId] ASC
+)
+)
