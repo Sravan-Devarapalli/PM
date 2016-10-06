f06ec47a6596e8ef6d42fb3078495073775c38dc
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/InsertVendor.proc.sql
@@ -0,0 +1,38 @@
+﻿CREATE PROCEDURE [dbo].[InsertVendor]
+(
+	@Id					INT OUT,
+	@Name				NVARCHAR (50),			
+	@ContactName		NVARCHAR (50),		
+	@Status				INT,						
+	@Email			    NVARCHAR (50),		
+	@TelephoneNumber	NVARCHAR(20),				
+	@VendorTypeId		INT,
+	@UserLogin          NVARCHAR(255)
+)
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+	BEGIN TRY
+	BEGIN TRAN  T1;
+
+	EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
+
+	INSERT INTO dbo.Vendor(Name, ContactName, Status, Email, TelephoneNumber, VendorTypeId)
+					Values(@Name, @ContactName, @Status, @Email, @TelephoneNumber, @VendorTypeId)
+
+	EXEC dbo.SessionLogUnprepare
+
+	SET @Id =SCOPE_IDENTITY()
+
+	COMMIT TRAN T1;
+	END TRY
+	BEGIN CATCH
+	ROLLBACK TRAN T1;
+		DECLARE @ErrorMessage NVARCHAR(MAX)
+		SET @ErrorMessage = ERROR_MESSAGE()
+
+		RAISERROR(@ErrorMessage, 16, 1)
+	END CATCH
+
+END
