5f9eaa760fef3bb99b137f67a2c1328800607285
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Reports/ByPerson/GroupByPersonByWorktype.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DataTransferObjects.Reports
+{
+    public class GroupByPersonByWorktype
+    {
+        public Person Person
+        {
+            get;
+            set;
+        }
+
+        public double BillableHours
+        {
+            get
+            {
+                return ProjectTotalHoursList != null ? ProjectTotalHoursList.Sum(d => d.BillableHours) : 0;
+            }
+        }
+
+        public double NonBillableHours
+        {
+            get
+            {
+                return ProjectTotalHoursList != null ? ProjectTotalHoursList.Sum(d => d.NonBillableHours) : 0;
+            }
+        }
+
+        public double TotalHours
+        {
+            get
+            {
+                return ProjectTotalHoursList != null ? ProjectTotalHoursList.Sum(p => p.TotalHours) : 0;
+            }
+        }
+
+        public List<TimeEntryByWorkType> ProjectTotalHoursList
+        {
+            get;
+            set;
+        }
+    }
+}
