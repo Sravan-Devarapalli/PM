99369ea248a4d3ffe0c3e5864a2a772bb21ac52b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CheckIfCommissionsExistsAfterTermination.proc.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[CheckIfCommissionsExistsAfterTermination]
+	(
+	@PersonId	INT,
+	@TerminationDate	DATETIME
+	)
+AS
+BEGIN
+	
+	SELECT	DISTINCT P.ProjectNumber,
+			P.Name
+	FROM	dbo.Attribution A
+	INNER JOIN dbo.Project P ON A.ProjectId = P.ProjectId
+	WHERE	A.AttributionRecordTypeId = 1 AND A.TargetId = @PersonId AND A.EndDate > @TerminationDate
+
+END
