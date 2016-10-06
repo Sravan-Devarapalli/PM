ed8c0b89038a59d0dfd686aee464a6a26a448f26
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/NoteDelete.proc.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE dbo.NoteDelete
+(
+	@NoteId   INT
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+	IF EXISTS(  
+				SELECT TOP 1 1 
+				FROM dbo.Note
+				WHERE NoteId = @NoteId
+			 )
+	 BEGIN
+		DELETE FROM dbo.Note
+		WHERE NoteId = @NoteId
+	 END
+END
