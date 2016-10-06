c60a4784c104ba949123d530bb217ef59d8f4cfa
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/CategoryItemBudget.table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[CategoryItemBudget] (
+    [ItemId]			INT NOT NULL,
+    [CategoryTypeId]    INT NOT NULL,
+    [MonthStartDate]	DATETIME NOT NULL,
+    [Amount]			DECIMAL(18,2),
+    CONSTRAINT PK_CategoryItemBudget PRIMARY KEY ([ItemId] ASC,[CategoryTypeId] ASC, [MonthStartDate] ASC),
+    CONSTRAINT FK_CategoryItemBudget_CategoryType FOREIGN KEY([CategoryTypeId]) 
+	REFERENCES [dbo].[BudgetCategoryType]([CategoryTypeId])
+);
