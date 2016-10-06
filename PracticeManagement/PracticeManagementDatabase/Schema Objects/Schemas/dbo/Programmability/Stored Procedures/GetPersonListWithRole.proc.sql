cd95d228d07fad6bb386b71b6c2fbd61f0aa6e8f
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetPersonListWithRole.proc.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[GetPersonListWithRole]
+(
+	@RoleName NVARCHAR(100)
+)
+AS
+BEGIN
+
+	 SELECT p.PersonId ,
+            ISNULL(p.PreferredFirstName,p.FirstName) AS FirstName,
+            p.LastName ,
+            p.IsDefaultManager
+    FROM    dbo.Person AS p
+			INNER JOIN v_UsersInRoles AS ur ON ur.UserName = p.Alias AND LOWER(ur.RoleName) = LOWER(@RoleName)
+    WHERE   p.PersonStatusId IN (1,5) -- Active And termination pending person only
+    ORDER BY p.LastName ,p.FirstName
+END
+
