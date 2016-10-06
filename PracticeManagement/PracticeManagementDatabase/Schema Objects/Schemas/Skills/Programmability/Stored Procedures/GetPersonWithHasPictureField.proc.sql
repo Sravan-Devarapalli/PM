bfbaefdcdc342241f9ccb5a09f0a49d8209e018f
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Programmability/Stored Procedures/GetPersonWithHasPictureField.proc.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [Skills].[GetPersonWithHasPictureField]
+(
+	@PersonId         INT
+)
+AS
+BEGIN
+
+	SELECT p.PersonId,
+	       p.LastName,
+		   p.FirstName,
+		   CASE WHEN p.PictureData IS NULL THEN 0
+		        ELSE 1 END AS 'HasPicture'
+	FROM dbo.[Person] p
+	WHERE p.PersonId = @PersonId
+END
