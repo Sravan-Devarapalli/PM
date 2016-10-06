401613ca103280c440d5339296d3843063597d4b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/Generic/LoadingProgress.ascx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PraticeManagement.Controls.Generic
+{
+    public partial class LoadingProgress : System.Web.UI.UserControl
+    {
+        public string DisplayText { get; set; }
+
+        public string AssociatedUpdatePanelID
+        {
+            set 
+            {
+                upTimeEntries.AssociatedUpdatePanelID = value;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(DisplayText))
+            {
+                DisplayText = "Please Wait..";
+            }            
+        }
+    }
+}
