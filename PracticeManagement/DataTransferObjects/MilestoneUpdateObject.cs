d5b40f05615786711cc893bb3be10e65ba601d1a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/MilestoneUpdateObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class MilestoneUpdateObject
+    {
+        [DataMember]
+        public bool? IsStartDateChangeReflectedForMilestoneAndPersons { get; set; }
+
+        [DataMember]
+        public bool? IsEndDateChangeReflectedForMilestoneAndPersons { get; set; }
+
+        [DataMember]
+        public bool? IsExtendedORCompleteOutOfRange { get; set; }
+    }
+}
