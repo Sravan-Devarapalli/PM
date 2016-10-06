f2c80f1237735b26f525cc27582b1b0d4f5982e9
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OpportunityGetByNumber.proc.sql
@@ -0,0 +1,13 @@
+﻿
+
+CREATE PROCEDURE dbo.OpportunityGetByNumber
+(
+	@OpportunityNumber   NVARCHAR(12)
+)
+AS
+	SET NOCOUNT ON
+
+	SELECT o.OpportunityId
+	FROM Opportunity AS o
+	WHERE o.OpportunityNumber = @OpportunityNumber
+
