7a52b5d0f9eb92968aa5bd271e6ec130562e2ff9
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetRecurringHolidaysList.proc.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[GetRecurringHolidaysList]
+AS
+BEGIN
+	SELECT Id
+			,Description + '<br />&nbsp;' + DateDescription AS 'Description'
+			,IsSet
+	FROM dbo.CompanyRecurringHoliday
+	ORDER BY [Month], ISNULL([Day], 31), CASE WHEN NumberInMonth IS NULL THEN 6 ELSE NumberInMonth END , DayOfTheWeek
+	
+END
+GO
