8349dc8534f1fc662a33e7086c419aa91efdd2d8
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetActiveOpportunitiesByOwnerId.proc.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[GetActiveOpportunitiesByOwnerId]
+(
+	@PersonId	INT
+)
+AS
+BEGIN 
+
+	SELECT op.OpportunityId,
+		   op.Name OpportunityName,
+		   op.OpportunityNumber
+	FROM dbo.Opportunity AS op 
+	WHERE op.OwnerId = @PersonId AND op.OpportunityStatusId =1
+	ORDER BY op.OpportunityNumber
+	
+END
