e50361bb25a68e112d810c9419c83dfc306c54d5
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetCurrentActivePracticeAreaManagerList.proc.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[GetCurrentActivePracticeAreaManagerList]
+AS
+BEGIN
+
+	SELECT   p.PersonId ,
+			p.LastName ,
+			p.FirstName 
+	FROM dbo.person AS p
+	INNER JOIN dbo.aspnet_Users AS u ON p.Alias = u.UserName
+	INNER JOIN dbo.aspnet_UsersInRoles AS ur ON u.UserId = ur.UserId
+	INNER JOIN dbo.aspnet_Roles AS r ON ur.RoleId = r.RoleId
+	WHERE r.RoleName = 'Practice Area Manager' 
+		AND p.PersonStatusId = 1
+	ORDER BY 3,2
+
+END
