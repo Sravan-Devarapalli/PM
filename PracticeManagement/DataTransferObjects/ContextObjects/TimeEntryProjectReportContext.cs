d39a2073365df5accf68ab4dafb0dc93524900da
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ContextObjects/TimeEntryProjectReportContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.ContextObjects
+{
+    /// <summary>
+    /// Represents Time Entry Project Report Context
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class TimeEntryProjectReportContext
+    {
+        [DataMember]
+        public int ProjectId { get; set; }
+
+        [DataMember]
+        public DateTime? StartDate { get; set; }
+
+        [DataMember]
+        public DateTime? EndDate { get; set; }
+
+        [DataMember]
+        public IEnumerable<int> PersonIds { get; set; }
+
+        [DataMember]
+        public int? MilestoneId { get; set; }
+    }
+}
