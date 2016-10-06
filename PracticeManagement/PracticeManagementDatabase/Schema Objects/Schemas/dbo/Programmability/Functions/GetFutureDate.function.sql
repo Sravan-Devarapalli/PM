d22c2cff101df98fbb1391d934cacf75d56af4cd
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetFutureDate.function.sql
@@ -0,0 +1,15 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 6-24-2008
+-- Updated by:	Anatoliy Lokshin
+-- Update date:	8-12-2008
+-- Description:	Gets a future date.
+-- =============================================
+CREATE FUNCTION [dbo].[GetFutureDate]()
+RETURNS DATETIME
+WITH SCHEMABINDING
+AS
+BEGIN
+	RETURN '2029-12-31'
+END
+
