e24e3b22e935cc6979bc26ba652b0e4180b8f034
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonDelete.proc.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[PersonDelete]
+(
+	@PersonId   INT
+)
+AS
+	SET NOCOUNT ON
+
+	DELETE dbo.Pay
+	 WHERE Person = @PersonId
+
+	DELETE dbo.PersonCalendar
+	 WHERE PersonId = @PersonId
+
+	DELETE dbo.PersonCalendarAuto
+	 WHERE PersonId = @PersonId
+
+	 DELETE dbo.PersonStatusHistory
+	 WHERE PersonId = @PersonId
+
+	DELETE dbo.Person
+	 WHERE PersonId = @PersonId
+
+	DELETE dbo.PersonHistory
+	 WHERE PersonId = @PersonId
+
