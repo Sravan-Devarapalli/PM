d45d838310353f5acb81bd848a9d95c906e5037b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonDivisionHistory.proc.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[PersonDivisionHistory]
+	(
+		@PersonID NVARCHAR(100)
+	)
+AS
+BEGIN
+		DECLARE @FutureDate DATETIME
+		SELECT @FutureDate = dbo.GetFutureDate()
+		SELECT p.FirstName +', ' +p.LastName AS Person,
+				CONVERT(DATE,dh.StartDate) AS Startdate,
+				ISNULL(DATEADD(dd,-1,dh.EndDate),@FutureDate) AS Enddate,
+				ISNULL(pd.DivisionName,'') AS Division
+		FROM dbo.v_DivisionHistory dh
+		INNER JOIN dbo.Person P ON dh.PersonId = p.PersonId AND p.EmployeeNumber = @PersonID
+		LEFT JOIN dbo.PersonDivision pd ON pd.DivisionId = dh.DivisionId
+END
