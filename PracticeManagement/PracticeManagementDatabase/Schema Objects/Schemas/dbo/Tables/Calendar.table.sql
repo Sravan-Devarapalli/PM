b0d9e1030e3d1e77d3a7cae40b099a6a1966a6ae
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Calendar.table.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Calendar] (
+    [Date]					DATETIME NOT NULL,
+    [DayOff]				BIT      NOT NULL,
+	[Year]					INT	NOT NULL,
+	[MonthStartDate]		DATETIME NOT NULL,
+	[MonthEndDate]			DATETIME NOT NULL,
+	[MonthNumber]			INT		 NOT NULL,
+	[DaysInMonth]			INT NOT NULL,
+	[DaysInYear]			INT NOT NULL,
+	[IsRecurring]			BIT NULL,
+	[RecurringHolidayId]	INT NULL,
+	[HolidayDescription]	NVARCHAR(255),
+	[RecurringHolidayDate]  DATETIME NULL,
+	QuarterStartDate		DATETIME NOT NULL,
+	QuarterEndDate			DATETIME NOT NULL
+);
+
