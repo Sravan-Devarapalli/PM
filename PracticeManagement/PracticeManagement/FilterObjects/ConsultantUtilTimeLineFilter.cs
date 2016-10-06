945a8f2e65d4c18ff267486b72f0c51dcdb3380e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/FilterObjects/ConsultantUtilTimeLineFilter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PraticeManagement.FilterObjects
+{
+    [Serializable]
+    public class ConsultantUtilTimeLineFilter
+    {
+        public int? PersonId { get; set; }
+
+        public string ChartTitle { get; set; }
+
+        public bool ActivePersons { get; set; }
+
+        public bool ProjectedPersons { get; set; }
+
+        public string PracticesSelected { get; set; }
+
+        public string DivisionsSelected { get; set; }
+
+        public bool ActiveProjects { get; set; }
+
+        public bool ProjectedProjects { get; set; }
+
+        public bool ExperimentalProjects { get; set; }
+
+        public bool InternalProjects { get; set; }
+
+        public bool ProposedProjects { get; set; }
+
+        public bool CompletedProjects { get; set; }
+
+        public string TimescalesSelected { get; set; }
+
+        public bool ExcludeInternalPractices { get; set; }
+
+        public string SortDirection { get; set; }
+
+        public int SortId { get; set; }
+
+        public int AvgUtil { get; set; }
+
+        public string Period { get; set; }
+
+        public string DetalizationSelectedValue { get; set; }
+
+        public DateTime BegPeriod { get; set; }
+
+        public DateTime EndPeriod { get; set; }
+
+        public bool FiltersChanged { get; set; }
+    }
+}
