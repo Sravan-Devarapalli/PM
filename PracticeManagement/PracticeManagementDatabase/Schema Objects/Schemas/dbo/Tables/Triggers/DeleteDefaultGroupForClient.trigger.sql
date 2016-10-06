36a4d032151971a8ad177878f4a403f1733e250a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Triggers/DeleteDefaultGroupForClient.trigger.sql
@@ -0,0 +1,17 @@
+﻿CREATE TRIGGER DeleteDefaultGroupForClient 
+   ON  dbo.Client 
+   AFTER DELETE
+AS 
+BEGIN
+	SET NOCOUNT ON;
+
+	DELETE FROM dbo.ProjectGroup 
+	WHERE ClientId IN ( SELECT d.ClientId FROM deleted AS d)
+
+	DELETE FROM dbo.PricingList 
+	WHERE ClientId IN ( SELECT d.ClientId FROM deleted AS d)
+
+	DELETE FROM dbo.BusinessGroup 
+	WHERE ClientId IN ( SELECT d.ClientId FROM deleted AS d)
+END
+
