8acf214492f66c75516a3eafb58867c9d2036b12
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/GuestPages/InternalError.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PraticeManagement
+{
+	public partial class InternalError : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
+
