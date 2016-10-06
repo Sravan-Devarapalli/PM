2acc4a3493556294bc7b96917f922fe41d50e384
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetPersonVacationDaysTable.function.sql
@@ -0,0 +1,17 @@
+﻿CREATE FUNCTION [dbo].[GetPersonVacationDaysTable]
+(
+	@StartDate DATETIME,
+	@EndDate DATETIME
+)
+RETURNS TABLE
+AS
+	RETURN
+	SELECT PersonId,COUNT(*) AS VacationDays
+	FROM dbo.v_PersonCalendar
+	WHERE Date BETWEEN @StartDate AND @EndDate AND 
+		  DayOff = 1 
+		  AND ActualHours > 0--if a person has added Timeoff  for complete 8 hr then the day is treated as vacation day.
+		  AND DATEPART(dw, Date) NOT IN (7, 1) AND
+		  DayOff != CompanyDayOff  
+	GROUP BY PersonId
+
