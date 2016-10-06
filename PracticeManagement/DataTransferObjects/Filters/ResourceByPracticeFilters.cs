ed704b4dcef612553a8c64930f01048fef31d2c4
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Filters/ResourceByPracticeFilters.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTransferObjects.Filters
+{
+    [Serializable]
+    public class BadgeResourceFilters
+    {
+        public DateTime ReportStartDate
+        {
+            get;
+            set;
+        }
+        public DateTime ReportEndDate
+        {
+            get;
+            set;
+        }
+
+        public int SelectedView
+        {
+            get;
+            set;
+        }
+
+        public string PracticeIds
+        {
+            get;
+            set;
+        }
+
+        public string TitleIds
+        {
+            get;
+            set;
+        }
+
+        public string PersonStatusIds
+        {
+            get;
+            set;
+        }
+        public string PayTypeIds
+        {
+            get;
+            set;
+        }
+
+
+        public bool IsBadgedNotOnProject { get; set; }
+
+        public bool IsBadgedOnProject { get; set; }
+
+        public bool IsClockNotStarted { get; set; }
+
+    }
+}
+
