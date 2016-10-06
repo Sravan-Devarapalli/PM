72c0390c779c0ff35da95822e4ec602a21407758
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetProjectAccessesEmpNumbers.function.sql
@@ -0,0 +1,18 @@
+﻿CREATE FUNCTION [dbo].[GetProjectAccessesEmpNumbers]
+(
+	@ProjectId INT
+)
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN
+	DECLARE @Temp NVARCHAR(MAX) = ''
+  
+	SELECT @Temp = @Temp + CASE WHEN PGCP.Timescale IN (3,4) THEN Pers.EmployeeNumber ELSE Pers.PaychexID END +', '
+	FROM Project P
+	JOIN ProjectAccess PM ON PM.ProjectId = P.ProjectId
+	JOIN Person Pers ON Pers.PersonId = PM.ProjectAccessId
+	LEFT JOIN dbo.GetCurrentPayTypeTable() PGCP ON PGCP.PersonId = Pers.PersonId
+	WHERE P.ProjectId = @ProjectId
+
+	RETURN @Temp
+END
