2dfbb56a35fb9a8622f264ae35b3050a0bfc5e72
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetIsHourlyRevenueByPeriod.proc.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[GetIsHourlyRevenueByPeriod]
+(
+	@PersonId INT,
+	@ProjectId INT,
+	@StartDate DATETIME,
+	@EndDate DATETIME
+)
+AS
+BEGIN 
+
+	SELECT D.ChargeCodeDate
+		,CONVERT(BIT,(CASE WHEN SUM(CASE WHEN ISNULL(IsHourlyAmount,1) = 1  THEN 1 ELSE 0 END) > 0 THEN 1 ELSE 0 END)) AS 'IsHourlyRevenue'
+	FROM (select date AS ChargeCodeDate from dbo.Calendar where DATE BETWEEN @StartDate and @EndDate) D 
+	LEFT JOIN [dbo].[Milestone] M  ON D.ChargeCodeDate BETWEEN M.StartDate AND M.ProjectedDeliveryDate  AND M.ProjectId = @ProjectId
+	LEFT JOIN dbo.MilestonePerson MP ON M.MilestoneId = MP.MilestoneId AND MP.PersonId =  @PersonId
+	GROUP BY D.ChargeCodeDate
+		
+
+END
+
