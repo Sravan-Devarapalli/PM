8d6dc6195fe70438f476d5d2c3477e43cba2630a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OverheadRateTypeListAll.proc.sql
@@ -0,0 +1,17 @@
+﻿--The source is 
+-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 5-27-2008
+-- Description:	Retrieves the list the overhead rate types.
+-- =============================================
+CREATE PROCEDURE [dbo].[OverheadRateTypeListAll]
+AS
+	SET NOCOUNT ON
+
+	SELECT t.OverheadRateTypeId,
+	       t.Name,
+	       t.IsPercentage,
+	       t.HoursToCollect
+	  FROM dbo.OverheadRateType AS t
+	ORDER BY t.HoursToCollect, t.Name
+
