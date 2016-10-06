b860e98fafba2bd278c0c2e5a96eac2aabf16111
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/CFDivisionMapping.table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[CFDivisionMapping]
+(
+   Id			INT		IDENTITY (1, 1) NOT NULL,
+   DivisionId	INT		NOT NULL,
+   PracticeId	INT		NOT NULL,
+   CFDivisionId	INT		NOT NULL,
+   TitleId		INT		NULL
+)
+
+
