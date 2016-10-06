81c2770bf28f4cc5c40c0c10fc96783cfb616e1f
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_BadgeHistory_PersonId.fkey.sql
@@ -0,0 +1,6 @@
+﻿ALTER TABLE [dbo].[BadgeHistory]
+	ADD CONSTRAINT [FK_BadgeHistory_PersonId] 
+	FOREIGN KEY (PersonId)
+	REFERENCES dbo.Person (PersonId) ON DELETE NO ACTION ON UPDATE NO ACTION;	
+
+
