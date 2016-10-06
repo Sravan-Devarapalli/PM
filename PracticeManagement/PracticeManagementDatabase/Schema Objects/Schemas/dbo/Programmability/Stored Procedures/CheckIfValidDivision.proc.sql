f3bd941ec0fc240cecdf69b77fa022fff682f0c9
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CheckIfValidDivision.proc.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[CheckIfValidDivision]
+	(
+		@PersonId	INT,
+		@StartDate	DATETIME,
+		@EndDate	DATETIME
+	)
+AS
+BEGIN
+
+	SELECT	TOP 1 *
+	FROM	v_DivisionHistory DH 
+	WHERE	DH.PersonId = @PersonId AND DH.StartDate <= @EndDate AND (DH.EndDate IS NULL OR @StartDate < DH.EndDate) AND ISNULL(DH.DivisionId,0) NOT IN (SELECT DivisionId FROM dbo.PersonDivision WHERE DivisionName IN ('Consulting','Business Development','Technology Consulting','Business Consulting','Director'))
+	
+END
+
