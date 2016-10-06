d49eb3bc8821e7e8cad2bcad046f5007f3ccf7d6
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SaveMarginInfoDefaults.proc.sql
@@ -0,0 +1,30 @@
+﻿CREATE PROCEDURE [dbo].[SaveMarginInfoDefaults]
+(
+	@GoalTypeId   INT,
+	@ColorId    INT,
+	@StartRange INT,
+	@EndRange   INT,
+	@isDeletePreviousMarginInfo BIT
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	IF(@isDeletePreviousMarginInfo=1)
+	BEGIN
+	DELETE dbo.DefaultMarginColorInfo 
+	WHERE GoalTypeId = @GoalTypeId
+	END
+	
+	INSERT INTO [dbo].DefaultMarginColorInfo
+           (GoalTypeId
+           ,[ColorId]
+           ,[StartRange]
+           ,[EndRange])
+     VALUES
+           (@GoalTypeId
+           ,@ColorId
+           ,@StartRange
+           ,@EndRange)
+	
+END
