c78e12422767f3f231cb10b0b13b3ea221d72820
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/PK_OverheadRateType.pkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[OverheadRateType]
+    ADD CONSTRAINT [PK_OverheadRateType] PRIMARY KEY CLUSTERED ([OverheadRateTypeId] ASC) WITH (IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF);
+
+
