bbe6e0fe593dfdaea620c8c935695a737db21d69
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/ActivityLog_Old.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using PraticeManagement.Controls;
+
+namespace PraticeManagement
+{
+	public partial class ActivityLog : PracticeManagementPageBase
+	{
+		protected void Page_Init(object sender, EventArgs e)
+		{
+		}
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			if (!IsPostBack)
+			{
+			}
+		}
+
+        protected override void Display()
+        {
+        }
+	}
+}
+
