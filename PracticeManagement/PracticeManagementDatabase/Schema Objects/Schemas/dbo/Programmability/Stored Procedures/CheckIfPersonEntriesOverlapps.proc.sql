a45f41c4b82cb03758450e329f43d565a37a992e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CheckIfPersonEntriesOverlapps.proc.sql
@@ -0,0 +1,29 @@
+﻿CREATE PROCEDURE [dbo].[CheckIfPersonEntriesOverlapps]
+	(
+		@MilestoneId	INT,
+		@PersonId		INT,
+		@StartDate		DATETIME,
+		@EndDate		DATETIME
+	)
+AS
+BEGIN
+		DECLARE @ProjectId	INT,
+				@NotValid	BIT = 0
+
+		SELECT @ProjectId = ProjectId FROM dbo.Milestone WHERE MilestoneId = @MilestoneId
+
+		IF EXISTS 
+		(
+			SELECT 1 
+			FROM dbo.MilestonePersonEntry MPE
+			JOIN dbo.MilestonePerson MP ON MP.MilestonePersonId = MPE.MilestonePersonId
+			JOIN dbo.Milestone M ON M.MilestoneId = MP.MilestoneId 
+			WHERE MP.PersonId = @PersonId AND M.ProjectId = @ProjectId AND MP.MilestoneId <> @MilestoneId
+			AND (@StartDate <= MPE.EndDate AND MPE.StartDate <= @EndDate)
+		)
+		BEGIN
+			SET @NotValid = 1
+		END
+
+		SELECT @NotValid
+END
