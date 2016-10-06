51cf33c024404c122d1d38fb245fa9773d89c291
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetSubstituteDayDetails.proc.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[GetSubstituteDayDetails]
+(
+	@SubstituteDayDate	DATETIME,
+	@PersonId		INT
+)
+AS
+BEGIN
+	
+	SELECT PC.Date 'HolidayDate', C.HolidayDescription 'HolidayDescription'
+	FROM dbo.PersonCalendar PC
+	INNER JOIN dbo.Calendar C ON PC.PersonId = @PersonId AND PC.SubstituteDate = @SubstituteDayDate AND C.Date = PC.Date
+
+END
