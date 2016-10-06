bdb3d9146553d91788fb1786146fc1dcbc01c0ec
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/IsPersonAlreadyAddedtoMilestone.proc.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[IsPersonAlreadyAddedtoMilestone]
+(
+	@MilestoneId         INT,
+	@PersonId            INT
+)
+AS
+	SET NOCOUNT ON
+
+	IF EXISTS(SELECT 1 FROM dbo.MilestonePerson WHERE MilestoneId = @MilestoneId AND PersonId = @PersonId)
+	BEGIN
+		SELECT 1 AS Result
+	END
+	ELSE
+	BEGIN
+		SELECT 0 AS Result
+	END
+
