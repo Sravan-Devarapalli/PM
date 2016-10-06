d145f51e1cd0faf7891fb492b6aad9a9c65e738a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/OpportunityStatusType.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// Determines an opportunity statuses.
+    /// </summary>
+    [DataContract]
+    public enum OpportunityStatusType
+    {
+        [EnumMember]
+        Active = 1,
+
+        [EnumMember]
+        Lost = 2,
+
+        [EnumMember]
+        Inactive = 3,
+
+        [EnumMember]
+        Won = 4,
+
+        [EnumMember]
+        Experimental = 5
+    }
+}
