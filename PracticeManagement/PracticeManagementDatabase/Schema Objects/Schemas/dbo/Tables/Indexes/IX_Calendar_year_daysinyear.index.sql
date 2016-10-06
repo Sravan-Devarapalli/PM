7db7def30d5d31109364671021878b51dfd559ce
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Indexes/IX_Calendar_year_daysinyear.index.sql
@@ -0,0 +1,8 @@
+﻿CREATE NONCLUSTERED INDEX [IX_Calendar_year_daysinyear] ON [dbo].[Calendar] 
+(
+	[Year] ASC,
+	daysinyear ASC
+)WITH (SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+
+
+
