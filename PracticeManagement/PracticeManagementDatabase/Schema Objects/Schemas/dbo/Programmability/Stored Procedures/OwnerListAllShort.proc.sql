99774405e99ed9bede46d1bdd05e4d3a55c8e9a3
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OwnerListAllShort.proc.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[OwnerListAllShort]
+	@PersonStatusIds NVARCHAR(30) = NULL
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DECLARE @PersonStatusIdsList TABLE(ID INT)
+	INSERT INTO @PersonStatusIdsList (ID)
+	SELECT ResultId 
+	FROM dbo.ConvertStringListIntoTable(@PersonStatusIds)
+
+	SELECT DISTINCT p.PersonId,
+	       p.FirstName,
+	       p.LastName
+	FROM dbo.aspnet_Users AS u
+	INNER JOIN dbo.Person AS p ON u.UserName = p.Alias
+	INNER JOIN dbo.aspnet_UsersInRoles AS ur ON u.UserId = ur.UserId
+	INNER JOIN dbo.aspnet_Roles AS r ON ur.RoleId = r.RoleId
+	WHERE (@PersonStatusIds IS NULL OR p.PersonStatusId IN (SELECT ID FROM @PersonStatusIdsList))
+		   AND r.RoleName IN ('Project lead' , 'Practice Area Manager' ,'Business Unit Manager','Client Director')
+	ORDER BY p.LastName, p.FirstName
+END	
+
