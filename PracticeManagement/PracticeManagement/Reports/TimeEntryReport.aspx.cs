ef57d912dd2fa89a516d75dec94678c5e15f34d9
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Reports/TimeEntryReport.aspx.cs
@@ -0,0 +1,19 @@
+﻿using PraticeManagement.Controls;
+
+namespace PraticeManagement
+{
+    public partial class TimeEntryReport : PracticeManagementPageBase
+    {
+        #region Overrides of PracticeManagementPageBase
+
+        /// <summary>
+        /// Displays the data.
+        /// </summary>
+        protected override void Display()
+        {
+        }
+
+        #endregion
+    }
+}
+
