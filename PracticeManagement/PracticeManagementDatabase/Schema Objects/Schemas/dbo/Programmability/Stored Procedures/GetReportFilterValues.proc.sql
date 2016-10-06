eea9b847d34758d910b8c835bece63a69742b664
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetReportFilterValues.proc.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[GetReportFilterValues]
+	@CurrentUserId INT,
+	@ReportId	   INT,
+	@PreviousUserId INT,
+	@SessionId     NVARCHAR (MAX)
+AS
+BEGIN
+	SELECT ReportFilters 
+	FROM ReportFilterValues 
+	WHERE CurrentUserId=@CurrentUserId AND ReportId=@ReportId AND PreviousUserId=@PreviousUserId AND SessionId=@SessionId
+
+END
+
