f1f61deaa3b0abdaecad9b4902df20bf02e15092
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_OverheadFixedRateTimescale_TimescaleId.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[OverheadFixedRateTimescale]
+    ADD CONSTRAINT [FK_OverheadFixedRateTimescale_TimescaleId] FOREIGN KEY ([TimescaleId]) REFERENCES [dbo].[Timescale] ([TimescaleId]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
