1022d625984bee2c506043ab2a18ae342aefc0bf
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetMilestonePersonHoursInPeriod.function.sql
@@ -0,0 +1,24 @@
+﻿CREATE FUNCTION GetMilestonePersonHoursInPeriod 
+(
+	-- Add the parameters for the function here
+	@MilestonePersonId int
+)
+RETURNS decimal(18,2)
+AS
+BEGIN
+	-- Declare the return variable here
+	DECLARE @HoursInPeriod decimal(18,2)
+
+	SELECT  @HoursInPeriod = SUM(dbo.PersonProjectedHoursPerDay(ISNULL(pcal.DayOff, cal.DayOff),cal.DayOff,pcal.ActualHours,MPE.HoursPerDay))
+	FROM dbo.Milestone AS m
+	INNER JOIN dbo.MilestonePerson AS mp on m.MilestoneId = mp.MilestoneId
+	INNER JOIN dbo.MilestonePersonEntry AS mpe on mpe.MilestonePersonId = mp.MilestonePersonId	
+	INNER JOIN Calendar AS cal ON cal.Date BETWEEN mpe.StartDate AND mpe.EndDate
+	LEFT JOIN PersonCalendar AS pcal ON cal.Date = pcal.Date AND pcal.PersonId = mp.PersonId	
+	WHERE mp.MilestonePersonId = @MilestonePersonId	
+	
+	-- Return the result of the function
+	RETURN @HoursInPeriod
+
+END
+
