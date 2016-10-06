057d8bea708d2edf1f2997bf3e8f5ac0fb3df767
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Config/ConsultantUtilTimelineReport.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.UI;
+using PraticeManagement.Controls;
+namespace PraticeManagement.Config
+{
+    public partial class ConsultantUtilTimelineReport : PracticeManagementPageBase, IPostBackEventHandler
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected override void Display()
+        {
+        }
+
+        public void RaisePostBackEvent(string eventArgument)
+        {
+            utf.SaveFilters();
+            Redirect(eventArgument);
+        }
+    }
+}
