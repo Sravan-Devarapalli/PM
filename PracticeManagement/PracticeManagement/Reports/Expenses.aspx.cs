3ee2fa4eef6c1c3bf57ef2e73da649de494bb666
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Reports/Expenses.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using PraticeManagement.Controls;
+
+namespace PraticeManagement.Reporting
+{
+    public partial class Expense : PracticeManagementPageBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override void Display()
+        {
+
+        }
+    }
+}
