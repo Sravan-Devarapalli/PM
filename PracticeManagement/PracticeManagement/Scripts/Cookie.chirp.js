7c762dca15f20a3eb9b616e431649122e719a0ed
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Scripts/Cookie.chirp.js
@@ -0,0 +1,23 @@
+﻿function Get_Cookie(name)
+{
+	var start = document.cookie.indexOf(name + "=");
+	var len = start + name.length + 1;
+	if ((!start) && (name != document.cookie.substring(0, name.length)))
+	{
+		return null;
+	}
+	if (start == -1) return null;
+	var end = document.cookie.indexOf(";", len);
+	if (end == -1) end = document.cookie.length;
+	return unescape(document.cookie.substring(len, end));
+};
+
+function Delete_Cookie(name, path, domain)
+{
+	if (Get_Cookie(name))
+	{
+		var d = new Date();
+		document.cookie = name + "=" + ((path) ? ";path=" + path : "") + ((domain) ? ";domain=" + domain : "") + ";expires=" + d.toGMTString() + ";" + ";";
+	}
+};
+
