84a1646b08747e08f937a883a76a0dce9347bd19
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_PersonCalendar_TimeTypeId.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[PersonCalendar]
+	ADD CONSTRAINT [FK_PersonCalendar_TimeTypeId] 
+	FOREIGN KEY (TimeTypeId)
+	REFERENCES [dbo].[TimeType] (TimeTypeId) ON DELETE NO ACTION ON UPDATE NO ACTION;
