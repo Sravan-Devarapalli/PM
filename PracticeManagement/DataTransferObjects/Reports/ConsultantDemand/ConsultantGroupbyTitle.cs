c2693889b8ac89f480726dec726e9ecd703eb178
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Reports/ConsultantDemand/ConsultantGroupbyTitle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace DataTransferObjects.Reports.ConsultingDemand
+{
+    [DataContract]
+    [Serializable]
+    public class ConsultantGroupbyTitle
+    {
+        [DataMember]
+        public string Title { get; set; }
+
+        public string HtmlEncodedTitle
+        {
+            get
+            {
+                return HttpUtility.HtmlEncode(Title);
+            }
+        }
+
+        [DataMember]
+        public List<ConsultantDemandDetailsByMonthByTitle> ConsultantDetails { get; set; }
+    }
+}
