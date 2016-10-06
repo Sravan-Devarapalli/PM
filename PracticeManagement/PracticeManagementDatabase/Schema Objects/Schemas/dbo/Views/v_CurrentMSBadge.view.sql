201816dbbfcf6b22f0d9e0b5905add8979a6d024
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_CurrentMSBadge.view.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW [dbo].[v_CurrentMSBadge]
+	AS 
+	WITH LatestDeactivationHistory
+	AS
+	(
+	Select PersonId, MAX(OrganicBreakEndDate) as OrgBrkEndDate
+	FROM BadgeDeactivationHisroty 
+	GROUP BY PersonId
+	)
+
+	SELECT B.*,M.IsBlocked,M.BlockStartDate,M.BlockEndDate,M.DeactivatedDate,M.ExcludeInReports,M.ManageServiceContract
+	FROM BadgeHistoryForReports B
+	LEFT JOIN LatestDeactivationHistory D ON D.PersonId=B.PersonId
+	LEFT JOIN dbo.MSBadge M ON M.PersonId = B.PersonId
+	WHERE ((CONVERT(DATE,dbo.GettingPMTime(GETUTCDATE()))>=B.BadgeStartDate AND CONVERT(DATE,dbo.GettingPMTime(GETUTCDATE()))<= B.BadgeEndDate) OR 
+	(CONVERT(DATE,dbo.GettingPMTime(GETUTCDATE()))>=B.BreakStartDate AND CONVERT(DATE,dbo.GettingPMTime(GETUTCDATE()))<= B.BreakEndDate) or (CONVERT(DATE,dbo.GettingPMTime(GETUTCDATE()))<B.BadgeStartDate and CONVERT(DATE,dbo.GettingPMTime(GETUTCDATE()))< B.BadgeEndDate))
+	 AND (D.OrgBrkEndDate IS NULL OR B.BadgeStartDate>=D.OrgBrkEndDate)   
+
