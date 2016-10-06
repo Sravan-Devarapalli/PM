f4aa7a7e025671c8c1266bd51a97aa79158f3e5d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/ClientDAL/TimescaleDAL.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using DataAccess.Other;
+using DataTransferObjects;
+
+namespace DataAccess
+{
+    /// <summary>
+    /// Provides an access to the data in the Timescale table
+    /// </summary>
+    public static class TimescaleDAL
+    {
+        #region Constants
+
+        private const string TimescaleGetByIdProcedure = "dbo.TimescaleGetById";
+        private const string TimescaleGetAllProcedure = "dbo.TimeScaleGetAll";
+
+        private const string TimescaleIdParam = "@TimescaleId";
+
+        private const string TimescaleIdColumn = "TimescaleId";
+        private const string NameColumn = "Name";
+
+        #endregion Constants
+
+        #region Methods
+
+        /// <summary>
+        /// Retrives a <see cref="Timescale"/> by its ID.
+        /// </summary>
+        /// <param name="timescaleId">An ID of the <see cref="Timescale"/> to be retrieved.</param>
+        /// <returns>A <see cref="Timescale"/> object if found and null otherwise.</returns>
+        public static Timescale GetById(TimescaleType timescaleId)
+        {
+            using (SqlConnection connection = new SqlConnection(DataSourceHelper.DataConnection))
+            using (SqlCommand command = new SqlCommand(TimescaleGetByIdProcedure, connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue(TimescaleIdParam, (int)timescaleId);
+
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader(CommandBehavior.SingleRow))
+                {
+                    List<Timescale> result = new List<Timescale>(1);
+
+                    ReadTimescales(reader, result);
+
+                    return result.Count > 0 ? result[0] : null;
+                }
+            }
+        }
+
+        public static List<Timescale> GetAll()
+        {
+            using (SqlConnection connection = new SqlConnection(DataSourceHelper.DataConnection))
+            using (SqlCommand command = new SqlCommand(TimescaleGetAllProcedure, connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    List<Timescale> result = new List<Timescale>();
+
+                    ReadTimescales(reader, result);
+
+                    return result;
+                }
+            }
+        }
+
+        private static void ReadTimescales(DbDataReader reader, List<Timescale> result)
+        {
+            if (!reader.HasRows) return;
+            int timescaleIdIndex = reader.GetOrdinal(TimescaleIdColumn);
+            int nameIndex = reader.GetOrdinal(NameColumn);
+
+            while (reader.Read())
+            {
+                Timescale timescale = new Timescale { Id = reader.GetInt32(timescaleIdIndex), Name = reader.GetString(nameIndex) };
+
+                result.Add(timescale);
+            }
+        }
+
+        #endregion Methods
+    }
+}
