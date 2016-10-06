a5c3acff7b3dfa7331c7db772c9183b5f0f9df28
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/Generic/Buttons/ShadowedTextButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PraticeManagement.Controls.Generic.Buttons
+{
+    public class ShadowedTextButton : LinkButton
+    {
+        //Renders custom button with the following markup        
+        //<span class="btn-body"> 
+        //    <span class="btn-text">Add Anything What You Need Here Twice</span> 
+        //    <span class="btn-text-shadow">Add Anything What You Need Here Twice</span> 
+        //</span> 
+
+        protected override void  RenderContents(HtmlTextWriter writer)
+        {
+            writer.AddAttribute("class", "btn-body");
+            writer.RenderBeginTag(HtmlTextWriterTag.Span);
+
+            writer.AddAttribute("class", "btn-text");
+            writer.RenderBeginTag(HtmlTextWriterTag.Span);
+            writer.Write(Text);
+            writer.RenderEndTag();
+
+            writer.AddAttribute("class", "btn-text-shadow");
+            writer.RenderBeginTag(HtmlTextWriterTag.Span);
+            writer.Write(Text);
+            writer.RenderEndTag();
+
+            writer.RenderEndTag();
+        }
+    }
+}
+
