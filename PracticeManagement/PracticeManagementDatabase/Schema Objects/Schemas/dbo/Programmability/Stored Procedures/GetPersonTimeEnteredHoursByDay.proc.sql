4df9451b97c4c833fb4288a11c1293dd8221fb07
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetPersonTimeEnteredHoursByDay.proc.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE [dbo].[GetPersonTimeEnteredHoursByDay]
+	@PersonId INT, 
+	@Date		DATETIME,
+	@IncludePTOAndHoliday	BIT
+AS
+BEGIN
+	DECLARE @Hours REAL,
+		@PTOChargeCodeId INT,
+		@HolidayChargeCodeId INT
+
+	SELECT @PTOChargeCodeId = Id
+	FROM dbo.ChargeCode
+	WHERE TimeTypeId = dbo.GetPTOTimeTypeId()
+
+	SELECT @HolidayChargeCodeId = Id
+	FROM dbo.ChargeCode
+	WHERE TimeTypeId = dbo.GetHolidayTimeTypeId()
+	
+	SELECT @Hours = SUM(TEH.ActualHours)
+	FROM dbo.TimeEntry TE
+	INNER JOIN dbo.TimeEntryHours TEH ON TEH.TimeEntryId = te.TimeEntryId 
+	WHERE TE.PersonId = @PersonId
+	AND TE.ChargeCodeDate = @Date
+	AND (@IncludePTOAndHoliday = 1 OR (@IncludePTOAndHoliday = 0 AND TE.ChargeCodeId NOT IN (@PTOChargeCodeId, @HolidayChargeCodeId)))
+	GROUP BY TE.PersonId, TE.ChargeCodeDate
+
+	SELECT @Hours
+END
