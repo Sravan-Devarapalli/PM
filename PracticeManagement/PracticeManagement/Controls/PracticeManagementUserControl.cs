96aef6f7bcc0506e2a060216966fd2d48dd53190
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/PracticeManagementUserControl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Web.UI;
+using PraticeManagement.Events;
+
+namespace PraticeManagement.Controls
+{
+    public class PracticeManagementUserControl : UserControl
+    {
+        #region Protected Methods
+
+        protected new PracticeManagementPageBase Page
+        {
+            get { return base.Page as PracticeManagementPageBase; }
+        }
+
+        protected T GetViewStateValue<T>(string key, T defValue)
+        {
+            var value = ViewState[key];
+
+            return value == null ? defValue : (T) value;
+        }
+
+        protected void SetViewStateValue<T>(string key, T value){
+            ViewState[key] = value;    
+        }
+
+        #endregion
+
+        #region
+
+        public EventHandler<ErrorEventArgs> CustomError;
+
+        #endregion
+    }
+}
+
