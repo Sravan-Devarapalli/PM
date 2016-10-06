8efac2c3d9e106f337a0ef5c5f4d3b6e1458cd70
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ColorInformation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [Serializable]
+    [DataContract]
+    public class ColorInformation
+    {
+        [DataMember]
+        public string ColorValue
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int ColorId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string ColorDescription
+        {
+            get;
+            set;
+        }
+    }
+}
