dfe7c9bb0e884c13f4bedfcfbbba1e521658ef58
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Triggers/UpdateProjectWithLinkedOpportunity.trigger.sql
@@ -0,0 +1,20 @@
+﻿CREATE TRIGGER UpdateProjectWithLinkedOpportunity 
+   ON  dbo.Opportunity 
+   AFTER INSERT, UPDATE
+AS 
+BEGIN
+	SET NOCOUNT ON;
+
+	Declare @OpportunityId int
+	Declare @ProjectId int
+
+	select 
+		@ProjectId = i.ProjectId, 
+		@OpportunityId = i.OpportunityId 
+	from inserted as i
+
+	update dbo.Project
+	set OpportunityId = @OpportunityId
+	where ProjectId = @ProjectId
+END
+
