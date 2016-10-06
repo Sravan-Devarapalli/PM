84e191c33747301281c35bab06cf4850352bb686
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SaveAnnouncement.proc.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[SaveAnnouncement]
+	@Text	NVARCHAR(MAX),
+	@RichText	NVARCHAR(MAX)
+AS
+	DECLARE @date DATETIME
+
+	SET @date = dbo.InsertingTime()
+
+	IF NOT EXISTS (SELECT 1 FROM Announcements)
+	BEGIN
+		INSERT INTO Announcements(Text, RichText, Date)
+		VALUES (@Text, @RichText, @date)
+	END
+	ELSE
+	BEGIN
+		UPDATE Announcements
+		SET RichText = @RichText, [Text] = @Text
+	END
