0589f579cd5b5f91fd673eec5d39f9890557bbd6
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/Constraints/CKPersonSkill_LastUsed.chkconst.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [Skills].[PersonSkill]  WITH CHECK ADD  CONSTRAINT [CKPersonSkill_LastUsed] CHECK  (([LastUsed]>=(1970) AND [LastUsed]<=datepart(year,getdate())))
+GO
+ALTER TABLE [Skills].[PersonSkill] CHECK CONSTRAINT [CKPersonSkill_LastUsed]
+GO
