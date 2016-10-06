76d1a2593b6b1fef5db0665b7a0dcaa4ea40df75
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/FilterObjects/ConsultingDemandFilter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PraticeManagement.FilterObjects
+{
+    [Serializable]
+    public class ConsultingDemandFilter
+    {
+        public string FiltersChanged { get; set; }//0 or 1.
+
+        public string PeriodSelected { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+    }
+}
