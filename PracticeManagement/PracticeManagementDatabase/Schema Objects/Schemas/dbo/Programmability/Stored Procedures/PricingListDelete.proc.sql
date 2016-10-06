4e122585d4f8575335bbce16a4d69234bf6a26ff
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PricingListDelete.proc.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[PricingListDelete]
+(
+	@PricingListId INT,
+	@UserLogin          NVARCHAR(255)
+)
+AS
+BEGIN
+	-- Start logging session
+	EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
+
+	DELETE dbo.PricingList 
+	WHERE PricingListId = @PricingListId
+
+	-- End logging session
+	EXEC dbo.SessionLogUnprepare
+
+END
