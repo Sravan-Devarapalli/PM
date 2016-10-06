eb7417c09112ff24ea7303fa5d809315b53bd951
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/CalendarLegend.ascx.cs
@@ -0,0 +1,14 @@
+﻿
+namespace PraticeManagement.Controls
+{
+    public partial class CalendarLegend : System.Web.UI.UserControl
+    {
+        public bool disableChevron
+        {
+            set
+            {
+                this.dvCollapsiblePanel.Visible = !value;
+            }
+        }
+    }
+}
