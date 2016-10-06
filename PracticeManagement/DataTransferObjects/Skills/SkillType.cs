cf3cef5d50b373054c1dd4dde322bd4aac584d41
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Skills/SkillType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Skills
+{
+    [DataContract]
+    [Serializable]
+    public class SkillType : LookupBase
+    {
+    }
+}
