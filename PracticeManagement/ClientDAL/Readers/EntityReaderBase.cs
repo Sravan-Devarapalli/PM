5ea6ac71ef6047204d1e2510b3403f2b9caa386e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/ClientDAL/Readers/EntityReaderBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Common;
+
+namespace DataAccess.Readers
+{
+    public abstract class EntityReaderBase<T>
+    {
+        public DbDataReader Reader { get; private set; }
+
+        public virtual void SetReader(DbDataReader value)
+        {
+            Reader = value;
+        }
+
+        protected EntityReaderBase(DbDataReader reader)
+        {
+            Reader = reader;
+        }
+
+        protected EntityReaderBase()
+        {
+        }
+
+        public abstract T ReadEntity();
+    }
+}
