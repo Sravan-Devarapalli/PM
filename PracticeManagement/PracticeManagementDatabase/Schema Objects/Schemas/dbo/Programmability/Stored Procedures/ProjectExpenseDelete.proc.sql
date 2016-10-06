153ee5cb5b1cccd89f61a1d157fcd468ced983f6
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectExpenseDelete.proc.sql
@@ -0,0 +1,14 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2010-04-20
+-- =============================================
+CREATE PROCEDURE dbo.ProjectExpenseDelete 
+    @ExpenseId int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DELETE FROM [dbo].[ProjectExpense]
+	WHERE Id = @ExpenseId
+end 
+
