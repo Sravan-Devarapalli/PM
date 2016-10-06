ef6cac37d0adbb466f3cfcd5e0afd36367b07b78
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/RecruiterCommissionHistory.table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[RecruiterCommissionHistory]
+(
+	[Id]			 INT IDENTITY(1,1) NOT NULL,
+    [RecruitId]      INT             NOT NULL,
+    [RecruiterId]    INT             NOT NULL,
+    [HoursToCollect] INT             NOT NULL,
+    [Amount]         DECIMAL (18, 2) NULL,
+	[StartDate]		 DATETIME NOT NULL,
+	[EndDate]		 DATETIME NULL,
+	CONSTRAINT PK_RecruiterCommissionHistory PRIMARY KEY ([Id])
+)
