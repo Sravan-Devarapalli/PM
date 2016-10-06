baaf22a51b8c505384d8a2f032e13368e15f44ef
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/UserPassWordHistory.table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[UserPassWordHistory] (
+     Id										 INT IDENTITY(1,1) NOT NULL,	
+    [UserId]                                 UNIQUEIDENTIFIER NOT NULL,
+    [Password]                               NVARCHAR (128)   NOT NULL,
+    [PasswordFormat]                         INT             CONSTRAINT Df_UserPassWordHistory_PasswordFormat DEFAULT ((0)) NOT NULL,
+    [PasswordSalt]                           NVARCHAR (128)   NOT NULL,
+    [LastPasswordChangedDate]                DATETIME         NOT NULL,
+    CONSTRAINT PK_UserPassWordHistory_Id     PRIMARY KEY CLUSTERED(Id),
+    CONSTRAINT FK_UserPassWordHistory_UserId FOREIGN KEY(UserId) REFERENCES  dbo.aspnet_Membership(UserId)
+);
