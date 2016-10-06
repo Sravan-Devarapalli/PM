422af04830f2882bb2062e69667891db742b4d24
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/Generic/Filtering/PersonReportFilter.ascx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DataTransferObjects;
+using PraticeManagement.Controls;
+using System.Web.Security;
+
+namespace PraticeManagement.Controls.Generic.Filtering
+{
+    public partial class PersonReportFilter : System.Web.UI.UserControl
+    {
+        public List<int> SelectedValues
+        {
+            get { return this.cblPersons.SelectedValues;}
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                var currentPerson = DataHelper.CurrentPerson;
+                var personRoles = Roles.GetRolesForUser(currentPerson.Alias);
+                string statusIdsList = GetStatusIds();
+                int? personId = null;
+                if(!personRoles.Any(s => s=="System Administrator"))
+                {
+                    personId = currentPerson.Id;
+                }
+                DataHelper.FillTimeEntryPersonList(this.cblPersons, Resources.Controls.AllPersons, null, statusIdsList, personId);
+            }
+
+            cbInActive.InputAttributes.Add("style", "float: left !Important;width:20px;margin-right:0px;");
+            cbProjected.InputAttributes.Add("style", "float: left !Important;width:20px;margin-right:0px;");
+            cbTerminated.InputAttributes.Add("style", "float: left !Important;width:20px;margin-right:0px;");
+
+        }
+
+        //protected void odsPersons_Selecting(object sender, ObjectDataSourceSelectingEventArgs e)
+        //{
+        //    e.InputParameters["PersonStatusIdsList"] = GetStatusIds();
+        //}
+
+        private string GetStatusIds()
+        {
+            string statusList = ((int)PersonStatusType.Active).ToString();
+            if (this.cbInActive.Checked)
+                statusList += "," + ((int)PersonStatusType.Inactive).ToString();
+            if (this.cbProjected.Checked)
+                statusList += "," + ((int)PersonStatusType.Contingent).ToString();
+            if (this.cbTerminated.Checked)
+                statusList += "," + ((int)PersonStatusType.Terminated).ToString();
+
+            return statusList;
+        }
+
+        protected void status_OnCheckedChanged(object sendeer, EventArgs e)
+        {
+            var currentPerson = DataHelper.CurrentPerson;
+            var personRoles = Roles.GetRolesForUser(currentPerson.Alias);
+            string statusIdsList = GetStatusIds();
+            int? personId = null;
+            if (!personRoles.Any(s => s == "System Administrator"))
+            {
+                personId = currentPerson.Id;
+            }
+            DataHelper.FillTimeEntryPersonList(this.cblPersons, Resources.Controls.AllPersons, null, statusIdsList, personId);
+        }
+    }
+}
