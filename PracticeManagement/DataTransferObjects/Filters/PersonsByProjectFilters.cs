cd65713db842da0853a1a9940c6587189393e7b2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Filters/PersonsByProjectFilters.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTransferObjects.Filters
+{
+    [Serializable]
+    public class PersonsByProjectFilters
+    {
+        public string ClientIds
+        {
+            get;
+            set;
+        }
+
+        public string PayTypeIds
+        {
+            get;
+            set;
+        }
+        public string PersonStatusIds
+        {
+            get;
+            set;
+        }
+        public string ProjectStatusIds
+        {
+            get;
+            set;
+        }
+        public string PracticeIds
+        {
+            get;
+            set;
+        }
+        public bool ExcludeInternalPractices
+        {
+            get;
+            set;
+        }
+    }
+}
+
