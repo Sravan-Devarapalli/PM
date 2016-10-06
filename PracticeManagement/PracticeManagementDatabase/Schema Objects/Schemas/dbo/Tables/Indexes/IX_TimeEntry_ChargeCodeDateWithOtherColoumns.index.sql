07ecbd2513ca1b4c757ff022aead99d7b5d75757
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Indexes/IX_TimeEntry_ChargeCodeDateWithOtherColoumns.index.sql
@@ -0,0 +1,7 @@
+﻿CREATE NONCLUSTERED INDEX IX_TimeEntry_ChargeCodeDateWithOtherColoumns ON [dbo].[TimeEntry] 
+(
+[ChargeCodeDate]
+)
+INCLUDE ([TimeEntryId],[PersonId],[ChargeCodeId]) WITH (SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+
+
