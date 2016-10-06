3e4eaeda4a3f1a49e14cca07f06ea07f226c2155
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ProjectStatus.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// Represents the stutus of the <see cref="Project"/>.
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class ProjectStatus
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets an ID of the status.
+        /// </summary>
+        [DataMember]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Sets or sets a name of the status.
+        /// </summary>
+        [DataMember]
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        #endregion Properties
+
+        #region Computed values
+
+        public ProjectStatusType StatusType
+        {
+            get
+            {
+                return (ProjectStatusType)Id;
+            }
+        }
+
+        #endregion Computed values
+
+        #region Methods
+
+        /// <summary>
+        /// Returns the hash code for the instance.
+        /// </summary>
+        /// <returns>The hash code.</returns>
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        /// <summary>
+        /// Determines whether the instance is equals to the another one.
+        /// </summary>
+        /// <param name="obj">The object to compare with.</param>
+        /// <returns>true if the objects are equal and false otherwise.</returns>
+        public override bool Equals(object obj)
+        {
+            return (obj is ProjectStatus) && ((ProjectStatus)obj).Id == Id;
+        }
+
+        #endregion Methods
+    }
+}
