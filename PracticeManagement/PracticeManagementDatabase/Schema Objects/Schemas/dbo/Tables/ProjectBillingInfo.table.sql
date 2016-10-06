ca90b2732241a19490e1f7e5434054043e49355e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ProjectBillingInfo.table.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[ProjectBillingInfo] (
+    [ProjectId]       INT            NOT NULL,
+    [BillingContact]  NVARCHAR (100) NULL,
+    [BillingPhone]    NVARCHAR (25)  NULL,
+    [BillingEmail]    NVARCHAR (100) NULL,
+    [BillingType]     NVARCHAR (25)  NULL,
+    [BillingAddress1] NVARCHAR (100) NULL,
+    [BillingAddress2] NVARCHAR (100) NULL,
+    [BillingCity]     NVARCHAR (50)  NULL,
+    [BillingState]    NVARCHAR (50)  NULL,
+    [BillingZip]      NVARCHAR (10)  NULL,
+    [PurchaseOrder]   NVARCHAR (25)  NULL
+);
+
+
