3271cc2b5d927f1fb72365037a11199c1bda70df
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_OverheadFixedRate_RateType.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[OverheadFixedRate]
+    ADD CONSTRAINT [FK_OverheadFixedRate_RateType] FOREIGN KEY ([RateType]) REFERENCES [dbo].[OverheadRateType] ([OverheadRateTypeId]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
