bfc63ca871cdea170e6432f75e329d3bb545d66a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SaveRecruitingMetrics.proc.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[SaveRecruitingMetrics]
+(
+	@RecruitingMetricsId	INT,
+	@Name					NVARCHAR(50),
+	@SortOrder				INT
+)
+AS
+BEGIN
+	UPDATE RecruitingMetrics
+	SET Name = @Name,
+		SortOrder = @SortOrder
+	WHERE RecruitingMetricsId = @RecruitingMetricsId
+END
+	
