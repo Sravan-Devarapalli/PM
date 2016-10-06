5b582e77be7574ccc2f390065e264d3293792b8b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/SessionLogData.table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE dbo.SessionLogData
+(
+	SessionID         INT NOT NULL,
+	UserLogin         NVARCHAR(255) NULL,
+	PersonID          INT NULL,
+	LastName          NVARCHAR(100) NULL,
+	FirstName         NVARCHAR(100) NULL,
+	SystemUser        NVARCHAR(255) NOT NULL DEFAULT SYSTEM_USER,
+	Workstation       NVARCHAR(128) NULL DEFAULT HOST_NAME(),
+	ApplicationName   NVARCHAR(128) NULL DEFAULT APP_NAME(),
+	CONSTRAINT PK_SessionLogData PRIMARY KEY CLUSTERED (SessionID)
+)
