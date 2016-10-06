704f719f2687f5febed86967ee2879e5a0700ead
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/DashBoardType.table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[DashBoardType]
+(
+	 [Id] INT            NOT NULL,
+     [Name]            NVARCHAR (100) NOT NULL,
+	 CONSTRAINT PK_DashBoardType_Id PRIMARY KEY CLUSTERED(Id)
+)
+
+
