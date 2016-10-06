f677bb0734764bd06df70cc04d93c236c03f1ad5
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OpportunityPrioritiesListAll.proc.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE dbo.OpportunityPrioritiesListAll
+AS
+BEGIN
+	SET NOCOUNT ON
+	SELECT	Id,
+			Priority,
+			Description,
+			DisplayName
+	FROM dbo.OpportunityPriorities AS OP
+	ORDER BY OP.sortOrder
+END
