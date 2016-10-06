9d0d8f7a8284656e9a1be39dd148ee3e3334c278
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Filters/TimeReports.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTransferObjects.Filters
+{
+    [Serializable]
+    public class TimeReports
+    {
+
+
+        public bool IncludePersonsWithNoTime
+        {
+            get;
+            set;
+        }
+        public string ReportPeriod
+        {
+            get;
+            set;
+        }
+        public string SelectedView
+        {
+            get;
+            set;
+        }
+
+        public DateTime? StartDate
+        {
+            get;
+            set;
+        }
+
+        public DateTime? EndDate
+        {
+            get;
+            set;
+        }
+
+        public string ProjectNumber
+        {
+            get;
+            set;
+        }
+
+        public string Person
+        {
+            get;
+            set;
+        }
+
+    }
+}
+
