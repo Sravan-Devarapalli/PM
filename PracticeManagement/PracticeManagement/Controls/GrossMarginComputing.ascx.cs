e18974d0ceb67e7e139977f2af20ffb1da8f97d3
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/GrossMarginComputing.ascx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PraticeManagement.Controls
+{
+	public partial class GrossMarginComputing : System.Web.UI.UserControl
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
