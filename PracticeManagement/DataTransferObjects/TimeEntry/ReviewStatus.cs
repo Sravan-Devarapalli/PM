1bb0c9a3be22a21777426e4150751d62516bcbf2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/TimeEntry/ReviewStatus.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace DataTransferObjects.TimeEntry
+{
+    /// <summary>
+    /// Time entry review status
+    /// </summary>
+    [DataContract]
+    public enum ReviewStatus
+    {
+        [EnumMember]
+        Undefined = 0,//We are not using this value in PM but to overcome issues we kept this Undefined value.
+
+        [EnumMember]
+        Pending = 1,
+
+        [EnumMember]
+        Approved = 2,
+
+        [EnumMember]
+        Declined = 3
+    }
+}
