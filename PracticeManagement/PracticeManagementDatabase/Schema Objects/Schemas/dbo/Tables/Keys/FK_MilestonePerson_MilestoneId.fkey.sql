89a7d40aa343a0d7a8776bde8dcc364d58eeed8c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_MilestonePerson_MilestoneId.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[MilestonePerson]
+    ADD CONSTRAINT [FK_MilestonePerson_MilestoneId] FOREIGN KEY ([MilestoneId]) REFERENCES [dbo].[Milestone] ([MilestoneId]) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+
