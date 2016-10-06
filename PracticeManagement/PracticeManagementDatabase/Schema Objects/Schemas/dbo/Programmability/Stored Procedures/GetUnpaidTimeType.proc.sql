6114b76f94d906bd4359182ec16e66c2340fdd09
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetUnpaidTimeType.proc.sql
@@ -0,0 +1,13 @@
+﻿-- =============================================
+-- Author:	Sainath C
+-- Create date: 01-06-2012
+-- =============================================
+CREATE PROCEDURE [dbo].[GetUnpaidTimeType]
+AS
+BEGIN
+  SELECT  TT.TimeTypeId,
+		  TT.Name
+	FROM dbo.TimeType AS TT
+    WHERE   TT.TimeTypeId = dbo.GetUnpaidTimeTypeId()
+END
+	
