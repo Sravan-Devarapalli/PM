0276cddb9cf4dd58ef3aa4d4cd0ff782e50bd1df
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ClientMarginColorInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [Serializable]
+    [DataContract]
+    public class ClientMarginColorInfo
+    {
+        [DataMember]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int EndRange
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int StartRange
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public ColorInformation ColorInfo
+        {
+            get;
+            set;
+        }
+    }
+}
