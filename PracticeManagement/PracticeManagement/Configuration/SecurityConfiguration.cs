d43acc8fd6f932e130f70d65ea8eabde5f37a8bb
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Configuration/SecurityConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace PraticeManagement.Configuration
+{
+    public class PersonConfiguration : ConfigurationSection
+    {
+        #region Constants
+
+        private const string CLIENT_ID_PROPERTY = "clientId";
+        private const string PersonSectionName = "practiceManagement/personConfig";
+
+        #endregion
+
+        #region Properties
+
+        public static PersonConfiguration Current
+        {
+            get
+            {
+                return ConfigurationManager.GetSection(PersonSectionName) as PersonConfiguration;
+            }
+        }
+
+        [ConfigurationProperty(CLIENT_ID_PROPERTY)]
+        public int ClientId
+        {
+            get
+            {
+                return (int)this[CLIENT_ID_PROPERTY];
+            }
+            set
+            {
+                this[CLIENT_ID_PROPERTY] = value;
+            }
+        }
+
+        #endregion
+    }
+}
+
