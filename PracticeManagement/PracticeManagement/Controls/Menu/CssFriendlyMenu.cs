6c5d0e6fa85b9c90076f973235491881983b5fee
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/Menu/CssFriendlyMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Web.UI;
+namespace PraticeManagement.Controls.Menu
+{
+    public class CssFriendlyMenu : System.Web.UI.WebControls.Menu
+    {
+        [
+         Category("Appearance"),
+         Description("The CSS class applied to the UpdatePanel rendering")
+         ]
+        public string CssClassPrefix
+        {
+            get
+            {
+                string s = (string)ViewState["CssClassPrefix"];
+                return (s != null) ? s : string.Empty;
+            }
+            set
+            {
+                ViewState["CssClassPrefix"] = value;
+            }
+        }
+    }
+}
+
