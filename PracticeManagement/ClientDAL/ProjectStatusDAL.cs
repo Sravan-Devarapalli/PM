d222c06bab2393c921ed3eefa79146567b7f380b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/ClientDAL/ProjectStatusDAL.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using DataAccess.Other;
+using DataTransferObjects;
+
+namespace DataAccess
+{
+    /// <summary>
+    /// Provides an access to the ProjectStatus table.
+    /// </summary>
+    public static class ProjectStatusDAL
+    {
+        #region Constants
+
+        private const string ProjectStatusListAllProcedure = "dbo.ProjectStatusListAll";
+
+        private const string ProjectStatusIdColumn = "ProjectStatusId";
+        private const string NameColumn = "Name";
+
+        #endregion Constants
+
+        #region Methods
+
+        /// <summary>
+        /// Retrives the list of the project statuses.
+        /// </summary>
+        /// <returns>The list of the <see cref="ProjectStatus"/> objects.</returns>
+        public static List<ProjectStatus> ProjectStatusListAll()
+        {
+            using (SqlConnection connection = new SqlConnection(DataSourceHelper.DataConnection))
+            using (SqlCommand command = new SqlCommand(ProjectStatusListAllProcedure, connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    List<ProjectStatus> result = new List<ProjectStatus>();
+
+                    ReadProjectStatuses(reader, result);
+
+                    return result;
+                }
+            }
+        }
+
+        private static void ReadProjectStatuses(DbDataReader reader, List<ProjectStatus> result)
+        {
+            if (!reader.HasRows) return;
+            int projectStatusIdIndex = reader.GetOrdinal(ProjectStatusIdColumn);
+            int nameIndex = reader.GetOrdinal(NameColumn);
+
+            while (reader.Read())
+            {
+                ProjectStatus status = new ProjectStatus
+                    {
+                        Id = reader.GetInt32(projectStatusIdIndex),
+                        Name = reader.GetString(nameIndex)
+                    };
+
+                result.Add(status);
+            }
+        }
+
+        #endregion Methods
+    }
+}
