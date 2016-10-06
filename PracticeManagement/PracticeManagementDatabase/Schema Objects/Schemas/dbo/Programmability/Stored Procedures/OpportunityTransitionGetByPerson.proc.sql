6713cb3d8acd39932b0bceda598357f6e255a93e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OpportunityTransitionGetByPerson.proc.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE OpportunityTransitionGetByPerson 
+	@PersonId INT 
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	select 
+		ot.OpportunityTransitionId,
+		ot.OpportunityTransitionStatusId,
+		ot.OpportunityTransitionStatusName,
+		ot.OpportunityId,
+		ot.TargetPersonId,
+		ot.OpportunityNumber,
+		ot.OpportunityName,
+		ot.Priority,
+		ot.ClientName,
+		ot.LastUpdated as 'LastUpdate'
+	from v_OpportunityTransition as ot
+	where ot.TargetPersonId = @PersonId
+			AND ot.OpportunityStatusId = 1 --get only active Opportunities according to bug# 2608
+END
+
