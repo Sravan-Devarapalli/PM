b00cddb852fdd69a9f4e0f715e5b9f33c95f7e1e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Reports/UtilizationByProject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Reports
+{
+    [DataContract]
+    [Serializable]
+    public class UtilizationByProject
+    {
+        [DataMember]
+        public Project Project 
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public decimal ProjectedHours
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int Utilization
+        {
+            get;
+            set;
+        }
+    }
+}
+
