d3670ae48f84fcc657837b29227e47792929d33f
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DefaultRecruiterCommissionGetById.proc.sql
@@ -0,0 +1,24 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 9-02-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	Selects the default recruiter commission by the specified ID
+-- =============================================
+CREATE PROCEDURE [dbo].[DefaultRecruiterCommissionGetById]
+(
+	@DefaultRecruiterCommissionHeaderId   INT
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+	DECLARE @FutureDate DATETIME
+	SET @FutureDate = dbo.GetFutureDate()
+
+	SELECT h.DefaultRecruiterCommissionHeaderId, h.PersonId, h.StartDate,
+	       CASE h.EndDate WHEN @FutureDate THEN CAST(NULL AS DATETIME) ELSE h.EndDate END AS EndDate,
+	       dbo.MakeDefaultRecruiterCommissionText(h.defaultrecruitercommissionheaderid) AS TextLine
+	  FROM dbo.DefaultRecruiterCommissionHeader as h
+	 WHERE h.DefaultRecruiterCommissionHeaderId = @DefaultRecruiterCommissionHeaderId
+
+END
