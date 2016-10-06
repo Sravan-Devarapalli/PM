f6b4d9e7d7c6094cd8bed00fc2963df67bbf8dce
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Skills/PersonQualification.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Skills
+{
+    [DataContract]
+    [Serializable]
+    public class PersonQualification
+    {
+        [DataMember]
+        public Person Person
+        {
+            set;
+            get;
+        }
+
+        [DataMember]
+        public QualificationType QualificationType
+        {
+            set;
+            get;
+        }
+
+        [DataMember]
+        public string Info
+        {
+            set;
+            get;
+        }
+
+        [DataMember]
+        public int? DisplayOrder
+        {
+            set;
+            get;
+        }
+
+        [DataMember]
+        public DateTime ModificationDate
+        {
+            set;
+            get;
+        }
+    }
+}
