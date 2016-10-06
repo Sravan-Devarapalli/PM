fffe2f9ee6462d51057d27d5d485524b31446a1e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Indexes/IX_Pay_Index2.index.sql
@@ -0,0 +1,11 @@
+﻿CREATE NONCLUSTERED INDEX [IX_Pay_Index2] ON [dbo].[Pay] 
+(
+	[Person] ASC,
+	[EndDate] ASC,
+	[StartDate] ASC,
+	[Timescale] ASC,
+	[BonusHoursToCollect] ASC,
+	[BonusAmount] ASC,
+	[VacationDays] ASC
+)
+INCLUDE ( [Amount]) WITH (SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
