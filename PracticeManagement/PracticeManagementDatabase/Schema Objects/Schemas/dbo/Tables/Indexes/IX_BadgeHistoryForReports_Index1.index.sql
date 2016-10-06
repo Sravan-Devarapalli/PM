a348075868c1fb0dc3a96aaf657c699956ae3907
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Indexes/IX_BadgeHistoryForReports_Index1.index.sql
@@ -0,0 +1,6 @@
+﻿CREATE NONCLUSTERED INDEX [IX_BadgeHistoryForReports_Index1] ON [dbo].[BadgeHistoryForReports]
+(
+	PersonId ASC
+)INCLUDE ( [BadgeStartDate]) WITH (SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+
+
