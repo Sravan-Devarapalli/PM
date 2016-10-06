fe96d6eeb947a416836268476529e62b03332998
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Milestone.table.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Milestone] (
+    [ProjectId]             INT             NOT NULL,
+    [MilestoneId]           INT             IDENTITY (1, 1) NOT NULL,
+    [Description]           NVARCHAR (255)  NOT NULL,
+    [Amount]                DECIMAL (18, 2) NULL,
+    [StartDate]             DATETIME        NOT NULL,
+    [ProjectedDeliveryDate] DATETIME        NOT NULL,
+    [IsHourlyAmount]        BIT             NOT NULL,
+    [IsChargeable]          BIT             NOT NULL,
+    [ConsultantsCanAdjust]  BIT             NOT NULL,
+	[IsDefault]				BIT				NOT NULL DEFAULT(0)
+);
+
+
