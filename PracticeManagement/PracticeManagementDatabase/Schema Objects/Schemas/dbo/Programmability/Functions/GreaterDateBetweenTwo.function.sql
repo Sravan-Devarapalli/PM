f95a3567dac3c85d47f8bea66207877ffa328c5d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GreaterDateBetweenTwo.function.sql
@@ -0,0 +1,17 @@
+﻿CREATE FUNCTION [dbo].[GreaterDateBetweenTwo]
+(
+	@Date1	DATETIME,
+	@Date2	DATETIME
+)
+RETURNS DATETIME
+AS
+BEGIN
+	
+    IF @Date1 >= ISNULL(@Date2,@Date1)
+        RETURN @Date1
+    ELSE
+        RETURN @Date2
+
+    RETURN NULL
+
+END
