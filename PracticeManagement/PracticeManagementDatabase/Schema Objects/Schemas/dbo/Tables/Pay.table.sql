ec2130a8ad8e01b162986bbc63c8f2d5fce26315
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Pay.table.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[Pay] (
+    [Person]              INT             NOT NULL,
+    [StartDate]           SMALLDATETIME   NOT NULL,
+    [EndDate]             SMALLDATETIME   NOT NULL,
+    [Amount]              DECIMAL (18, 2) NOT NULL,
+    [Timescale]           INT             NOT NULL,
+    [TimesPaidPerMonth]   INT             NULL,
+    [Terms]               INT             NULL,
+    [VacationDays]        INT             NULL,
+    [BonusAmount]         DECIMAL (18, 2) NOT NULL,
+    [BonusHoursToCollect] INT             NULL,
+    [DefaultHoursPerDay]  DECIMAL (18, 2) NOT NULL,
+	[SeniorityId]		  INT			  NULL,
+	[TitleId]			  INT             NULL,
+	[PracticeId]		  INT			  NULL,
+	[IsActivePay]		  BIT			  NULL,
+	[SLTApproval]		  BIT			  NOT NULL CONSTRAINT DF_Pay_SLTApproval DEFAULT(0),
+	[SLTPTOApproval]	  BIT			  NOT NULL CONSTRAINT DF_Pay_SLTPTOApproval DEFAULT(0),
+	[DivisionId]		  INT			  NULL,
+	[VendorId]			  INT			  NULL
+);
+
+
