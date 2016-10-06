030d1dbf9f7820048c4a24fd8b2695819db0801b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/BadgeHistory.table.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[BadgeHistory]
+(
+	[Id]				INT IDENTITY(1,1) NOT NULL,
+	[PersonId]			INT         NOT NULL,
+	BadgeStartDate		DATETIME	NULL,
+	BadgeEndDate		DATETIME	NULL,
+	ProjectPlannedEndDate		DATETIME	NULL,
+	BreakStartDate		DATETIME	NULL,
+	BreakEndDate		DATETIME	NULL,
+	BadgeStartDateSource	NVARCHAR(30) NULL,
+	ProjectPlannedEndDateSource	NVARCHAR(30) NULL,
+	BadgeEndDateSource NVARCHAR(30) NULL,
+	ModifiedDate		DATETIME	NOT NULL,
+	ModifiedBy			INT			NULL,
+	DeactivatedDate		DATETIME	NULL,
+	OrganicBreakStartDate	DATETIME	NULL,
+	OrganicBreakEndDate		DATETIME	NULL,
+	ExcludeInReports	BIT			NULL
+	CONSTRAINT PK_BadgeHistory PRIMARY KEY (Id)
+)
+
