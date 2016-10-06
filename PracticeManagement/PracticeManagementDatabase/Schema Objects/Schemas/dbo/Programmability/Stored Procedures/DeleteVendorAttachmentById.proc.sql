164bf094250f920201a57bc8edae110310401019
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DeleteVendorAttachmentById.proc.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[DeleteVendorAttachmentById]
+(
+	@AttachmentId	INT,
+	@VendorId		INT,
+	@UserLogin      NVARCHAR(255)
+)
+AS
+BEGIN
+
+    IF EXISTS(SELECT 1 FROM VendorAttachment WHERE VendorId = @VendorId)
+    BEGIN
+    EXEC SessionLogPrepare @UserLogin = @UserLogin
+    
+		DELETE
+		FROM [dbo].VendorAttachment
+		WHERE VendorId = @VendorId
+			AND (Id = @AttachmentId OR @AttachmentId IS NULL)
+		
+	EXEC dbo.SessionLogUnprepare
+	END
+
+END
