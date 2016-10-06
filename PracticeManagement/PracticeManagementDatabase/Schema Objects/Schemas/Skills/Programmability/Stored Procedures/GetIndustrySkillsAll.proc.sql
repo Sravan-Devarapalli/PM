d865f09f5f90f690764f13995da67bf4a4562bee
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Programmability/Stored Procedures/GetIndustrySkillsAll.proc.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [Skills].[GetIndustrySkillsAll]
+AS
+BEGIN
+	SELECT I.IndustryId 'IndustryId' ,
+			I.Description 'IndustryName',
+			I.DisplayOrder
+	FROM Skills.Industry I
+	WHERE I.IsActive = 1 AND I.IsDeleted = 0
+	ORDER BY I.Description
+END
+
