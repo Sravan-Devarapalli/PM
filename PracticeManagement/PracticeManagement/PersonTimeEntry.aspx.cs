12ca8cf82ae2b2e906ab1ad50628fc3b37cc4cbe
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/PersonTimeEntry.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Xml.Linq;
+using System.Globalization;
+
+namespace PraticeManagement
+{
+	public partial class PersonTimeEntry : System.Web.UI.Page
+	{
+		private const string WeekLabelFormat = "{0} {1}–{2}, {3}";
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			if (!IsPostBack)
+			{
+				calSelection.SelectedDate = DateTime.Today;
+
+				// TODO: Remove the dummy code
+				gvWeeklyView.DataSource = new string[] { string.Empty, string.Empty, string.Empty, string.Empty };
+				gvWeeklyView.DataBind();
+
+				gvTimeOff.DataSource = new string[] { string.Empty };
+				gvTimeOff.DataBind();
+			}
+		}
+
+		protected void btnToday_Click(object sender, EventArgs e)
+		{
+			calSelection.SelectedDate = DateTime.Today;
+		}
+
+		protected void Page_PreRender(object sender, EventArgs e)
+		{
+			DateTime today = calSelection.SelectedDate;
+			CultureInfo curentCulture = CultureInfo.CurrentUICulture;
+			lblWeek.Text =
+				string.Format(WeekLabelFormat,
+				curentCulture.DateTimeFormat.MonthNames[today.Month],
+				today.AddDays(-(double)today.DayOfWeek).Day,
+				today.AddDays(7.0 - (double)today.DayOfWeek).Day,
+				today.Year);
+		}
+	}
+}
+
