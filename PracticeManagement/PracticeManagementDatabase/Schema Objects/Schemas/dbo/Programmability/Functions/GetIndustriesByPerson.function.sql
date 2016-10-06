645ea89123dc7336d53ddc997f6875ada454b6e7
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetIndustriesByPerson.function.sql
@@ -0,0 +1,17 @@
+﻿CREATE FUNCTION [dbo].[GetIndustriesByPerson]
+(
+	@PersonID	INT
+)
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN
+  
+	DECLARE @Temp NVARCHAR(MAX) = ''
+  
+	SELECT @Temp = @Temp + I.Description+ ', '
+	FROM Skills.PersonIndustry P
+	JOIN Skills.Industry I ON I.IndustryId = P.IndustryId
+	WHERE P.PersonId = @PersonId
+
+	RETURN @Temp
+END
