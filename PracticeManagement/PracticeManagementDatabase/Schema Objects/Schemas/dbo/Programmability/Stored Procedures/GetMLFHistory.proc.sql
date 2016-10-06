07beaee97569f9644bab4a3b965f52694a3a1278
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetMLFHistory.proc.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[GetMLFHistory]
+AS
+BEGIN
+	SET NOCOUNT ON
+	SELECT 
+	mlfh.StartDate,
+	mlfh.EndDate,
+	mlfh.[W2-Hourly] as 'W2Salary_Rate',
+	mlfh.[W2-Salary] as 'W2Hourly_Rate',
+	mlfh.[1099] as '_1099_Hourly_Rate'
+	FROM [dbo].[MinimumLoadFactorHistoryForUI] as mlfh
+END
