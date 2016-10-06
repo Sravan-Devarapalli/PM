2ca559c6cd7ec37f716c8a9636a2cc8187821e05
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/PracticeStatusHistory.table.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE dbo.PracticeStatusHistory
+(
+	PracticeId		INT NOT NULL,
+	IsActive		BIT NOT NULL,
+	StartDate		DATETIME NOT NULL,
+	EndDate			DATETIME NULL,
+	CONSTRAINT PK_PracticeStatusHistory PRIMARY KEY CLUSTERED
+	(
+		PracticeId ASC,	
+		IsActive   ASC,	
+		StartDate  ASC
+	),
+	CONSTRAINT FK_PracticeStatusHistory_PracticeId FOREIGN KEY (PracticeId)
+	REFERENCES dbo.Practice(PracticeId)
+)
