4854c04fd32282df3e9d9c948387d4ce82f70084
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ContextObjects/ConsultantMilestonesContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DataTransferObjects.ContextObjects
+{
+    public class ConsultantMilestonesContext
+    {
+        public int PersonId { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public bool IncludeActiveProjects { get; set; }
+
+        public bool IncludeProjectedProjects { get; set; }
+
+        public bool IncludeInactiveProjects { get; set; }
+
+        public bool IncludeCompletedProjects { get; set; }
+
+        public bool IncludeInternalProjects { get; set; }
+
+        public bool IncludeExperimentalProjects { get; set; }
+
+        public bool IncludeProposedProjects { get; set; }
+
+        public bool IncludeDefaultMileStone { get; set; }
+    }
+}
