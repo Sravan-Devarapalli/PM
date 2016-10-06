52e686565197cb5af242bea66f53e30a69bdcdeb
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/TerminationReason.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// Data Transfer Object for a Termination Reason
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class TerminationReason
+    {
+        [DataMember]
+        public int? Id { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public bool IsW2SalaryRule { get; set; }
+
+        [DataMember]
+        public bool IsW2HourlyRule { get; set; }
+
+        [DataMember]
+        public bool Is1099Rule { get; set; }
+
+        [DataMember]
+        public bool IsContigent { get; set; }
+
+        [DataMember]
+        public bool IsVisible { get; set; }
+    }
+}
