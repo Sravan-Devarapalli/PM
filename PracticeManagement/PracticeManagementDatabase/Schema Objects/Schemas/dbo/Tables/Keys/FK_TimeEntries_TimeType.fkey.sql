ccf2e551d9dfcfdd28f435db30328f33fd68a86c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_TimeEntries_TimeType.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[TimeEntries]
+    ADD CONSTRAINT [FK_TimeEntries_TimeType] FOREIGN KEY ([TimeTypeId]) REFERENCES [dbo].[TimeType] ([TimeTypeId]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
