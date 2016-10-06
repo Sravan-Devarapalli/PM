c6df0f8c636868057db1a0b8ccdbea4879a2c3cb
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Filters/PTOReportFilters.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTransferObjects.Filters
+{
+    [Serializable]
+    public class PTOReportFilters
+    {
+        public string Period
+        {
+            get;
+            set;
+        }
+
+        public DateTime ReportStartDate
+        {
+            get;
+            set;
+        }
+
+        public DateTime ReportEndDate
+        {
+            get;
+            set;
+        }
+
+        public int Granularity
+        {
+            get;
+            set;
+        }
+
+        public bool IncludeActivePeople
+        {
+            get;
+            set;
+        }
+
+        public bool IncludeProjectedPeople
+        {
+            get;
+            set;
+        }
+
+        public bool IsW2Hourly
+        {
+            get;
+            set;
+        }
+
+        public bool IsW2Salary
+        {
+            get;
+            set;
+        }
+
+        public string TitleIds
+        {
+            get;
+            set;
+        }
+
+        public string PracticeAreaIds
+        {
+            get;
+            set;
+        }
+
+        public string DivisionIds
+        {
+            get;
+            set;
+        }
+
+        public int SortId
+        {
+            get;
+            set;
+        }
+
+        public string SortDirection
+        {
+            get;
+            set;
+        }
+    }
+}
+
