4c17fe8922635bbf76e16c09b8b381e0a1a9e7dd
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Financials/RangeType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Financials
+{
+    [DataContract]
+    [Serializable]
+    public class RangeType
+    {
+        [DataMember]
+        public DateTime StartDate { get; set; }
+
+        [DataMember]
+        public DateTime EndDate { get; set; }
+
+        [DataMember]
+        public string Range
+        {
+            get;
+            set;
+        }
+    }
+}
