898710cc9b8598aa0d882b68f249d8e1b934ea29
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/PersonProjectGroupPermissions.function.sql
@@ -0,0 +1,33 @@
+﻿CREATE FUNCTION dbo.PersonProjectGroupPermissions
+(
+	@PersonId	INT
+)
+RETURNS 
+@Result table
+(
+	TargetId int
+)
+AS
+BEGIN
+	-- Insert permissions from permissions table
+	insert into @result
+		select p.TargetId 
+		from v_Permissions as p 
+		where p.PermissionTypeId = 2 -- Project Group 
+				and p.PersonId = @PersonId
+
+	-- Select top record from result set
+	delete 
+	from @result
+	where TargetId IS NULL
+
+	-- If it's null, then person has all permissions
+	IF (@@RowCount > 0)
+		-- Insert all targets
+		insert into @result
+			select pg.GroupId
+			from dbo.ProjectGroup as pg
+
+	RETURN
+END
+
