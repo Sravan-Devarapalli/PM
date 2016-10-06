9fde0a14919c177735377da8db2e863556a762a2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonsGetBySeniorityAndStatus.proc.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[PersonsGetBySeniorityAndStatus]
+	@SeniorityId int, 
+	@PersonStatusId    int = NULL
+AS
+	SELECT PersonId,
+		   FirstName,
+		   LastName,
+		   IsDefaultManager,
+		   HireDate
+	FROM dbo.Person P
+	JOIN dbo.Seniority S ON P.SeniorityId = S.SeniorityId
+	WHERE S.SeniorityValue = @SeniorityId
+			AND (PersonStatusId = @PersonStatusId OR @PersonStatusId IS NULL)
+
