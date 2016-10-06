b8539b905f8c50cd4a2d1089bfdede02ed63ebad
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ProjectDivision.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Collections;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class ProjectDivision 
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public bool IsExternal { get; set; }
+
+        public static int Compare(ProjectDivision obj1, ProjectDivision obj2)
+        {
+            if (obj1 == null && obj2 != null)
+            {
+                return -1;
+            }
+            else if (obj1 != null && obj2 == null)
+            {
+                return 1;
+            }
+            else if (obj1 == null && obj2 == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return string.Compare(obj1.Name, obj2.Name);
+            }
+        }
+    }
+}
+
