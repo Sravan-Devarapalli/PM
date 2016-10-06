8e6c2bd9fe1bccc8f5e6467d726304caba1f4a61
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetPasswordHistoryByUserName.proc.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[GetPasswordHistoryByUserName]
+(
+	@UserName   NVARCHAR(256)
+)
+AS
+BEGIN
+SET NOCOUNT ON;
+	DECLARE @COUNT INT 
+	SELECT @COUNT = s.Value FROM Settings AS s
+	WHERE s.SettingsKey='OldPasswordCheckCount' AND s.TypeId = 4
+
+	IF(@COUNT > 0)
+	BEGIN
+	SET @COUNT = @COUNT -1
+	END
+	
+	SELECT TOP(@COUNT) u.Id, u.Password ,u.PasswordSalt
+	FROM UserPassWordHistory AS u
+	INNER JOIN aspnet_Membership AS a 
+	ON a.Email = @UserName and a.UserId =u.UserId
+	order by u.Id desc
+END
+GO
