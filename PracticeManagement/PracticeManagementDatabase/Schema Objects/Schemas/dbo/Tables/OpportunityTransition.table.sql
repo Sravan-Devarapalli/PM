8cce24846d1724cc00272d71cd866af310e5ace1
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/OpportunityTransition.table.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[OpportunityTransition] (
+    [OpportunityTransitionId]       INT             IDENTITY (1, 1) NOT NULL,
+    [OpportunityId]                 INT             NOT NULL,
+    [OpportunityTransitionStatusId] INT             NOT NULL,
+    [TransitionDate]                DATETIME        NOT NULL,
+    [PersonId]                      INT             NOT NULL,
+    [NoteText]                      NVARCHAR (2000) NULL,
+    [OpportunityTransitionTypeId]   INT             NOT NULL,
+    [TargetPersonId]                INT             NULL,
+	PreviousChangedId				INT				NULL,
+	NextChangedId					INT				NULL
+);
+
+
