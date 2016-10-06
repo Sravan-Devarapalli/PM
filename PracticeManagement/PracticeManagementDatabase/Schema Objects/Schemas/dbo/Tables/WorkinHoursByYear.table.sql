6548c100dbd6cc62d1ab91b5aae99fd2228d3967
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/WorkinHoursByYear.table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[WorkinHoursByYear]
+(
+	[Year]					INT	NOT NULL,
+	[YearStartDate]			DATE NOT NULL,
+	[YearEndDate]			DATE NOT NULL,
+	[DaysInYear]			INT NOT NULL,
+	[HoursInYear]			INT NOT NULL
+	CONSTRAINT PK_WorkinHoursByYear PRIMARY KEY ([Year] ASC,[YearStartDate] ASC, [YearEndDate] ASC),
+)
+
