8a41c798d13d813d59bb61b7a4fe2418581e1daf
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/PersonInfo.ascx.cs
@@ -0,0 +1,45 @@
+﻿
+using System.ComponentModel;
+using DataTransferObjects;
+
+namespace PraticeManagement.Controls
+{
+	public partial class PersonInfo : System.Web.UI.UserControl
+	{
+		private Person personValue;
+
+        public string FirstName
+        {
+            get
+            {
+                return lblPersonFirstName.Text;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lblPersonLastName.Text;
+            }
+        }
+
+		[Localizable(false)]
+		[Browsable(false)]
+		[Bindable(true)]
+		public Person Person
+		{
+			get
+			{
+				return personValue;
+			}
+			set
+			{
+				personValue = value;
+				lblPersonFirstName.Text = personValue.FirstName;
+				lblPersonLastName.Text = personValue.LastName;
+			}
+		}
+	}
+}
+
