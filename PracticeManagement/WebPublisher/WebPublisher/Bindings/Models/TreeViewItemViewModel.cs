655ab87e2c3ce950e9a879fea05825548321a489
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/WebPublisher/WebPublisher/Bindings/Models/TreeViewItemViewModel.cs
@@ -0,0 +1,262 @@
+﻿using System.Collections.ObjectModel;
+
+namespace WebPublisher.Bindings.Models
+{    
+
+    /// <summary>
+    /// Base class for all ViewModel classes displayed by TreeViewItems.  
+    /// This acts as an adapter between a raw data object and a TreeViewItem.
+    /// </summary>
+    public class TreeViewItemViewModel : WebPublisher.Bindings.NotifyPropertyChanged
+    {
+        #region Data
+
+        static readonly TreeViewItemViewModel DummyChild = new TreeViewItemViewModel();
+
+        readonly ObservableCollection<TreeViewItemViewModel> _children;
+        readonly TreeViewItemViewModel _parent;
+
+        bool _isExpanded;
+        bool _isSelected;
+        bool? _isChecked = false;
+        protected bool _lazyLoadCildren;
+        #endregion // Data
+
+        #region Constructors
+
+        protected TreeViewItemViewModel(TreeViewItemViewModel parent, bool lazyLoadChildren)
+        {
+            _parent = parent;
+            _children = new ObservableCollection<TreeViewItemViewModel>();
+            _lazyLoadCildren = lazyLoadChildren;
+            if (_lazyLoadCildren)
+                _children.Add(DummyChild);
+        }
+
+        // This is used to create the DummyChild instance.
+        private TreeViewItemViewModel()
+        {
+        }
+
+        #endregion // Constructors
+
+        #region Presentation Members
+        #region Node members
+        public string FullPath
+        {
+            get
+            {
+                string fullPath = string.Empty;
+                if (_parent != null)
+                {
+                    fullPath = _parent.FullPath;
+                }
+                
+                fullPath = System.IO.Path.Combine(fullPath, Name);
+               
+                return fullPath;
+            }
+        }
+        private string _name;
+        public string Name 
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (_name == value)
+                {
+                    return;
+                }
+
+                _name = value;
+                Notify("Name");
+            }
+        }
+        #endregion
+
+        #region Children
+        protected virtual string GetImageUri()
+        {
+            return string.Empty;
+        }
+        /// <summary>
+        /// Returns the logical child items of this object.
+        /// </summary>
+        public ObservableCollection<TreeViewItemViewModel> Children
+        {
+            get { return _children; }
+        }
+
+        public System.Windows.Media.ImageSource Image
+        {
+            get
+            {
+                return WebPublisher.Helpers.ResourceHelper.GetBitmap(GetImageUri());
+            }
+        }
+        #endregion // Children
+
+        #region HasLoadedChildren
+
+        /// <summary>
+        /// Returns true if this object's Children have not yet been populated.
+        /// </summary>
+        public bool HasDummyChild
+        {
+            get { return this.Children.Count == 1 && this.Children[0] == DummyChild; }
+        }
+
+        #endregion // HasLoadedChildren
+
+        #region IsExpanded
+
+        /// <summary>
+        /// Gets/sets whether the TreeViewItem 
+        /// associated with this object is expanded.
+        /// </summary>
+        public bool IsExpanded
+        {
+            get { return _isExpanded; }
+            set
+            {
+                if (value != _isExpanded)
+                {
+                    _isExpanded = value;
+                    this.Notify("IsExpanded");
+                }
+
+                // Expand all the way up to the root.
+                if (_isExpanded && _parent != null)
+                    _parent.IsExpanded = true;
+
+                // Lazy load the child items, if necessary.
+                if (this.HasDummyChild)
+                {
+                    this.Children.Remove(DummyChild);
+                    this.LoadChildren();
+                }
+            }
+        }
+
+        #endregion // IsExpanded
+
+        #region IsSelected
+
+        /// <summary>
+        /// Gets/sets whether the TreeViewItem 
+        /// associated with this object is selected.
+        /// </summary>
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set
+            {
+                if (value != _isSelected)
+                {
+                    _isSelected = value;
+                    this.Notify("IsSelected");
+                }
+            }
+        }
+
+        #endregion // IsSelected
+
+        #region IsChecked
+
+        /// <summary>
+        /// Gets/sets the state of the associated UI toggle (ex. CheckBox).
+        /// The return value is calculated based on the check state of all
+        /// child FooViewModels.  Setting this property to true or false
+        /// will set all children to the same check state, and setting it 
+        /// to any value will cause the parent to verify its check state.
+        /// </summary>
+        public bool? IsChecked
+        {
+            get { return _isChecked; }
+            set { this.SetIsChecked(value, true, true); }
+        }
+
+        void SetIsChecked(bool? value, bool updateChildren, bool updateParent)
+        {
+            if (value == _isChecked)
+                return;
+
+            _isChecked = value;
+
+            if (updateChildren && _isChecked.HasValue && !HasDummyChild)
+            {
+                foreach (var c in this.Children)
+                {
+                    c.SetIsChecked(_isChecked, true, false);
+                }
+            }
+
+            if (updateParent && _parent != null)
+                _parent.VerifyCheckState();
+
+            this.Notify("IsChecked");
+        }
+
+        void VerifyCheckState()
+        {
+            bool? state = null;
+            for (int i = 0; i < this.Children.Count; ++i)
+            {
+                bool? current = this.Children[i].IsChecked;
+                if (i == 0)
+                {
+                    state = current;
+                }
+                else if (state != current)
+                {
+                    state = null;
+                    break;
+                }
+            }
+            this.SetIsChecked(state, false, true);
+        }
+
+
+        #endregion // IsChecked
+
+        public void Reload()
+        {
+            if (_isExpanded || !_lazyLoadCildren)
+            {
+                this.LoadChildren();
+            }
+        }
+
+        protected virtual void Sort()
+        {
+
+        }
+       
+        #region LoadChildren
+
+        /// <summary>
+        /// Invoked when the child items need to be loaded on demand.
+        /// Subclasses can override this to populate the Children collection.
+        /// </summary>
+        protected virtual void LoadChildren()
+        {
+        }
+
+        #endregion // LoadChildren
+
+        #region Parent
+
+        public TreeViewItemViewModel Parent
+        {
+            get { return _parent; }
+        }
+
+        #endregion // Parent
+
+        #endregion // Presentation Members
+    }
+}
+
