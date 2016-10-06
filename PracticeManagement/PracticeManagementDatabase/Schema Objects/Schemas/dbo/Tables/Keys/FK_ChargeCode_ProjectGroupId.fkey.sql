c8907e495b1b00b4e10531d8e4acafdecf3e0040
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_ChargeCode_ProjectGroupId.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[ChargeCode]
+	ADD CONSTRAINT [FK_ChargeCode_ProjectGroupId] 
+	FOREIGN KEY (ProjectGroupId)
+	REFERENCES dbo.ProjectGroup (GroupId)  ON DELETE NO ACTION ON UPDATE NO ACTION;
