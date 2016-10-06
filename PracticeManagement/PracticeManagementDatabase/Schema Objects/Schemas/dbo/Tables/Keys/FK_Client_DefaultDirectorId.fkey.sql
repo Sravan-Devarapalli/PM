fcb6a8dbbdd686562148efba045870b87ca20b6b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_Client_DefaultDirectorId.fkey.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE [dbo].[Client]
+    ADD CONSTRAINT [FK_Client_DefaultDirectorId] FOREIGN KEY ([DefaultDirectorID]) 
+	REFERENCES [dbo].[Person] ([PersonId]) ON DELETE NO ACTION ON UPDATE NO ACTION;
