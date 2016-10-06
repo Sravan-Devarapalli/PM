029e7a865b45e9ae950b0ff6074cebf8873c2a53
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/OverheadFixedRate.table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[OverheadFixedRate] (
+    [OverheadFixedRateId] INT             IDENTITY (1, 1) NOT NULL,
+    [Description]         NVARCHAR (255)  NOT NULL,
+    [Rate]                DECIMAL (18, 5) NOT NULL,
+    [StartDate]           DATETIME        NOT NULL,
+    [EndDate]             DATETIME        NULL,
+    [RateType]            INT             NOT NULL,
+    [Inactive]            BIT             NOT NULL,
+    [IsCogs]              BIT             NOT NULL,
+	[IsMinimumLoadFactor] BIT NOT NULL	CONSTRAINT DF_OverheadFixedRate_IsMinimumLoadFactor DEFAULT 0
+);
+
+
