23335a4797fa60dd2abbfd890dad626823bdaa05
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/ContactInfoType.table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [Skills].[ContactInfoType](
+	[ContactInfoTypeId] [int] NOT NULL IDENTITY(1,1),
+	[Description] [nvarchar](max) NULL,
+	[DisplayOrder] [tinyint] NULL,
+	[IsActive]		[bit] NOT NULL CONSTRAINT DF_ContactInfoType_IsActive DEFAULT(1),
+	[IsDeleted]		[bit] NOT NULL CONSTRAINT DF_ContactInfoType_IsDeleted DEFAULT(0),
+ CONSTRAINT [PK_ContactInfoType] PRIMARY KEY CLUSTERED 
+(
+	[ContactInfoTypeId] ASC
+)
+)
