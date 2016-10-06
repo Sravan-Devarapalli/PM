297a1cbc0d40c82b4b020174dcd01689bd0f3256
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/EmployeeManagementSystemDatabase/Schema Objects/Schemas/dbo/Tables/PersonIndustry.table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[PersonIndustry](
+	[IndustryId] [int] NOT NULL,
+	[PersonId] [bigint] NOT NULL,
+	[YearsExperience] [int] NOT NULL,
+	[DisplayOrder] [tinyint] NULL,
+	[ModificationDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_PersonIndustry] PRIMARY KEY CLUSTERED 
+(
+	[IndustryId] ASC,
+	[PersonId] ASC
+)
+)
