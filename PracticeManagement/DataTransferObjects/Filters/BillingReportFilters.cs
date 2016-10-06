79f4351c87f60c5864b6a79a603a1260fe67faea
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Filters/BillingReportFilters.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTransferObjects.Filters
+{
+    [Serializable]
+    public class BillingReportFilters
+    {
+        public string AccountIds
+        {
+            get;
+            set;
+        }
+        public DateTime? StartDate
+        {
+            get;
+            set;
+        }
+        public DateTime? EndDate
+        {
+            get;
+            set;
+        }
+        public string PracticeIds
+        {
+            get;
+            set;
+        }
+        public string BusinessUnitIds
+        {
+            get;
+            set;
+        }
+        public string ExecutiveInchargeIds
+        {
+            get;
+            set;
+        }
+        public string UnitOfMeassure
+        {
+            get;
+            set;
+        }
+
+        public string DirectorFilteredIds
+        {
+            get;
+            set;
+        }
+
+        public string AccountFilteredIds
+        {
+            get;
+            set;
+        }
+
+        public string PracticeFilteredIds
+        {
+            get;
+            set;
+        }
+
+        public string SalespersonFilteredIds
+        {
+            get;
+            set;
+        }
+
+        public string SeniorManagerFilteredIds
+        {
+            get;
+            set;
+        }
+
+        public string ProjectManagerFilteredIds
+        {
+            get;
+            set;
+        }
+
+        public bool isFiltersApplied
+        {
+            get;
+            set;
+        }
+    }
+}
+
