a61cb93cf7d69bb73fe11c07fe7d41872dd4eb4b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/OverHeadHistory.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class OverHeadHistory
+    {
+        #region Properties
+
+        [DataMember]
+        public Decimal W2Salary_Rate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Decimal W2Hourly_Rate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Decimal _1099_Hourly_Rate
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a start date when the overhead acts.
+        /// </summary>
+        [DataMember]
+        public DateTime StartDate
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets an end date when the overhaed acts.
+        /// </summary>
+        [DataMember]
+        public DateTime? EndDate
+        {
+            get;
+            set;
+        }
+
+        #endregion Properties
+    }
+}
