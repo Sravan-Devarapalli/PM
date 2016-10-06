577e71005c1808260f95d2c4b2cbe0051e428482
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/OverheadRateTypes.cs
@@ -0,0 +1,13 @@
+﻿namespace DataTransferObjects
+{
+    /// <summary>
+    /// Enlists the possible types of the person overhead
+    /// </summary>
+    public enum OverheadRateTypes
+    {
+        HourlyAddOn = 1,
+        BillRateMultiplier = 2,
+        MonthlyCost = 3,
+        PayRateMultiplier = 4
+    }
+}
