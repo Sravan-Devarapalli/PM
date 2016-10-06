8c36e85ab48fce276714fc40fba31b1e8b12418c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetAllTitles.proc.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [dbo].[GetAllTitles]
+AS
+BEGIN
+
+	SELECT T.TitleId
+	 , T.Title
+	 , TT.TitleTypeId
+	 , TT.TitleType
+	 , TT.SortOrder AS TitleTypeSortOrder
+	 , T.SortOrder
+	 , T.PTOAccrual
+	 , T.MinimumSalary
+	 , T.MaximumSalary
+	 , CASE WHEN EXISTS (SELECT 1 FROM Person AS p WHERE p.TitleId = T.TitleId ) OR
+				 EXISTS(SELECT 1 FROM Pay AS pa	WHERE pa.TitleId = T.TitleId) 
+			THEN CAST(1 AS BIT)				   	 
+			ELSE CAST(0 AS BIT)
+	   END AS [TitleInUse]
+	 , T.ParentId
+	 , T.PositionId
+	FROM dbo.Title T
+	INNER JOIN dbo.TitleType TT ON T.TitleTypeId = TT.TitleTypeId
+	WHERE T.Active = 1 
+	ORDER BY TT.SortOrder,T.SortOrder
+END
+
