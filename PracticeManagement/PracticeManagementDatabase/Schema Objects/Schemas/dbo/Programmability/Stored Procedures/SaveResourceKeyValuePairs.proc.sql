1f4b9a0d97571fbcd04b72282410023c70232934
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SaveResourceKeyValuePairs.proc.sql
@@ -0,0 +1,33 @@
+﻿CREATE PROCEDURE dbo.SaveResourceKeyValuePairs
+(
+	@TypeId   INT,
+	@ResourcesKey NVARCHAR(255),
+	@Value      NVARCHAR(MAX)
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+	IF NOT EXISTS(SELECT TOP 1 1 
+				  FROM [dbo].[StringResources]
+				  WHERE @ResourcesKey = ResourcesKey 
+				  AND TypeId = @TypeId 
+				 )
+	BEGIN
+		INSERT INTO [dbo].[StringResources]
+					([TypeId]
+					,[ResourcesKey]
+					,[Value])
+		 VALUES
+					(@TypeId
+					,@ResourcesKey
+					,@Value)
+           
+	END
+	ELSE 
+	BEGIN
+		UPDATE [dbo].[StringResources]
+		SET  Value = @Value
+		WHERE ResourcesKey = @ResourcesKey  AND TypeId = @TypeId
+	END
+END
+           
