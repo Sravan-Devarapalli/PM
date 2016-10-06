9ee35e6735710b6cce10d1aaf851233e086dda58
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/SeniorityCategory.table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[SeniorityCategory] (
+    [SeniorityCategoryId]	INT   NOT NULL,
+    [Name]			NVARCHAR (30) NOT NULL,
+	CONSTRAINT PK_SeniorityCategory_SeniorityCategoryId PRIMARY KEY (SeniorityCategoryId)
+);
+
+
