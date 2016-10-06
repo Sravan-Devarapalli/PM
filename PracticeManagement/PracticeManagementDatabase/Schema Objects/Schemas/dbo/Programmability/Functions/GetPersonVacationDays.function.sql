75a7f6845388604aa10df6375578c52ddd6916df
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetPersonVacationDays.function.sql
@@ -0,0 +1,24 @@
+﻿CREATE FUNCTION [dbo].[GetPersonVacationDays]
+(
+	@PersonId INT,
+	@startDate DATETIME,
+	@DaysForward INT
+)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @result INT
+	
+	DECLARE @EndRange DATETIME	
+	SET @EndRange = DATEADD(dd , @DaysForward, @StartDate) - 1
+
+	SELECT @result = COUNT(*) 
+	FROM dbo.v_PersonCalendar
+	WHERE Date BETWEEN @startDate AND @EndRange AND 
+		  @PersonId = PersonId AND 
+		  DayOff = 1 AND
+		  DATEPART(dw, Date) NOT IN (7, 1) AND
+		  DayOff != CompanyDayOff  
+
+	RETURN @result
+END
