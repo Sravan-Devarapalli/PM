e51223eefc20d6901b897b1deb83d1a905338d5d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/OpportunityPriority.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace DataTransferObjects
+{
+    [Serializable]
+    [DataContract]
+    public class OpportunityPriority
+    {
+        [DataMember]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string Priority
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public bool InUse { get; set; }
+
+        [DataMember]
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int SortOrder
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Sales Stage
+        /// </summary>
+        [DataMember]
+        public string DisplayName
+        {
+            get;
+            set;
+        }
+
+        public string HtmlEncodedDisplayName
+        {
+            get
+            {
+                return HttpUtility.HtmlEncode(DisplayName);
+            }
+        }
+    }
+}
