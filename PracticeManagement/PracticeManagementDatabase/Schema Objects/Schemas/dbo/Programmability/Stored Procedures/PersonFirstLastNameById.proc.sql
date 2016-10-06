29c5782875ba8a1a613743e30933dfe6526905e2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonFirstLastNameById.proc.sql
@@ -0,0 +1,30 @@
+﻿-- =============================================
+-- Updated by : Sainath.CH
+-- Update Date: 05-31-2012
+-- =============================================
+CREATE PROCEDURE [dbo].[PersonFirstLastNameById]
+	@PersonId int
+AS
+BEGIN
+	DECLARE @NOW DATETIME ,@FutureDate DATETIME
+	SELECT @NOW = dbo.GettingPMTime(GETUTCDATE()), @FutureDate = dbo.GetFutureDate()
+
+	SELECT	P.FirstName,
+			P.PreferredFirstName,
+			P.LastName,
+			P.IsStrawman,
+			P.HireDate,
+			TS.Name AS Timescale,
+			P.IsOffshore,
+			P.PersonStatusId,
+			ISNULL(P.Alias,'') AS Alias,
+			P.EmployeeNumber,
+			P.SeniorityId,
+			S.Name AS Seniority
+	FROM dbo.Person P
+	LEFT JOIN dbo.Pay PA ON PA.Person = P.PersonId 
+							AND @NOW BETWEEN PA.StartDate  AND ISNULL(PA.EndDate-1,@FutureDate) 
+	LEFT JOIN dbo.Timescale TS ON PA.Timescale = TS.TimescaleId
+	LEFT JOIN dbo.Seniority S ON S.SeniorityId = P.SeniorityId
+	WHERE PersonId = @PersonId
+END
