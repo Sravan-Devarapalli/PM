f327f6d82b180ccfb868aba120438e36601fc526
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/Generic/FormatedTextBox.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Web.UI.WebControls;
+
+namespace PraticeManagement.Controls.Generic
+{
+    public class FormatedTextBox : TextBox
+    {
+        private const string DATA_FORMAT_STRING_VIEWSTATE = "23B828AC-F2F8-470A-B288-B7FFD9EC2EE9";
+
+        public DateTime DateText
+        {
+            get
+            {
+                return Utils.Generic.ParseDate(base.Text);
+            }
+            set
+            {
+                base.Text = value.ToString(DataFormatString);
+            }
+        }
+
+        public double DoubleText
+        {
+            get
+            {
+                return Convert.ToDouble(base.Text);
+            }
+
+            set
+            {
+                base.Text = value.ToString(Constants.Formatting.DoubleFormat);
+            }
+        }
+
+        public string DataFormatString
+        {
+            get
+            {
+                string dfs = ViewState[DATA_FORMAT_STRING_VIEWSTATE] as string;
+                return dfs ?? string.Empty;
+            }
+
+            set
+            {
+                ViewState[DATA_FORMAT_STRING_VIEWSTATE] = value;
+            }
+        }
+    }
+}
+
