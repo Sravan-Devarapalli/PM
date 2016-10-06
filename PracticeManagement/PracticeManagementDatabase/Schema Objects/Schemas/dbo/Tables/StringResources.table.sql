959725f224f78c2a86e20b93a3fea1690f7c74f5
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/StringResources.table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE dbo.StringResources (
+    Id                    INT IDENTITY(1,1) NOT NULL,
+    TypeId				  INT NOT NULL,
+    ResourcesKey          NVARCHAR(255) NOT NULL,
+    Value                 NVARCHAR(MAX) NOT NULL,
+   CONSTRAINT PK_StringResources_ResourcesKey PRIMARY KEY CLUSTERED(ResourcesKey),
+   CONSTRAINT FK_StringResources_TypeId FOREIGN KEY(TypeId) REFERENCES  dbo.ResourceType(TypeId)
+);
+
+
