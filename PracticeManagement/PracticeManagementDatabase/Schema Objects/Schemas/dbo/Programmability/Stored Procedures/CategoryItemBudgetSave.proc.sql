0140ebf0c6c0dedf4bd62e1b1c15e583dfb07dad
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CategoryItemBudgetSave.proc.sql
@@ -0,0 +1,33 @@
+﻿CREATE PROCEDURE dbo.CategoryItemBudgetSave
+(
+	@ItemId		INT,
+	@CategoryTypeId	INT,
+	@MonthStartDate	DATETIME,
+	@Amount			DECIMAL(18,2)
+)
+AS
+BEGIN
+	IF EXISTS (SELECT 1 FROM dbo.CategoryItemBudget
+				WHERE ItemId = @ItemId AND CategoryTypeId = @CategoryTypeId
+						AND MonthStartDate = @MonthStartDate)
+	BEGIN
+	 UPDATE dbo.CategoryItemBudget
+	 SET Amount = @Amount
+	 WHERE ItemId = @ItemId 
+			AND CategoryTypeId = @CategoryTypeId
+			AND MonthStartDate = @MonthStartDate
+	END
+	ELSE
+	BEGIN
+		INSERT INTO [dbo].[CategoryItemBudget]
+           ([ItemId]
+           ,[CategoryTypeId]
+           ,[MonthStartDate]
+           ,[Amount])
+     VALUES
+           (@ItemId
+           ,@CategoryTypeId
+           ,@MonthStartDate
+           ,@Amount)
+	END
+END
