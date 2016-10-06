f4c0127a6f69133c22f2428e9315607cd0ccc59e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/OverheadAdministration.table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[OverheadAdministration] (
+    [OverheadAdministrationId] INT             NOT NULL,
+    [Description]              NVARCHAR (250)  NOT NULL,
+    [HourlyRate]               DECIMAL (18, 2) NOT NULL
+);
+
+
