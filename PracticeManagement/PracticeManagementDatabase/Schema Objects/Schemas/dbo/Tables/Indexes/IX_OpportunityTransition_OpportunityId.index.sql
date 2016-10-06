3bd684705bf1646a7be8c2d329639fc409a5c67d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Indexes/IX_OpportunityTransition_OpportunityId.index.sql
@@ -0,0 +1,5 @@
+﻿CREATE NONCLUSTERED INDEX [IX_OpportunityTransition_OpportunityId]
+    ON [dbo].[OpportunityTransition]([OpportunityId] ASC) WITH ( DROP_EXISTING = OFF, IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF, ONLINE = OFF, MAXDOP = 0)
+    ON [PRIMARY];
+
+
