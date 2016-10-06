ac8e4cda2eb4ab72e2f2b7fe4128cc63aa28b10e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementService/ITitleService.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using DataTransferObjects;
+
+namespace PracticeManagementService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ITitleService" in both code and config file together.
+    [ServiceContract]
+    [ServiceKnownType(typeof(Title))]
+    public interface ITitleService
+    {
+        [OperationContract]
+        List<Title> GetAllTitles();
+
+        [OperationContract]
+        List<TitleType> GetTitleTypes();
+
+        [OperationContract]
+        Title GetTitleById(int titleId);
+
+        [OperationContract]
+        void TitleInset(string title, int titleTypeId, int sortOrder, int pTOAccural, int? minimumSalary, int? maximumSalary, string userLogin);
+
+        [OperationContract]
+        void TitleUpdate(int titleId, string title, int titleTypeId, int sortOrder, int pTOAccural, int? minimumSalary, int? maximumSalary, string userLogin);
+
+        [OperationContract]
+        void TitleDelete(int titleId, string userLogin);
+    }
+}
+
