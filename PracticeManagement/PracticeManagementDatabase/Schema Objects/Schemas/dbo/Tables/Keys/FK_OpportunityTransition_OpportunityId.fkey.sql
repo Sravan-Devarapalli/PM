8762eb79223bb0f789bb7e6183d6d32fbc1df278
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_OpportunityTransition_OpportunityId.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[OpportunityTransition]
+    ADD CONSTRAINT [FK_OpportunityTransition_OpportunityId] FOREIGN KEY ([OpportunityId]) REFERENCES [dbo].[Opportunity] ([OpportunityId]) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+
