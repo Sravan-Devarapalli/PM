bce57a091c63f1aa523b2798d4c280d0ca78bd36
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/RestrictionPanel.ascx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Web.UI;
+using DataTransferObjects;
+
+namespace PraticeManagement.Controls
+{
+    public partial class RestrictionPanel : UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack) return;
+
+            DataHelper.FillSalespersonList(
+                msddSalespersons,
+                Resources.Controls.AllSalespersonsText,
+                true);
+
+            DataHelper.FillProjectOwnerList(
+                msddPracticeManagers,
+                "All People with Project Access",
+                null, true);
+
+            DataHelper.FillPracticeList(
+                msddPractices, Resources.Controls.AllPracticesText);
+
+            DataHelper.FillClientsAndGroupsCheckBoxListInPersonDetailPage(
+                msddClients, msddGroups);
+        }
+
+        public void ApplyPermissions(PersonPermission permissions)
+        {
+            ApplyPermissionsToControl(
+                msddClients, permissions, PermissionTarget.Client);
+            ApplyPermissionsToControl(
+                msddGroups, permissions, PermissionTarget.Group);
+            ApplyPermissionsToControl(
+                msddPractices, permissions, PermissionTarget.Practice);
+            ApplyPermissionsToControl(
+                msddPracticeManagers, permissions, PermissionTarget.PracticeManager);
+            ApplyPermissionsToControl(
+                msddSalespersons, permissions, PermissionTarget.Salesperson);
+        }
+
+        public PersonPermission GetPermissions()
+        {
+            var p = new PersonPermission();
+
+            p.SetPermissions(PermissionTarget.Client, msddClients.SelectedValues);
+            p.SetPermissions(PermissionTarget.Group, msddGroups.SelectedValues);
+            p.SetPermissions(PermissionTarget.Salesperson, msddSalespersons.SelectedValues);
+            p.SetPermissions(PermissionTarget.PracticeManager, msddPracticeManagers.SelectedValues);
+            p.SetPermissions(PermissionTarget.Practice, msddPractices.SelectedValues);
+
+            return p;
+        }
+
+        private static void ApplyPermissionsToControl(
+            ScrollingDropDown control, PersonPermission permissions, PermissionTarget pt)
+        {
+            control.SelectItems(
+                permissions.GetPermissions(pt));
+        }
+    }
+}
+
