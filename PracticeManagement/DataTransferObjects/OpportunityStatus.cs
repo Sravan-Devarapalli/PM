9bc33bb58ec7355ca174b5052ea08dcf136c69a2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/OpportunityStatus.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// Reprecentes an opportunity status.
+    /// </summary>
+    [Serializable]
+    [DataContract]
+    public class OpportunityStatus
+    {
+        /// <summary>
+        /// Gets or sets an ID of the opportunity status.
+        /// </summary>
+        [DataMember]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a name of the opportunity status.
+        /// </summary>
+        [DataMember]
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
