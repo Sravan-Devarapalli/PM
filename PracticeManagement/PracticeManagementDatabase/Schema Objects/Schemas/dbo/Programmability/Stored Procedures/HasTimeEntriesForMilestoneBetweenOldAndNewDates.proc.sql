4b0cd09cb61e1bc78d36d0c19543e9d8a5de045a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/HasTimeEntriesForMilestoneBetweenOldAndNewDates.proc.sql
@@ -0,0 +1,56 @@
+﻿CREATE PROCEDURE [dbo].[HasTimeEntriesForMilestoneBetweenOldAndNewDates]
+(
+@MilestoneId	INT,
+@StartDate   DATETIME = NULL,
+@EndDate     DATETIME  = NULL
+)
+AS
+BEGIN
+DECLARE @Result BIT , 
+		@MileStoneStartDate  DATETIME, 
+		@MileStoneEndDate  DATETIME,
+		@SwapStartDate  DATETIME,
+		@SwapEndDate  DATETIME,
+		@OldStartDate DATETIME,
+		@OldEndDate DATETIME,
+		@NewStartDate DATETIME,
+		@NewEndDate DATETIME
+
+SELECT @NewStartDate = @StartDate, @NewEndDate = @EndDate
+ 
+SELECT @MileStoneStartDate = m.StartDate
+	   ,@MileStoneEndDate =m.ProjectedDeliveryDate,
+	   @OldStartDate =m.StartDate,
+	   @OldEndDate =m.ProjectedDeliveryDate
+	   FROM Milestone AS m
+	   WHERE m.MilestoneId = @MilestoneId
+
+IF(@StartDate > @MileStoneStartDate)
+BEGIN
+  SET @SwapStartDate = @StartDate
+  SELECT @StartDate = @MileStoneStartDate , @MileStoneStartDate = @SwapStartDate
+END
+
+IF(@EndDate > @MileStoneEndDate)
+BEGIN
+  SET @SwapEndDate = @EndDate
+  SELECT @EndDate = @MileStoneEndDate , @MileStoneEndDate = @SwapEndDate
+END
+
+IF EXISTS(SELECT 1 FROM v_TimeEntries as te	where te.MilestoneId = @MilestoneId   AND @OldStartDate < @NewStartDate AND
+			 MilestoneDate  BETWEEN @StartDate AND @MileStoneStartDate - 1 )
+BEGIN 
+	SELECT @Result=1
+END
+ELSE IF EXISTS(SELECT 1 FROM v_TimeEntries as te	where te.MilestoneId = @MilestoneId AND @OldEndDate > @NewEndDate AND
+			 MilestoneDate  BETWEEN @EndDate + 1 AND @MileStoneEndDate)
+BEGIN 
+	SELECT @Result=1
+END
+ELSE
+BEGIN
+	SELECT @Result=0
+END
+	SELECT @Result AS HasTimeEntries
+END
+GO
