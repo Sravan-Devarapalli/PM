83eaa047ecfa0e0889c3bf719bb9e983ad47a105
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ExpenseBasis.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// Represents an ExpenseBasis database entity
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class ExpenseBasis
+    {
+        /// <summary>
+        /// Gets or sets an ID of the Expense Basis
+        /// </summary>
+        [DataMember]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a name of the Expense Basis
+        /// </summary>
+        [DataMember]
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
