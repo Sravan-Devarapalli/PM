2c4db93aabe1ef08601bcca4fb4883097175fcbd
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetProjectManagerNames.function.sql
@@ -0,0 +1,28 @@
+﻿-- =============================================
+-- Author:		ThulasiRam.P
+-- Create date: 02-27-2012
+-- Description:	Gets Project Manager Names By ; separated for a single project
+-- =============================================
+
+CREATE FUNCTION [dbo].[GetProjectManagerNames]
+(
+  @ProjectId INT
+)
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN
+  
+	DECLARE @Temp NVARCHAR(MAX) = ''
+  
+	SELECT @Temp = @Temp + Pers.LastName + ', ' +ISNULL(Pers.PreferredFirstName,Pers.FirstName) +'; '
+	FROM Project P
+	JOIN ProjectAccess PM ON PM.ProjectId = P.ProjectId
+	JOIN Person Pers ON Pers.Personid = PM.ProjectAccessId
+	WHERE P.ProjectId = @ProjectId
+
+	RETURN @Temp
+END
+
+
+GO
+
