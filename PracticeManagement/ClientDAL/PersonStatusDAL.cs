56cf48b28cd7f416a2df975c9f386d2ff7a8d162
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/ClientDAL/PersonStatusDAL.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using DataAccess.Other;
+using DataTransferObjects;
+
+namespace DataAccess
+{
+    /// <summary>
+    /// Provides an access to the PersonStatus table.
+    /// </summary>
+    public static class PersonStatusDAL
+    {
+        #region Constants
+
+        private const string PersonStatusListAllProcedure = "dbo.PersonStatusListAll";
+
+        private const string PersonStatusIdColumn = "PersonStatusId";
+        private const string NameColumn = "Name";
+
+        #endregion Constants
+
+        #region Methods
+
+        /// <summary>
+        /// Retrives the list of the person statuses.
+        /// </summary>
+        /// <returns>The list of the <see cref="PersonStatus"/> objects.</returns>
+        public static List<PersonStatus> PersonStatusListAll()
+        {
+            using (SqlConnection connection = new SqlConnection(DataSourceHelper.DataConnection))
+            using (SqlCommand command = new SqlCommand(PersonStatusListAllProcedure, connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    List<PersonStatus> result = new List<PersonStatus>();
+
+                    ReadPersonStatuses(reader, result);
+
+                    return result;
+                }
+            }
+        }
+
+        private static void ReadPersonStatuses(DbDataReader reader, List<PersonStatus> result)
+        {
+            if (!reader.HasRows) return;
+            int personStatusIdIndex = reader.GetOrdinal(PersonStatusIdColumn);
+            int nameIndex = reader.GetOrdinal(NameColumn);
+
+            while (reader.Read())
+            {
+                PersonStatus status = new PersonStatus
+                    {
+                        Id = reader.GetInt32(personStatusIdIndex),
+                        Name = reader.GetString(nameIndex)
+                    };
+
+                result.Add(status);
+            }
+        }
+
+        #endregion Methods
+    }
+}
