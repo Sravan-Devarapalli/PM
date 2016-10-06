849118887de57c662ccc71dbf720f5aea56f0ef7
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/EmailNotificationsListProjectedProjects.proc.sql
@@ -0,0 +1,39 @@
+﻿
+
+
+
+-- =============================================
+-- Author:		Artem Dolya
+-- Create date: 2010-06-11
+-- Description:	Retruns Template with recipients of Projected project
+-- =============================================
+CREATE PROCEDURE [dbo].[EmailNotificationsListProjectedProjects]
+	@EmailTemplateId int		
+AS 
+    BEGIN
+        SET NOCOUNT ON ;
+	
+	SELECT  distinct
+		pr.ProjectId,
+		p.Alias as ProjectOwner, 
+		sp.Alias as Salesperson, 
+		pm.Alias as PracticeOwner, 
+		c.Name AS ClientName, 
+		pr.Name as ProjectName, 
+		pr.StartDate
+		
+		FROM Project pr 
+		    INNER JOIN ProjectAccess projManagers ON projManagers.ProjectId = pr.ProjectId
+			INNER JOIN Person p ON p.PersonId = projManagers.ProjectAccessId
+			INNER JOIN Practice pa ON pr.PracticeId = pa.PracticeId
+			INNER JOIN Person pm ON pm.PersonId = pa.PracticeManagerId
+			INNER JOIN Client c ON c.ClientId = pr.ClientId 
+			INNER JOIN Person sp ON sp.PersonId = c.DefaultSalespersonID
+			
+		WHERE pr.ProjectStatusId = 2 AND pr.StartDate <= GETDATE()
+    
+		SELECT EmailTemplateSubject, EmailTemplateBody FROM EmailTemplate  et WHERE et.EmailTemplateId = @EmailTemplateId  
+    END
+
+
+
