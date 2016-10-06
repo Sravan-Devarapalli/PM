6a1b8f5ac15a1ed04a1b6934e2d98fc4ac7c8694
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimeEntriesGetByPerson.proc.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE TimeEntriesGetByPerson
+	@PersonId	INT,
+	@StartDate	datetime = NULL,
+	@EndDate	datetime = NULL
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	
+	select te.*
+	from v_TimeEntries as te
+	where te.PersonId = @PersonId
+		AND te.MilestoneDate between ISNULL(@StartDate, te.MilestoneDate) and ISNULL(@EndDate, te.MilestoneDate)
+	order by te.ProjectId, te.MilestoneDate
+END
+
