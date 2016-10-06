61cf28fd5626c053b867564b31d1c5a84219cbb3
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OpportunityExcelSet.proc.sql
@@ -0,0 +1,38 @@
+﻿CREATE PROCEDURE [dbo].[OpportunityExcelSet]
+AS 
+BEGIN
+SELECT O.OpportunityNumber AS 'Opportunity'
+	 , O.ClientName AS 'Account'
+	 ,O.BusinessGroupName AS 'Business Group'
+	 , O.GroupName AS 'Business Unit'
+	 , O.BuyerName AS 'Buyer'
+	 , O.Name AS 'Opportunity Name'
+	  ,O.BusinessTypeName AS 'New/Extension'
+	 , O.DisplayName AS 'Sales Stage'
+	 , O.OpportunityStatusName AS 'Status'
+	 , CONVERT(NVARCHAR(10),CONVERT(DATE,O.CloseDate)) AS 'Close Date'
+	 , CONVERT(NVARCHAR(10),CONVERT(DATE,O.ProjectedStartDate)) AS 'Project Start Date'
+	 , CONVERT(NVARCHAR(10),CONVERT(DATE,O.ProjectedEndDate)) AS 'Project End Date'
+	 , CONVERT(NVARCHAR(50), CONVERT(money,O.EstimatedRevenue),1)  AS 'Estimated Revenue'
+	 , O.SalespersonLastName + ',' + ISNULL(O.SalespersonPreferredFirstName,O.SalespersonFirstName) AS 'Salesperson'
+	 , p.LastName + ',' + ISNULL(p.PreferredFirstName,p.FirstName) AS 'Owner'
+	 , O.PracticeName AS 'Practice Area'
+	 , CASE WHEN proj.ProjectId IS NULL 
+	        THEN
+			   'NO'
+		    ELSE
+			   'YES'
+	   END AS 'Linked to Project?'
+	 , proj.ProjectNumber AS 'Project Number'
+	  ,O.PricingListName AS 'Pricing List'
+FROM
+	dbo.v_Opportunity O
+	INNER JOIN dbo.Person p
+		ON o.OwnerId = p.PersonId
+	LEFT JOIN dbo.Project proj
+		ON proj.ProjectId = o.ProjectId
+WHERE
+	O.OpportunityStatusId = 1
+END
+
+
