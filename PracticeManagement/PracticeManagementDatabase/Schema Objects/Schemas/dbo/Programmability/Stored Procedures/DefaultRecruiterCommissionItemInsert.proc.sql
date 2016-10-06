a731bff9e0fb83ec943a4465674cfb00341c9446
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DefaultRecruiterCommissionItemInsert.proc.sql
@@ -0,0 +1,20 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 9-02-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	Adds an item to the specified default recruiter commission.
+-- =============================================
+CREATE PROCEDURE [dbo].[DefaultRecruiterCommissionItemInsert]
+(
+	@DefaultRecruiterCommissionHeaderId   INT,
+	@HoursToCollect                       INT,
+	@Amount                               DECIMAL(18,2)
+)
+AS
+	SET NOCOUNT ON
+
+	INSERT INTO dbo.DefaultRecruiterCommissionItem
+	            (DefaultRecruiterCommissionHeaderId, HoursToCollect, Amount)
+	     VALUES (@DefaultRecruiterCommissionHeaderId, @HoursToCollect, @Amount)
+
