7b051188c26ba7add7787758734ff56ee498fe69
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/Generic/SelectCutOff/SelectCutOffExtender.js
@@ -0,0 +1,39 @@
+﻿Type.registerNamespace("PraticeManagement.Controls.Generic.SelectCutOff");
+
+PraticeManagement.Controls.Generic.SelectCutOff.SelectCutOffBehavior = function(element) {
+    PraticeManagement.Controls.Generic.SelectCutOff.SelectCutOffBehavior.initializeBase(this, [element]);
+}
+
+PraticeManagement.Controls.Generic.SelectCutOff.SelectCutOffBehavior.prototype = {
+    initialize: function() {
+        PraticeManagement.Controls.Generic.SelectCutOff.SelectCutOffBehavior.callBaseMethod(this, 'initialize');
+
+        $addHandlers(this.get_element(),
+                     { 'mousedown': this._onFocus,
+                         'blur': this._onBlur,
+                         'change': this._onBlur
+                     },
+                     this);
+    },
+
+    dispose: function() {
+        // Cleanup code 
+        PraticeManagement.Controls.Generic.SelectCutOff.SelectCutOffBehavior.callBaseMethod(this, 'dispose');
+    },
+
+    _onFocus: function() {
+        var target = this.get_element();
+        if (target != null) {
+            target.setAttribute("class", this.extendedCssClass);
+        }
+    },
+
+    _onBlur: function() {
+        var target = this.get_element();
+        if (target != null) {
+            target.setAttribute("class", this.normalCssClass);
+        }
+    }
+}
+
+PraticeManagement.Controls.Generic.SelectCutOff.SelectCutOffBehavior.registerClass('PraticeManagement.Controls.Generic.SelectCutOff.SelectCutOffBehavior', AjaxControlToolkit.BehaviorBase);
