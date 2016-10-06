2255edeb3e9152d0ffaf7c542bdada5a82203aa4
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SavePracticesIsNotesRequiredDetails.proc.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[SavePracticesIsNotesRequiredDetails]
+	@NotesRequiredPracticeIdsList NVARCHAR(250), 
+	@NotesExemptedPracticeIdsList NVARCHAR(250)
+AS
+BEGIN
+	
+	UPDATE dbo.Practice 
+	SET IsNotesRequired = 1
+	WHERE PracticeId IN (SELECT ResultId FROM dbo.ConvertStringListIntoTable(@NotesRequiredPracticeIdsList))
+
+	UPDATE dbo.Practice 
+	SET IsNotesRequired = 0
+	WHERE PracticeId IN (SELECT ResultId FROM dbo.ConvertStringListIntoTable(@NotesExemptedPracticeIdsList))
+
+END
+
