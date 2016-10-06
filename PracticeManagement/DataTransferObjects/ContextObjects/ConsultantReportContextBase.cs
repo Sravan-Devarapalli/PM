d851d9d605004bd56a1515aed175693763a6bf2d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ContextObjects/ConsultantReportContextBase.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.ContextObjects
+{
+    /// <summary>
+    /// Pepresents project cloning context
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    //[DebuggerDisplay("ProjectCloningContext: ")]
+    public class ConsultantReportContextBase
+    {
+        [DataMember]
+        public DateTime Start { get; set; }
+
+        [DataMember]
+        public bool? ActivePersons { get; set; }
+
+        [DataMember]
+        public bool? ProjectedPersons { get; set; }
+
+        [DataMember]
+        public bool ActiveProjects { get; set; }
+
+        [DataMember]
+        public bool ProjectedProjects { get; set; }
+
+        [DataMember]
+        public bool ProposedProjects { get; set; }
+
+        [DataMember]
+        public bool InternalProjects { get; set; }
+
+        [DataMember]
+        public bool ExperimentalProjects { get; set; }
+
+        [DataMember]
+        public bool CompletedProjects { get; set; }
+    }
+}
