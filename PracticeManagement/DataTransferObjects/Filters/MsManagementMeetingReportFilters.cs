802097a2566f1b676d313690441b02769dae3d9c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Filters/MsManagementMeetingReportFilters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTransferObjects.Filters
+{
+     [Serializable]
+    public class MsManagementReportFilters
+    {
+        public string PayTypeIds
+        {
+            get;
+            set;
+        }
+
+        public string PersonStatusIds
+        {
+            get;
+            set;
+        }
+    }
+}
+
