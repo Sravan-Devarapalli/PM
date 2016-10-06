5935799f3016dc1a4136b3cbff4da04f8b5a73e6
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Filters/ProjectFeedbackReportFilter.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTransferObjects.Filters
+{
+    [Serializable]
+    public class ProjectFeedbackReportFilter
+    {
+        public string PayTypeIds
+        {
+            get;
+            set;
+        }
+
+        public string ClientIds
+        {
+            get;
+            set;
+        }
+
+        public string BusinessUnitIds
+        {
+            get;
+            set;
+        }
+
+        public string ExecutiveInChargeIds
+        {
+            get;
+            set;
+        }
+
+        public string PracticeIds
+        {
+            get;
+            set;
+        }
+
+        public bool ExcludeInternalPractices
+        {
+            get;
+            set;
+        }
+
+        public string ProjectStatusIds
+        {
+            get;
+            set;
+        }
+
+        public string ProjectIds
+        {
+            get;
+            set;
+        }
+
+        public string ResourceIds
+        {
+            get;
+            set;
+        }
+
+        public string TitleIds
+        {
+            get;
+            set;
+        }
+
+        public string StartDateMonths
+        {
+            get;
+            set;
+        }
+
+        public string EndDateMonths
+        {
+            get;
+            set;
+        }
+
+        public string ProjectAccessIds
+        {
+            get;
+            set;
+        }
+
+        public string ProjectFeedbackStatusIds
+        {
+            get;
+            set;
+        }
+
+        public string ReportPeriod
+        {
+            get;
+            set;
+        }
+
+        public DateTime? ReportStartDate
+        {
+            get;
+            set;
+        }
+
+        public DateTime? ReportEndDate
+        {
+            get;
+            set;
+        }
+
+    }
+}
+
