fbd978db4d54850357032a802e9e3b23e9d9ef80
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ProjectAttachment.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class ProjectAttachment
+    {
+        [DataMember]
+        public int AttachmentId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public String AttachmentFileName
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public ProjectAttachmentCategory Category
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Byte[] AttachmentData
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int AttachmentSize
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime? UploadedDate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string Uploader
+        {
+            get;
+            set;
+        }
+    }
+}
