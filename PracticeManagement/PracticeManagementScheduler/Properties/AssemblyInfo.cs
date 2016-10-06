789a3990e42b108a1bafc3da0342ccb480f04842
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementScheduler/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PracticeManagementScheduler")]
+[assembly: AssemblyDescription("Practice Management Notification Scheduler")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Logic 20/20")]
+[assembly: AssemblyProduct("Practice Management")]
+[assembly: AssemblyCopyright("Copyright © Logic 20/20 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e82f862c-5b76-4181-bbfe-3f31757de1ec")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("0.900.*")]
