eb4596db6b3747e1837696f7679daa3c6e24986b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Config/Persons.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using PraticeManagement.Controls;
+namespace PraticeManagement.Config
+{
+    public partial class Persons : PracticeManagementPageBase
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override void Display()
+        {
+
+        }
+
+    }
+}
+
