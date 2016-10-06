d61a898a56df9836b4d3b9e991d6f10dfaa44073
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetVendorAttachments.proc.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[GetVendorAttachments]
+(
+	@Id INT
+)
+AS
+BEGIN
+	SELECT  VA.Id,
+			VA.FileName,
+			DATALENGTH(VA.AttachmentData) AS AttachmentSize,
+			VA.UploadedDate,
+			P.PersonId,
+			P.LastName,
+			P.FirstName
+	FROM dbo.VendorAttachment VA
+	LEFT JOIN dbo.Person P ON VA.ModifiedBy = P.PersonId
+	WHERE VendorId = @Id
+END
