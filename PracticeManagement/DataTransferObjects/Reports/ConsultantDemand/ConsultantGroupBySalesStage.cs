926b52d3ab4420a08f7b5e5b988fbe236d1da15d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Reports/ConsultantDemand/ConsultantGroupBySalesStage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Reports.ConsultingDemand
+{
+    [DataContract]
+    [Serializable]
+    public class ConsultantGroupBySalesStage
+    {
+        [DataMember]
+        public String SalesStage { get; set; }
+
+        [DataMember]
+        public List<ConsultantDemandDetailsByMonth> ConsultantDetailsBySalesStage { get; set; }
+
+        public int TotalCount
+        {
+            get
+            {
+                return ConsultantDetailsBySalesStage != null ? ConsultantDetailsBySalesStage.Sum(p => p.Count) : 0;
+            }
+        }
+    }
+}
