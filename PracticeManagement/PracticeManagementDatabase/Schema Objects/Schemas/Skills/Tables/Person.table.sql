1f4822df3dd7deb45ffbf861a226fc6930217ae4
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/Person.table.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [Skills].[Person](
+	[PersonId] [bigint] IDENTITY(1,1) NOT NULL,
+	[Alias] [nvarchar](100) NOT NULL,
+	[FirstName] [nvarchar](100) NOT NULL,
+	[MiddleName] [nvarchar](100) NULL,
+	[LastName] [nvarchar](100) NOT NULL,
+	[IsManager] [bit] NOT NULL,
+	[TitleId] [int] NULL,
+	[ManagerId] [bigint] NULL,
+	[ImageUrl] [nvarchar](max) NULL,
+	[ModificationDate] [datetime] NOT NULL,
+	[TenantId]		[int] NOT NULL,
+ CONSTRAINT [PK_Person] PRIMARY KEY CLUSTERED 
+(
+	[TenantId] ASC,
+	[PersonId] ASC
+)
+)
