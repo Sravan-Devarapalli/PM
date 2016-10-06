9f4bc2a5fdaf9bb0714c6c0800ba8f7aa605c969
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ExpenseBasisType.cs
@@ -0,0 +1,10 @@
+﻿namespace DataTransferObjects
+{
+    public enum ExpenseBasisType
+    {
+        AbsolutAmount = 1,
+        RevenuePercentage = 2,
+        CogsPercentage = 3,
+        GrossMarginPercentage = 4
+    }
+}
