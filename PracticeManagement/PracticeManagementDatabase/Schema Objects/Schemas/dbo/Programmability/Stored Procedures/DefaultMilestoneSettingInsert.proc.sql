b2dc8d6d23cc15eaf60541d36dcd0dd41af971b9
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DefaultMilestoneSettingInsert.proc.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[DefaultMilestoneSettingInsert]
+(
+	@ClientId		INT = NULL,
+	@ProjectId		INT = NULL,
+	@MilestoneId	INT = NULL,
+	@LowerBound		INT = NULL,
+	@UpperBound		INT = NULL
+)
+AS
+	SET NOCOUNT ON
+
+	IF NOT EXISTS (SELECT 1 FROM dbo.DefaultMilestoneSetting)
+		INSERT INTO [dbo].[DefaultMileStoneSetting](ClientId,ProjectId,MileStoneId,ModifiedDate, LowerBound, UpperBound)
+		SELECT @ClientId,@ProjectId,@MileStoneId,GETUTCDATE(),@LowerBound,@UpperBound
+	ELSE
+		UPDATE [dbo].[DefaultMileStoneSetting]
+		SET ClientId		= ISNULL(@ClientId,ClientId),
+			ProjectId		= ISNULL(@ProjectId,ProjectId),
+			MilestoneId		= ISNULL(@MilestoneId,MilestoneId),
+			ModifiedDate	= GETUTCDATE(),
+			LowerBound		= ISNULL(@LowerBound,LowerBound),
+			UpperBound		= ISNULL(@UpperBound,UpperBound)
+
