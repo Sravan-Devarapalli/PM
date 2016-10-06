3342ec4e1882cf4e67570e83143b4b23178ea881
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MilestonePersonListByProjectShort.proc.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE dbo.MilestonePersonListByProjectShort 
+(
+	@ProjectId   NVARCHAR(MAX)
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+	
+	SELECT mp.MilestonePersonId,
+	       mp.PersonId,
+	       mp.FirstName,
+		   mp.PreferredFirstName,
+	       mp.LastName,
+	       mp.ProjectId,
+		   mp.SeniorityId
+	  FROM dbo.v_MilestonePerson AS mp
+	 WHERE mp.ProjectId IN (SELECT * FROM dbo.ConvertStringListIntoTable(@ProjectId))
+END
+
