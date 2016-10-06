9a5cd3f7c02444c14e0e619de1fca992a855053d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/UpdateStatusForClient.proc.sql
@@ -0,0 +1,23 @@
+﻿-- =============================================
+-- Author:		ThulasiRam.P
+-- Create date: 06-08-2012
+-- Description:	Update Status for a client.
+-- =============================================
+CREATE PROCEDURE dbo.UpdateStatusForClient
+(
+	@ClientID           INT,
+	@Inactive           BIT,
+	@UserLogin          NVARCHAR(255)
+)
+
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	-- Start logging session
+	EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
+
+	Update dbo.Client
+	SET Inactive = @Inactive
+	WHERE ClientId = @ClientID
+END
