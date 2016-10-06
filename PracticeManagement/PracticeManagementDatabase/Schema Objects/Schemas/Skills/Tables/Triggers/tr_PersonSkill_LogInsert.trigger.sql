84c1131ec103a99bcadc4b801c46a3e183707579
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/Triggers/tr_PersonSkill_LogInsert.trigger.sql
@@ -0,0 +1,88 @@
+﻿CREATE TRIGGER [Skills].[tr_PersonSkill_LogInsert]
+ON [Skills].[PersonSkill]
+FOR INSERT
+AS 
+BEGIN
+    SET NOCOUNT ON
+
+	
+	-- Ensure the temporary table exists
+	--EXEC SessionLogPrepare @UserLogin = NULL
+	
+	DECLARE @CurrentPMTime DATETIME 
+	SET @CurrentPMTime = dbo.InsertingTime()
+	
+	;WITH NEW_VALUES AS
+	(
+		SELECT i.*,
+				S.Description 'SkillsDescription',
+				SL.Description 'SkillLevel',
+				P.LastName + ', ' + P.FirstName [Person],
+				SC.SkillCategoryId 'SkillCategoryId',
+				SC.Description 'SkillCategory',
+				ST.SkillTypeId 'SkillTypeId',
+				ST.Description 'SkillType'
+		  FROM inserted AS i
+			   INNER JOIN Skills.Skill AS S ON S.SkillId = i.SkillId
+			   INNER JOIN Skills.SkillLevel AS SL ON i.SkillLevelId = SL.SkillLevelId
+			   INNER JOIN dbo.Person AS P ON P.PersonId = i.PersonId
+			   INNER JOIN Skills.SkillCategory AS SC ON SC.SkillCategoryId = S.SkillCategoryId
+			   INNER JOIN Skills.SkillType AS ST ON ST.SkillTypeId = SC.SkillTypeId
+	)
+
+	-- Log an activity
+	INSERT INTO dbo.UserActivityLog
+	            (ActivityTypeID,
+	             SessionID,
+	             SystemUser,
+	             Workstation,
+	             ApplicationName,
+	             UserLogin,
+	             PersonID,
+	             LastName,
+	             FirstName,
+				 Data,
+	             LogData,
+	             LogDate)
+	SELECT 3 AS ActivityTypeID, --Added
+	       l.SessionID,
+	       l.SystemUser,
+	       l.Workstation,
+	       l.ApplicationName,
+	       l.UserLogin,
+	       l.PersonID,
+	       l.LastName,
+	       l.FirstName,
+	       Data =  CONVERT(NVARCHAR(MAX),(SELECT NEW_VALUES.PersonId,
+												NEW_VALUES.SkillsDescription,
+												NEW_VALUES.SkillType,
+												NEW_VALUES.SkillCategory,
+												NEW_VALUES.Person,
+												NEW_VALUES.SkillLevel,
+												NEW_VALUES.YearsExperience,
+												NEW_VALUES.LastUsed
+					    FROM NEW_VALUES
+			           WHERE (NEW_VALUES.SkillId = i.SkillId AND NEW_VALUES.PersonId = i.PersonId)
+					  FOR XML AUTO, ROOT('PersonSkill'))),
+			LogData = (SELECT   NEW_VALUES.PersonId,
+								NEW_VALUES.SkillId,
+								NEW_VALUES.SkillTypeId,
+								NEW_VALUES.SkillCategoryId,
+								NEW_VALUES.SkillLevelId,
+								NEW_VALUES.YearsExperience,
+								NEW_VALUES.LastUsed
+					    FROM NEW_VALUES
+			           WHERE (NEW_VALUES.SkillId = i.SkillId AND NEW_VALUES.PersonId = i.PersonId)
+					  FOR XML AUTO, ROOT('PersonSkill'), TYPE),
+			@CurrentPMTime
+
+	  FROM inserted AS i
+			FULL JOIN deleted AS d ON i.SkillId = d.SkillId AND i.PersonId = d.PersonId
+	       INNER JOIN dbo.SessionLogData AS l ON l.SessionID = @@SPID
+	  WHERE d.SkillId IS NULL AND d.PersonId IS NULL
+
+	-- End logging session
+	--EXEC dbo.SessionLogUnprepare
+
+END
+
