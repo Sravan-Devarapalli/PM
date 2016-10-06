c77a457631a15b9469606ccb21c342b085c0bfd9
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OpportunityTransitionDelete.proc.sql
@@ -0,0 +1,16 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2010-07-05
+-- Description:	Removes OpportunityTransition
+-- =============================================
+CREATE PROCEDURE dbo.OpportunityTransitionDelete
+	@OpportunityTransitionId	INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	delete 
+	from OpportunityTransition
+	where OpportunityTransitionId = @OpportunityTransitionId
+END
+
