a5655bed4dc3e52f7c590ba75ea3dfa351f3ad35
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Reports/ByPerson/GroupByDateByPerson.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataTransferObjects.Reports
+{
+    public class GroupByDateByPerson
+    {
+        public DateTime Date
+        {
+            get;
+            set;
+        }
+
+        public int TimeEntrySectionId { get; set; }
+
+        public List<GroupByPersonByWorktype> ProjectTotalHours
+        {
+            get;
+            set;
+        }
+
+        public double TotalHours
+        {
+            get
+            {
+                return ProjectTotalHours != null ? ProjectTotalHours.Sum(p => p.TotalHours) : 0;
+            }
+        }
+    }
+}
