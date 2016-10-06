1df71ad9a5640e48183dbb18cd277369bd1f356e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetVendorById.proc.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[GetVendorById]
+(
+	@Id	INT
+)
+AS
+	SET NOCOUNT ON
+
+	SELECT v.Id,
+		   v.Name,
+		   v.ContactName,
+		   v.Email,
+		   v.Status,
+		   v.TelephoneNumber,
+		   vt.Id as VendorTypeId,
+		   vt.Name as VendorTypeName
+	FROM dbo.Vendor v
+	JOIN dbo.VendorType vt on v.VendorTypeId=vt.Id
+		 WHERE v.Id=@Id
