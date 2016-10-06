766652efe6ecd5561c58d64030e78d5c365f680e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetPersonDivisions.proc.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[GetPersonDivisions]
+AS
+BEGIN
+	SELECT pd.DivisionId,
+		   pd.DivisionName,
+		   pd.Inactive,
+		   pers.FirstName,
+		   pers.LastName,
+		   pers.PersonId
+		   FROM dbo.persondivision pd
+		   LEFT JOIN dbo.Person AS pers ON pd.DivisionOwnerId = pers.PersonId
+		   WHERE Inactive=0
+		   ORDER BY pd.DivisionName
+END 
+
