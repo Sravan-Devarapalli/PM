cb7c647c6808dfc66fc344a712319532ebe8e13a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Indexes/IX_DefaultRecruiterCommissionHeader_PersonStartDate.index.sql
@@ -0,0 +1,6 @@
+﻿CREATE UNIQUE NONCLUSTERED INDEX [IX_DefaultRecruiterCommissionHeader_PersonStartDate]
+    ON [dbo].[DefaultRecruiterCommissionHeader]([PersonId] ASC, [StartDate] ASC)
+    INCLUDE([EndDate]) WITH ( DROP_EXISTING = OFF, IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF, ONLINE = OFF, MAXDOP = 0)
+    ON [PRIMARY];
+
+
