69055f97eaecaa500512e24c69816cee96ac5e73
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/PersonDivision.table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[PersonDivision]
+(
+	[DivisionId]	           INT IDENTITY(1,1) NOT NULL,
+	[DivisionName]	           NVARCHAR(100) NOT NULL,
+	[PracticeDirectorId]	   INT	NULL,
+	[Inactive]                 BIT NOT NULL  CONSTRAINT DF_PersonDivision_Inactive DEFAULT(0),
+	[ShowSetPracticeOwnerLink] BIT NOT NULL,
+	[DivisionOwnerId]		   INT           NULL
+	CONSTRAINT DF_PersonDivision_ShowSetPracticeOwnerLink DEFAULT(0)
+	CONSTRAINT [PK_PersonDivision_DivisionId]	PRIMARY KEY ([DivisionId])
+)
+
