e7795443478aad423fbdc95b96b988688ccfa3cf
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetUnpaidTimeTypeId.function.sql
@@ -0,0 +1,16 @@
+﻿-- =============================================
+-- Author:	Sainath C
+-- Create date: 01-06-2012
+-- =============================================
+CREATE FUNCTION [dbo].[GetUnpaidTimeTypeId]()
+RETURNS INT
+AS
+BEGIN
+	DECLARE @Id	INT
+
+	SELECT @Id = TT.TimeTypeId
+	FROM dbo.TimeType TT
+	WHERE 
+	TT.Code = 'W9350'--Here 'W9350' is the code of Unpaid work type.
+	RETURN @Id
+END
