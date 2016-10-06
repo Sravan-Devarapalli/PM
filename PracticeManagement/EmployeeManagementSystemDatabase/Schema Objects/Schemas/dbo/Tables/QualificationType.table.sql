a71cc770e1ee5e41c5331f899a1f59437d327e2d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/EmployeeManagementSystemDatabase/Schema Objects/Schemas/dbo/Tables/QualificationType.table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[QualificationType](
+	[QualificationTypeId]	[int] NOT NULL IDENTITY(1,1), 
+	[Description] [nvarchar](max) NULL,
+	[DisplayOrder] [tinyint] NULL,
+ CONSTRAINT [PK_QualificationType] PRIMARY KEY CLUSTERED 
+(
+	[QualificationTypeId] ASC
+)
+)
+
