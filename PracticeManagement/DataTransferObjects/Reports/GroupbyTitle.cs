4f35e39532f140f42182c1ea57802dee7071d4c2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Reports/GroupbyTitle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Reports
+{
+    [DataContract]
+    [Serializable]
+    public class GroupbyTitle
+    {
+        [DataMember]
+        public Title Title
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public List<BadgedResourcesByTime> ResourcesCount
+        {
+            get;
+            set;
+        }
+    }
+}
+
