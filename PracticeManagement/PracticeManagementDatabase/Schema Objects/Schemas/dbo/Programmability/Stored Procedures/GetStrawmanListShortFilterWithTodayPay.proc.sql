6fecab97443a24787dbc14b53d268376999c85a6
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetStrawmanListShortFilterWithTodayPay.proc.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[GetStrawmanListShortFilterWithTodayPay]
+AS
+BEGIN
+	DECLARE @Today DATETIME
+	SELECT @Today = CONVERT(DATE, dbo.GettingPMTime(GETUTCDATE()))
+	
+	SELECT	P.PersonId,
+			P.LastName,
+			P.FirstName
+	FROM dbo.Person P
+	JOIN dbo.Pay PA ON PA.Person = P.PersonId AND @Today BETWEEN PA.StartDate AND PA.EndDate - 1
+	WHERE P.IsStrawman = 1
+
+END
