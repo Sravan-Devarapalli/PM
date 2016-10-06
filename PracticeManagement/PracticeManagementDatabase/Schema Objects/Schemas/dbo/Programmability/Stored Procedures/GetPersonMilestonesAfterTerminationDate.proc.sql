e0d00ed7c845240eba4c8c147296508c07a2bf8a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetPersonMilestonesAfterTerminationDate.proc.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE dbo.GetPersonMilestonesAfterTerminationDate
+(
+	@PersonId	INT,
+	@TerminationDate	DATETIME 
+)
+AS
+BEGIN 
+	
+	SELECT DISTINCT
+			ProjectId,
+			ProjectNumber,
+			ProjectName,
+			MilestoneId,
+			MilestoneName,
+			M.MilestoneStartDate			
+	FROM dbo.v_MilestonePerson  M
+	WHERE M.PersonId = @PersonId
+			AND EndDate >= @TerminationDate
+			AND M.MilestoneId NOT IN (SELECT  MilestoneId FROM DefaultMilestoneSetting)
+	ORDER BY ProjectNumber,M.MilestoneStartDate
+END
