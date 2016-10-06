b0803b81f8aa6c4e1f65c95b567084fef48a6b37
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/Generic/DuplicateOptionsRemove/DupilcateOptionsRemoveExtender.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.UI;
+using AjaxControlToolkit;
+[assembly: WebResource("PraticeManagement.Controls.Generic.DuplicateOptionsRemove.DupilcateOptionsRemoveExtender.js", "text/javascript")]
+namespace PraticeManagement.Controls.Generic.DuplicateOptionsRemove
+{
+    using System.ComponentModel;
+    using System;
+    [ClientScriptResource("PraticeManagement.Controls.Generic.DuplicateOptionsRemove.DuplicateOptionsRemoveBehavior", "PraticeManagement.Controls.Generic.DuplicateOptionsRemove.DupilcateOptionsRemoveExtender.js")]
+    [TargetControlType(typeof(Control))]
+    public class DupilcateOptionsRemoveExtender : ExtenderControlBase
+    {
+        [ExtenderControlProperty]
+        [DefaultValue("")]
+        [ClientPropertyName("controlsToCheck")]
+        public string ControlsToCheck
+        {
+            get { return GetPropertyValue("controlsToCheck", string.Empty); }
+            set { SetPropertyValue("controlsToCheck", value); }
+        }
+
+        [ExtenderControlProperty]
+        [DefaultValue("")]
+        [ClientPropertyName("plusButtonClientID")]
+        public string PlusButtonClientID
+        {
+            get { return GetPropertyValue("plusButtonClientID", string.Empty); }
+            set { SetPropertyValue("plusButtonClientID", value); }
+        }
+      
+    }
+}
+
+
