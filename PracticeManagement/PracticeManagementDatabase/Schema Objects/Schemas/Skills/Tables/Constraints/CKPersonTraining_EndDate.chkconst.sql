ae40c506691904c359f368160de0f3040806d177
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/Constraints/CKPersonTraining_EndDate.chkconst.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [Skills].[PersonTraining]  WITH CHECK ADD  CONSTRAINT [CKPersonTraining_EndDate] CHECK  (([EndDate]>='1970-1-1' AND [EndDate]<=getdate()))
+GO
+ALTER TABLE [Skills].[PersonTraining] CHECK CONSTRAINT [CKPersonTraining_EndDate]
+GO
