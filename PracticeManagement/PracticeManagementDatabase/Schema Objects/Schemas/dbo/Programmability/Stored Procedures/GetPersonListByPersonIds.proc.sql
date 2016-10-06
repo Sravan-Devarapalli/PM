d88358e282a17c175701d4d5eb788fe2487f7491
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetPersonListByPersonIds.proc.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE GetPersonListByPersonIds
+(
+	@PersonIds NVARCHAR(MAX)
+)
+AS
+BEGIN
+	SELECT  P.PersonId,
+			P.FirstName,
+			P.LastName,
+			P.IsDefaultManager,
+			P.PersonStatusId
+	FROM dbo.Person P
+	WHERE P.PersonId in (SELECT ResultId FROM dbo.ConvertStringListIntoTable(@PersonIds))
+END
