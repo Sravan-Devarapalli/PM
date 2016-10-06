343c23205230c3031cbc054d67b1705e71292762
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ManagedParametersByPerson.table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[ManagedParametersByPerson]
+(
+	PersonId				INT				NOT NULL,
+	ActualRevenuePerHour	DECIMAL(32,2)	NULL,
+	TargetRevenuePerHour	DECIMAL(32,2)	NULL,
+	HoursUtilization		DECIMAL(32,2)	NULL,
+	TargetRevenuePerAnnum	DECIMAL(32,2)	NULL,
+	CONSTRAINT PK_ManagedParametersByPerson PRIMARY KEY (PersonId)
+)
+
