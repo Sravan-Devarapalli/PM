867843c3fa7a28baac55133c76689805babfa0d2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/PersonCalendar.table.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[PersonCalendar] (
+    [Date]				DATETIME	NOT NULL,
+    [PersonId]			INT			NOT NULL,
+    [DayOff]			BIT			NOT NULL,
+	[ActualHours]		REAL		NULL,
+	[IsSeries]			BIT NOT NULL CONSTRAINT DF_PersonCalendar_IsSeries DEFAULT 0,
+	[TimeTypeId]		INT NULL,
+	[SubstituteDate]	DATETIME NULL,
+	[Description]		NVARCHAR(500) NULL,
+	[IsFromTimeEntry]	BIT NULL,
+	[ApprovedBy]		INT NULL,
+	[SeriesId]          BIGINT NOT NULL CONSTRAINT DF_PersonCalendar_SeriesId DEFAULT 0
+);
+
