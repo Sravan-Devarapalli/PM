de1ac6cdbe97a948c9b37e5184b4baa1529a1919
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataAccessTest/PracticeDALTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using DataAccess;
+using DataTransferObjects;
+using NUnit.Framework;
+
+namespace DataAccessTest
+{
+    [TestFixture]
+    public class PracticeDALTest
+    {
+        [Test]
+        public void PracticeListAllTest()
+        {
+            List<Practice> list = PracticeDAL.PracticeListAll();
+            Assert.AreEqual(4, list.Count);
+        }
+    }
+}
