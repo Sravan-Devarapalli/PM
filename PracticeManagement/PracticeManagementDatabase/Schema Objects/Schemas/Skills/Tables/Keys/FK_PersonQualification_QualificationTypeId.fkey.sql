71e45a2da636a30c4910f030bbcbd49b0be60172
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/Keys/FK_PersonQualification_QualificationTypeId.fkey.sql
@@ -0,0 +1,6 @@
+﻿ALTER TABLE [Skills].[PersonQualification]  WITH CHECK ADD  CONSTRAINT [FK_PersonQualification_QualificationTypeId] FOREIGN KEY([QualificationTypeId])
+REFERENCES [Skills].[QualificationType] ([QualificationTypeId])
+ON UPDATE CASCADE
+GO
+ALTER TABLE [Skills].[PersonQualification] CHECK CONSTRAINT [FK_PersonQualification_QualificationTypeId]
+GO
