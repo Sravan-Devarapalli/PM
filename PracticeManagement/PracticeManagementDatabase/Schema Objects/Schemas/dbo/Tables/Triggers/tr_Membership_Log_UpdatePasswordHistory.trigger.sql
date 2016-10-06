f7f1362ebe02b4b257d70b6ca179edead963be71
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Triggers/tr_Membership_Log_UpdatePasswordHistory.trigger.sql
@@ -0,0 +1,27 @@
+﻿CREATE TRIGGER [dbo].[tr_Membership_Log_UpdatePasswordHistory]
+   ON  [dbo].[aspnet_Membership]
+   AFTER  UPDATE
+AS 
+BEGIN
+	SET NOCOUNT ON;
+	  
+	  DECLARE @OldPasswordChangedDate DATETIME ,@NewPasswordChangedDate DATETIME 
+	  
+	 SELECT  @OldPasswordChangedDate = d.LastPasswordChangedDate 
+	 FROM deleted AS d
+	  
+	 SELECT  @NewPasswordChangedDate = i.LastPasswordChangedDate 
+		FROM inserted AS i
+		
+    IF(@OldPasswordChangedDate != @NewPasswordChangedDate)	
+    BEGIN								
+	  INSERT INTO UserPassWordHistory([UserId]
+	   ,[Password]
+	   ,[PasswordFormat]
+	   ,[PasswordSalt]
+	   ,[LastPasswordChangedDate])
+	   SELECT d.UserId,d.Password,d.PasswordFormat,d.PasswordSalt,d.LastPasswordChangedDate    
+	   FROM  deleted AS d 
+	END
+END
+
