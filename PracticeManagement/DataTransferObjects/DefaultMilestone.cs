3d4297bee39fba319f5af7348316969b01bdfee9
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/DefaultMilestone.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class DefaultMilestone
+    {
+        /// <summary>
+        /// Id of the Default Milestone
+        /// </summary>
+        [DataMember]
+        public int MilestoneId
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Project Id of the Milstone.
+        /// </summary>
+        [DataMember]
+        public int ProjectId
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Client Id of the Default Milestone's Project.
+        /// </summary>
+        [DataMember]
+        public int ClientId
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Date on which milestone is set as Default milestone.
+        /// </summary>
+        [DataMember]
+        public DateTime ModifiedDate
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// No. of days to be used as lower bound to select a milestone.
+        /// </summary>
+        [DataMember]
+        public int LowerBound
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///  No. of days to be used as upper bound to select a milestone.
+        /// </summary>
+        [DataMember]
+        public int UpperBound
+        {
+            get;
+            set;
+        }
+    }
+}
