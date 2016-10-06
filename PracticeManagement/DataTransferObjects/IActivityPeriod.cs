bb5cbe1fcfe613c7c7dcf01453e7515aa2e8d35c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/IActivityPeriod.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using DataTransferObjects.TimeEntry;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// Represents date period and corresponding methods and properties
+    /// </summary>
+    public interface IActivityPeriod
+    {
+        /// <summary>
+        /// Start date
+        /// </summary>
+        DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// End date
+        /// </summary>
+        DateTime EndDate { get; set; }
+
+        /// <summary>
+        /// Empty DatePoint for each date of the period
+        /// </summary>
+        List<DatePoint> EmptyPeriodPoints { get; set; }
+
+        /// <summary>
+        /// Projected values for each date of the period
+        /// </summary>
+        List<DatePoint> ProjectedActivity { get; set; }
+
+        /// <summary>
+        /// Actual hours for each date of the period
+        /// </summary>
+        List<TimeEntryRecord> ActualActivity { get; set; }
+    }
+}
+
