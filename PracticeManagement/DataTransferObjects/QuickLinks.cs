2958da67f28d4811f0deb488d25a534986608dab
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/QuickLinks.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [Serializable]
+    [DataContract]
+    public class QuickLinks
+    {
+        [DataMember]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string LinkName
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string VirtualPath
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DashBoardType DashBoardType
+        {
+            get;
+            set;
+        }
+    }
+}
