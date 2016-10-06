878eb41940fd86bbd13fd22771b160b99de9d102
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_Opportunity_RevenueType.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[Opportunity]
+    ADD CONSTRAINT [FK_Opportunity_RevenueType] FOREIGN KEY ([RevenueType]) REFERENCES [dbo].[RevenueType] ([RevenueTypeId]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
