e49eea37dbf569b618e5fb6d94f5464d9f1f8a95
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/PersonSkill.table.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [Skills].[PersonSkill](
+	[SkillId] [int] NOT NULL,
+	[PersonId] [int] NOT NULL,
+	[YearsExperience] [int] NULL,
+	[SkillLevelId] [int] NOT NULL,
+	[LastUsed] [int] NOT NULL,
+	[DisplayOrder] [tinyint] NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_PersonSkill] PRIMARY KEY CLUSTERED 
+(
+	[SkillId] ASC,
+	[PersonId] ASC
+)
+)
