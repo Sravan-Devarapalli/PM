b163b287eecf40e89f397d7e6065a73df80ece56
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/ProjectNameCell.ascx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace PraticeManagement.Controls
+{
+    public partial class ProjectNameCell : System.Web.UI.UserControl
+    {
+        #region Property
+        
+        /// <summary>
+        /// Project Name Button Id
+        /// </summary>
+        public string ButtonProjectNameId
+        {            
+            set
+            {
+                btnProjectName.ID = value;
+            }
+        }
+
+        /// <summary>
+        /// Project Name Button Text
+        /// </summary>
+        public string ButtonProjectNameText
+        {            
+            set
+            {
+                btnProjectName.Text = value;
+            }
+        }
+        
+        /// <summary>
+        /// Project Name Button ToolTip content
+        /// </summary>
+        public string ButtonProjectNameToolTip
+        {            
+            set
+            {
+                lblTooltipContent.Text = value;
+            }
+        }
+
+        /// <summary>
+        /// Project Name Button ToolTip content
+        /// </summary>
+        public string ButtonProjectNameHref
+        {
+            set
+            {
+                btnProjectName.NavigateUrl = value;
+            }
+        }
+
+        #endregion
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
