33b3f11dc3372f618c1aa6dc0c89c6dd221a1c88
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetProjectByIdShort.proc.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[GetProjectByIdShort]
+(
+	@ProjectId int
+)
+AS
+BEGIN
+	SELECT ProjectId,Name,ProjectNumber,EndDate
+	FROM [dbo].[Project]
+	WHERE ProjectId = @ProjectId
+END
