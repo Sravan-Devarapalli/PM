816ae28d1eb8d105db5eebe275e135fe4f702869
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_OpportunityHistory.view.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW dbo.v_OpportunityHistory
+AS 
+SELECT opt.OpportunityId, opt.OpportunityNumber, stat.[Name] AS 'Status', trans.TransitionDate, 
+		pers.FirstName + ' ' + pers.LastName AS 'Responsible', pers.PersonId, trans.NoteText
+FROM dbo.Opportunity AS opt
+LEFT OUTER JOIN dbo.OpportunityTransition AS trans ON trans.OpportunityId = opt.OpportunityId
+INNER JOIN dbo.OpportunityTransitionStatus AS stat ON stat.OpportunityTransitionStatusId = trans.OpportunityTransitionStatusId
+INNER JOIN dbo.Person AS pers ON pers.PersonId = trans.PersonId
