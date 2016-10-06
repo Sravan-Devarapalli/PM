1fbaa627eb4423025148954ef87527e08731f5e0
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CapabilityInsert.proc.sql
@@ -0,0 +1,43 @@
+﻿CREATE PROCEDURE [dbo].[CapabilityInsert]
+(
+	@Name NVARCHAR(100),
+	@PracticeId INT,
+	@IsActive BIT,
+	@UserLogin NVARCHAR(MAX)
+)
+AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRAN CapabilityInsert_Tran;
+
+		SELECT @Name = REPLACE(REPLACE(REPLACE(LTRIM(RTRIM(@Name)),' ','<>'),'><',''),'<>',' ')
+
+		DECLARE @Error NVARCHAR(MAX)
+		IF EXISTS(SELECT 1 FROM dbo.[PracticeCapabilities] WHERE CapabilityName = @Name)
+		BEGIN
+			SET @Error = 'Capability name already exists. Please enter a different capability name.'
+			RAISERROR(@Error,16,1)
+		END
+
+		EXEC SessionLogPrepare @UserLogin = @UserLogin
+
+		Insert [dbo].[PracticeCapabilities](CapabilityName,IsActive,PracticeId)
+		VALUES (@Name,@IsActive,@PracticeId)
+
+		COMMIT TRAN CapabilityInsert_Tran;
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN CapabilityInsert_Tran;
+		DECLARE	 @ERROR_STATE	tinyint
+				,@ERROR_SEVERITY		tinyint
+				,@ERROR_MESSAGE		    nvarchar(2000)
+				,@InitialTranCount		tinyint
+
+		SET	 @ERROR_MESSAGE		= ERROR_MESSAGE()
+		SET  @ERROR_SEVERITY	= ERROR_SEVERITY()
+		SET  @ERROR_STATE		= ERROR_STATE()
+		RAISERROR ('%s', @ERROR_SEVERITY, @ERROR_STATE, @ERROR_MESSAGE)
+	END CATCH
+	EXEC dbo.SessionLogUnprepare
+END
+
