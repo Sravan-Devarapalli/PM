c93fbb37333ddd8db3575f560407a25e6bd8aa0e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/TitleType.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [Serializable]
+    [DataContract]
+    public class TitleType
+    {
+        [DataMember]
+        public int TitleTypeId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string TitleTypeName
+        {
+            get;
+            set;
+        }
+
+        public int StartPosition
+        {
+            get;
+            set;
+        }
+
+        public int EndPosition
+        {
+            get;
+            set;
+        }
+    }
+}
