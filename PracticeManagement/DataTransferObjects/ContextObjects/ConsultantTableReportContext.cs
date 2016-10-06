5aad293cebc796f2b2e4c0a2bb121ce5fefb1303
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ContextObjects/ConsultantTableReportContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.ContextObjects
+{
+    /// <summary>
+    /// Represents project cloning context
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class ConsultantTableReportContext : ConsultantReportContextBase
+    {
+        [DataMember]
+        public DateTime End { get; set; }
+    }
+}
+
