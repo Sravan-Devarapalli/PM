6099f051a19956eaf4cfbf56345eb63d7438cce6
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/BusinessGroup.table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[BusinessGroup]
+(
+	[BusinessGroupId]	INT				IDENTITY (0, 1) NOT NULL,
+    [ClientId]			INT				NOT NULL,
+	[Code]				NVARCHAR(6)		NOT NULL,
+    [Name]				NVARCHAR (100)	NOT NULL,
+	[Active]			BIT				NOT NULL CONSTRAINT DF_BusinessGroup_Active DEFAULT  1,
+    PRIMARY KEY CLUSTERED ([BusinessGroupId] ASC) WITH (IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF)
+)
+
