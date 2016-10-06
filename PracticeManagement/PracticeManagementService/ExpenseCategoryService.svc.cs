fcf1546a9890a5b8cc0a4659396f058408e06641
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementService/ExpenseCategoryService.svc.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel.Activation;
+using DataAccess;
+using DataTransferObjects;
+
+namespace PracticeManagementService
+{
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class ExpenseCategoryService : IExpenseCategoryService
+    {
+        /// <summary>
+        /// Retrieves the list of the categories for the fixed expenses
+        /// </summary>
+        /// <returns>The list of the <see cref="ExpenseCategory"/> objects.</returns>
+        public List<ExpenseCategory> GetCategoryList()
+        {
+            return ExpenseCategoryDAL.ExpenseCategoryListAll();
+        }
+
+        /// <summary>
+        /// Saves an <see cref="ExpenseCategory"/> into the database.
+        /// </summary>
+        /// <param name="category">The <see cref="ExpenseCategory"/> the data be saved from.</param>
+        public void SaveExpenseCategoryDetail(ExpenseCategory category)
+        {
+            if (category.Id == default(int))
+            {
+                ExpenseCategoryDAL.ExpenseCategoryInsert(category);
+            }
+            else
+            {
+                ExpenseCategoryDAL.ExpenseCategoryUpdate(category);
+            }
+        }
+
+        /// <summary>
+        /// Deletes an <see cref="ExpenseCategory"/> from the database.
+        /// </summary>
+        /// <param name="category">The <see cref="ExpenseCategory"/> the data be deleted from.</param>
+        public void DeleteExpenseCategory(ExpenseCategory category)
+        {
+            ExpenseCategoryDAL.ExpenseCategoryDelete(category);
+        }
+    }
+}
