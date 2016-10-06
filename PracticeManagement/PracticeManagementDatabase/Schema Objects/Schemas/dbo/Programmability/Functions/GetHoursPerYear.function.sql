58668aeff69e4107cc7d2e41d232364e776cb965
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetHoursPerYear.function.sql
@@ -0,0 +1,15 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 5-28-2008
+-- Updated by:	Anatoliy Lokshin
+-- Update date:	8-26-2008
+-- Description:	Gets a number of hours pe year
+-- =============================================
+CREATE FUNCTION [dbo].[GetHoursPerYear]()
+RETURNS DECIMAL
+WITH SCHEMABINDING
+AS
+BEGIN
+	RETURN 2080;
+END
+
