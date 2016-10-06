155ec28230e72b77026fc8b0af560bcf5a4363c7
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabaseUI/ConnectionProperties.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace PracticeManagementDatabaseUI
+{
+    public class ConnectionProperties
+    {
+        public string ServerName { get; set; }
+        public string DatabaseName { get; set; }
+
+        public string ConnectionString 
+        { 
+            get 
+            {
+                var builder = new SqlConnectionStringBuilder();
+                builder["Server"] = ServerName;
+                builder["Connect Timeout"] = 5;
+                builder["Trusted_Connection"] = true;
+
+                return builder.ConnectionString;
+            } 
+        }
+    }
+}
+
