7644883b7960a1baee42dab8cdcc9d7c5488f892
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Indexes/IX_TimeEntryHistory_ChargeCodeId_ChargeCodeDate_ModifiedDate.index.sql
@@ -0,0 +1,3 @@
+﻿CREATE NONCLUSTERED INDEX IX_TimeEntryHistory_ChargeCodeId_ChargeCodeDate_ModifiedDate
+ON [dbo].[TimeEntryHistory] ([ChargeCodeId],[ChargeCodeDate],[ModifiedDate])
+INCLUDE ([PersonId],[Note],[OldHours],[ActualHours],[IsChargeable])
