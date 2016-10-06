db15c144817ff075b27435b7febe6a2f5a68b9e2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonPasswordInsert.proc.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[PersonPasswordInsert]
+	@personId int, 
+	@encodedPassword NVARCHAR(128)
+AS
+BEGIN
+	IF EXISTS (SELECT personId FROM PersonPassword WHERE personId = @personId)
+	BEGIN
+		UPDATE PersonPassword
+		SET password = @encodedPassword
+		WHERE personId = @personId
+	END
+	ELSE
+	BEGIN
+	INSERT INTO PersonPassword
+				(personId
+				,password)
+			VALUES
+				(@personId
+				,@encodedPassword)
+	END
+END
+
