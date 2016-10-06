dc55ba07a358cbf2e3a361343065e1166866e359
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetBusinessUnitsForClients.proc.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[GetBusinessUnitsForClients]
+(
+  @AccountIds	NVARCHAR(MAX)
+)
+AS
+BEGIN
+	
+	DECLARE @AccountIdsTable TABLE ( Ids INT )
+	
+	INSERT INTO @AccountIdsTable( Ids)
+	SELECT ResultId
+	FROM dbo.ConvertStringListIntoTable(@AccountIds)
+
+	SELECT	PG.GroupId,
+			PG.Name AS GroupName,
+			PG.Code AS GroupCode,
+			C.ClientId,
+			C.Name AS ClientName,
+			C.Code AS ClientCode
+	FROM dbo.ProjectGroup PG
+	INNER JOIN dbo.Client C ON C.ClientId = PG.ClientId
+	WHERE C.ClientId IN (SELECT Ids FROM @AccountIdsTable)
+END
