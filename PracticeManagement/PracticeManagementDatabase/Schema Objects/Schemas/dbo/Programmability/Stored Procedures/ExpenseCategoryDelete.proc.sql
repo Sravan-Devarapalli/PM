0d90c34013071fc01b2b86d0ff482ad4aba7b069
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ExpenseCategoryDelete.proc.sql
@@ -0,0 +1,27 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 7-22-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	Deletes an Expense Category
+-- =============================================
+CREATE PROCEDURE [dbo].[ExpenseCategoryDelete]
+(
+	@ExpenseCategoryId   INT
+)
+AS
+	SET NOCOUNT ON
+	DECLARE @ErrorMessage NVARCHAR(2048)
+
+	IF EXISTS (SELECT 1 FROM dbo.MonthlyExpense AS e WHERE e.ExpenseCategoryId = @ExpenseCategoryId)
+	BEGIN
+		SELECT @ErrorMessage = [dbo].[GetErrorMessage](70010)
+		RAISERROR (@ErrorMessage, 16, 1)
+	END
+	ELSE
+	BEGIN
+		DELETE
+		  FROM dbo.ExpenseCategory
+		 WHERE ExpenseCategoryId = @ExpenseCategoryId
+	END
+
