33b21c3e3c1dee099bfdf96e9682edfc48dafaca
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/TimeEntry/TimeEntrySums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.TimeEntry
+{
+    [DataContract]
+    [Serializable]
+    public class TimeEntrySums
+    {
+        [DataMember]
+        public double TotalActualHours { get; set; }
+
+        [DataMember]
+        public double TotalForecastedHours { get; set; }
+    }
+}
