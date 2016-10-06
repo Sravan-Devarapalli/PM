254e9b3e354b7633756738d9415fd259da82c933
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SaveSchedularLog.proc.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE dbo.SaveSchedularLog
+(
+	@LastRun	DATETIME,
+	@Status		NVARCHAR(255),
+	@Comments	NVARCHAR(4000),
+	@NextRun	DATETIME
+)
+AS
+BEGIN
+	INSERT INTO [dbo].[SchedularLog]
+           ([LastRun]
+           ,[Status]
+           ,[Comments]
+           ,[NextRun]
+           )
+     VALUES
+           (@LastRun
+           ,@Status
+           ,@Comments
+           ,@NextRun)
+END
