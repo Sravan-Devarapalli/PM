f1fa51fd92f812b3d608c5b6c07ee2b1852505b9
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementService/IAuthService.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceModel;
+
+namespace PracticeManagementService
+{
+    [ServiceContract]
+    public interface IAuthService
+    {
+        /// <summary>
+        /// Verifies that the specified user name and password exist in the database.
+        /// </summary>
+        /// <param name="username">The name of the user to validate.</param>
+        /// <param name="password">The password for the specified user.</param>
+        /// <returns>
+        /// true if the specified username and password are valid; otherwise, false.
+        /// If the user specified does not exist in the database, the ValidateUser method returns false.
+        /// </returns>
+        [OperationContract]
+        bool ValidateUser(string username, string password);
+    }
+}
