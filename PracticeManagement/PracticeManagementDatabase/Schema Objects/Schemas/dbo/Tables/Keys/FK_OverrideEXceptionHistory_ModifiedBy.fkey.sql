48e6d74d7fed9ba052dd9c14ef53d2bd672e74af
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_OverrideEXceptionHistory_ModifiedBy.fkey.sql
@@ -0,0 +1,6 @@
+﻿ALTER TABLE [dbo].[OverrideExceptionHistory]
+	ADD CONSTRAINT [FK_OverrideEXceptionHistory_ModifiedBy] 
+	FOREIGN KEY (ModifiedBy)
+	REFERENCES dbo.Person (PersonId) ON DELETE NO ACTION ON UPDATE NO ACTION;	
+
+
