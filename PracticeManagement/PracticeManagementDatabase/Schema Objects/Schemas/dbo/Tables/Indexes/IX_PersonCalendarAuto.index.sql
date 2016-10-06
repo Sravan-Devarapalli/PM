0ee53cdd2a40ddb4d9bef274ab7ede7906286fb6
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Indexes/IX_PersonCalendarAuto.index.sql
@@ -0,0 +1,5 @@
+﻿CREATE NONCLUSTERED INDEX [IX_PersonCalendarAuto]
+    ON [dbo].[PersonCalendarAuto]([PersonId] ASC, [DayOff] ASC, [Date] ASC) WITH ( DROP_EXISTING = OFF, IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF, ONLINE = OFF, MAXDOP = 0)
+    ON [PRIMARY];
+
+
