8e02b791284e86156acf1015678a74373870e57e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/InsertTodayProjectsIntoCache.proc.sql
@@ -0,0 +1,30 @@
+﻿CREATE PROCEDURE [dbo].[InsertTodayProjectsIntoCache]
+AS
+BEGIN
+	DECLARE @InsertingTime DATETIME,
+			@ProjectId   NVARCHAR(2500)
+
+	DECLARE @Today	DATETIME
+	SELECT @Today = CONVERT(DATETIME,CONVERT(DATE,[dbo].[GettingPMTime](GETUTCDATE())))
+
+	SELECT @InsertingTime = dbo.InsertingTime()
+
+	SELECT  @ProjectId = ISNULL(@ProjectId,'') + ',' + CONVERT(VARCHAR,P.ProjectId)
+	FROM	dbo.Project AS P
+	WHERE	CONVERT(DATE,P.CreatedDate) = CONVERT(DATE,@Today)
+			AND P.StartDate IS NOT NULL AND P.EndDate IS NOT NULL
+			AND P.ProjectId NOT IN (SELECT ProjectId FROM dbo.DefaultMilestoneSetting)
+			AND P.IsAllowedToShow = 1
+
+    DELETE  PSC
+	FROM	[dbo].[ProjectSummaryCache] PSC
+	JOIN	dbo.Project AS P ON P.ProjectId = PSC.ProjectId
+	WHERE	CONVERT(DATE,P.CreatedDate) = CONVERT(DATE,@Today)
+			AND P.StartDate IS NOT NULL AND P.EndDate IS NOT NULL
+			AND P.ProjectId NOT IN (SELECT ProjectId FROM dbo.DefaultMilestoneSetting)
+			AND P.IsAllowedToShow = 1 AND CacheDate = CONVERT(DATE,@Today)
+
+	INSERT  [dbo].[ProjectSummaryCache]([ProjectId],[MonthStartDate],[MonthEndDate],RangeType,ProjectRevenue,ProjectRevenueNet,Cogs,GrossMargin,ProjectedhoursperMonth,Expense,ReimbursedExpense,ActualRevenue,ActualGrossMargin,PreviousMonthActualRevenue,PreviousMonthActualGrossMargin,IsMonthlyRecord,CreatedDate,CacheDate) 
+	EXEC dbo.FinancialsListByProjectPeriodTotal @UseActuals=1,@ProjectId = @ProjectId 
+	  
+END
