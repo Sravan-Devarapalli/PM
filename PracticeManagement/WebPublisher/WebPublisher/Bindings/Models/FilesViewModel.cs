bc815775b9d7e8fd37d573a51093a0e502ae4058
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/WebPublisher/WebPublisher/Bindings/Models/FilesViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace WebPublisher.Bindings.Models
+{
+    public class FilesViewModel : NotifyPropertyChanged
+    {
+        public ObservableCollection<FileViewModel> Files
+        {
+            get;
+            private set;
+        }
+
+        public FilesViewModel(string path) 
+        {
+            Files = new ObservableCollection<FileViewModel>();
+            if (!string.IsNullOrEmpty(path))
+            {
+                var files = Directory.GetFiles(path);
+                foreach (var file in files)
+                {
+                    Files.Add(new FileViewModel(file) { IsChecked = true });
+                }
+            }
+        }
+    }
+}
+
