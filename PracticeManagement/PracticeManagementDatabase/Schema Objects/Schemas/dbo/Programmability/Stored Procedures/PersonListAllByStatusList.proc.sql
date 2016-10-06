34e51ea5f69013a82cdd443dae89ddcd6d995050
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonListAllByStatusList.proc.sql
@@ -0,0 +1,53 @@
+﻿CREATE PROCEDURE [dbo].[PersonListAllByStatusList] 
+(
+	@PersonStatusIdsList NVARCHAR(225) = NULL,
+	@PersonId	INT = NULL
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	DECLARE @Query NVARCHAR(4000),
+			@Where NVARCHAR(4000),
+			@OrderBy NVARCHAR(100),
+			@PersonSeniorityValue INT
+
+	SELECT	@Where = '' , 
+			@OrderBy = ' ORDER BY LastName,FirstName',
+			@Query='SELECT PersonId,
+					   FirstName,
+					   LastName,
+					   IsDefaultManager,
+					   HireDate,
+					   TerminationDate,
+					   Alias,
+					   P.SeniorityId,
+					   S.Name AS SeniorityName
+				FROM dbo.Person P
+				JOIN dbo.Seniority S
+				ON P.SeniorityId = S.SeniorityId'
+	  
+	  IF(ISNULL(@PersonStatusIdsList,'')<>'')
+	  BEGIN
+		SET @Where = @Where + ' WHERE PersonStatusId IN (' + @PersonStatusIdsList + ')'
+	  END
+	  
+	  IF (ISNULL(@PersonId,'') <> '')
+	  BEGIN
+		SELECT @PersonSeniorityValue = S.SeniorityValue 
+		FROM dbo.Person P
+		JOIN dbo.Seniority S ON S.SeniorityId = P.SeniorityId
+		WHERE PersonId = @PersonId
+				
+		SET @Where = (CASE WHEN @Where='' THEN ' WHERE ' ELSE @Where+ ' AND ' END )
+						+ 'PersonId = ' + CONVERT(NVARCHAR,@PersonId) 
+						+ (CASE WHEN @PersonSeniorityValue <= 65 -- According to 2656, Managers and up should be able to see their subordinates, but not equals.
+								THEN ' OR S.SeniorityValue > '+ CONVERT(NVARCHAR,@PersonSeniorityValue)
+								ELSE '' END
+							)
+	  END
+
+	 SET @Query = @Query + @Where + @OrderBy
+	 EXEC(@Query)
+
+END
