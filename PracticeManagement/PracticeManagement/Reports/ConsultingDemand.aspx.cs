2536bdfe75ee1269f50e0c67437e719a8dd7f28e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Reports/ConsultingDemand.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.DataVisualization.Charting;
+using PraticeManagement.Controls;
+using System.Drawing;
+using PraticeManagement.Utils;
+using PraticeManagement.FilterObjects;
+using DataTransferObjects;
+
+namespace PraticeManagement.Reporting
+{
+    public partial class ConsultingDemand : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
