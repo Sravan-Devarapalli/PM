2c941bdb99d5577b16eb82341717e7b074bee474
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetVendorAttachmentData.proc.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[GetVendorAttachmentData]
+(
+	@VendorId	         INT,
+	@AttachmentId		 INT
+)
+AS
+BEGIN
+	SELECT TOP(1) va.AttachmentData 
+	FROM dbo.VendorAttachment AS va
+	WHERE va.VendorId = @VendorId
+		AND va.Id = @AttachmentId
+END
+
