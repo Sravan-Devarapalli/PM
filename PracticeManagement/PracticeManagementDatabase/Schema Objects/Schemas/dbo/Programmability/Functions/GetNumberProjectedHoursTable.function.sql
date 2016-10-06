0b0d89f17760293f0ff17e64b032b43906eed4db
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetNumberProjectedHoursTable.function.sql
@@ -0,0 +1,26 @@
+﻿CREATE FUNCTION [dbo].[GetNumberProjectedHoursTable]
+(
+      @startDate DATETIME,
+      @endDate DATETIME,
+   	  @ActiveProjects BIT = 1,
+	  @ProjectedProjects BIT = 1,
+	  @ExperimentalProjects BIT = 1,
+	  @ProposedProjects BIT =1,
+	  @InternalProjects	BIT = 1
+)
+RETURNS TABLE 
+AS
+
+RETURN 
+    SELECT  s.PersonId,SUM(s.HoursPerDay) ProjectedHours
+    FROM    dbo.v_MilestonePersonSchedule AS s
+    INNER JOIN dbo.Project AS pr ON pr.ProjectId = s.ProjectId
+    WHERE  s.MilestoneId <> (SELECT MilestoneId FROM dbo.DefaultMilestoneSetting)
+			AND s.Date BETWEEN @startDate AND @endDate AND 
+			(@ActiveProjects = 1 AND pr.ProjectStatusId = 3 OR		--  3 - Active
+			 @ProjectedProjects = 1 AND pr.ProjectStatusId = 2 OR	--  2 - Projected
+			 @ExperimentalProjects = 1 AND pr.ProjectStatusId = 5 OR	--  5 - Experimental
+			 @ProposedProjects =1 AND pr.ProjectStatusId = 7 OR --7-proposed
+			 @InternalProjects = 1 AND pr.ProjectStatusId = 6) --6 - Internal
+	GROUP BY s.PersonId 
+
