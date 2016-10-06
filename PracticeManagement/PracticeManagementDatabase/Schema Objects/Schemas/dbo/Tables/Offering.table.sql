1e3c92e6add70b0746523fbc5c9194eeba0baf9a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Offering.table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Offering]
+(
+	[OfferingId]    INT             IDENTITY (1, 1) NOT NULL,
+	[Name]          NVARCHAR (50)   NOT NULL,
+	CONSTRAINT [PK_Offering_OfferingId]	PRIMARY KEY ([OfferingId])
+)
+
