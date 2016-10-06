c988540ee63ef1b24c1eb731ce3f13ae16114978
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetChannelByID.proc.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[GetChannelByID]
+(
+	@ChannelId INT
+)
+AS
+BEGIN
+	SELECT C.ChannelId,
+		   C.ChannelName,
+		   C.IsSubChannelNamePicker
+	FROM dbo.Channel C
+	WHERE C.ChannelId=@ChannelId
+END
