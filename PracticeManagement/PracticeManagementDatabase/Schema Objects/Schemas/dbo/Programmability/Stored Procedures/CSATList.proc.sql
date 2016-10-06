869ae2481ee8ee563f68f5c2bcc464bd68a45d50
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CSATList.proc.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[CSATList]
+(
+	@ProjectId		INT = NULL
+)
+AS
+BEGIN
+
+	SELECT cs.CSATId,
+			cs.Comments,
+			cs.CompletionDate,
+			cs.ReviewStartDate,
+			cs.ReviewEndDate,
+			cs.ProjectId,
+			cs.ReferralScore,
+			cs.ReviewerId,
+			 re.LastName  + ', ' + re.FirstName AS [ReviewerName]
+	FROM dbo.ProjectCSAT cs
+	INNER JOIN dbo.Person re on cs.ReviewerId = re.PersonId
+	WHERE @ProjectId IS NULL OR cs.ProjectId = @ProjectId
+
+END
+
