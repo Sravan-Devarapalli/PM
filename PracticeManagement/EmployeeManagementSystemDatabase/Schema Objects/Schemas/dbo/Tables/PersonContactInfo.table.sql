bc4925150e64f582281d677f42992797175d1a6d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/EmployeeManagementSystemDatabase/Schema Objects/Schemas/dbo/Tables/PersonContactInfo.table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[PersonContactInfo](
+	[ContactInfoTypeId] [int] NOT NULL,
+	[PersonId] [bigint] NOT NULL,
+	[Info] [nvarchar](max) NULL,
+	[ModificationDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_PersonContactInfo] PRIMARY KEY CLUSTERED 
+(
+	[ContactInfoTypeId] ASC,
+	[PersonId] ASC
+)
+)
