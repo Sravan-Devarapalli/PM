49c18ec0e0745bffc32e825d22839d21461475d2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/DefaultMarginColorInfo.table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[DefaultMarginColorInfo]
+(
+	Id              INT IDENTITY(1,1) NOT NULL,
+	GoalTypeId		INT NOT NULL,
+	ColorId		    INT NOT NULL,
+	StartRange		INT NOT NULL,
+	EndRange		INT NOT NULL
+	CONSTRAINT PK_DefaultMarginColorInfo_Id PRIMARY KEY CLUSTERED(Id),
+    CONSTRAINT FK_DefaultMarginColorInfo_GoalTypeId FOREIGN KEY(GoalTypeId) REFERENCES  dbo.DefaultGoalType(Id),
+	CONSTRAINT FK_DefaultMarginColorInfo_ColorId FOREIGN KEY(ColorId) REFERENCES  dbo.Color([Id])
+);
+
