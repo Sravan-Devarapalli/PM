08798475940db33c606c462d339228363e317edf
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_PersonTimeEntryRecursiveSelection_TimeEntrySectionId.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[PersonTimeEntryRecursiveSelection]
+	ADD CONSTRAINT [FK_PersonTimeEntryRecursiveSelection_TimeEntrySectionId] 
+	FOREIGN KEY (TimeEntrySectionId)
+	REFERENCES TimeEntrySection (Id) ON DELETE NO ACTION ON UPDATE NO ACTION;
