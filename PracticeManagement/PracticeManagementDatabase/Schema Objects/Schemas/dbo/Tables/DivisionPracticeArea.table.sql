2a8ea4d5c05db1928b4bd1da0fba9d22cca17777
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/DivisionPracticeArea.table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[DivisionPracticeArea]
+(
+	[Division_Practice_Id]	UNIQUEIDENTIFIER,
+	[DivisionId]			INT,
+	[PracticeId]			INT
+	CONSTRAINT [PK_DivisionPracticeArea_Division_Practice_Id]	PRIMARY KEY (Division_Practice_Id)
+)
+
