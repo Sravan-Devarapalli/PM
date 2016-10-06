fb1370d9106e4f4a949f907bc8353f00858ef217
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ClientIsNoteRequiredUpdate.proc.sql
@@ -0,0 +1,24 @@
+﻿-- =============================================
+-- Author:		ThulasiRam.P
+-- Create date: 06-08-2012
+-- Description:	Update IsNoteRequired for a client.
+-- =============================================
+CREATE PROCEDURE [dbo].[ClientIsNoteRequiredUpdate]
+(
+	@ClientID INT,
+	@IsNoteRequired     BIT = 1,
+	@UserLogin          NVARCHAR(255)
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	-- Start logging session
+	EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
+
+	Update dbo.Client
+	SET IsNoteRequired = @IsNoteRequired
+	WHERE ClientId = @ClientID
+
+END
+
