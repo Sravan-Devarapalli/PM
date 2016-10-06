2eeed36b490e986a6afee1b490eccfbdebd207e6
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SaveVendorAttachment.proc.sql
@@ -0,0 +1,35 @@
+﻿CREATE PROCEDURE [dbo].[SaveVendorAttachment]
+(
+	@Id			  INT,
+    @FileName			  NVARCHAR(256),	
+	@AttachmentData	      VARBINARY(MAX),
+	@UploadedDate         DATETIME ,
+	@UserLogin            NVARCHAR(255)
+)
+AS
+	SET NOCOUNT ON
+	BEGIN
+	
+	-- Start logging session
+	EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
+
+	DECLARE @PersonId INT
+	SELECT @PersonId = PersonId FROM dbo.Person P WHERE P.alias = @UserLogin
+
+	  INSERT INTO VendorAttachment
+			   ([VendorId]
+			   ,[FileName]
+			   ,[AttachmentData]
+			   ,UploadedDate
+			   ,ModifiedBy
+			   )     
+		 VALUES
+			   (@Id
+			   ,@FileName
+			   ,@AttachmentData
+			   ,@UploadedDate
+			   ,@PersonId
+			   )
+      
+      EXEC dbo.SessionLogUnprepare
+	END
