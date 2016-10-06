cd5f4da338c191604226c1b1e50cd53f8fefcc90
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetDefaultMilestoneSetting.proc.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[GetDefaultMilestoneSetting]
+
+AS
+	SELECT TOP 1 
+			ClientId,
+			ProjectId,
+			MilestoneId,
+			ModifiedDate,
+			LowerBound,
+			UpperBound
+	FROM [DefaultMileStoneSetting]
+	ORDER BY ModifiedDate DESC
+
