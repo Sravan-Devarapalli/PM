d79cfa6c1c045fd437fa05dada5103d45960ae79
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Client.table.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Client] (
+	[ClientId]					INT	IDENTITY (1, 1) NOT NULL,
+	[Code]						NVARCHAR (5)	NOT NULL,
+	[DefaultDiscount]			DECIMAL (18, 2)	NOT NULL,
+	[DefaultTerms]				INT				NOT NULL,
+	[DefaultSalespersonID]		INT				NOT NULL,
+	[Name]						NVARCHAR (100)	NOT NULL,
+	[Inactive]					BIT				NOT NULL,
+	[IsChargeable]				BIT				NOT NULL,
+	[DefaultDirectorID]			INT,
+	[IsMarginColorInfoEnabled]	BIT				NULL,
+	[IsInternal]				BIT				NOT NULL,
+	[IsNoteRequired]            BIT				NOT NULL,
+	[IsHouseAccount]             BIT			NOT NULL CONSTRAINT DF_Client_IsHouseAccount DEFAULT 0
+);
+
