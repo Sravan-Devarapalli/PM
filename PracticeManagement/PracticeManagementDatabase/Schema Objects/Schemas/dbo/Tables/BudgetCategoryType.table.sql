659823005961bb19208d5c481f2dfce568928982
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/BudgetCategoryType.table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[BudgetCategoryType] (
+    [CategoryTypeId]   INT IDENTITY (1, 1) NOT NULL,
+    [Description]      NVARCHAR (225) NOT NULL,
+    CONSTRAINT PK_BudgetCategoryType PRIMARY KEY ([CategoryTypeId] ASC)
+);
+GO
+
