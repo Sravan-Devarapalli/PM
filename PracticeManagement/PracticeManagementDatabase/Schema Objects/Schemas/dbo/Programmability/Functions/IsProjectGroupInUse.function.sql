e37d14d99162369f0a1a7513fab278eb515fb31a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/IsProjectGroupInUse.function.sql
@@ -0,0 +1,27 @@
+﻿CREATE FUNCTION [dbo].[IsProjectGroupInUse]
+(
+	@ProjectGroupId INT,
+	@Code VARCHAR(6)
+)
+RETURNS INT
+AS
+BEGIN
+	IF(@Code = 'B0001')
+	BEGIN
+		RETURN 1
+	END
+	ELSE IF EXISTS(SELECT TOP 1 1 FROM dbo.Project WHERE GroupId = @ProjectGroupId)
+	BEGIN
+		RETURN 1
+	END
+	ELSE IF EXISTS(SELECT TOP 1 1 FROM dbo.Opportunity WHERE GroupId = @ProjectGroupId)
+	BEGIN
+		RETURN 1
+	END
+	ELSE IF EXISTS(SELECT TOP 1 1 FROM dbo.ChargeCode CC INNER JOIN dbo.TimeEntry TE on TE.ChargeCodeId = CC.Id AND cc.ProjectGroupId = @ProjectGroupId)
+	BEGIN
+		RETURN 1
+	END
+	RETURN 0
+	
+END
