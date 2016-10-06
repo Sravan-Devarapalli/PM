b0a4679b546701495bcbc53ce74e996fd4903152
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/aspnet_Profile.table.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[aspnet_Profile] (
+    [UserId]               UNIQUEIDENTIFIER NOT NULL,
+    [PropertyNames]        NTEXT            NOT NULL,
+    [PropertyValuesString] NTEXT            NOT NULL,
+    [PropertyValuesBinary] IMAGE            NOT NULL,
+    [LastUpdatedDate]      DATETIME         NOT NULL,
+    PRIMARY KEY CLUSTERED ([UserId] ASC) WITH (IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF),
+    FOREIGN KEY ([UserId]) REFERENCES [dbo].[aspnet_Users] ([UserId]) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    FOREIGN KEY ([UserId]) REFERENCES [dbo].[aspnet_Users] ([UserId]) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+
+GO
+EXECUTE sp_tableoption @TableNamePattern = N'[dbo].[aspnet_Profile]', @OptionName = N'text in row', @OptionValue = N'6000';
+
+
