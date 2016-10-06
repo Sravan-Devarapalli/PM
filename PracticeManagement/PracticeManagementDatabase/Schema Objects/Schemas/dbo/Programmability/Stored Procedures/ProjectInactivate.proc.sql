539269834e9f3bdde392f936ec423ba5ccd2e18a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectInactivate.proc.sql
@@ -0,0 +1,16 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 5-21-1008
+-- Description:	Mark a project inactive
+-- =============================================
+CREATE PROCEDURE dbo.ProjectInactivate
+(
+	@ProjectId   INT
+)
+AS
+	SET NOCOUNT ON
+
+	UPDATE dbo.Project
+	   SET ProjectStatusId = 3
+	 WHERE ProjectId = @ProjectId
+
