41dfd635bc550c9ca2ea0052f963e98742e697de
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/WebPublisher/WebPublisher/Settings/WebInstanceSection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace WebPublisher.Settings
+{
+    public class WebInstanceSection : ConfigurationSection
+    {
+        public const string SectionName = "websites";
+
+        public WebInstanceSection()
+        {
+            SectionInformation.AllowExeDefinition = ConfigurationAllowExeDefinition.MachineToLocalUser;
+        }
+
+        [ConfigurationProperty("websites")]
+        public WebInstanceElementCollection Default
+        {
+            get { return this["websites"] as WebInstanceElementCollection; }
+            set { this["websites"] = value; }
+        }
+
+        [ConfigurationProperty("msbuild_path", DefaultValue=@"C:\Windows\Microsoft.NET\Framework\v3.5\msbuild.exe")]
+        public string MSBuildPath
+        {
+            get { return this["msbuild_path"] as string; }
+            set { this["msbuild_path"] = value; }
+        }
+    }
+}
+
