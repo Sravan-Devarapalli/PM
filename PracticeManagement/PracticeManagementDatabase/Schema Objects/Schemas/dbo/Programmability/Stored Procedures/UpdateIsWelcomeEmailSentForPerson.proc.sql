7898b9c12439cb4f681b57f36cd8c4456482c5d0
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/UpdateIsWelcomeEmailSentForPerson.proc.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[UpdateIsWelcomeEmailSentForPerson] 
+	@PersonId             INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	Update Person
+	Set IsWelcomeEmailSent = 1
+	Where PersonId = @PersonId
+END
+GO
+
