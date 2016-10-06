1b4e8ac878db206c8d94fd3b5209395eabc73ef2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_MilestoneExpenses.view.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW dbo.v_MilestoneExpenses
+AS
+	SELECT pexp.MilestoneId,
+	pexp.projectid,
+		  CONVERT(DECIMAL(18,2),SUM(pexp.Amount)) Expense,
+		  CONVERT(DECIMAL(18,2),SUM(pexp.Reimbursement*0.01*pexp.Amount)) ReimbursedExpense 
+	FROM dbo.ProjectExpense as pexp 
+	GROUP BY pexp.MilestoneId,
+	pexp.projectid
