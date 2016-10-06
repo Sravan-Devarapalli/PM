ce43019bc3b50b13208a8f11466a0ade43e293a3
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ResourceType.table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE dbo.ResourceType
+(
+	[TypeId]   INT IDENTITY(1,1) NOT NULL,
+	[Description] NVARCHAR(255) NOT NULL,
+	CONSTRAINT PK_ResourceTypes_TypeId PRIMARY KEY CLUSTERED([TypeId])
+);
