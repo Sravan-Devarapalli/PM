fcd046b5ef8de9d3395355ec54679f4277f8b246
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/Keys/FK_PersonPractice_PracticeId.fkey.sql
@@ -0,0 +1,5 @@
+﻿ALTER TABLE [Skills].[PersonPractice]  WITH CHECK ADD  CONSTRAINT [FK_PersonPractice_PracticeId] FOREIGN KEY([TenantId],[PracticeId])
+REFERENCES [Skills].[Practice] ([TenantId],[PracticeId])
+ON UPDATE CASCADE
+GO
+ALTER TABLE [Skills].[PersonPractice] CHECK CONSTRAINT [FK_PersonPractice_PracticeId]
