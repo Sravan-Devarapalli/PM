bd28c68fe06e12fd467a1848456a90eb09370189
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/TimeEntryReviewStatus.table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[TimeEntryReviewStatus]
+(
+    [Id]       INT 		IDENTITY (1, 1) NOT NULL,
+    [Name]     NVARCHAR(50)	NOT NULL,
+	CONSTRAINT PK_TimeEntryReviewStatus_Id PRIMARY KEY CLUSTERED (Id ASC)
+)
+
