329f557c05d3469dc31da547d8cd5d142350ad64
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonStatusListAll.proc.sql
@@ -0,0 +1,14 @@
+﻿--The source is 
+-- =============================================
+-- Author:		Alexey Zvekov
+-- Create date: 7-1-2008
+-- Description:	List person statuses
+-- =============================================
+CREATE PROCEDURE [dbo].[PersonStatusListAll]
+AS
+	SET NOCOUNT ON
+	
+	SELECT s.PersonStatusId, s.Name
+	  FROM dbo.PersonStatus AS s
+	  WHERE s.IsPersonStatus = 1
+	ORDER BY s.PersonStatusId
