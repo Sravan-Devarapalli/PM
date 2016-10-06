f395ad0b79294e1e5f24d6427b67146e40718e27
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ExpensesClone.proc.sql
@@ -0,0 +1,21 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2010-07-23
+-- =============================================
+CREATE PROCEDURE ExpensesClone
+	@OldMilestoneId INT, 
+	@NewMilestoneId INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	--INSERT INTO [dbo].[ProjectExpense]
+	--		   ([Name]
+	--		   ,[Amount]
+	--		   ,[Reimbursement]
+	--		   ,[MilestoneId])
+	-- select Name, Amount, Reimbursement, @NewMilestoneId
+	-- from ProjectExpense
+	-- where MilestoneId = @OldMilestoneId
+END
+
