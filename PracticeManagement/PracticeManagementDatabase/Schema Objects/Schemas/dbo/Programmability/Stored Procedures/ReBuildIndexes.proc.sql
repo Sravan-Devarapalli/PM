a43014cd2fe5762b1a104b6b614b470a7bb450ca
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ReBuildIndexes.proc.sql
@@ -0,0 +1,45 @@
+﻿CREATE PROCEDURE [dbo].[ReBuildIndexes]
+AS
+BEGIN
+ 
+		EXEC sp_updatestats
+ 
+		DECLARE @TableName varchar(255) 
+		DECLARE TableCursor CURSOR FOR 
+		SELECT TABLE_NAME FROM information_schema.tables
+		WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA != 'DataSync' AND TABLE_SCHEMA = 'dbo' 
+		AND TABLE_NAME IN ('TimeEntryHistory',
+							'OpportunityPersons',
+							'Calendar',
+							'PersonCalendar',
+							'PersonCalendarAuto',
+							'MilestonePersonEntry',
+							'PersonHistory',
+							'UserActivityLog',
+							'WorkinHoursByYear',
+							'Project',
+							'PracticeCapabilities',
+							'TimeType',
+							'MilestonePerson',
+							'ChargeCode',
+							'ChargeCodeTurnOffHistory',
+							'PersonTimeEntryRecursiveSelection',
+							'ProjectTimeType',
+							'TimeEntry',
+							'TimeEntryHours')
+
+
+		OPEN TableCursor 
+		FETCH NEXT FROM TableCursor INTO @TableName 
+		WHILE @@FETCH_STATUS = 0 
+		BEGIN 
+		exec('ALTER INDEX ALL ON ' + @TableName + ' REBUILD')
+		FETCH NEXT FROM TableCursor INTO @TableName 
+		print @TableName
+		END 
+
+		CLOSE TableCursor 
+		DEALLOCATE TableCursor
+
+END
+
