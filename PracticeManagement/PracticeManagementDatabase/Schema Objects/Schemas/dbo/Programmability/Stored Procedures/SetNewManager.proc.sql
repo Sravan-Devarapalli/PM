b6d29fdfd0ddfb0fc5bfa8475a9fa2ed0d451a6e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SetNewManager.proc.sql
@@ -0,0 +1,17 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2010-01-18
+-- Description:	Sets new manager
+-- =============================================
+CREATE PROCEDURE dbo.SetNewManager 
+	@OldManagerId INT, 
+	@NewManagerId INT 
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	UPDATE dbo.Person
+	SET ManagerId = @NewManagerId
+	WHERE ManagerId = @OldManagerId
+END
+
