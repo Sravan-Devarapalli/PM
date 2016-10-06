ec5d12a46ecba5e4d894f52b64dca45e1f9f6811
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetTimeOffSeriesPeriod.proc.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[GetTimeOffSeriesPeriod]
+(
+	@PersonId INT, 
+	@Date	DATETIME
+)
+AS
+BEGIN
+
+DECLARE @SeriesID BIGINT 
+
+SELECT @SeriesID = PC.SeriesId 
+FROM  dbo.PersonCalendar AS PC
+WHERE PC.Date = @Date AND PC.PersonId = @PersonId
+
+        SELECT MIN(PC.Date) 'StartDate',  MAX(PC.Date) 'EndDate', 
+		PC.ApprovedBy 'ApprovedBy', p.FirstName + ', ' + p.LastName 'ApprovedByName'
+		FROM dbo.PersonCalendar AS PC
+		INNER JOIN dbo.Person AS P ON P.PersonId = Pc.PersonId
+		WHERE PC.SeriesId = @SeriesID
+		GROUP BY Pc.SeriesId, p.FirstName , p.LastName ,PC.ApprovedBy
+
+END
+
