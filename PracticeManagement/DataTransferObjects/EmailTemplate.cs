76bf2dbfb7c818ccf78a864e94f680011543b554
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/EmailTemplate.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class EmailTemplate
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets an ID of the email template.
+        /// </summary>
+        [DataMember]
+        public int? Id
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the email template.
+        /// </summary>
+        [DataMember]
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string EncodedName
+        {
+            get
+            {
+                return HttpUtility.HtmlEncode(Name);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the To address of the email template.
+        /// </summary>
+        [DataMember]
+        public string EmailTemplateTo
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the Cc address of the email template.
+        /// </summary>
+        [DataMember]
+        public string EmailTemplateCc
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the subject of the email template.
+        /// </summary>
+        [DataMember]
+        public string Subject
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the body of the email template.
+        /// </summary>
+        [DataMember]
+        public string Body
+        {
+            get;
+            set;
+        }
+
+        #endregion Properties
+    }
+}
