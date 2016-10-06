cc4ae354a3fc73d65ce7787862b9cfecfa9b9a68
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/IsBusinessGroupInUse.function.sql
@@ -0,0 +1,19 @@
+﻿CREATE FUNCTION [dbo].[IsBusinessGroupInUse]
+(
+	@BusinessGroupId INT,
+	@Code VARCHAR(6)
+)
+RETURNS BIT
+AS
+BEGIN
+	IF(@Code = 'BG0001')
+	BEGIN
+		RETURN 1
+	END
+	IF EXISTS(SELECT 1 FROM dbo.ProjectGroup WHERE BusinessGroupId = @BusinessGroupId)
+	BEGIN
+		RETURN 1
+	END
+		RETURN 0
+END
+
