c813715ef86d5a32fb3ed1385e99f84107704a3a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TitleDelete.proc.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[TitleDelete]
+(
+	@TitleId		INT,
+	@UserLogin  NVARCHAR(255) 
+)
+AS
+BEGIN
+	EXEC SessionLogPrepare @UserLogin = @UserLogin
+
+	DELETE dbo.Title
+	WHERE TitleId = @TitleId
+
+	EXEC SessionLogUnprepare 
+END
