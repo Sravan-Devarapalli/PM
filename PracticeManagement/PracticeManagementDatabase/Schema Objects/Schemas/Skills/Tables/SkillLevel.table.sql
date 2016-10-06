83680beccd3bf60684cc6533b0286efc79ab77ae
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/SkillLevel.table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [Skills].[SkillLevel](
+	[SkillLevelId]	[int] NOT NULL IDENTITY(1,1),
+	[Description]	[nvarchar](max) NULL,
+	[Definition]	[nvarchar](max) NULL,
+	[DisplayOrder]	[tinyint] NULL,
+	[IsActive]		[bit] NOT NULL CONSTRAINT DF_SkillLevel_IsActive DEFAULT(1),
+	[IsDeleted]		[bit] NOT NULL CONSTRAINT DF_SkillLevel_IsDeleted DEFAULT(0),
+ CONSTRAINT [PK_SkillLevel] PRIMARY KEY CLUSTERED 
+(
+	[SkillLevelId] ASC
+)
+)
