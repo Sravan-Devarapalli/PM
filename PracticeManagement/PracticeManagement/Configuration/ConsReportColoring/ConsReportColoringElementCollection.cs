8d727c3b482ccdc9c6147229ca5646f478140ddf
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Configuration/ConsReportColoring/ConsReportColoringElementCollection.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+
+namespace PraticeManagement.Configuration.ConsReportColoring
+{
+    public class ConsReportColoringElementCollection : ConfigurationElementCollection
+    {
+        public ConsReportColoringElement this[int index]
+        {
+            get
+            {
+                return BaseGet(index) as ConsReportColoringElement;
+            }
+        }
+
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new ConsReportColoringElement();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((ConsReportColoringElement)element).Title;
+        }
+    }
+}
