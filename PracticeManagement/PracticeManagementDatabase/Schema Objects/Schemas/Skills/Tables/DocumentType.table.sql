8e3d0716e197ce80429ccb0569f93312a5087f40
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/DocumentType.table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [Skills].[DocumentType](
+	[DocumentTypeId] [int] NOT NULL IDENTITY(1,1),
+	[Description] [nvarchar](max) NULL,
+	[DisplayOrder] [tinyint] NULL,
+	[IsActive]		[bit] NOT NULL CONSTRAINT DF_DocumentType_IsActive DEFAULT(1),
+	[IsDeleted]		[bit] NOT NULL CONSTRAINT DF_DocumentType_IsDeleted DEFAULT(0),
+ CONSTRAINT [PK_DocumentType] PRIMARY KEY CLUSTERED 
+(
+	[DocumentTypeId] ASC
+)
+)
