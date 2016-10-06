25043a522a68893ad0fe8e1b74c737f716ffcabf
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ProjectDivision.table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ProjectDivision]
+(
+	[DivisionId]    INT             IDENTITY (1, 1) NOT NULL,
+	[DivisionName]  NVARCHAR (50)   NOT NULL,
+	[IsExternal]    BIT             NOT NULL
+	CONSTRAINT [PK_ProjectDivision_DivisionId]	PRIMARY KEY (DivisionId)
+)
+
