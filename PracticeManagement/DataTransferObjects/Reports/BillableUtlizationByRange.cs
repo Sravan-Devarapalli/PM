99cdd299fee03565d8744203d3c5551c7cc04e45
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Reports/BillableUtlizationByRange.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Reports
+{
+    [DataContract]
+    [Serializable]
+    public class BillableUtlizationByRange
+    {
+        [DataMember]
+        public DateTime StartDate { get; set; }
+
+        [DataMember]
+        public DateTime EndDate { get; set; }
+
+        [DataMember]
+        public double BillableUtilization { get; set; }
+
+        [DataMember]
+        public double BillableHours { get; set; }
+
+        [DataMember]
+        public int AvailableHours { get; set; }
+
+        [DataMember]
+        public string RangeType
+        {
+            get;
+            set;
+        }
+    }
+}
