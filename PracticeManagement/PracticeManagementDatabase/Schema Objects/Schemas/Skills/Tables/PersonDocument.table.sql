4185e0548310c7d20bd95364dd4d654abe320463
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/PersonDocument.table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [Skills].[PersonDocument](
+	[DocumentTypeId] [int] NOT NULL,
+	[PersonId] [int] NOT NULL,
+	[Url] [nvarchar](max) NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_PersonDocument] PRIMARY KEY CLUSTERED 
+(
+	[DocumentTypeId] ASC,
+	[PersonId] ASC
+)
+)
