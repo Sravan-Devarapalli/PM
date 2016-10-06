5bb99fdbc7a4bbcacb3ea835026385d614f6f6d3
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/RecruiterCommissionDelete.proc.sql
@@ -0,0 +1,22 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 5-21-2008
+-- Updated by:	Anatoliy Lokshin
+-- Update date:	12-01-2008
+-- Description:	Deletes a recruiter commission for the given recruit
+-- =============================================
+CREATE PROCEDURE [dbo].[RecruiterCommissionDelete]
+(
+	@RecruitId            INT,
+	@RecruiterId          INT,
+	@OLD_HoursToCollect   INT
+)
+AS
+	SET NOCOUNT ON
+
+	DELETE
+	  FROM dbo.RecruiterCommission
+	 WHERE RecruitId = @RecruitId
+	   AND RecruiterId = @RecruiterId
+	   AND HoursToCollect = @OLD_HoursToCollect
+
