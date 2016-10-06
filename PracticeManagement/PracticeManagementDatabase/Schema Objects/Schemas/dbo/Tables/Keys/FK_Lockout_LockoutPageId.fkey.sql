c6e5b44e51b6a251c0c156e621f4b2d1df90ecb0
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_Lockout_LockoutPageId.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[Lockout]
+	ADD CONSTRAINT [FK_Lockout_LockoutPageId] 
+	FOREIGN KEY (LockoutPageId)
+	REFERENCES LockoutPages (LockoutPageId)	ON DELETE NO ACTION ON UPDATE NO ACTION;
