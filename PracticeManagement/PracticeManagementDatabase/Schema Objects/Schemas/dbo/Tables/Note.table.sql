40e80940667e19cfcc30c957ef16b142e14294d1
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Note.table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Note] (
+    [NoteId]       INT            IDENTITY (1, 1) NOT NULL,
+    [TargetId]     INT            NOT NULL,
+    [PersonId]     INT            NOT NULL,
+    [CreateDate]   DATETIME       NOT NULL,
+    [NoteText]     NVARCHAR (MAX) NOT NULL,
+    [NoteTargetId] INT            NOT NULL
+);
+
+
