d545e2b32ce84105c06ef012f5a82b229b92d17b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_PersonCalendarAuto_Date.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[PersonCalendarAuto]
+    ADD CONSTRAINT [FK_PersonCalendarAuto_Date] FOREIGN KEY ([Date]) REFERENCES [dbo].[Calendar] ([Date]) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+
