7def03cb2cca6b3e27f225360d6d6d3f93c76ffb
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OpportunityTransitionInsert.proc.sql
@@ -0,0 +1,45 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 11-04-2008
+-- Updated by:	Anatoliy Lokshin
+-- Update date: 11-11-2008
+-- Description:	Insert a new opportunity transition.
+-- =============================================
+CREATE PROCEDURE [dbo].[OpportunityTransitionInsert]
+(
+	@OpportunityId                   INT,
+	@OpportunityTransitionStatusId   INT,
+	@PersonId                        INT,
+	@NoteText                        NVARCHAR(2000),
+	@TargetPersonId					 INT = NULL,
+	@OpportunityTransitionId		 INT = NULL OUTPUT
+)
+AS
+	SET NOCOUNT ON
+	
+	DECLARE @UserLogin NVARCHAR(255)
+	SELECT @UserLogin = p.Alias
+	FROM dbo.Person p 
+	WHERE p.PersonId = @PersonId
+	
+	-- Start logging session
+	EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
+
+	INSERT INTO dbo.OpportunityTransition
+	            (OpportunityId, OpportunityTransitionStatusId, PersonId, NoteText, TargetPersonId)
+	     VALUES (@OpportunityId, @OpportunityTransitionStatusId, @PersonId, @NoteText, @TargetPersonId)
+
+	IF @OpportunityTransitionStatusId = 7 -- Lost
+	BEGIN
+		-- Change the opportunity status
+		UPDATE dbo.Opportunity
+		   SET OpportunityStatusId = 2
+		 WHERE OpportunityId = @OpportunityId
+	END
+
+	SET @OpportunityTransitionId = SCOPE_IDENTITY()
+	
+	
+	-- End logging session
+	EXEC dbo.SessionLogUnprepare
+
