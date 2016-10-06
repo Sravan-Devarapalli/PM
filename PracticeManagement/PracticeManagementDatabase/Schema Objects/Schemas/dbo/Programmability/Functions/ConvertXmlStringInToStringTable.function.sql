284a59880aac78307d8d7da9e7e76cca0513fa78
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/ConvertXmlStringInToStringTable.function.sql
@@ -0,0 +1,29 @@
+﻿-- =========================================================================
+-- Author:		Sainath.CH
+-- Create date: 04-27-2012
+--@OrderList = 
+--<names>
+--		<name> value1 </name>
+--		<name> value2 </name>
+--		<name> value3 </name>
+--	</names>
+-- =========================================================================
+CREATE FUNCTION [dbo].[ConvertXmlStringInToStringTable] ( @OrderList XML )
+RETURNS @ParsedList TABLE
+    (
+      ResultString NVARCHAR(1024)
+    )
+AS 
+    BEGIN
+        SET ANSI_NULLS ON;
+        IF ( @OrderList IS NOT NULL ) 
+            BEGIN
+                INSERT  INTO @ParsedList
+                        ( ResultString
+                        )
+                        SELECT  t.c.value('.[1]', 'NVARCHAR(1024)') AS ResultString
+                        FROM    @OrderList.nodes('Names/Name') AS t ( c )
+            END
+        RETURN
+    END
+
