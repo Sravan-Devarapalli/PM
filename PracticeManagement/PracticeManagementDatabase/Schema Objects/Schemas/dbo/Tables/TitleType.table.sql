b74ad6c87284a9987cf92c2a19c59ce7229fba6e
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/TitleType.table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[TitleType]
+(
+	TitleTypeId INT	NOT NULL,
+	TitleType NVARCHAR(100) NULL,
+	SortOrder		INT NOT NULL CONSTRAINT DF_TitleType_SortOrder DEFAULT(0),
+	CONSTRAINT PK_TitleType_TitleTypeId     PRIMARY KEY CLUSTERED(TitleTypeId)
+)
+
