cd4c017179317cda957eb4af5e565a8be9b7e537
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/UserTemporaryCredentials.table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[UserTemporaryCredentials]
+(
+	[UserName]                               NVARCHAR (256)	  NOT NULL,
+	[Password]                               NVARCHAR (128)   NOT NULL,
+	[PasswordFormat]                         INT              DEFAULT ((0)) NOT NULL,
+	[PasswordSalt]                           NVARCHAR (128)   NOT NULL,
+	[CreatedDate]							 DATETIME		  NOT NULL,
+	CONSTRAINT PK_UserTemporaryCredentials PRIMARY KEY  CLUSTERED ([UserName])
+)
+
