6325d3abfd24bc82680be5bbc10d4a94c49075dc
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetAdministrativeChargeCodeValues.proc.sql
@@ -0,0 +1,21 @@
+﻿-- =============================================
+-- Author:		ThulasiRam.P
+-- Create date: 2012-02-10
+-- Description:	Gets Administrative ChargeCodeValues for AdministrativeTimeType 
+-- =============================================
+CREATE PROCEDURE dbo.GetAdministrativeChargeCodeValues
+(
+  @TimeTypeId INT
+)
+AS
+BEGIN
+
+	SELECT TOP(1)	CC.ClientId AS 'ClientId', 
+			CC.ProjectGroupId AS 'GroupId', 
+			CC.ProjectId AS 'ProjectId' 
+	FROM dbo.ChargeCode CC
+	WHERE CC.TimeTypeId = @TimeTypeId
+
+	
+END
+
