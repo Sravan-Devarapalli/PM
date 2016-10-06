969546005e78b7b0edd67c728a944f2dd76a0da3
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/DefaultCommission.table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[DefaultCommission] (
+    [PersonId]         INT             NOT NULL,
+    [StartDate]        SMALLDATETIME   NOT NULL,
+    [EndDate]          SMALLDATETIME   NOT NULL,
+    [FractionOfMargin] DECIMAL (18, 2) NOT NULL,
+    [type]             INT             NOT NULL,
+    [MarginTypeId]     INT             NULL
+);
+
+
