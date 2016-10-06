1ea460d90b105cacc47b2f95a93f790c641d88fa
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Employment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// Data Transfer Object for a Person entity
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class Employment
+    {
+        [DataMember]
+        public int PersonId { get; set; }
+
+        [DataMember]
+        public DateTime HireDate { get; set; }
+
+        [DataMember]
+        public DateTime? TerminationDate { get; set; }
+
+        [DataMember]
+        public int? TerminationReasonId { get; set; }
+    }
+}
