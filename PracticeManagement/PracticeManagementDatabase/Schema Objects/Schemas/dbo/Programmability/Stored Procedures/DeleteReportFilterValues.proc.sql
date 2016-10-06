a8e361db81f7d274a54479994f127b91094ffd94
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DeleteReportFilterValues.proc.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[DeleteReportFilterValues]
+	@CurrentUserId INT,
+	@PreviousUserId INT,
+	@SessionId     NVARCHAR (MAX)
+AS
+	BEGIN
+
+	DELETE FROM ReportFilterValues 
+		   WHERE CurrentUserId=@CurrentUserId AND PreviousUserId=@PreviousUserId AND SessionId=@SessionId
+	END
+
