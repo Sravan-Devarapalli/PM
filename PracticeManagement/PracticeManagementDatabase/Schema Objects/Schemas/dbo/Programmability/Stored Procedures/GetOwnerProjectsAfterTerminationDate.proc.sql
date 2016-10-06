4adc81efe32ba298474534d067a8b5e0e5bd0439
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetOwnerProjectsAfterTerminationDate.proc.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[GetOwnerProjectsAfterTerminationDate]
+(
+	@PersonId	INT,
+	@TerminationDate	DATETIME 
+	
+)
+AS
+BEGIN 
+	--Returns the active project if the given personid is project Owner.
+
+	SELECT proj.ProjectId,
+		   proj.Name ProjectName,
+		   proj.ProjectNumber
+	FROM dbo.Project AS proj 
+	WHERE proj.ProjectManagerId = @PersonId AND proj.ProjectStatusId =3 --Active Status
+	ORDER BY proj.ProjectNumber
+	
+END
