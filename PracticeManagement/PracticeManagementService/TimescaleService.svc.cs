a31570f6bf24fea1ed216cbe26e1129119282d96
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementService/TimescaleService.svc.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel.Activation;
+using DataAccess;
+using DataTransferObjects;
+
+namespace PracticeManagementService
+{
+    // NOTE: If you change the class name "TimescaleService" here, you must also update the reference to "TimescaleService" in Web.config.
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class TimescaleService : ITimescaleService
+    {
+        #region ITimescaleService Members
+
+        /// <summary>
+        /// Retrives a <see cref="Timescale"/> by its ID.
+        /// </summary>
+        /// <param name="timescaleId">An ID of the <see cref="Timescale"/> to be retrieved.</param>
+        /// <returns>A <see cref="Timescale"/> object if found and null otherwise.</returns>
+        public Timescale GetById(TimescaleType timescaleId)
+        {
+            return TimescaleDAL.GetById(timescaleId);
+        }
+
+        /// <summary>
+        /// Retrives all the timescale types.
+        /// </summary>
+        /// <returns></returns>
+        public List<Timescale> GetAll()
+        {
+            return TimescaleDAL.GetAll();
+        }
+
+        #endregion ITimescaleService Members
+    }
+}
