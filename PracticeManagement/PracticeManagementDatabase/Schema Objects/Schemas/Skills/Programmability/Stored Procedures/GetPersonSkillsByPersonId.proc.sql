eb6207954817b2efe0c871a037761910bd962120
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Programmability/Stored Procedures/GetPersonSkillsByPersonId.proc.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [Skills].[GetPersonSkillsByPersonId]
+	@PersonId INT
+AS
+BEGIN
+
+	SELECT PS.SkillId
+			,PS.SkillLevelId
+			,PS.YearsExperience
+			,PS.LastUsed
+			,sc.SkillCategoryId
+			,st.SkillTypeId 
+	FROM [Skills].[PersonSkill] PS
+	JOIN Skills.Skill s ON PS.SkillId = s.SkillId
+	JOIN Skills.SkillCategory sc ON sc.SkillCategoryId = s.SkillCategoryId
+	JOIN Skills.SkillType st ON st.SkillTypeId = sc.SkillTypeId
+	WHERE PS.PersonId = @PersonId 
+			AND s.IsActive = 1
+			AND s.IsDeleted = 0
+
+END
