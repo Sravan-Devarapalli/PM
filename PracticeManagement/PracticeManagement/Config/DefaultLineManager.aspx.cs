a1faa8e8fed04f3c5be5d8c4055515da84bc8aeb
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Config/DefaultLineManager.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using PraticeManagement.Controls;
+using System.Web.UI;
+
+namespace PraticeManagement.Config
+{
+    public partial class DefaultLineManager : PracticeManagementPageBase, IPostBackEventHandler
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected override void Display()
+        {
+        }
+
+        public new void ClearDirty()
+        {
+            base.ClearDirty();
+        }
+
+        #region RaisePostBackEventHandler
+        
+        public void RaisePostBackEvent(string eventArgument)
+        {
+            if (defaultManager.SavedNewDefaultManager())
+            {
+                Redirect(eventArgument);
+            }
+        }
+        
+        #endregion
+    }
+}
