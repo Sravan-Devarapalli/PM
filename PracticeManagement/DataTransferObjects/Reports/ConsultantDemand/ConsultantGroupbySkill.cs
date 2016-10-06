f9b16a377592012743126c7c995fc6cd9471b03b
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Reports/ConsultantDemand/ConsultantGroupbySkill.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace DataTransferObjects.Reports.ConsultingDemand
+{
+    [DataContract]
+    [Serializable]
+    public class ConsultantGroupbySkill
+    {
+        [DataMember]
+        public string Skill { get; set; }
+
+        public string HtmlEncodedSkill
+        {
+            get
+            {
+                return HttpUtility.HtmlEncode(Skill);
+            }
+        }
+
+        [DataMember]
+        public List<ConsultantDemandDetailsByMonthBySkill> ConsultantDetails { get; set; }
+    }
+}
