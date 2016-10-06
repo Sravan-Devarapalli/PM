afe0bcaab0881bc17d40fb99adc8802865edbdb6
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetOpportunityPriorities.proc.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[GetOpportunityPriorities]
+@IsInserted BIT
+AS
+BEGIN
+	SET NOCOUNT ON
+	SELECT  	OP.Id,
+			OP.Priority,
+			OP.Description,
+			OP.DisplayName,
+			CASE 
+			WHEN EXISTS(SELECT TOP 1 o.PriorityId FROM  v_Opportunity AS o WHERE o.PriorityId = op.Id)
+				THEN CAST(1 AS BIT)
+			ELSE CAST(0 AS BIT)
+		END AS 'InUse'
+	FROM dbo.OpportunityPriorities AS OP
+	WHERE  op.IsInserted = @IsInserted
+	ORDER BY OP.sortOrder
+END
+
+GO
