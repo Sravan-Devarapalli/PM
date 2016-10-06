5aa65de9d45cc43fbc489ed2c55cf2e501dfe09f
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/PricingList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class PricingList
+    {
+        public static string DefaultPricingListName
+        {
+            get { return Constants.GroupNames.DefaultPricingListName; }
+        }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public int? PricingListId { get; set; }
+
+        [DataMember]
+        public int ClientId { get; set; }
+
+        [DataMember]
+        public bool InUse { get; set; }
+
+        [DataMember]
+        public bool IsActive { get; set; }
+
+        [DataMember]
+        public bool IsDefault { get; set; }
+
+        public string HtmlEncodedName
+        {
+            get
+            {
+                return HttpUtility.HtmlEncode(Name);
+            }
+        }
+    }
+}
