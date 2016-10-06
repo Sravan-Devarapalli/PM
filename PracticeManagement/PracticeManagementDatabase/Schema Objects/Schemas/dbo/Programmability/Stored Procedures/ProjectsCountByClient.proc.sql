70cd6d9fafbf8d2977d45405a4d6fcbc0605deef
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectsCountByClient.proc.sql
@@ -0,0 +1,14 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2010-02-22
+-- Description:	Counts number of projects for given client
+-- =============================================
+CREATE PROCEDURE dbo.ProjectsCountByClient
+	@ClientId INT
+AS
+BEGIN
+		SELECT COUNT(*)
+		  FROM dbo.v_Project AS p
+		 WHERE p.ClientId = @ClientId
+END
+
