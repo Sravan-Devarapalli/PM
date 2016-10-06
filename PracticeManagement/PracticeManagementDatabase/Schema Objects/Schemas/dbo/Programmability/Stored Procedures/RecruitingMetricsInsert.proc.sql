c80c871ce18b2cfaed31c7430ab296c4d7c2380c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/RecruitingMetricsInsert.proc.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[RecruitingMetricsInsert]
+(
+	@Name						NVARCHAR(50),
+	@RecruitingMetricsTypeId	INT,
+	@SortOrder					INT
+)
+AS
+BEGIN
+
+	INSERT INTO RecruitingMetrics(Name,RecruitingMetricsTypeId,SortOrder)
+	VALUES (@Name,@RecruitingMetricsTypeId,@SortOrder)
+
+END
