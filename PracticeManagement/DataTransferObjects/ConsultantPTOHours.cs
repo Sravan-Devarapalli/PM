1aca077f3a22c9286028a5a830e4b933f71e22e2
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/ConsultantPTOHours.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Reports
+{
+    [DataContract]
+    [Serializable]
+    public class ConsultantPTOHours
+    {
+
+
+        [DataMember]
+        public Person Person
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public SortedList<DateTime, double> TimeOffDates
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Dictionary<DateTime, string> CompanyHolidayDates
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int PersonVacationDays
+        {
+            get;
+            set;
+        }
+
+        public double TotalPTOHours
+        {
+            get
+            {
+                double hours = 0;
+                foreach (var t in TimeOffDates)
+                {
+                    hours += t.Value;
+                }
+                return hours;
+            }
+        }
+    }
+}
+
+
