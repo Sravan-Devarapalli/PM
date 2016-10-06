c9f66fb1195facc30fc0d8df6ee2b97051b330c7
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/WebPublisher/WebPublisher/Settings/ScriptSection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace WebPublisher.Settings
+{
+    public class ScriptSection: ConfigurationSection
+    {
+        public const string SectionName = "sqlscripts";
+
+        public ScriptSection()
+        {
+            SectionInformation.AllowExeDefinition = ConfigurationAllowExeDefinition.MachineToLocalUser;
+        }
+
+        [ConfigurationProperty("scripts")]
+        public ScriptElementCollection Scripts
+        {
+            get { return this["scripts"] as ScriptElementCollection; }
+            set { this["scripts"] = value; }
+        }
+    }
+}
+
