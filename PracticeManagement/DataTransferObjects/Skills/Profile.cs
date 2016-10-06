f8929b5fe1198377628b72defbee53b4d0eb3c18
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Skills/Profile.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Skills
+{
+    [DataContract]
+    [Serializable]
+    public class Profile
+    {
+        [DataMember]
+        public int? ProfileId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string ProfileName
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string ProfileUrl
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int ModifiedBy
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string ModifiedByName
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime ModifiedDate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public bool IsDefault
+        {
+            get;
+            set;
+        }
+    }
+}
