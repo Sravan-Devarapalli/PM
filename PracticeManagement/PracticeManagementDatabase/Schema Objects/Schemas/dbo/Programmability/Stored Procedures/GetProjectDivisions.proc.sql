27b06aa8cabfb7330b094ed8b7bf159368f2f46a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetProjectDivisions.proc.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[GetProjectDivisions]
+AS
+BEGIN
+	SELECT pd.DivisionId,
+		   pd.DivisionName,
+		   pd.IsExternal
+		   FROM dbo.ProjectDivision pd
+		   ORDER BY pd.DivisionName
+END 
