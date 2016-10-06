833510a7c8b5445397f0238031aedb5c7455f79a
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/PracticeCapability.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class PracticeCapability
+    {
+        [DataMember]
+        public int CapabilityId { get; set; }
+
+        [DataMember]
+        public int PracticeId { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public bool InUse { get; set; }
+
+        [DataMember]
+        public bool IsActive { get; set; }
+
+        public string HtmlEncodedName
+        {
+            get
+            {
+                return HttpUtility.HtmlEncode(Name);
+            }
+        }
+
+        [DataMember]
+        public string PracticeAbbreviation { get; set; }
+
+        public string MergedName
+        {
+            get
+            {
+                return PracticeAbbreviation + " - " + Name;
+            }
+        }
+    }
+}
