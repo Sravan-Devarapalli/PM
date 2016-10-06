5b96c98202cd3cc0a881c6947bf95022e3012215
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/PK_OverheadFixedRateTimescale.pkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[OverheadFixedRateTimescale]
+    ADD CONSTRAINT [PK_OverheadFixedRateTimescale] PRIMARY KEY CLUSTERED ([OverheadFixedRateId] ASC, [TimescaleId] ASC) WITH (IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF);
+
+
