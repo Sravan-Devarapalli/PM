ffa3c0bccce6a4b24af8e82ff42aefd74876a77d
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_TimeTrack_PersonId.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[TimeTrack]
+	ADD CONSTRAINT [FK_TimeTrack_PersonId] 
+	FOREIGN KEY ([PersonId]) 
+	REFERENCES [dbo].[Person] ([PersonId]) ON DELETE NO ACTION ON UPDATE NO ACTION;
