c93433948726236be8672a79e6e602cf0297baee
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagement/Controls/Generic/SelectCutOff/SelectCutOffExtender.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.UI;
+using AjaxControlToolkit;
+
+[assembly: WebResource("PraticeManagement.Controls.Generic.SelectCutOff.SelectCutOffExtender.js", "text/javascript")]
+
+namespace PraticeManagement.Controls.Generic.SelectCutOff
+{
+    using System.ComponentModel;
+
+    /// <summary>
+    /// Extender that fixes width of select element in IE.
+    /// </summary>
+    [ClientScriptResource("PraticeManagement.Controls.Generic.SelectCutOff.SelectCutOffBehavior", "PraticeManagement.Controls.Generic.SelectCutOff.SelectCutOffExtender.js")]
+    [TargetControlType(typeof(Control))]
+    public class SelectCutOffExtender : ExtenderControlBase
+    {
+        [ExtenderControlProperty]
+        [DefaultValue("")]
+        [ClientPropertyName("normalCssClass")]
+        public string NormalCssClass
+        {
+            get { return GetPropertyValue("normalCssClass", string.Empty); }
+            set { SetPropertyValue("normalCssClass", value); }
+        }
+
+        [ExtenderControlProperty]
+        [DefaultValue("")]
+        [ClientPropertyName("extendedCssClass")]
+        public string ExtendedCssClass
+        {
+            get { return GetPropertyValue("extendedCssClass", string.Empty); }
+            set { SetPropertyValue("extendedCssClass", value); }
+        }
+    }
+}
