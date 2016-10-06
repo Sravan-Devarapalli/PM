4d25cd71975a2201391acabec421951d1b351384
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/Employer.table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [Skills].[Employer](
+	[EmployerId] [int] NOT NULL IDENTITY(1,1),
+	[Description] [nvarchar](max) NULL,
+	[DisplayOrder] [tinyint] NULL,
+	[IsActive]		[bit] NOT NULL CONSTRAINT DF_Employer_IsActive DEFAULT(1),
+	[IsDeleted]		[bit] NOT NULL CONSTRAINT DF_Employer_IsDeleted DEFAULT(0),
+ CONSTRAINT [PK_Employer] PRIMARY KEY CLUSTERED 
+(
+	[EmployerId] ASC
+)
+)
