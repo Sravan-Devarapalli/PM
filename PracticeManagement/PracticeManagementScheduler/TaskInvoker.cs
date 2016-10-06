f024d3ac19225cf73782d48de223cd9855340c30
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementScheduler/TaskInvoker.cs
@@ -0,0 +1,22 @@
+﻿using DataTransferObjects.ContextObjects;
+using PracticeManagementScheduler.Settings;
+
+namespace PracticeManagementScheduler
+{
+    public class TaskInvoker
+    {
+        public static void Invoke(TaskElement task)
+        {
+            var emailContext = new EmailContext
+                                   {
+                                       EmailTemplateId = task.TemplateId,
+                                       StorerProcedureName = task.StoredProcName
+                                   };
+
+            var emailData = Utils.GetEmailData(emailContext);
+
+            var emails = Utils.PrepareEmails(emailData);
+        }
+    }
+}
+
