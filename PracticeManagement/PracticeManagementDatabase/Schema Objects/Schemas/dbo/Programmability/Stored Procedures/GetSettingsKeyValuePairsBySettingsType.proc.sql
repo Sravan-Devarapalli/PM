e13d51d16152c625eceeb36466efcfd0839b6c59
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetSettingsKeyValuePairsBySettingsType.proc.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE dbo.GetSettingsKeyValuePairsBySettingsType
+(
+	@TypeId   INT
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	SELECT SettingsKey,
+           Value
+	FROM dbo.Settings
+	WHERE TypeId=@TypeId
+END
