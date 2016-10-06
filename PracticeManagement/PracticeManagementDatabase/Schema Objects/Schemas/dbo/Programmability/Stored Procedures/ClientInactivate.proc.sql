9755ef90bdd26488ec036ed8e37ca7913de5eff7
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ClientInactivate.proc.sql
@@ -0,0 +1,17 @@
+﻿-- =============================================
+-- Author:		Skip Sailors
+-- Create date: 4-22-1008
+-- Description:	Mark a client inactive
+-- =============================================
+CREATE PROCEDURE [dbo].[ClientInactivate] 
+	-- Add the parameters for the stored procedure here
+	@ClientID int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	Update Client
+		Set Inactive = 1
+		Where ClientId = @ClientID
+END
+
