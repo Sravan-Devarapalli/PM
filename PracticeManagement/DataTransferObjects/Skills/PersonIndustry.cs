d67a2dd1c64aea32bd07967cd6a5ddaa5b61d60c
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/DataTransferObjects/Skills/PersonIndustry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Skills
+{
+    [DataContract]
+    [Serializable]
+    public class PersonIndustry
+    {
+        [DataMember]
+        public Person Person { get; set; }
+
+        [DataMember]
+        public Industry Industry { get; set; }
+
+        [DataMember]
+        public int YearsExperience { get; set; }
+
+        [DataMember]
+        public int DisplayOrder { get; set; }
+
+        [DataMember]
+        public DateTime? ModificationDate { get; set; }
+    }
+}
