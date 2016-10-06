977fb39fdeb546ecdd61bb4edc10a66711c1db57
sravan <sravan>
2016-10-06 09:33:00 -0500

    add from branch - Milestone v0.920 none

---
+++ PracticeManagement/PracticeManagementService/IProjectStatusService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+using DataTransferObjects;
+
+namespace PracticeManagementService
+{
+    [ServiceContract]
+    public interface IProjectStatusService
+    {
+        /// <summary>
+        /// Retrives the list of the project statuses.
+        /// </summary>
+        /// <returns>The list of the <see cref="ProjectStatus"/> objects.</returns>
+        [OperationContract]
+        List<ProjectStatus> GetProjectStatuses();
+    }
+}
