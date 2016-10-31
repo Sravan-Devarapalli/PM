03a9670615e48f378fc9a79c2b701fb9389879c9
sravan <sravan>
2016-10-31 04:31:00 -0500

    rebase from - Milestone v0.920 Comments - At Risk changes

--- PracticeManagement/PracticeManagement/ProjectDetail.aspx.designer.cs
+++ PracticeManagement/PracticeManagement/ProjectDetail.aspx.designer.cs
@@ -274,6 +274,15 @@ namespace PraticeManagement {
         protected global::System.Web.UI.WebControls.CustomValidator custProjectStatus;
         
         /// <summary>
+        /// custAtRiskStatus control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CustomValidator custAtRiskStatus;
+        
+        /// <summary>
         /// cvIsInternal control.
         /// </summary>
         /// <remarks>
