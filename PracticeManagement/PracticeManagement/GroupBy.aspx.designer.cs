ca9de1dfef6988cf2bc6519bacbadc4513f38f8a
sravan <sravan>
2016-10-31 04:32:00 -0500

    rebase from - Milestone v0.920 Comments - At Risk changes

--- PracticeManagement/PracticeManagement/GroupBy.aspx.designer.cs
+++ PracticeManagement/PracticeManagement/GroupBy.aspx.designer.cs
@@ -229,6 +229,15 @@ namespace PraticeManagement {
         protected global::System.Web.UI.WebControls.CheckBox chbExperimental;
         
         /// <summary>
+        /// chbAtRisk control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox chbAtRisk;
+        
+        /// <summary>
         /// cblPractice control.
         /// </summary>
         /// <remarks>
