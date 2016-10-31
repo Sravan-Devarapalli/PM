063392af342d2dfd0d4c78476030ed76f621d7b4
sravan <sravan>
2016-10-31 04:22:00 -0500

    rebase from - Milestone v0.920 Comments - At Risk changes

--- PracticeManagement/PracticeManagement/Reports/ExpenseReport.aspx.designer.cs
+++ PracticeManagement/PracticeManagement/Reports/ExpenseReport.aspx.designer.cs
@@ -238,6 +238,15 @@ namespace PraticeManagement.Reports {
         protected global::System.Web.UI.WebControls.CheckBox chbCompleted;
         
         /// <summary>
+        /// chbAtRisk control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox chbAtRisk;
+        
+        /// <summary>
         /// cblAccounts control.
         /// </summary>
         /// <remarks>
