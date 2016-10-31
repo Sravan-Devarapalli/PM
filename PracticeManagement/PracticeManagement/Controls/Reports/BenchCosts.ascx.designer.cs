79c74eb4766fe7c1d2f11690fbc002b8fe9aa5bd
sravan <sravan>
2016-10-31 04:29:00 -0500

    rebase from - Milestone v0.920 Comments - At Risk changes

--- PracticeManagement/PracticeManagement/Controls/Reports/BenchCosts.ascx.designer.cs
+++ PracticeManagement/PracticeManagement/Controls/Reports/BenchCosts.ascx.designer.cs
@@ -328,6 +328,15 @@ namespace PraticeManagement.Controls.Reports {
         protected global::System.Web.UI.WebControls.CheckBox chbExperimentalProjects;
         
         /// <summary>
+        /// chbATRiskProjects control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox chbATRiskProjects;
+        
+        /// <summary>
         /// tpSortOptions control.
         /// </summary>
         /// <remarks>
