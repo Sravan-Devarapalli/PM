51831dee266e5d5af2c5545ef59a9f7b5fedd16f
sravan <sravan>
2016-11-12 04:56:00 -0500

    Comments - Defect Number: 3364; Defect RecordID: 3360; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: Adding 'At Risk' status to Project Details; Defect Description: Add a Project Status option of AtRisk. Key factors in connection with this status: a.Only Finance will be able to set that status b.Time Entry will be allowed if a Project Status is atRisk (now only allowed when it is Active) c.Ensure that this is a filter option in all the related reports
    Hi Nick,
    
    Please find the following questions/assumptions for 'Adding new project status 'AtRisk' task.
    We are assuming that the persons with Practice Area= 'Finance' able to set Project status to 'AtRisk'. Correct?
    Currently, we are allowing only Active projects in Time Entry page. Now, we need to allow projects with either of the statuses 'Active' or 'AtRisk' in Time Entry page. Correct?
    In projects > Summary page, we indicate Project status with colored pointers. Please let us know the color indicator for 'AtRisk' status.
    1) Yes, same individuals (roles) that can set a project to Active can set a project to AtRisk (Finance and Admin)
    2)Correct. An AtRisk project should behave EXACTLY the same as an Active project
    3)Please send us the current colors and what they represent
    
    Additional questions:
    1)AtRiskstatus is visible to all users, but can only be selected and saved by members of Finance (and Admin) group
    2)Only Finance can change from AtRisk to any other project status
    
    -Nick
    
    Nick Callahan | IT Director |Logic20/20
    
    Hi Nick,
    
    Please find the below points and let us know your response.
    'AtRisk' status will not be available in Project Status drop down field when user tries to add a new project via Projects > Add Project.
    'AtRisk' status will be available to userin Project Status drop down fieldwhen user navigates to already existing project.
    Persons with Role ='System Administrator' or 'Operation' have access to change Project status to 'Active'. Now, the same rules will apply for 'AtRisk' also.
    Currently, Project Status drop down field of an existing project with status ='Active' or 'Internal' or 'Completed' is in disabled state if the logged in user has role other than 'System Administrator' and 'Operation'. We are implementing the samebehaviorfor 'AtRisk' status also.
    Thanks
    
    
    1)Correct
    2)Correct
    3)Correct
    4)Correct
    
    Hi Nick,
    
    Please find the following colors which are being used in Projects > Summary page to indicate Project Status and let us know the color code for 'AtRisk' status.
    
    Status Color code
    Active Green
    Inactive Maroon
    Completed Red
    Projected Yellow
    Proposed Blue
    Internal Orange
    Experimental Grey
    
    Dark Red.

--- PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetProjectsByClientId.proc.sql
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetProjectsByClientId.proc.sql
@@ -16,7 +16,7 @@ BEGIN
 			PS.Name AS ProjectStatusName
     FROM dbo.Project P
 	INNER JOIN dbo.ProjectStatus AS PS ON PS.ProjectStatusId = P.ProjectStatusId
-    WHERE P.ClientId = @ClientId AND P.ProjectStatusId IN (2,3,4,6)
+    WHERE P.ClientId = @ClientId AND P.ProjectStatusId IN (2,3,4,6,8)
 	
 	/*
 	ProjectStatusId	Name
