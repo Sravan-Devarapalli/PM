6817159e75ccb13485f5876fd71cb8272a776ff1
sravan <sravan>
2017-08-04 10:22:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/MarginExceptionResponse.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[MarginExceptionResponse]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [RequestId] INT NOT NULL, 
+    [Status] INT NOT NULL, 
+    [ResponseDate] DATETIME NOT NULL
+)
+
