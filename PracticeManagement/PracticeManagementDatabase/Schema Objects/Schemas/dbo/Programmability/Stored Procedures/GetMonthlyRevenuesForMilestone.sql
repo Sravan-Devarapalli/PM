ce2717e52abc436ec0b0243204b3b9d680891c2f
sravan <sravan>
2017-08-04 10:23:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetMonthlyRevenuesForMilestone.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[GetMonthlyRevenuesForMilestone]
+(
+	@MilestoneId	INT
+)
+AS
+	SET NOCOUNT ON
+
+	SELECT FMR.Id,
+		   FMR.MilestoneId,
+		   FMR.StartDate,
+		   FMR.EndDate,
+		   FMR.Amount
+	FROM FixedMilestoneMonthlyRevenue FMR
+	WHERE FMR.MilestoneId=@MilestoneId
