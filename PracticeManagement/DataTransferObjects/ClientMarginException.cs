b5871fcaeffe73641497bbcfadaf85e15885df6f
sravan <sravan>
2017-08-04 10:43:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/DataTransferObjects/ClientMarginException.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransferObjects
+{
+    [Serializable]
+    [DataContract]
+    public class ClientMarginException
+    {
+        [DataMember]
+        public int? Id
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime StartDate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime EndDate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public ApprovalLevel Level
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int MarginThreshold
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public decimal Revenue
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int MarginGoal
+        {
+            get;
+            set;
+        }
+    }
+}
+
