675c519b18a14fa1dd09f9b47e96a3f94afbb425
sravan <sravan>
2017-08-04 10:22:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ClientMarginGoal.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[ClientMarginGoal]
+(
+	[Id] INT NOT NULL IDENTITY (1, 1), 
+    [ClientId] INT NOT NULL, 
+    [StartDate] DATETIME NOT NULL, 
+    [EndDate] DATETIME NOT NULL, 
+    [MarginGoal] INT NOT NULL, 
+    [Comments] NVARCHAR(100) NULL
+)
+
+GO
+
+CREATE CLUSTERED INDEX [IX_ClientMarginGoal] ON [dbo].[ClientMarginGoal] (Id, ClientId)
+
