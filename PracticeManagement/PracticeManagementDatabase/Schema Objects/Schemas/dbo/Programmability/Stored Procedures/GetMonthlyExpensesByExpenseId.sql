5a268eff8aee5dbe9e75a2d47157208a1148b5c9
sravan <sravan>
2017-08-04 10:23:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetMonthlyExpensesByExpenseId.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[GetMonthlyExpensesByExpenseId]
+(
+	@ExpenseId	INT
+)
+AS
+	SET NOCOUNT ON
+
+	SELECT PME.Id,
+		   PME.ExpenseId,
+		   PME.StartDate,
+		   PME.EndDate,
+		   PME.EstimatedAmount,
+		   PME.ActualAmount
+	FROM ProjectMonthlyExpense PME
+	WHERE PME.ExpenseId=@ExpenseId
