9d50c08e4b035d6bd58425b7b3be1626e6654af1
sravan <sravan>
2017-08-04 10:23:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/InsertMarginThreshold.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[InsertMarginThreshold]
+(
+	@StartDate			DATETIME,
+	@EndDate			DATETIME,
+	@ThresholdVariance  INT
+)
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+		INSERT INTO dbo.MarginThreshold(StartDate, EndDate,ThresholdVariance)
+					Values(@StartDate,@EndDate,@ThresholdVariance)
+
+END
