1c97f48128470b556cc1ea68eb4217c686b0c03a
sravan <sravan>
2017-08-04 10:22:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ProjectBudgetFFMilestoneMonthlyRevenue.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[ProjectBudgetFFMilestoneMonthlyRevenue]
+(
+	[ProjectId] INT NOT NULL, 
+    [MilestoneId] INT NOT NULL, 
+    [StartDate] DATETIME NOT NULL, 
+    [EndDate] DATETIME NOT NULL, 
+    [Amount] DECIMAL(18, 2) NOT NULL 
+)
+
+GO
+
+CREATE CLUSTERED INDEX [IX_ProjectBudgetFFMilestoneMonthlyRevenue] ON [dbo].[ProjectBudgetFFMilestoneMonthlyRevenue] (ProjectId,MilestoneId)
+
