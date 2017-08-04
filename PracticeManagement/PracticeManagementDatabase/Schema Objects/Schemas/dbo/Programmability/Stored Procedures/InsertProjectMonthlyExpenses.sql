5ec126a647c481336f1f0570bb0947697d92df2b
sravan <sravan>
2017-08-04 10:23:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/InsertProjectMonthlyExpenses.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[InsertProjectMonthlyExpenses]
+(
+	@ExpenseId	INT,
+	@StartDate	DATETIME,
+	@EndDate	DATETIME,
+	@EstimatedAmount	DECIMAL(18,2),
+	@ActualAmount	DECIMAL(18,2)
+)
+AS
+	SET NOCOUNT ON
+
+	
+	INSERT INTO ProjectMonthlyExpense(ExpenseId,StartDate,EndDate, EstimatedAmount,ActualAmount)
+	VALUES(@ExpenseId,@StartDate,@EndDate, @EstimatedAmount,@ActualAmount)
+
