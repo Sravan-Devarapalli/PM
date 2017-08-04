7def72b0f865c0c4a6be61f2ac353e887c50364a
sravan <sravan>
2017-08-04 10:23:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetBudgetResetToAddress.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[GetBudgetResetToAddress]
+(
+	@ProjectId INT
+)
+AS
+	SET NOCOUNT ON
+
+	SELECT EIC.Alias+','+Manger.Alias+','+EM.Alias
+	FROM Project p
+	left join Person as EIC on p.ExecutiveInChargeId=EIC.PersonId
+	left join Person as Manger on p.ProjectManagerId=Manger.PersonId
+	LEFT JOIN Person as EM on p.EngagementManagerId=EM.PersonId
+	WHERE p.ProjectId=@ProjectId
+
