a3d609a0314b7762bcc3ce0226938e8390f384f6
sravan <sravan>
2017-08-04 10:23:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CleanBudgetRecordsForNonActiveProject.sql
@@ -0,0 +1,36 @@
+﻿CREATE PROCEDURE [dbo].[CleanBudgetRecordsForNonActiveProject]
+(
+	@ProjectId INT
+)
+AS
+	
+		UPDATE PBH
+		SET PBH.IsActive=CAST(0 as BIT)
+		FROM ProjectBudgetHistory PBH
+		WHERE PBH.ProjectId = @ProjectId
+
+		UPDATE Project
+			SET Budget=NULL,
+				BudgetSetDate=null
+		WHERE ProjectId = @ProjectId
+
+		DELETE PBPE
+		FROM ProjectBudgetPersonEntry PBPE
+		WHERE PBPE.ProjectId = @ProjectId
+
+		DELETE PBME
+		FROM ProjectBudgetMonthlyExpense PBME
+		JOIN ProjectBudgetExpense PBE ON PBE.Id= PBME.ExpenseId
+		WHERE PBE.ProjectId = @ProjectId
+		
+		DELETE PBE
+		FROM ProjectBudgetExpense PBE
+		WHERE PBE.ProjectId = @ProjectId
+
+		DELETE PBFM
+ 		FROM ProjectBudgetFFMilestoneMonthlyRevenue PBFM
+		JOIN ProjectBudgetHistory PBH ON PBH.MilestoneId = PBFM.MilestoneId 
+		WHERE PBH.ProjectId = @ProjectId
+
+
+
