53148a739c9156ceb9f059e0c0b0c32987b99150
sravan <sravan>
2017-08-04 10:23:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetActiveAndAtRiskProjectsByClient.sql
@@ -0,0 +1,48 @@
+﻿CREATE PROCEDURE [dbo].[GetActiveAndAtRiskProjectsByClient]
+(
+	@ClientId	INT,
+	@UserLogin	NVARCHAR(255)
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DECLARE @UserHasHighRoleThanProjectLead INT = NULL
+	DECLARE @PersonId INT
+
+	SELECT @PersonId = P.PersonId
+	FROM Person P
+	WHERE P.Alias = @UserLogin
+
+	IF EXISTS ( SELECT 1
+				FROM aspnet_Users U
+				JOIN aspnet_UsersInRoles UR ON UR.UserId = U.UserId
+				JOIN aspnet_Roles R ON R.RoleId = UR.RoleId
+				WHERE U.UserName = @UserLogin AND R.LoweredRoleName = 'project lead')
+	BEGIN
+		SET @UserHasHighRoleThanProjectLead = 0
+		
+		SELECT @UserHasHighRoleThanProjectLead = COUNT(*)
+		FROM aspnet_Users U
+		JOIN aspnet_UsersInRoles UR ON UR.UserId = U.UserId
+		JOIN aspnet_Roles R ON R.RoleId = UR.RoleId
+		WHERE U.UserName = @UserLogin
+		AND R.LoweredRoleName IN ('system administrator','client director','business unit manager','salesperson','practice area manager','senior leadership','operations')			
+	END
+	
+    SELECT  P.ProjectId,
+			P.Name AS 'ProjectName',
+			P.ProjectNumber,
+			PS.Name AS ProjectStatusName
+    FROM dbo.Project P
+	INNER JOIN dbo.ProjectStatus AS PS ON PS.ProjectStatusId = P.ProjectStatusId
+    WHERE P.ClientId = @ClientId AND P.ProjectStatusId IN (3,8,4) -- active, atrisk, completed
+		  AND (@UserHasHighRoleThanProjectLead IS NULL
+					OR @UserHasHighRoleThanProjectLead > 0
+					OR (@UserHasHighRoleThanProjectLead = 0
+						AND ( EXISTS (SELECT 1 FROM dbo.ProjectAccess projManagers2 WHERE projManagers2.ProjectId = P.ProjectId AND projManagers2.ProjectAccessId = @PersonId) OR P.SalesPersonId = @PersonId)
+						)
+				)
+	ORDER BY P.ProjectNumber
+END
+
