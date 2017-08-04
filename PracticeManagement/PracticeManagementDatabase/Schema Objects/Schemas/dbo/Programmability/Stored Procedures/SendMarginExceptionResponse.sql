799c48e1a0e3fb8167740c5474cb449fd949a5c2
sravan <sravan>
2017-08-04 10:23:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SendMarginExceptionResponse.sql
@@ -0,0 +1,49 @@
+﻿CREATE PROCEDURE [dbo].[SendMarginExceptionResponse]
+(
+	@RequestId	INT,
+	@Status		INT,
+	@UserAlias  NVARCHAR(100),
+	@TierTwoStatus INT,
+	@Comments   NVARCHAR(MAX),
+	@Recipient	NVARCHAR(100) OUT
+)
+AS
+BEGIN
+
+	    DECLARE @UserId			INT,
+			    @CurrentPMTime  DATETIME
+
+		SELECT @CurrentPMTime = dbo.GettingPMTime(GETUTCDATE())
+		SELECT @userId = PersonId FROM dbo.Person WHERE Alias = @UserAlias
+		EXEC dbo.SessionLogUnprepare
+
+		EXEC dbo.SessionLogPrepare @UserLogin = @UserAlias
+
+		--status 1- Request, 2- Accept, 3- Decline
+
+		UPDATE M
+			SET M.ApprovedBy=@UserId,
+				M.TierOneStatus= CASE WHEN @TierTwoStatus=1 THEN M.TierOneStatus ELSE @Status END,
+				M.TierTwoStatus= CASE WHEN @TierTwoStatus=1 THEN @Status ELSE M.TierTwoStatus END,
+				M.ResponseDate=@CurrentPMTime
+		FROM MarginExceptionRequest M
+		WHERE M.Id=@RequestId
+		EXEC dbo.SessionLogUnprepare
+		
+		EXEC dbo.SessionLogPrepare @UserLogin = @UserAlias
+
+		UPDATE P
+			SET p.ExceptionMargin=case when m.IsRevenueException=0 then M.TargetMargin ELSE NULL END,
+			p.ExceptionRevenue=M.TargetRevenue
+		FROM Project p
+		JOIN MarginExceptionRequest M on M.Projectid=P.ProjectId
+		WHERE M.Id=@RequestId and M.TierOneStatus=2 and (M.TierTwoStatus=2 OR M.TierTwoStatus=0)
+		EXEC dbo.SessionLogUnprepare
+
+		SELECT @Recipient = Alias 
+		FROM Person P
+		JOIN MarginExceptionRequest M on M.Requestor=P.PersonId
+		WHERE M.Id=@RequestId
+
+		EXEC dbo.SessionLogUnprepare
+END
