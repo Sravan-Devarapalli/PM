df17403f030f31ef5d279b8e37df4beebd2db9bb
sravan <sravan>
2017-08-04 10:43:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/DataTransferObjects/MonthlyRevenue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransferObjects
+{
+    public class MonthlyRevenue
+    {
+        [DataMember]
+        public int? Id
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int? MilestoneId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime StartDate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime EndDate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Decimal Amount
+        {
+            get;
+            set;
+        }
+    }
+}
+
