d019ee19c19574dd8961dfaa253d30236d7ac919
sravan <sravan>
2017-08-04 10:23:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetMarginExceptionThresholds.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[GetMarginExceptionThresholds]
+AS
+
+SELECT M.Id,
+	   M.StartDate,
+	   M.EndDate,
+	   M.ApprovalLevelId,
+	   l.ApprovalLevel,
+	   M.MarginGoal,
+	   M.Revenue
+FROM MarginException M
+JOIN MarginExceptionApprovalLevel L ON M.ApprovalLevelId=L.Id
