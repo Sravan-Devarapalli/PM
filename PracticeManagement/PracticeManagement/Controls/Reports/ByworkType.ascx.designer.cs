2f713b0fd20bdaebd13421c77721968ef72cb26b
sravan <sravan>
2017-08-04 10:59:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

--- PracticeManagement/PracticeManagement/Controls/Reports/ByworkType.ascx.designer.cs
+++ PracticeManagement/PracticeManagement/Controls/Reports/ByworkType.ascx.designer.cs
@@ -157,15 +157,6 @@ namespace PraticeManagement.Controls.Reports {
         protected global::System.Web.UI.WebControls.Button btnExportToExcel;
         
         /// <summary>
-        /// btnExportToPDF control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnExportToPDF;
-        
-        /// <summary>
         /// repWorkType control.
         /// </summary>
         /// <remarks>
