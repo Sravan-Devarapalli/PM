bc90e7a4b16f51d91524b407d92c264085ee9c34
sravan <sravan>
2017-08-04 10:43:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/DataTransferObjects/Reports/PayRate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransferObjects.Reports
+{
+    [DataContract]
+    [Serializable]
+    public class PayRate
+    {
+        [DataMember]
+        public int PersonId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public PracticeManagementCurrency BillRate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public PracticeManagementCurrency PersonCost
+        {
+            get;
+            set;
+        }
+    }
+}
+
