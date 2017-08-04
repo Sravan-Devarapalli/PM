641cd46dbc9886202d2329ae9dcba9f77b4c15ad
sravan <sravan>
2017-08-04 10:43:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/DataTransferObjects/PeriodicalExpense.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransferObjects
+{
+    public class PeriodicalExpense
+    {
+        [DataMember]
+        public int? Id
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime StartDate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime EndDate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Decimal EstimatedExpense
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Decimal? ActualExpense
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int? ProjectExpenseId
+        {
+            get;
+            set;
+        }
+
+        public Decimal Variance
+        {
+            get
+            {
+
+                return EstimatedExpense - (ActualExpense != null ? ActualExpense.Value : 0);
+            }
+        }
+    }
+}
+
