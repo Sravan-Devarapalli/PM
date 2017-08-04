f4114e17105a0b85b525e264d47d7550ca99510c
sravan <sravan>
2017-08-04 10:22:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/BudgetResetDeclineHistory.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[BudgetResetDeclineHistory]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    [RequestId] INT NOT NULL, 
+    [DeclinedBy] INT NULL, 
+    [DeclinedDate] DATETIME NULL, 
+    [Comments] NVARCHAR(MAX) NULL, 
+    CONSTRAINT [FK_BudgetResetDeclineHistory_BudgetResetRequestHistory] FOREIGN KEY ([RequestId]) REFERENCES [BudgetResetRequestHistory]([RequestId])
+)
+
