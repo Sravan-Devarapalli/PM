878d347651a14d5a9fdfc4383ecb50f6dfad4512
sravan <sravan>
2017-08-04 10:23:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetClientMarginGoalLog.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[GetClientMarginGoalLog]
+(
+	@ClientId INT
+)
+AS
+BEGIN
+
+		SELECT C.ClientId,
+			   C.Activity,
+			   C.LogTime as LogDate,
+			   C.OldStartDate,
+			   C.NewStartDate,
+			   C.OldEndDate,
+			   C.NewEndDate,
+			   C.OldMarginGoal,
+			   C.NewMarginGoal,
+			   C.Comments,
+			   p.LastName+', '+p.FirstName as PersonName
+		FROM ClientMarginGoalHistory C
+		JOIN Person p ON C.PersonId =p.PersonId
+		WHERE C.ClientId = @ClientId
+
+END
