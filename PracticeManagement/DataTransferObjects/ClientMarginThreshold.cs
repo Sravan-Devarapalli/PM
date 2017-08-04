d0ef7e718c2982e988f6c5d4a36ae958943cb748
sravan <sravan>
2017-08-04 10:43:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/DataTransferObjects/ClientMarginThreshold.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransferObjects
+{
+    [Serializable]
+    [DataContract]
+    public class ClientMarginThreshold
+    {
+
+        [DataMember]
+        public int? Id
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime StartDate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime EndDate
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int ThresholdVariance
+        {
+            get;
+            set;
+        }
+    }
+}
+
