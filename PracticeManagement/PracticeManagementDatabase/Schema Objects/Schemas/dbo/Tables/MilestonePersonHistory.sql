612769626b76c5d5098274df51f1645e0c6679c9
sravan <sravan>
2017-08-04 10:22:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/MilestonePersonHistory.sql
@@ -0,0 +1,38 @@
+﻿CREATE TABLE [dbo].[MilestonePersonHistory]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY (1, 1),
+    [ProjectId] INT NOT NULL, 
+    [MilestoneId] INT NOT NULL, 
+    [MilestonePersonId] INT NOT NULL, 
+    [OldStartDate] DATETIME NULL, 
+    [NewStartDate] DATETIME NULL, 
+    [OldEndDate] DATETIME NULL, 
+    [NewEndDate] DATETIME NULL, 
+    [OldHoursPerDay] DECIMAL(4, 2) NULL, 
+    [NewHoursPerDay] DECIMAL(4, 2) NULL, 
+    [OldAmount] DECIMAL(18, 2) NULL, 
+    [NewAmount] DECIMAL(18, 2) NULL, 
+    [LogTime] DATETIME NULL, 
+    [UpdatedBy] INT NULL, 
+    [PersonId] INT NULL, 
+    [MilestonePersonEntryId] INT NULL, 
+    [RoleId] INT NULL
+)
+
+GO
+
+CREATE NONCLUSTERED INDEX [NCIX_MilestonePersonHistory_ProjectId_LogTime] ON [dbo].[MilestonePersonHistory]
+(
+	[ProjectId] ASC,
+	[LogTime] ASC
+)
+INCLUDE ( 	[MilestoneId],
+	[MilestonePersonId],
+	[NewStartDate],
+	[NewEndDate],
+	[NewHoursPerDay],
+	[NewAmount],
+	[PersonId],
+	[MilestonePersonEntryId]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+GO
+
