549d86f05d60e635716087d2cbcf19cc1d7b374e
sravan <sravan>
2017-08-04 10:22:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/FixedMilestoneMonthlyRevenue.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[FixedMilestoneMonthlyRevenue]
+(
+	[Id]            INT       PRIMARY KEY      IDENTITY (1, 1) NOT NULL, 
+    [MilestoneId] INT NOT NULL, 
+    [StartDate] DATETIME NULL, 
+    [EndDate] DATETIME NULL, 
+    [Amount] DECIMAL(18, 2) NULL
+)
+
+GO
+
+
+CREATE NONCLUSTERED INDEX IX_FixedMilestoneMonthlyRevenue_Index1 ON [dbo].[FixedMilestoneMonthlyRevenue] 
+(
+	[MilestoneId] ASC,
+	[StartDate] ASC,
+	[EndDate] ASC
+)
+INCLUDE ( [Amount]) WITH (SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+
