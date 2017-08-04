fe385d04dcbe36df3930cd4a54a254c740b945f2
sravan <sravan>
2017-08-04 10:23:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/UpdateFixedMilestoneMonthlyRevenues.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[UpdateFixedMilestoneMonthlyRevenues]
+(
+	@Id	INT,
+	@MilestoneId	INT,
+	@StartDate	DATETIME,
+	@EndDate	DATETIME,
+	@Amount 	DECIMAL(18,2)
+)
+AS
+	SET NOCOUNT ON
+
+	UPDATE FMR
+	SET	FMR.StartDate=@StartDate,
+		FMR.EndDate=@EndDate,
+		FMR.Amount =@Amount
+	FROM FixedMilestoneMonthlyRevenue FMR
+	WHERE FMR.Id=@Id AND FMR.MilestoneId=@MilestoneId
