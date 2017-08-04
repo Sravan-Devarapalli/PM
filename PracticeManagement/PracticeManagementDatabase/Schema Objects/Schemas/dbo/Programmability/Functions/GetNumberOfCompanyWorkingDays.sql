b4c29250dbcf2a43bbc639a9b8dcfbfa71b2d91b
sravan <sravan>
2017-08-04 10:18:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetNumberOfCompanyWorkingDays.sql
@@ -0,0 +1,18 @@
+﻿CREATE FUNCTION [dbo].[GetNumberOfCompanyWorkingDays]
+(
+	@startDate DATETIME,
+	@endDate DATETIME
+)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @res INT
+
+	SELECT @res = COUNT(*) 
+	FROM dbo.Calendar
+	WHERE Date BETWEEN @startDate AND @endDate AND 
+		  DayOff = 0 
+
+	RETURN @res
+END
+
