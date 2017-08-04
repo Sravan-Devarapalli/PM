61b5f81a58044bac2a0e81486e022592392afb9d
sravan <sravan>
2017-08-04 10:21:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

--- PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_Project.view.sql
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_Project.view.sql
@@ -1,5 +1,30 @@
 ﻿CREATE VIEW dbo.v_Project
 AS
+	WITH BudgetRequest
+	AS(
+		  SELECT BRH.ProjectId, 
+				 BRH.RequestId,
+				 CAST(1 as BIT) as IsBudgetResetPending,
+				 p.FirstName+', '+p.LastName AS requestorName,
+				 BRH.BudgetToDate
+		  FROM dbo.BudgetResetRequestHistory BRH
+		  LEFT JOIN dbo.BudgetResetApprovalHistory BAH on BRH.RequestId=BAH.RequestId
+		  LEFT JOIN dbo.BudgetResetDeclineHistory BDH on BRH.requestid=BDH.requestid
+		  LEFT JOIN dbo.Person p on p.PersonId= BRH.RequestedBy 
+		  WHERE BAH.RequestId IS NULL and BDH.id is  null
+	),
+	
+	BudgetRequestEnable
+	AS(
+		SELECT DISTINCT MPH.projectid,
+			   CAST(1 as BIT) as EnableBudgetRequest
+		FROM MilestonePersonHistory MPH
+		LEFT JOIN Project p on p.ProjectId=MPH.ProjectId
+		where MPH.LogTime>p.BudgetSetDate
+	)
+
+	
+		  
 	SELECT p.ClientId,
 		   c.IsMarginColorInfoEnabled,
 		   p.ProjectId,
@@ -56,7 +81,15 @@ AS
 			o.Name as OfferingName,
 			p.IsClientTimeEntryRequired,
 			p.PreviousProjectNumber,
-			p.OutsourceId
+			p.OutsourceId,
+			p.Budget,
+			BR.IsBudgetResetPending,
+			BR.RequestId as 'BudgetResetRequestId',
+			BRE.EnableBudgetRequest,
+			p.ExceptionMargin,
+			p.ExceptionRevenue,
+			BR.requestorName,
+			BR.BudgetToDate
 	  FROM dbo.Project AS p
 		   INNER JOIN dbo.Practice AS r ON p.PracticeId = r.PracticeId
 		   INNER JOIN dbo.Client AS c ON p.ClientId = c.ClientId
@@ -66,6 +99,7 @@ AS
 		   LEFT JOIN dbo.Channel ch on ch.ChannelId=p.ChannelId
 		   LEFT JOIN dbo.Revenue re ON re.RevenueTypeId=p.RevenueTypeId
 		   LEFT JOIN dbo.Offering o ON o.OfferingId=p.OfferingId
-		   
+		   LEFT JOIN BudgetRequest BR ON BR.ProjectId=p.ProjectId
+		   LEFT JOIN BudgetRequestEnable BRE ON BRE.ProjectId=p.ProjectId
 
 
