19022d3eec0d05269d6ccdb3c4f207186edddb31
sravan <sravan>
2017-08-04 10:22:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/MarginException.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[MarginException]
+(
+	[Id] INT NOT NULL IDENTITY (1, 1), 
+    [StartDate] DATETIME NOT NULL, 
+    [EndDate] DATETIME NOT NULL, 
+    [ApprovalLevelId] INT NOT NULL, 
+    [MarginGoal] INT NOT NULL, 
+    [Revenue] DECIMAL(18, 2) NOT NULL
+)
+
+GO
+
+CREATE CLUSTERED INDEX [IX_MarginException] ON [dbo].[MarginException] ([Id])
+
