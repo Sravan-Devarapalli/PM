cdf622ce19e2be952f98aa2a01cbde3063effd9a
sravan <sravan>
2017-08-04 10:21:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

--- PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_MilestoneExpenses.view.sql
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_MilestoneExpenses.view.sql
@@ -2,8 +2,8 @@
 AS
 	SELECT pexp.MilestoneId,
 	pexp.projectid,
-		  CONVERT(DECIMAL(18,2),SUM(pexp.Amount)) Expense,
-		  CONVERT(DECIMAL(18,2),SUM(pexp.Reimbursement*0.01*pexp.Amount)) ReimbursedExpense 
+		  CONVERT(DECIMAL(18,2),SUM(pexp.ExpectedAmount)) Expense,
+		  CONVERT(DECIMAL(18,2),SUM(pexp.Reimbursement*0.01*pexp.ExpectedAmount)) ReimbursedExpense 
 	FROM dbo.ProjectExpense as pexp 
 	GROUP BY pexp.MilestoneId,
 	pexp.projectid
