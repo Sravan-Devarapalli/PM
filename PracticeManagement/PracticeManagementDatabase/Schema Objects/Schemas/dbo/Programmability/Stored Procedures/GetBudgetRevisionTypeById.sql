afb75491de17da8738ecead4f1d2f6648f5f3e3c
sravan <sravan>
2017-08-04 10:23:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetBudgetRevisionTypeById.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[GetBudgetRevisionTypeById]
+(
+	@Id INT 
+)
+AS
+	DECLARE @result NVARCHAR(max)=''
+	IF EXISTS(select 1 from BudgetResetType where id = @Id)
+	BEGIN
+		SELECT @result= Name
+		FROM BudgetResetType where id = @Id
+	END
+	SELECT 	@result
+
