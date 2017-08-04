e631abee1fe4b33706711d0d4b1ef4b4a9cc0826
sravan <sravan>
2017-08-04 10:43:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/DataTransferObjects/ProjectBudgetManagement.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class ProjectBudgetManagement
+    {
+
+
+        [DataMember]
+        public int ProjectId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public List<ProjectBudgetResource> BudgetResources
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public ProjectRevenue BudgetSummary
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public ProjectRevenue ProjectedSummary
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public ProjectRevenue ActualsSummary
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public ProjectRevenue EACSummary {
+            get;
+            set;
+            //get {
+            //    return new ProjectRevenue
+            //    {
+            //        Hours = ActualsSummary.Hours + ProjectedSummary.Hours,
+            //        Revenue = ActualsSummary.Revenue + ProjectedSummary.Revenue,
+            //        Expenses = ActualsSummary.Expenses + ProjectedSummary.Expenses,
+            //        Margin = ActualsSummary.Margin + ProjectedSummary.Margin
+            //    };
+            //}
+        }
+    }
+}
+
