f6cc8524f9d0873f9c58ab3cc4e38d51f7956398
sravan <sravan>
2017-08-04 10:23:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetMarginExceptionThresholdsForPeriod.sql
@@ -0,0 +1,36 @@
+﻿CREATE PROCEDURE [dbo].[GetMarginExceptionThresholdsForPeriod]
+(
+	@StartDate	DATETIME,
+	@EndDate	DATETIME,
+	@ClientId	INT
+)
+AS
+BEGIN
+	
+	DECLARE @ClientIdLocal INT,
+			@MarginGoal    INT = -1,
+			@StartDateLocal DATETIME = @StartDate,
+			@EndDateLocal  DATETIME =@EndDate
+
+	SELECT @ClientIdLocal=@ClientId
+
+	SELECT @MarginGoal=c.MarginGoal
+	FROM ClientMarginGoal C
+	WHERE C.ClientId=@ClientIdLocal AND (C.StartDate<= @StartDateLocal AND C.EndDate>=@StartDateLocal)
+
+	if(@MarginGoal=-1)
+	BEGIN
+		SELECT @MarginGoal=50 -- use set insted of Select 
+	END
+
+	SELECT ME.Id,
+		   ME.StartDate,
+		   ME.EndDate,
+		   ME.ApprovalLevelId,
+		   ME.MarginGoal AS 'MarginThreshold',
+		   ME.Revenue,
+		   @MarginGoal as 'MarginGoal'
+	FROM MarginException ME
+	WHERE ME.StartDate<=@StartDateLocal AND ME.EndDate>=@StartDateLocal
+
+END 
