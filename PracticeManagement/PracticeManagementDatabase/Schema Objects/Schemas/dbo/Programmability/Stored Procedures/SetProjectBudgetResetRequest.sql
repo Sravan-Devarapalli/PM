d41d55103d2476449fe7fa2e34d6f31d75ad546f
sravan <sravan>
2017-08-04 10:23:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SetProjectBudgetResetRequest.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [dbo].[SetProjectBudgetResetRequest]
+(
+	@ProjectId INT,
+	@UserAlias NVARCHAR(255),
+	@Comments  NVARCHAR(MAX),
+	@ResetType INT,
+	@BudgetToDate DATETIME = NULL
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DECLARE @userId INT,
+			@CurrentPMTime DATETIME
+	SELECT @CurrentPMTime = dbo.GettingPMTime(GETUTCDATE())
+	SELECT @userId = PersonId FROM dbo.Person WHERE Alias = @UserAlias
+
+	EXEC dbo.SessionLogPrepare @UserLogin = @UserAlias
+
+	 INSERT INTO dbo.BudgetResetRequestHistory(ProjectId,RequestedBy,RequestDate, Comments, ResetType, BudgetToDate)
+	 VALUES (@ProjectId,@userId,@CurrentPMTime, @Comments, @ResetType, @BudgetToDate)
+
+	 EXEC dbo.SessionLogUnprepare
+
+END
+
