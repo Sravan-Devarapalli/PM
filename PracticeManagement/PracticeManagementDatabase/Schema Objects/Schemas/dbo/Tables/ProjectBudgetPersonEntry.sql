fdae9a8e06f661aa6996287a5201a1a8d51952c6
sravan <sravan>
2017-08-04 10:22:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ProjectBudgetPersonEntry.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[ProjectBudgetPersonEntry]
+(
+	[ProjectId] INT NOT NULL, 
+    [MilestoneId] INT NOT NULL, 
+    [PersonId] INT NOT NULL, 
+    [StartDate] DATETIME NOT NULL, 
+    [EndDate] DATETIME NOT NULL, 
+    [Amount] DECIMAL(18, 2) NOT NULL, 
+    [HoursPerDay] DECIMAL(8, 2) NOT NULL 
+)
+
+GO
+
+CREATE NONCLUSTERED  INDEX [IX_ProjectBudgetPersonEntry] ON [dbo].[ProjectBudgetPersonEntry] 
+(ProjectId ASC, MilestoneId ASC, PersonId ASC )
+INCLUDE (StartDate, EndDate, HoursPerDay)
+GO
+
+CREATE CLUSTERED INDEX [IX_ProjectBudgetPersonEntry_ProjectId] ON [dbo].[ProjectBudgetPersonEntry] (ProjectId, MilestoneId)
+
