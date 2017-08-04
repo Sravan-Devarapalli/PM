c46171b02103091a4f186e68b7e81ae54046460a
sravan <sravan>
2017-08-04 10:22:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/BudgetResetRequestHistory.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[BudgetResetRequestHistory]
+(
+	[RequestId] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    [ProjectId] INT NULL, 
+    [RequestedBy] INT NULL, 
+    [RequestDate] DATETIME NULL,
+	[Comments] NVARCHAR(MAX) NOT NULL, 
+    [ResetType] INT NOT NULL, 
+    [BudgetToDate] DATETIME NULL
+    
+)
+
+
+
+
