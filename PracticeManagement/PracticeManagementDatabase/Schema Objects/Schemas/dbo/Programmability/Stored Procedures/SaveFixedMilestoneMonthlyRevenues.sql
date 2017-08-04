4484c13c82b4ffe9b3bb62db28151f9723a2eff2
sravan <sravan>
2017-08-04 10:23:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SaveFixedMilestoneMonthlyRevenues.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[SaveFixedMilestoneMonthlyRevenues]
+(
+	@MilestoneId	INT,
+	@StartDate	DATETIME,
+	@EndDate	DATETIME,
+	@Amount	DECIMAL(18,2)
+)
+AS
+	SET NOCOUNT ON
+
+	
+	INSERT INTO FixedMilestoneMonthlyRevenue(MilestoneId,StartDate,EndDate,Amount)
+	VALUES(@MilestoneId,@StartDate,@EndDate,@Amount)
+
