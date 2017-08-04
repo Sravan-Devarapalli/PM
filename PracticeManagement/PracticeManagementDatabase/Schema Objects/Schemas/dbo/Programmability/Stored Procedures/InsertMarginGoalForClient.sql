1a2ce1f04a6dcb984fa1418b20deb75791888d0b
sravan <sravan>
2017-08-04 10:23:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/InsertMarginGoalForClient.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[InsertMarginGoalForClient]
+(
+	@ClientId	INT,
+	@StartDate	DATETIME,
+	@EndDate		DATETIME,
+	@MarginGoal	INT,
+	@Comments    NVARCHAR(MAX) = NULL,
+	@UserLogin                NVARCHAR(255)
+)
+AS
+BEGIN
+		SET NOCOUNT ON;
+
+		EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
+
+		INSERT INTO ClientMarginGoal(ClientId, StartDate, EndDate, MarginGoal, Comments)
+		VALUES(@ClientId,@StartDate,@EndDate,@MarginGoal,@Comments)
+
+		EXEC dbo.SessionLogUnprepare
+END
+
