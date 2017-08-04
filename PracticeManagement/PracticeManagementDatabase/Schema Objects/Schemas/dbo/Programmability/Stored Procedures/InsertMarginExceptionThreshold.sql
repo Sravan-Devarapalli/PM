d74e35c07916f9bcb7fa297364a70e1a06dce963
sravan <sravan>
2017-08-04 10:23:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/InsertMarginExceptionThreshold.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[InsertMarginExceptionThreshold]
+(
+	@StartDate			DATETIME,
+	@EndDate			DATETIME,
+	@ApprovalLevel		INT,
+	@MarginGoal			INT,
+	@Revenue			DECIMAL(18, 2)
+)
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+		INSERT INTO dbo.MarginException(StartDate,EndDate,ApprovalLevelId,MarginGoal,Revenue)
+					Values(@StartDate,@EndDate,@ApprovalLevel,@MarginGoal,@Revenue)
+
+END
