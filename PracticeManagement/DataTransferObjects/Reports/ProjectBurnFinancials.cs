562d9965f17349c39b8ac9a355229a4edfa44bdc
sravan <sravan>
2017-08-04 10:43:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/DataTransferObjects/Reports/ProjectBurnFinancials.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransferObjects.Reports
+{
+    [DataContract]
+    [Serializable]
+    public class ProjectBurnFinancials
+    {
+        [DataMember]
+        public int ProjectId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public List<ProjectBudgetResource> Resources
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Dictionary<DateTime, ComputedFinancials> Financials
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public List<ProjectExpense> Expenses
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public decimal MarginGoal
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int? BudgetAmount
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int TotalProjectDays
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int RemainingDays
+        {
+            get;
+            set;
+        }
+
+        public int CompletedDays
+        {
+            get {
+                return TotalProjectDays - RemainingDays;
+            }
+        }
+
+    }
+}
+
