9028c02156da6bcea228a14ff93cfcd2bbf48f4d
sravan <sravan>
2017-08-04 10:22:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ProjectBudgetHistory.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[ProjectBudgetHistory]
+(
+	[ProjectId] INT NOT NULL , 
+    [Revenue] DECIMAL(18, 2) NULL, 
+    [Expense] DECIMAL(18, 2) NULL, 
+    [ReimbursedExpense] DECIMAL(18, 2) NULL, 
+    [Discount] DECIMAL(5, 2) NULL, 
+    [COGS] DECIMAL(18, 2) NULL, 
+    [IsActive] BIT NOT NULL, 
+    [LogTime] DATETIME NULL, 
+    [UpdatedBy] INT NULL, 
+    [MilestoneId] INT NULL DEFAULT Null, 
+    [IsHourlyAmount] INT NULL DEFAULT NULL, 
+    CONSTRAINT [FK_ProjectBudgetHistory_Project] FOREIGN KEY ([ProjectId]) REFERENCES [Project]([ProjectId])
+)
+
+GO
+
+CREATE CLUSTERED INDEX [IX_ProjectBudgetHistory_ProjectId] ON [dbo].[ProjectBudgetHistory] ([ProjectId])
+
