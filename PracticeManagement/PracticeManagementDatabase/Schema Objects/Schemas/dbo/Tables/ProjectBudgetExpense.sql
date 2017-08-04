6b0bf32d61bd6a52205c1284b54fae51d814bbc4
sravan <sravan>
2017-08-04 10:22:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ProjectBudgetExpense.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[ProjectBudgetExpense]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name]          NVARCHAR (50)   NOT NULL,
+    [Amount]        DECIMAL (18, 2) NOT NULL,
+	[Reimbursement] DECIMAL (18, 2) NOT NULL default 0,
+    [ProjectId]		INT             NOT NULL,
+	[StartDate]		DATETIME		NOT NULL,
+	[EndDate]		DATETIME		NOT NULL,
+	[MilestoneId]	INT				NULL, 
+    [ExpenseTypeId] INT NOT NULL,
+)
+
