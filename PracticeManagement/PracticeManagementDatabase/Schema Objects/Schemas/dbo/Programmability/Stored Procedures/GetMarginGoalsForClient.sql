38d1ec4e0b4811c4bb358e796c6d86fa258b6049
sravan <sravan>
2017-08-04 10:23:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetMarginGoalsForClient.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[GetMarginGoalsForClient]
+(
+	@ClientId	INT
+)
+AS
+BEGIN
+	SELECT CM.Id,
+		   CM.ClientId,
+		   CM.StartDate,
+		   CM.EndDate,
+		   CM.MarginGoal,
+		   CM.Comments
+	FROM ClientMarginGoal CM
+	WHERE CM.ClientId=@ClientId
+END
+
