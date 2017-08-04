14270c1b0d0a2d835f999fcf4ff008d678e51119
sravan <sravan>
2017-08-04 10:23:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DeleteMarginGoal.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[DeleteMarginGoal]
+(
+	@Id  INT,
+	@UserLogin	NVARCHAR(255)
+)
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+		
+		EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
+
+		DELETE FROM ClientMarginGoal WHERE Id=@Id
+
+		EXEC dbo.SessionLogUnprepare
+END
