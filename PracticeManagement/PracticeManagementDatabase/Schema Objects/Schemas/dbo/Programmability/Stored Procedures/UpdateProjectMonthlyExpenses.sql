6854565bd5067a25691769eed854ee9fc3600af2
sravan <sravan>
2017-08-04 10:23:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/UpdateProjectMonthlyExpenses.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[UpdateProjectMonthlyExpenses]
+(
+	@Id	INT,
+	@ExpenseId	INT,
+	@StartDate	DATETIME,
+	@EndDate	DATETIME,
+	@EstimatedAmount	DECIMAL(18,2),
+	@ActualAmount	DECIMAL(18,2)
+)
+AS
+	SET NOCOUNT ON
+
+	UPDATE PME
+	SET	PME.StartDate=@StartDate,
+		PME.EndDate=@EndDate,
+		PME.EstimatedAmount =@EstimatedAmount,
+		PME.ActualAmount=@ActualAmount
+	FROM ProjectMonthlyExpense PME
+	WHERE PME.Id=@Id AND PME.ExpenseId=@ExpenseId
+
+
