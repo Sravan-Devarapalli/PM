ed522475fee8605cff74030c2394d9b1bff2a6f4
sravan <sravan>
2017-08-04 10:22:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/MarginExceptionApprovalLevel.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[MarginExceptionApprovalLevel]
+(
+	[Id] INT NOT NULL IDENTITY (1, 1), 
+    [ApprovalLevel] NVARCHAR(12) NOT NULL
+)
+
+GO
+
+CREATE CLUSTERED INDEX [IX_MarginExceptionApprovalLevel] ON [dbo].[MarginExceptionApprovalLevel] ([Id])
+
