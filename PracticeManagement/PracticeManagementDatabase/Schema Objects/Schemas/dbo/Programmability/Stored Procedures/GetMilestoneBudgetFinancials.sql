f25d8bb1a6cdf0f15f2dfdb81be7cfa256ccaf45
sravan <sravan>
2017-08-04 10:23:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetMilestoneBudgetFinancials.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[GetMilestoneBudgetFinancials]
+(
+	@MilestoneId      INT
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	Select 1 AS FinanceType,
+		   PBH.ProjectId,
+		   PBH.MilestoneId,
+		   ISNULL(PBH.Revenue,0)  as 'Revenue',
+		   ISNULL(PBH.Revenue,0)-ISNULL((ISNULL(PBH.Revenue,0)*ISNULL(PBH.Discount,0))/100,0) AS 'RevenueNet',
+		   ISNULL(PBH.COGS,0)  AS 'Cogs',
+		   (ISNULL(PBH.Revenue,0)+ISNULL(PBH.ReimbursedExpense,0)-ISNULL(PBH.Expense,0) -ISNULL((ISNULL(PBH.Revenue,0)*ISNULL(PBH.Discount,0))/100,0)-PBH.COGS) as 'GrossMargin',
+		   PBH.Expense,
+		   PBH.ReimbursedExpense
+	FROM ProjectBudgetHistory PBH
+	WHERE PBH.IsActive=1 AND PBH.MilestoneId = @MilestoneId
+
+END
+
