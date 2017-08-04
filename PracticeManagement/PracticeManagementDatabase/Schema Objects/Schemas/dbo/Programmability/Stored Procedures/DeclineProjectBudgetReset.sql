eae3268b7153c2053dfcfba5ad7f107812620d89
sravan <sravan>
2017-08-04 10:23:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DeclineProjectBudgetReset.sql
@@ -0,0 +1,33 @@
+﻿CREATE PROCEDURE [dbo].[DeclineProjectBudgetReset]
+(
+    @UserAlias          NVARCHAR(255),
+	@RequestId			INT = 0,
+	@Comments			NVARCHAR(MAX)
+)
+AS
+
+	DECLARE @CurrentPMTime DATETIME,
+			@RequestIdLocal INT,
+			@UpdatedBy INT, 
+			@RequesterAlias NVARCHAR(255) =''
+		
+	 SELECT @UpdatedBy = PersonId FROM dbo.Person WHERE Alias = @UserAlias
+	 SELECT @CurrentPMTime = dbo.GettingPMTime(GETUTCDATE()),
+			@RequestIdLocal=@RequestId
+			
+
+  IF EXISTS(SELECT 1 FROM dbo.BudgetResetRequestHistory WHERE RequestId=@RequestId)
+	BEGIN
+		EXEC dbo.SessionLogPrepare @UserLogin = @UserAlias
+		INSERT INTO dbo.BudgetResetDeclineHistory(RequestId, DeclinedBy, DeclinedDate, Comments)
+		VALUES (@RequestIdLocal,@UpdatedBy,@CurrentPMTime, @Comments)
+		EXEC dbo.SessionLogUnprepare
+
+		SELECT @RequesterAlias=p.Alias
+		FROM dbo.BudgetResetRequestHistory b
+		JOIN dbo.Person p on p.PersonId=b.RequestedBy
+		WHERE b.RequestId=@RequestId
+	END
+
+SELECT  @RequesterAlias
+
