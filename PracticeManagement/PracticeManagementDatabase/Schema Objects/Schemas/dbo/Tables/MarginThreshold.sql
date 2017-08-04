1d082699e07aff0a6ea720940daf63003901662f
sravan <sravan>
2017-08-04 10:22:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/MarginThreshold.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[MarginThreshold]
+(
+	[Id] INT NOT NULL IDENTITY (1, 1), 
+    [StartDate] DATETIME NOT NULL, 
+    [EndDate] DATETIME NOT NULL, 
+    [ThresholdVariance] INT NOT NULL
+)
+
+GO
+
+CREATE CLUSTERED INDEX [IX_MarginThreshold_Id] ON [dbo].[MarginThreshold] ([Id])
+
