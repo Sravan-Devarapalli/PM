a99fe78d1667eb0241daee2db3e619c44dccecb4
sravan <sravan>
2017-08-04 10:23:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/UpdateProjectExpenseActualAmount.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[UpdateProjectExpenseActualAmount]
+(
+	@ExpenseId	INT
+)
+AS
+	SET NOCOUNT ON
+
+	DECLARE @ActualAmount DECIMAL(18,2),
+			@EstAmount  DECIMAL(18,2)
+
+	select @ActualAmount= SUM(ISNULL(PME.ActualAmount,0)),
+	@EstAmount=SUM(ISNULL(PME.EstimatedAmount,0))
+	FROM ProjectMonthlyExpense PME
+	WHERE PME.ExpenseId=@ExpenseId
+	
+	UPDATE PE
+	SET PE.Amount=@ActualAmount,
+		PE.ExpectedAmount=@EstAmount
+	FROM ProjectExpense PE
+	WHERE PE.Id = @ExpenseId
+
+
