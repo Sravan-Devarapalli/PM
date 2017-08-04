a7185b06982e1718bbadac55423b6cfb58b74dfb
sravan <sravan>
2017-08-04 10:59:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

--- PracticeManagement/PracticeManagement/Reports/ProjectSummaryReport.aspx.designer.cs
+++ PracticeManagement/PracticeManagement/Reports/ProjectSummaryReport.aspx.designer.cs
@@ -148,6 +148,15 @@ namespace PraticeManagement.Reporting {
         protected global::System.Web.UI.WebControls.DropDownList ddlView;
         
         /// <summary>
+        /// chbShowNonBillable control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox chbShowNonBillable;
+        
+        /// <summary>
         /// msgError control.
         /// </summary>
         /// <remarks>
