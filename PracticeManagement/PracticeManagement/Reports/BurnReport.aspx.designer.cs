c068dc7732386df5e5fa768a4560985f55e7d99b
sravan <sravan>
2017-08-23 04:15:00 -0500

    Comments - Defect Number: 3367; Defect RecordID: 3363; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00001-Budget/Baseline; Defect Description:

--- PracticeManagement/PracticeManagement/Reports/BurnReport.aspx.designer.cs
+++ PracticeManagement/PracticeManagement/Reports/BurnReport.aspx.designer.cs
@@ -67,6 +67,15 @@ namespace PraticeManagement.Reports {
         protected global::System.Web.UI.WebControls.DropDownList ddlPeriod;
         
         /// <summary>
+        /// selectedProjectLength control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField selectedProjectLength;
+        
+        /// <summary>
         /// mpeCustomDates control.
         /// </summary>
         /// <remarks>
