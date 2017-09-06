fd7c46ad8825d8575bf9bb729695347e36a97a5d
sravan <sravan>
2017-09-06 03:56:00 -0500

    Comments - Defect Number: 3369; Defect RecordID: 3365; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: Margin Visibility issue; Defect Description: Working with Anna --- when she looks P001218  Digital Marketing
    Financial Summary  Hidden margins
    Budget Mgmt --- Can see all information
    Burn Report  Hidden margins
    Budget Comparison  can see all information
    
    This is a BIG security issue ---- all reports should have margin hidden
    
    -EllenB
    
    
    Hi Nick,
    
    We identified that the issue is because of the flags to be set depending on the role were not set accordingly. We resolved it and we will deploy this to UAT today.
    
    Thanks

--- PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/BudgetManagementByProject.sql
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/BudgetManagementByProject.sql
@@ -42,6 +42,174 @@ BEGIN
 
 		CREATE Clustered index cix_ActualTimeEntriesToDate on #ActualTimeEntriesToDate(ProjectId, PersonId, ChargeCodeDate,IsHourlyAmount)
 			
+		;WITH MileStoneEntries
+		AS
+		(
+			SELECT  m.ProjectId,
+					m.[MilestoneId],
+					mp.PersonId,
+					cal.Date,
+					MPE.Id,
+					MPE.Amount,
+					m.IsHourlyAmount,	
+					m.IsDefault,
+					SUM(mpe.HoursPerDay) AS ActualHoursPerDay,
+					SUM(CONVERT(DECIMAL(4,2),CASE WHEN cal.DayOff = 0  THEN mpe.HoursPerDay -- No Time-off and no company holiday
+						WHEN ( cal.companydayoff = 1) OR ( cal.companydayoff = 0 AND ISNULL(cal.TimeoffHours,8) = 8) THEN 0 -- only company holiday OR person complete dayoff
+						ELSE mpe.HoursPerDay * (1-(cal.TimeoffHours/8)) --person partial day off
+					END)) AS HoursPerDay,
+					SUM(CONVERT(DECIMAL(4,2),CASE WHEN cal.DayOff = 0  THEN mpe.HoursPerDay -- No Time-off and no company holiday
+						WHEN ( cal.companydayoff = 1) OR ( cal.companydayoff = 0 AND ISNULL(cal.TimeoffHours,8) = 8) THEN 0 -- only company holiday OR person complete dayoff
+						ELSE mpe.HoursPerDay * (1-(cal.TimeoffHours/8)) --person partial day off
+					END) * mpe.Amount) AS PersonMilestoneDailyAmount--PersonLevel
+					FROM dbo.Project P 
+					INNER JOIN dbo.[Milestone] AS m ON P.ProjectId=m.ProjectId AND p.IsAllowedToShow = 1 AND p.projectid != 174
+					INNER JOIN dbo.MilestonePerson AS mp ON m.[MilestoneId] = mp.[MilestoneId]
+					INNER JOIN dbo.MilestonePersonEntry AS mpe ON mp.MilestonePersonId = mpe.MilestonePersonId
+					INNER JOIN dbo.PersonCalendarAuto AS cal ON cal.Date BETWEEN mpe.Startdate AND mpe.EndDate AND cal.PersonId = mp.PersonId 
+					WHERE P.ProjectId =@ProjectId AND cal.Date BETWEEN mpe.Startdate AND mpe.EndDate AND cal.PersonId = mp.PersonId 
+					GROUP BY  m.ProjectId,m.[MilestoneId],mp.PersonId,cal.Date,m.IsHourlyAmount ,m.IsDefault,MPE.Id,MPE.Amount
+		),
+		CTE 
+		AS 
+		(
+			SELECT s.Date, s.MilestoneId, SUM(HoursPerDay) AS HoursPerDay
+			FROM MileStoneEntries AS s
+			WHERE s.IsHourlyAmount = 0
+			GROUP BY s.Date, s.MilestoneId
+		),
+
+		MonthlyHours
+		AS
+		(
+			SELECT C.MonthStartDate, C.MonthEndDate,C.MonthNumber, s.MilestoneId, SUM(HoursPerDay) AS HoursPerMonth
+			FROM dbo.v_MilestonePersonSchedule AS s
+			INNER JOIN dbo.Calendar C ON C.Date = s.Date 
+			WHERE s.IsHourlyAmount = 0
+			GROUP BY s.MilestoneId, C.MonthStartDate, C.MonthEndDate,C.MonthNumber
+		),
+
+		MilestoneRevenueRetrospective
+		AS
+		(
+			 SELECT --Milestones with a fixed amount and monthly revenues
+					m.MilestoneId,
+					cal.Date,
+					m.IsHourlyAmount,
+					ISNULL((FMR.Amount/ NULLIF(MH.HoursPerMonth,0))* d.HoursPerDay,0) AS MilestoneDailyAmount,
+					p.Discount,
+					d.HoursPerDay
+				FROM dbo.FixedMilestoneMonthlyRevenue FMR
+				JOIN Milestone M on M.MilestoneId=FMR.MilestoneId
+				JOIN Project p on p.ProjectId=m.ProjectId
+				INNER JOIN dbo.Calendar AS cal ON cal.Date BETWEEN FMR.StartDate AND FMR.EndDate
+				JOIN MonthlyHours MH on MH.milestoneid=M.MilestoneId AND cal.Date BETWEEN MH.MonthStartDate AND MH.MonthEndDate
+				INNER JOIN CTE AS d ON d.date = cal.Date and m.MilestoneId = d.MileStoneId
+				INNER JOIN V_WorkinHoursByYear HY ON cal.date BETWEEN HY.[YearStartDate] AND HY.[YearEndDate]
+
+				UNION ALL
+
+			SELECT -- Milestones with a fixed amount
+					m.MilestoneId,
+					cal.Date,
+					m.IsHourlyAmount,
+					ISNULL((m.Amount/ NULLIF(MTHours.TotalHours,0))* d.HoursPerDay,0) AS MilestoneDailyAmount ,
+					p.Discount,
+					d.HoursPerDay/* Milestone Total  Hours per day*/
+				FROM dbo.Project AS p 
+					INNER JOIN dbo.Milestone AS m ON m.ProjectId = p.ProjectId AND P.IsAdministrative = 0 AND P.ProjectId != 174 AND  m.IsHourlyAmount = 0
+					INNER JOIN dbo.Calendar AS cal ON cal.Date BETWEEN m.StartDate AND m.ProjectedDeliveryDate
+					INNER JOIN (
+									SELECT s.MilestoneId, SUM(s.HoursPerDay) AS TotalHours
+									FROM CTE AS s 
+									GROUP BY s.MilestoneId
+								) AS MTHours  ON MTHours.MilestoneId  = m.MilestoneId
+					INNER JOIN CTE AS d ON d.date = cal.Date and m.MilestoneId = d.MileStoneId
+					LEFT JOIN (select distinct milestoneid from dbo.FixedMilestoneMonthlyRevenue) FMR on m.MilestoneId=FMR.MilestoneId
+				WHERE FMR.MilestoneId IS NULL
+				UNION ALL
+			SELECT -- Milestones with a hourly amount
+					mp.MilestoneId,
+					mp.Date,
+					mp.IsHourlyAmount,
+					ISNULL(SUM(mp.Amount * mp.HoursPerDay), 0) AS MilestoneDailyAmount,
+					MAX(p.Discount) AS Discount,
+					SUM(mp.HoursPerDay) AS HoursPerDay/* Milestone Total  Hours per day*/
+				FROM MileStoneEntries mp
+					INNER JOIN dbo.Project AS p ON mp.ProjectId = p.ProjectId AND mp.IsHourlyAmount = 1
+			GROUP BY mp.MilestoneId, mp.Date, mp.IsHourlyAmount
+		),
+		cteFinancialsRetrospectiveActualHours
+		as
+		(
+			SELECT	pro.ProjectId,
+				Per.PersonId,
+				c.Date,
+				AE.BillableHOursPerDay,
+				AE.NonBillableHoursPerDay,
+				ISNULL(ME.IsHourlyAmount,AE.IsHourlyAmount) AS IsHourlyAmount,
+				ME.HoursPerDay AS PersonHoursPerDay,
+				ME.ActualHoursPerDay,
+				r.Discount,
+				r.HoursPerDay,
+				CASE
+					   WHEN ME.IsHourlyAmount = 1 OR r.HoursPerDay = 0
+					   THEN ME.PersonMilestoneDailyAmount
+					   ELSE ISNULL(r.MilestoneDailyAmount * ME.HoursPerDay / r.HoursPerDay, r.MilestoneDailyAmount)
+				   END AS PersonMilestoneDailyAmount,--Person Level Daily Amount
+				   CASE
+					   WHEN ME.IsHourlyAmount = 1 OR r.HoursPerDay = 0
+					   THEN ISNULL(ME.PersonMilestoneDailyAmount * r.Discount / 100, 0)
+					   ELSE ISNULL(r.MilestoneDailyAmount * ME.HoursPerDay * r.Discount / (r.HoursPerDay * 100), r.MilestoneDailyAmount * r.Discount / 100)
+				   END AS PersonDiscountDailyAmount, --Entry Level Daily Discount Amount
+				 CASE
+					   WHEN ME.IsHourlyAmount = 1
+					   THEN ME.Amount
+					   WHEN ME.IsHourlyAmount = 0 AND r.HoursPerDay = 0
+					   THEN 0
+					   ELSE r.MilestoneDailyAmount / r.HoursPerDay
+				   END AS BillRate,
+					 CASE 
+					WHEN p.Timescale = 4
+					THEN p.HourlyRate * 0.01 * ME.Amount
+					ELSE p.HourlyRate
+				   END AS PayRate, 	-- new payrate that takes into account that % unit is used in the Amount instead of $ unit
+				  p.BonusRate,
+					SUM(CASE o.OverheadRateTypeId
+								   -- Multipliers
+								   WHEN 2 THEN
+									   (CASE
+											 WHEN r.IsHourlyAmount = 1
+											 THEN ME.Amount
+											 WHEN r.IsHourlyAmount = 0 OR r.HoursPerDay = 0
+											 THEN 0
+											 ELSE r.MilestoneDailyAmount / r.HoursPerDay
+										 END) * o.Rate / 100 
+								   WHEN 4 THEN p.HourlyRate * o.Rate / 100
+								   -- Fixed
+								   WHEN 3 THEN (o.Rate * 12 / (C.DaysInYear * 8)) 
+								   ELSE o.Rate
+							   END) AS OverheadRate,
+	           			ISNULL(p.HourlyRate * MLFO.Rate / 100,0) MLFOverheadRate,
+					(CASE WHEN p.Timescale = 2
+						 THEN ISNULL(p.HourlyRate * p.VacationDays * ME.HoursPerDay,0)/(C.DaysInYear * 8)
+					ELSE 0 END)  VacationRate
+		FROM #ActualTimeEntriesToDate AS AE --ActualEntriesByPerson
+				FULL JOIN MileStoneEntries AS ME ON ME.ProjectId = AE.ProjectId AND AE.PersonId = ME.PersonId AND ME.Date = AE.ChargeCodeDate 
+				INNER JOIN dbo.Person Per ON per.PersonId = ISNULL(ME.PersonId,AE.PersonId)
+				INNER JOIN dbo.Project Pro ON Pro.ProjectId = ISNULL(ME.ProjectId,AE.ProjectId) 
+				INNER JOIN dbo.Calendar C ON c.Date = ISNULL(ME.Date,AE.ChargeCodeDate)
+				INNER JOIN dbo.GetFutureDateTable() FD ON 1=1 --For improving query performance we are using table valued function instead of scalar function.
+				LEFT JOIN dbo.[v_PersonPayRetrospective] AS p ON p.PersonId = per.PersonId AND p.Date = c.Date
+				LEFT JOIN v_MLFOverheadFixedRateTimescale MLFO ON MLFO.TimescaleId = p.Timescale AND c.Date BETWEEN MLFO.StartDate AND ISNULL(MLFO.EndDate,FD.FutureDate)
+				LEFT JOIN dbo.v_OverheadFixedRateTimescale AS o ON p.Date BETWEEN o.StartDate AND ISNULL(o.EndDate, FD.FutureDate) AND o.TimescaleId = p.Timescale
+				LEFT JOIN MilestoneRevenueRetrospective AS r ON ME.MilestoneId = r.MilestoneId AND c.Date = r.Date
+			GROUP BY pro.ProjectId,Per.PersonId,c.Date,C.DaysInYear,AE.BillableHOursPerDay,AE.NonBillableHoursPerDay,ME.IsHourlyAmount,ME.HoursPerDay,ME.PersonMilestoneDailyAmount,
+					p.Timescale,p.HourlyRate,p.BonusRate,p.VacationDays,
+					r.HoursPerDay,r.MilestoneDailyAmount,r.Discount,MLFO.Rate,ME.Amount,ME.Id,ME.ActualHoursPerDay,AE.IsHourlyAmount
+		)
+
+
 				
 		SELECT f.ProjectId,
 				f.Date,
@@ -56,11 +224,10 @@ BEGIN
 				f.IsHourlyAmount,
 				f.BillRate,
 				f.ActualHoursPerDay,
-				AE.BillableHOursPerDay,
-      			AE.NonBillableHoursPerDay
+				f.BillableHOursPerDay,
+      			f.NonBillableHoursPerDay
 		INTO #FinancialsRetro
-		FROM v_FinancialsRetrospective f
-		FULL JOIN #ActualTimeEntriesToDate AE on AE.ProjectId=f.ProjectId AND AE.PersonId = f.PersonId AND f.Date = AE.ChargeCodeDate
+		FROM cteFinancialsRetrospectiveActualHours f
 		WHERE f.ProjectId = @ProjectIdLocal 
 				
 
@@ -71,7 +238,6 @@ BEGIN
 					f.Date,
 					SUM(CASE WHEN f.IsHourlyAmount = 0  THEN f.PersonMilestoneDailyAmount ELSE 0 END ) AS FixedActualRevenuePerDay,
 					(ISNULL(SUM(CASE WHEN f.IsHourlyAmount = 1 THEN f.BillRate* f.ActualHoursPerDay ELSE 0 END),0) / ISNULL(NULLIF(SUM(CASE WHEN f.IsHourlyAmount = 1 THEN f.ActualHoursPerDay ELSE 0 END),0),1)) * MAX(CASE WHEN f.IsHourlyAmount = 1 THEN  f.BillableHOursPerDay ELSE 0 END) HourlyActualRevenuePerDay,
-					--SUM(CASE WHEN f.IsHourlyAmount = 0 and (@ActualsEndDate IS NULL OR f.Date<=@ActualsEndDate)  THEN 	f.PersonMilestoneDailyAmount ELSE 0 END)- (
 								(ISNULL( MAX( CASE WHEN f.IsHourlyAmount = 0   THEN  f.BillableHOursPerDay + f.NonBillableHoursPerDay ELSE 0 END ),0) * 
 								ISNULL( CASE WHEN ISNULL(SUM(CASE WHEN f.IsHourlyAmount = 0 THEN f.ActualHoursPerDay ELSE 0 END),0) > 0 
 											THEN SUM((CASE WHEN f.SLHR >=  f.PayRate + f.MLFOverheadRate THEN f.SLHR ELSE  f.PayRate + f.MLFOverheadRate END)* CASE WHEN f.IsHourlyAmount = 0 THEN f.ActualHoursPerDay ELSE 0 END) / SUM(CASE WHEN f.IsHourlyAmount = 0 THEN f.ActualHoursPerDay ELSE 0 END)
@@ -79,8 +245,6 @@ BEGIN
 										END ,0)
 							)
 							 AS FixedActualCostPerDay,
-						--((ISNULL(SUM(CASE WHEN f.IsHourlyAmount = 1 THEN f.BillRate* f.ActualHoursPerDay ELSE 0 END),0) / ISNULL(NULLIF(SUM(CASE WHEN f.IsHourlyAmount = 1 THEN f.ActualHoursPerDay ELSE 0 END),0),1)) * MAX(CASE WHEN f.IsHourlyAmount = 1 THEN  f.BillableHOursPerDay ELSE 0 END))
-						-- -  (
 							(	MAX( CASE WHEN f.IsHourlyAmount = 1 THEN  f.BillableHOursPerDay + f.NonBillableHoursPerDay ELSE 0 END ) * 
 								ISNULL( CASE WHEN ISNULL(SUM(CASE WHEN f.IsHourlyAmount = 1 THEN f.ActualHoursPerDay ELSE 0 END),0) > 0 
 											THEN SUM((CASE WHEN f.SLHR >=  f.PayRate + f.MLFOverheadRate THEN f.SLHR ELSE  f.PayRate + f.MLFOverheadRate END)* CASE WHEN f.IsHourlyAmount = 1 THEN f.ActualHoursPerDay ELSE 0 END) / SUM(CASE WHEN f.IsHourlyAmount = 1 THEN f.ActualHoursPerDay ELSE 0 END)
