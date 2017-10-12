6c93ac291fa45b546e1a82fc0d656dd979402183
sravan <sravan>
2017-10-12 02:53:00 -0500

    Comments - Defect Number: 3379; Defect RecordID: 3375; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: Vendor's and Person's email, Phone number changes ; Defect Description:

--- PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Vendor.table.sql
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Vendor.table.sql
@@ -4,7 +4,7 @@
 	 [Name]				NVARCHAR (50)			NOT NULL,
 	 [ContactName]		NVARCHAR (50)			NOT NULL,
 	 [Status]			INT						NOT NULL,
-	 [Email]			NVARCHAR (50)			NOT NULL,
+	 [Email]			NVARCHAR (100)			NOT NULL,
 	 [TelephoneNumber]	NVARCHAR (20)				NOT NULL,
 	 [VendorTypeId]		INT						NOT NULL
 	 CONSTRAINT [PK_Vendor_Id]	PRIMARY KEY ([Id])
