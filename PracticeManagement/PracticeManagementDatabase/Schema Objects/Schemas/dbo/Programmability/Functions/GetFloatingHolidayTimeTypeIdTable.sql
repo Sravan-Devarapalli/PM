e406c7a8d376d178af6a184fd8f012bc89e7494a
sravan <sravan>
2017-11-03 02:12:00 -0500

    Comments - Defect Number: 3382; Defect RecordID: 3378; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: Addition Floating Holiday as an option in the drop down of the Time  Calendar  Add Time Off menu; Defect Description: Keerthi,
    Requesting two changes that we are hoping can be accomplished next week inadditionto the Seapine to VSO conversion:
    Email triggering for Margin Exceptions
    AdditionofFloatingHoliday
    
    Requirements below. We can discuss Monday (9/25).
    
    -NickC
    
    
    Email triggering for Margin Exceptions
    Currently, when a Margin Exception is requested, emails are triggered to the CFO (Ellen Boyer) and Division Owner (Will Schmidt). These emails occur at the same time (simultaneous).
    The email for Tier 2 approval should be sent to the CFOafterthe Tier 1 approval has been received. If no Tier 1 approval is received, no email to CFO.
    
    Could we please get an estimateofthe effort to make this correction.
    
    AdditionofFloatingHoliday(FH)
    AddFloatingHolidayas an option in the drop downofthe Time  Calendar  Add Time Off menu (highlighted field, below).
    FloatingHolidaywould be displayed in an alternate color on the Calendar view (a color not already in use..you pick)
    For Utilization reports, thefloatingholidaywould appear like any other dayoftype PTO
    For Report  Time  Reports and for all other PM calculations, the FH would behave like any otherHoliday. For the ADP export associated with Report  Time  Reports By Time Period  By Resource, the FH would appear in theHolidaycolumn.
    
    Could we please get an estimateofthe effort to make this change.
    
    Hi,
    
    Please find the following questions/assumptions and let us know your response.
    As we are addingFloatingHolidayas an option in the drop downofthe Time  Calendar  Add Time Off menu, user can able to enter 0.25hrs to 8 hrs for a day. Do we need to consider theFloatingHolidayas normalholiday(the Day's hours will not be considered for assigning resource in project) or allow user to enter any numberofhours similar to PTO(If user enter 3 hrs then remaining hours 5hrs(8-3) will be consideredfor assigning resource in project).
    Please let us know whether all users(i.e, W2 hourly/Salary and 1099 Hourly/POR) can addFloatingHoliday.
    Currently in Time Entry page, user has an option to enter PTO hours(as shown in below image). Please let us know if you want us to implement the same behaviorforFloatingHoliday.
    In Reports > Time > PTO report, please let us know how to indicatefloatingholidayin this report.
    As partofaddingFloatingHoliday, we need to change the Revenue/Margin calculations in the below screens. We are assumingFloatingHolidayshould behave like any otherholidayin the following pages. Please let us know if we are wrong.
    Milestone Details- Revenue and Margin Calculations
    Project Details > Financial Summary, Milestones tab
    Project Details > Budget Management
    Reports > Time > Reports > By Project and By Person
    Reports > Financials > Burn Report
    Reports > Financials > Budget Comparisonreport
    Reports > Financials > Attainment Report
    Reports > Financials > Billing
    Reports > Projects > Projects List
    Projects > Summary
    Thanks
    
    Keerthi,
    
    Responses below. We can discuss when we synch tomorrow.
    
    -NickC
    
    From:Sai Keerthi Madireddy [mailto:keerthi.madireddy@ggktech.com]
    Sent:Tuesday, September 26, 2017 7:06 AM
    To:Nick Callahan <nickc@logic2020.com>
    Cc:Kris Rushing Raines <KrisR@logic2020.com>; Logic 20/20 Core Team <logic2020@ggktech.com>; Kelly Spencer <KellyS@logic2020.com>; Ellen Boyer <EllenB@logic2020.com>
    Subject:Re:FloatingHolidayChange
    
    Hi,
    
    Please find the following questions/assumptions and let us know your response.
    As we are addingFloatingHolidayas an option in the drop downofthe Time  Calendar  Add Time Off menu, user can able to enter 0.25hrs to 8 hrs for a day. Do we need to consider theFloatingHolidayas normalholiday(the Day's hours will not be considered for assigning resource in project) or allow user to enter any numberofhours simi

---
+++ PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetFloatingHolidayTimeTypeIdTable.sql
@@ -0,0 +1,10 @@
+﻿CREATE FUNCTION [dbo].[GetFloatingHolidayTimeTypeIdTable]
+(
+)
+RETURNS TABLE 
+AS
+	RETURN SELECT tt.TimeTypeId AS FHTimeTypeId
+			FROM TimeType tt 
+			WHERE tt.Code = 'W9610' --Here 'W9320' is code of Floating Holiday Work Type.
+GO
+
